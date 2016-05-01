0afbefae867c80f7e524f555b4d054b7a2852106
Michael Steinbaugh <mike@steinbaugh.com>
2016-05-01 07:40:00 -0400

    Fixed the function

--- R/assign_multiple.R
+++ R/assign_multiple.R
@@ -1,11 +1,13 @@
 assign_multiple <- function(..., envir = .GlobalEnv, prefix = NULL) {
-  list <- list(...)
-  ##### switch to other option from lapply here...
-    invisible(lapply(seq_along(list), function(x) {
-    name <- deparse(substitute(list[[x]]))
+  # The `-1` here removes `assign_multiple`
+  names <- sapply(match.call(expand.dots = TRUE)[-1], deparse)
+  data <- list(...)
+  invisible(lapply(seq_along(data), function(x) {
+    name <- names[x]
     if (!is.null(prefix)) {
       name <- paste(c(prefix, name), collapse = "_")
     }
-    assign(name, list[[x]], envir = get(envir))
+    assign(name, data[[x]], envir = get(envir))
   }))
+  ls.str(get(envir))
 }
