8576aeec6fa1cf55e77bb7c910ab6b1cce1570af
Michael Steinbaugh <mike@steinbaugh.com>
2016-05-03 07:29:11 -0400

    Update setup.R

--- data-raw/setup.R
+++ data-raw/setup.R
@@ -1,3 +1,13 @@
+install.packages("devtools")
+library(devtools)
+
+install.packages("roxygen2")
+library(roxygen2)
+
+install.packages("lintr")
+library(lintr)
+lintr::lint_package()
+
 devtools::use_data_raw()
 devtools::use_travis()
 devtools::check()
