2c10a770fbbe0582455b285343e62eb1e4ff5413
Michael Steinbaugh <mike@steinbaugh.com>
2016-07-15 15:11:08 -0400

    Add DAVID function

---
+++ R/davidQuery.R
@@ -0,0 +1,63 @@
+#' RDAVIDWebService query
+#'
+#' @param email Registered email address
+#' @param foregroundGenes
+#' @param backgroundGenes
+#' @param idType Gene identifier type
+#'
+#' @export
+#'
+#' @examples
+#' davidQuery(email = "user@david.ncifcrf.gov",
+#'            foregroundGenes = "WBGene00004804",
+#'            backgroundGenes = c("WBGene00004804", "WBGene00006351"),
+#'            idType = "WORMBASE_GENE_ID")
+davidQuery <- function(email, foregroundGenes, backgroundGenes, idType) {
+  # RDAVIDWebService requires a registered email address:
+  # http://david.abcc.ncifcrf.gov/content.jsp?file=WS.html
+
+  url = "https://david.ncifcrf.gov/webservice/services/DAVIDWebService.DAVIDWebServiceHttpSoap12Endpoint/"
+  david <- RDAVIDWebService::DAVIDWebService$new(email = email, url = url)
+
+  # Foreground list
+  # The foreground list should be contained within the background list.
+  fg <- addList(david,
+                foregroundGenes,
+                idType = idType,
+                listName = "isClass",
+                listType = "Gene")
+
+  # Background list
+  bg <- addList(david,
+                backgroundGenes,
+                idType = idType,
+                listName = "all",
+                listType = "Background")
+
+  # Inspect to see the proportion of genes recognized by DAVID.
+  fg
+  bg
+
+  # Inspect "david" object to see the gene lists are correct.
+  david
+
+  # Specifiy annotation categories.
+  # setAnnotationCategories(david, c("GOTERM_BP_ALL",
+  #                                  "GOTERM_MF_ALL",
+  #                                  "GOTERM_CC_ALL",
+  #                                  "INTERPRO"))
+
+  # Get functional annotation chart as R object.
+  chart <- getFunctionalAnnotationChart(david)
+  assign("davidChart", chart, envir = .GlobalEnv)
+
+  # Get functional annotation clustering (limited to 3000 genes).
+  cluster <- getClusterReport(david)
+  assign("davidCluster", cluster, envir = .GlobalEnv)
+
+  # Print functional annotation chart to file.
+  getFunctionalAnnotationChartFile(david, "davidChart.tsv")
+
+  # Print functional annotation clustering to file (limited to 3000 genes).
+  getClusterReportFile(david, "davidCluster.tsv")
+}
