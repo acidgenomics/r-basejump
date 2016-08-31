5481a487247ab51b988b6cfac317cc800b0ac8b5
Michael Steinbaugh <mike@steinbaugh.com>
2016-08-31 13:35:30 -0400

    Version 0.4.6 minor update

--- R/loadpkg.R
+++ R/loadpkg.R
@@ -4,7 +4,4 @@
 loadpkg <- function() {
   devtools::load_all()
   sapply(list.files(path = "R", pattern = "*.R", full.names = TRUE), source)
-  # if (file.exists("R/sysdata.rda")) {
-  #   load("R/sysdata.rda", .GlobalEnv)
-  # }
 }
