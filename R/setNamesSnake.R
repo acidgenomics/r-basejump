e52aa790f74dd5dc7fea215de7726f7a6a87763e
Michael Steinbaugh <mjsteinbaugh@users.noreply.github.com>
2017-01-09 10:52:39 -0500

    Update setNamesSnake.R

--- R/setNamesSnake.R
+++ R/setNamesSnake.R
@@ -1,12 +1,9 @@
 #' Set names as snake_case
 #'
-#' @importFrom magrittr set_names
-#'
-#' @param data \code{data.frame}, \code{list}, or \code{tibble}
-#'
-#' @return data Same data but with reformatted snake_case names
 #' @export
+#' @param data \code{data.frame}, \code{list}, or \code{tibble}
+#' @return data Same data but with reformatted snake_case names
 setNamesSnake <- function(data) {
     data %>%
-        magrittr::set_names(data, snake(names(.)))
+        setNames(snake(names(.)))
 }
