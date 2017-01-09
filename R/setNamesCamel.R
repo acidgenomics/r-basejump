5dd5c41480a508c3ac68f95dbe8f2ff958487a16
Michael Steinbaugh <mjsteinbaugh@users.noreply.github.com>
2017-01-09 10:51:58 -0500

    Update setNamesCamel.R

--- R/setNamesCamel.R
+++ R/setNamesCamel.R
@@ -1,11 +1,9 @@
 #' Set names as camelCase
 #'
-#' @export
-#' @importFrom magrittr set_names
 #' @keywords general
 #' @param data \code{data.frame}, \code{list}, or \code{tibble}
 #' @return data Same data but with reformatted camelCase names
 setNamesCamel <- function(data) {
     data %>%
-        magrittr::set_names(., camel(names(.)))
+        setNames(camel(names(.)))
 }
