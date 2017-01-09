1242267c68e3bf9e765d0f54b038c43b38794cb8
Michael Steinbaugh <mjsteinbaugh@users.noreply.github.com>
2017-01-09 10:51:40 -0500

    Create setNames.R

---
+++ R/setNames.R
@@ -0,0 +1,9 @@
+#' setNames
+#'
+#' @export
+#' @importFrom stats setNames
+#' @name setNames
+#' @param object an object for which a names attribute will be meaningful
+#' @param nm a character vector of names to assign to the object
+#' @rdname setNames
+NULL
