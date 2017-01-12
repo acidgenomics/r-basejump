ffeba636e15f197956960162caf37a1bb61b5bef
Michael Steinbaugh <mike@steinbaugh.com>
2017-01-12 07:21:57 -0500

    NAMESPACE update

--- R/multiassign.R
+++ R/multiassign.R
@@ -2,12 +2,11 @@
 #'
 #' Optionally, you can specify a name prefix and the desired environment.
 #'
+#' @export
 #' @param ... \code{list} of variables to assign.
 #' @param envir Desired \code{environment} (optional).
 #' @param prefix Name prefix (optional).
-#'
 #' @return Assigns variables to new \code{environment} with a name prefix, if desired.
-#' @export
 multiAssign <- function(..., envir = .GlobalEnv, prefix = NULL) {
     # The `-1` here removes the function name:
     names <- sapply(match.call(expand.dots = TRUE)[-1], deparse)
