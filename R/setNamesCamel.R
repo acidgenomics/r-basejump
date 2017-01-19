f7825a110ffabdeb95a594086e2a25265a66e345
Michael Steinbaugh <mike@steinbaugh.com>
2017-01-19 10:36:17 -0500

    Export setNamesCamel

--- R/setNamesCamel.R
+++ R/setNamesCamel.R
@@ -1,5 +1,6 @@
 #' Set names as camelCase
 #'
+#' @export
 #' @keywords general
 #' @param data \code{data.frame}, \code{list}, or \code{tibble}
 #' @return data Same data but with reformatted camelCase names
