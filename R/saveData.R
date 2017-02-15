7744d15ec0d66a9258b3e08a31ad0efe69355663
Michael Steinbaugh <mike@steinbaugh.com>
2017-02-15 15:39:42 -0500

    Initial commit

---
+++ R/saveData.R
@@ -0,0 +1,28 @@
+#' Save data files
+#'
+#' This function makes it easy to save data in the correct format. Based off
+#' \code{use_data()} from the \code{devtools} package. Note that this function
+#' will overwrite existing data files.
+#' @export
+#' @param ... Unquoted names of existing objects to save
+#' @param dir Target directory
+#' @examples
+#' \dontrun{
+#' x <- 1:10
+#' y <- 1:100
+#' saveData(x, y)
+#' }
+saveData <- function(..., dir = "data") {
+    if (!isString(dir)) {
+        stop("dir must be a string")
+    }
+    if (!file.exists(dir)) {
+        dir.create(dir)
+    }
+    objs <- get_objs_from_dots(dots(...))
+    paths <- file.path(dir, paste0(objs, ".rda"))
+    message("Saving ", paste(unlist(objs), collapse = ", "),
+            " as ", paste(basename(paths), collapse = ", "))
+    mapply(save, list = objs, file = paths)
+    invisible()
+}
