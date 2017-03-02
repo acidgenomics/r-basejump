b61a809b1ed1c7511c7b8e763fe3889193cc6ddc
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-01 23:52:21 -0500

    downloadFile function update

--- R/downloadFile.R
+++ R/downloadFile.R
@@ -1,31 +1,31 @@
-#' Find and download a file from a remote server
+#' Match and download a file from a remote directory
 #' @export
 #' @importFrom RCurl getURL
-#' @importFrom stringr str_split
+#' @importFrom stringr str_split str_subset
 #' @importFrom utils download.file
 #' @keywords web
-#' @param url URL of remote directory
+#' @param remoteDir Remote directory URL
 #' @param subset Match \code{string}
 #' @param rename Rename the file, if desired
-#' @param data_raw Append \code{data-raw} to saved file name
-downloadFile <- function(url, subset, rename = NULL, data_raw = TRUE) {
-  remoteFile <- RCurl::getURL(url, dirlistonly = TRUE) %>%
-    stringr::str_split(., "\n") %>%
-    .[[1]] %>%
-    stringr::str_subset(., subset)
-  if (is.null(rename)) {
-    filePath <- remoteFile
-  } else {
-    filePath <- rename
-  }
-  if (isTRUE(data_raw)) {
-    if (!file.exists("data-raw")) {
-      dir.create("data-raw")
+#' @param localDir Directory where to save file locally
+downloadFile <- function(remoteDir,
+                         string,
+                         rename = NULL,
+                         localDir = "data-raw") {
+    remoteFile <- RCurl::getURL(remoteDir, dirlistonly = TRUE) %>%
+        stringr::str_split(., "\n") %>% .[[1]] %>%
+        stringr::str_subset(., string)
+
+    # Rename, if desired
+    if (!is.null(rename)) {
+        localFile <- remoteFile
+    } else {
+        localFile <- rename
     }
-    filePath <- file.path("data-raw", filePath)
-  }
-  if (!file.exists(filePath)) {
-    utils::download.file(paste0(url, remoteFile), filePath)
-  }
-  return(filePath)
+
+    remoteFile <- paste0(remoteDir, remoteFile)
+    localFile <- file.path(localDir, localFile)
+
+    utils::download.file(remoteFile, localFile)
+    return(localFile)
 }
