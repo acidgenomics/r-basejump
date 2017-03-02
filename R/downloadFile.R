9b43307a7742c856b61b058e1647a5757d8f1928
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-02 00:10:33 -0500

    Add compression option

--- R/downloadFile.R
+++ R/downloadFile.R
@@ -1,16 +1,19 @@
 #' Match and download a file from a remote directory
 #' @export
 #' @importFrom RCurl getURL
+#' @importFrom R.utils gzip
 #' @importFrom stringr str_split str_subset
 #' @importFrom utils download.file
 #' @keywords web
 #' @param remoteDir Remote directory URL
 #' @param string \code{string} to match against remote file names
 #' @param rename Rename the local file, if desired
+#' @param compress Whether to compress the file (gzip) after download
 #' @param localDir Directory where to save file locally
 downloadFile <- function(remoteDir,
                          string,
                          rename = NULL,
+                         compress = FALSE,
                          localDir = "data-raw") {
     # Create the local directory, if necessary
     if (!is.null(localDir)) {
@@ -36,5 +39,12 @@ downloadFile <- function(remoteDir,
     localFile <- file.path(localDir, localFile)
 
     utils::download.file(remoteFile, localFile)
+
+    # Compress, if desired
+    if (isTRUE(compress)) {
+        R.utils::gzip(localFile, overwrite = TRUE)
+        localFile <- paste0(localFile, ".gz")
+    }
+
     return(localFile)
 }
