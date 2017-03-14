afb1f11e1e72baee006e361bd4a8e4c05e3633be
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-14 08:16:10 -0400

    bioc wrapper function

--- R/bioc.R
+++ R/bioc.R
@@ -1,6 +1,26 @@
-#' Load Bioconductor
+# Modified version of `rafalib::install_bioc()`
+# https://github.com/rafalab/rafalib/blob/master/R/install_bioc.R
+utils::globalVariables("biocLite")
+#' Install or update Bioconductor packages
+#'
+#' This is function simply a wrapper for \code{biocLite}. It first sources the
+#' code from the Bioconductor website then calls \code{biocLite}.
+#'
 #' @export
-bioc <- function() {
-    source("https://bioconductor.org/biocLite.R")
-    # biocLite()
+#' @param ... Passthrough to \code{biocLite()}
+#' @examples
+#' \dontrun{
+#' # Load Bioconductor and update packages
+#' bioc()
+#'
+#' # Install `limma`
+#' bioc("limma")
+#' }
+bioc <- function(...) {
+    internet <- try(source("http://bioconductor.org/biocLite.R"), silent = TRUE)
+    if (!class(internet) == "try-error") {
+        biocLite(...)
+    } else {
+        stop("No connection to http://bioconductor.org.")
+    }
 }
