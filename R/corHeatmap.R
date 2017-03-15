e8cd0c68eab5a6b1a69dbe20da67c91db4dd832b
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-15 13:10:40 -0400

    Documentation fix

--- R/corHeatmap.R
+++ R/corHeatmap.R
@@ -1,18 +1,27 @@
 #' Correlation matrix heatmap
-#' 
+#'
 #' @author Michael Steinbaugh
 #' @keywords bcbio plot rnaseq
-#' 
+#'
 #' @import pheatmap
-#' 
+#'
 #' @param counts Counts matrix
-#' 
+#' @param method Correlation coefficient (or covariance) to be computed
+#' @param annotation Annotation data frame
+#' @param ... Passthrough to \code{pheatmap()}
+#'
 #' @return Heatmap
 #' @export
 corHeatmap <- function(counts,
                        method = "pearson",
                        annotation,
                        ...) {
+    if (!is.matrix(counts)) {
+        stop("A counts matrix is required.")
+    }
+    if (!is.data.frame(annotation)) {
+        stop("An annotation data.frame is required.")
+    }
     counts %>%
         cor(method = method) %>%
         pheatmap::pheatmap(main = paste(method, "correlation"),
