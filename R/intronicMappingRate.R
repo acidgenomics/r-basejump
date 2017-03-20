0aa83ea4af35f7833f8530c8214c8a63cfc72614
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-20 11:38:59 -0400

    roxygen update

---
+++ R/intronicMappingRate.R
@@ -0,0 +1,33 @@
+#' Intronic mapping rate plot
+#'
+#' @author Michael Steinbaugh
+#' @keywords bcbio plot rnaseq
+#'
+#' @import ggplot2
+#'
+#' @param summary \code{bcbio-rnaseq} summary report
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' intronicMappingRate(summary)
+#' }
+intronicMappingRate <- function(summary) {
+    summary %>%
+        ggplot2::ggplot(
+            ggplot2::aes_(x = ~description,
+                          # Multiple by 100 here for percentage
+                          y = ~intronic_rate * 100,
+                          fill = ~group)
+        ) +
+        ggplot2::ggtitle("intronic mapping rate") +
+        ggplot2::geom_bar(stat = "identity") +
+        ggplot2::geom_hline(color = "orange",
+                            size = 2,
+                            yintercept = 20) +
+        ggplot2::xlab("sample") +
+        ggplot2::ylab("intronic mapping rate (%)") +
+        ggplot2::ylim(0, 100) +
+        ggplot2::coord_flip()
+}
