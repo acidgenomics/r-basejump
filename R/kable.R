7bbe07d9e00bc97ef99096f3c92b1726b1aecae5
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-06 18:26:25 -0400

    Tweak kable defaults

---
+++ R/kable.R
@@ -0,0 +1,29 @@
+#' kable rework with custom defaults
+#'
+#' Display as data frame in RMarkdown chunk or \code{kable()} during knit
+#'
+#' @author Michael Steinbaugh
+#'
+#' @import knitr
+#'
+#' @param x An R object (typically a matrix or data frame)
+#' @param ... Pass through to \code{knitr::kable()}
+#'
+#' @export
+#'
+#' @examples
+#' kable(head(iris))
+kable <- function(x, ...) {
+    # Improve appearance in RStudio
+    if (!is.null(knitr::opts_knit$get("rmarkdown.pandoc.to"))) {
+        # Customize defaults
+        return(
+            knitr::kable(x,
+                         ...,
+                         # booktabs = TRUE,
+                         longtable = TRUE)
+        )
+    } else {
+        return(x)
+    }
+}
