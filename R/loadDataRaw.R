4604061c31738ba5b21a64a5a0baa24ce13cd941
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-06 18:30:37 -0400

    Rename and improve assignment

--- R/loadDataRaw.R
+++ R/loadDataRaw.R
@@ -1,4 +1,4 @@
-#' Load raw data
+#' Load data-raw directory
 #'
 #' Dynamically source or load raw data in the `data-raw` directory
 #'
@@ -9,12 +9,12 @@
 #' @param data Data object
 #'
 #' @export
-dataRaw <- function(data) {
+loadDataRaw <- function(data) {
     for (a in 1:length(data)) {
         if (!file.exists(paste0("data/", data[a], ".rda"))) {
             source(paste0("data-raw/", data[a], ".R"))
         } else {
-            load(paste0("data/", data[a], ".rda"), envir = globalenv())
+            load(paste0("data/", data[a], ".rda"), envir = parent.frame())
         }
     }
 }
