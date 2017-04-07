6bd011c70e3dbd0d7002628a47c9c79145f023d4
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-07 06:57:57 -0400

    Case utility functions

--- R/mixedCase.R
+++
@@ -1,6 +0,0 @@
-#' @keywords internal
-mixedCase <- function(string) {
-    string %>%
-        gsub("\\b([a-z])", "\\U\\1", ., perl = TRUE) %>%
-        gsub("([A-Z])([A-Z]+)", "\\1\\L\\2", ., perl = TRUE)
-}
