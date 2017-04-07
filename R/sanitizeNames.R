da849a5725927fb573c22493556aeba5fc96c137
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-07 08:08:46 -0400

    Update documentation

--- R/sanitizeNames.R
+++ R/sanitizeNames.R
@@ -1,21 +1,27 @@
-#' Sanitize names
+#' @rdname setNames
 #'
-#' This function prevents illegal characters from being set in variable names
-#' and column or row names. Additionally, it will prepend an \code{X} character
-#' if necessary
-#'
-#' @author Michael Steinbaugh
-#'
-#' @keywords internal
-#'
-#' @param data Data frame
-#'
-#' @return Sanitized data frame
+#' @return Sanitized data with valid names. Outputs in snake_case. Tested on
+#'   data frames, matrices, and lists.
 #' @export
+#'
+#' @examples
+#' sanitizeNames(head(iris))
 sanitizeNames <- function(data) {
-    names(data) <- makeNamesSnake(names(data))
-    if (is.matrix | is.data.frame) {
-        rownames(data) <- makeNamesSnake(rownames(data))
+    if (is.null(names(data))) {
+        stop("object doesn't contain names")
     }
+
+    # (Column) names
+    names(data) <- makeNamesSnake(names(data))
+
+    # Rows, if they exist
+    if (!is.null(rownames(data))) {
+        # Ignore numbered rownames
+        if (!identical(rownames(data), as.character(1:nrow(data)))) {
+            rownames(data) <- makeNamesSnake(rownames(data))
+        }
+
+    }
+
     return(data)
 }
