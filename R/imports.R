d3c2dcdc099c949daa61d683577aea5ff2dab51f
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-12 12:06:34 -0400

    Consolidate imports

---
+++ R/imports.R
@@ -0,0 +1,26 @@
+# General ====
+#' @importFrom knitr asis_output kable opts_knit
+#' @importFrom RCurl getURL
+#' @importFrom rmarkdown render
+#' @importFrom R.utils gzip
+#' @importFrom stats na.omit setNames
+#' @importFrom utils download.file globalVariables
+
+
+
+# Package development ====
+#' @import devtools
+#' @importFrom BiocCheck BiocCheck
+#' @importFrom pkgdown build_site
+
+
+
+# tidyverse ====
+# http://tidyverse.org/
+#' @import dplyr
+#' @import ggplot2
+#' @import magrittr
+#' @import readr
+#' @import stringr
+#' @import tibble
+NULL
