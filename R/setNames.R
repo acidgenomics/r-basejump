008c6908e5a4de490508c7a1c0f5bd28ddbf1dd4
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-12 12:19:51 -0400

    Consolidate

--- R/setNames.R
+++ R/setNames.R
@@ -41,6 +41,32 @@ setNamesSnake <- function(data) {
 
 
 #' @rdname setNames
+#' @export
+#' @examples
+#' sanitizeNames(head(iris))
+sanitizeNames <- function(data) {
+    if (is.null(names(data))) {
+        stop("object doesn't contain names")
+    }
+
+    # (Column) names
+    names(data) <- makeNamesSnake(names(data))
+
+    # Rows, if they exist
+    if (!is.null(rownames(data))) {
+        # Ignore numbered rownames
+        if (!identical(rownames(data), as.character(1:nrow(data)))) {
+            rownames(data) <- makeNamesSnake(rownames(data))
+        }
+
+    }
+
+    return(data)
+}
+
+
+
+#' @rdname setNames
 #' @keywords internal
 #' @usage NULL
 #' @export
