8fcbe8c9262549951adb5e7f11347fc5561446c5
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-12 12:16:46 -0400

    Consolidate kable utilities

--- R/kable.R
+++ R/kable.R
@@ -1,10 +1,15 @@
 #' Create tables in LaTeX, HTML, Markdown and reStructuredText
 #'
-#' Wrapper for \code{knitr::kable}. Improves appearance in RStudio RMarkdown
-#' chunks by returning a data frame instead of a kable outside of a knit.
-#' Customizes knit output defaults based on format.
+#' @rdname kable
 #'
 #' @author Michael Steinbaugh
+
+
+
+#' @rdname kable
+#' @description Wrapper for \code{knitr::kable} (knit table). Improves
+#'   appearance in RStudio RMarkdown chunks by returning a data frame instead of
+#'   a kable outside of a knit. Customizes knit output defaults based on format.
 #'
 #' @param x An R object (typically a matrix or data frame)
 #' @param ... Other arguments supported by \code{knitr::kable}
@@ -23,3 +28,28 @@ kable <- function(x, ...) {
     }
 }
 # For PDF: output == "latex"
+
+
+
+#' @rdname kable
+#' @description Handle multiple kables in a single RMarkdown chunk
+#'
+#' @param list List of tables (e.g. data frame, matrix)
+#' @param captions Caption character vector
+#'
+#' @return Knit tables, using \code{kable}
+#' @export
+#'
+#' @examples
+#' kables(list(iris, mtcars))
+kables <- function(list, captions = NULL) {
+    output <- opts_knit$get("rmarkdown.pandoc.to")
+    if (!is.null(output)) {
+        tables <- lapply(seq_along(list), function(a) {
+            kable(list[a], caption = captions[a])
+        })
+        return(asis_output(tables))
+    } else {
+        return(list)
+    }
+}
