aa3377fcf7c647a7ecc155c1bc3749d17a62556e
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-12 11:50:23 -0400

    Consolidate case functions

--- R/case.R
+++ R/case.R
@@ -7,8 +7,12 @@
 #' @param character Character vector
 #'
 #' @return Reformatted character vector
+
+
+
+#' @rdname case
+#' @description Title case
 #' @export
-#'
 #' @examples
 #' titleCase("RNA sequencing")
 titleCase <- function(character) {
@@ -16,4 +20,16 @@ titleCase <- function(character) {
         stop("character vector required")
     }
     gsub("\\b([a-z])", "\\U\\1", character, perl = TRUE)
+}
+
+
+
+#' @rdname case
+#' @export
+#' @examples
+#' firstCase("RNA sequencing")
+firstCase <- function(character) {
+    character %>%
+        titleCase %>%
+        gsub("([A-Z])([A-Z]+)", "\\1\\L\\2", ., perl = TRUE)
 }
