03ad62adc15edcd377a7c2ca9a16abcd91df29bb
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-14 16:51:33 -0400

    Initial commit

---
+++ R/dna.R
@@ -0,0 +1,45 @@
+#' DNA sequence utilites
+#' 
+#' @author Michael Steinbaugh
+#' 
+#' @param dna DNA sequence (ATGC nucleotides)
+#' 
+#' @return Modified DNA sequence
+
+# Modified versions based on \code{seqRFLP::revComp()}
+
+
+
+#' @rdname dna
+#' @description Complement
+#' @export
+#' @examples
+#' comp("ATGCATGC")
+comp <- function(dna) {
+    dna <- toupper(dna)
+    comp <- dna %>%
+        # AT base pair swap
+        gsub("A", "A1", .) %>%
+        gsub("T", "A", .) %>% 
+        gsub("A1", "T", .) %>%
+        # GC base pair swap
+        gsub("G", "G1", .) %>%
+        gsub("C", "G", .) %>% 
+        gsub("G1", "C", .)
+    return(comp)
+}
+
+
+
+#' @rdname dna
+#' @description Reverse complemen
+#' @examples
+#' revcomp("ATGCATGC")
+revcomp <- function(dna) {
+    dna <- toupper(dna)
+    comp <- comp(dna)
+    revcomp <- strsplit(comp, "")[[1]] %>%
+        .[order(seq_along(.), decreasing = TRUE)] %>%
+        paste(., sep = "", collapse = "")
+    return(revcomp)
+}
