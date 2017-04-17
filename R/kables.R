e415cac9a738ba27eeb38b830212c8383f49b114
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-17 17:27:51 -0400

    Don't reexport knitr::kable function

---
+++ R/kables.R
@@ -0,0 +1,27 @@
+#' Create tables in LaTeX, HTML, Markdown and reStructuredText
+#'
+#' Handle multiple kables in a single RMarkdown chunk
+#'
+#' @author Michael Steinbaugh
+#'
+#' @param list List of tables (e.g. data frame, matrix)
+#' @param captions Caption character vector
+#'
+#' @return Knit tables, using \code{kable}
+#' @export
+#'
+#' @examples
+#' kables(list(iris, mtcars))
+kables <- function(list, captions = NULL) {
+    output <- opts_knit$get("rmarkdown.pandoc.to")
+    if (!is.null(output)) {
+        tables <- lapply(seq_along(list), function(a) {
+            kable(list[a],
+                  caption = captions[a],
+                  row.names = FALSE)
+        })
+        return(asis_output(tables))
+    } else {
+        return(list)
+    }
+}
