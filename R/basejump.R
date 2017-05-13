f75deef00fd6008e9f0b3a3362c897e0aced77cf
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-13 16:45:00 -0400

    Update NAMESPACE

--- R/basejump.R
+++ R/basejump.R
@@ -2,48 +2,26 @@
 #'
 #' Base functions for bioinformatics and R package development.
 #'
-#' @import dplyr
-#' @import magrittr
-#' @import readr
-#' @import stringr
-#' @import tibble
 #' @importFrom BiocCheck BiocCheck
 #' @importFrom BiocInstaller biocValid
 #' @importFrom devtools build build_vignettes check document install load_all
 #'   test
+#' @importFrom dplyr arrange filter funs group_by left_join mutate mutate_all
+#'   select summarise_all top_n
 #' @importFrom knitr asis_output kable opts_knit
+#' @importFrom magrittr %>% set_colnames set_names set_rownames
 #' @importFrom R.utils gzip
 #' @importFrom RCurl getURL
-#' @importFrom rlang is_string
+#' @importFrom readr read_csv read_delim read_lines read_tsv write_csv
+#' @importFrom readxl read_excel
+#' @importFrom rlang !!! !! is_string sym syms
 #' @importFrom rmarkdown render
-#' @importFrom stats na.omit setNames
+#' @importFrom stats na.omit
+#' @importFrom stringr str_replace_all str_subset
+#' @importFrom tibble glimpse remove_rownames
 #' @importFrom utils download.file globalVariables
 "_PACKAGE"
 
 
 
-# Globals ====
 globalVariables(c(".", "biocLite"))
-
-
-
-# Re-exports ====
-#' @usage NULL
-#' @export
-magrittr::`%>%`
-
-#' @usage NULL
-#' @export
-rlang::is_string
-
-#' @usage NULL
-#' @export
-magrittr::set_colnames
-
-#' @usage NULL
-#' @export
-magrittr::set_names
-
-#' @usage NULL
-#' @export
-magrittr::set_rownames
