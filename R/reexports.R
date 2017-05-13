086cdc821095addc1d257474c48c513fa0e91bbb
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-13 16:45:00 -0400

    Update NAMESPACE

---
+++ R/reexports.R
@@ -0,0 +1,91 @@
+#' @usage NULL
+#' @export
+rlang::`!!!`
+
+#' @usage NULL
+#' @export
+rlang::`!!`
+
+#' @usage NULL
+#' @export
+magrittr::`%>%`
+
+#' @usage NULL
+#' @export
+dplyr::arrange
+
+#' @usage NULL
+#' @export
+dplyr::filter
+
+#' @usage NULL
+#' @export
+tibble::glimpse
+
+#' @usage NULL
+#' @export
+dplyr::group_by
+
+#' @usage NULL
+#' @export
+rlang::is_string
+
+#' @usage NULL
+#' @export
+dplyr::left_join
+
+#' @usage NULL
+#' @export
+dplyr::mutate
+
+#' @usage NULL
+#' @export
+readr::read_csv
+
+#' @usage NULL
+#' @export
+readr::read_delim
+
+#' @usage NULL
+#' @export
+readxl::read_excel
+
+#' @usage NULL
+#' @export
+readr::read_tsv
+
+#' @usage NULL
+#' @export
+tibble::remove_rownames
+
+#' @usage NULL
+#' @export
+dplyr::select
+
+#' @usage NULL
+#' @export
+magrittr::set_colnames
+
+#' @usage NULL
+#' @export
+magrittr::set_names
+
+#' @usage NULL
+#' @export
+magrittr::set_rownames
+
+#' @usage NULL
+#' @export
+rlang::sym
+
+#' @usage NULL
+#' @export
+rlang::syms
+
+#' @usage NULL
+#' @export
+dplyr::top_n
+
+#' @usage NULL
+#' @export
+readr::write_csv
