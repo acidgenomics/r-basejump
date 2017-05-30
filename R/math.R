b0094bd4f1e11157c0b6ce5beaf6d386e527c33e
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-30 14:26:29 +0200

    `geomean()` initial commit

--- R/math.R
+++ R/math.R
@@ -1,4 +1,42 @@
 ## General ====
+#' Geometric mean
+#'
+#' The geometric mean is the nth root of n products or e to the mean log of `x`.
+#' Useful for describing non-normal (i.e. geometric) distributions.
+#'
+#' @details Modified version of [psych::geometric.mean()].
+#'
+#' @note Not particularly useful if there are elements that are <= 0.
+#'
+#' @param object Vector or column data (data frame, matrix).
+#' @param nm.rm Remove `NA` values before processing.
+#'
+#' @return Geometric means.
+#' @export
+#'
+#' @examples
+#' # Vector
+#' vec <- seq(1,5)
+#' vec2 <- vec^2
+#' vec2[2] <- NA
+#' geomean(vec)
+#' geomean(vec2)
+#'
+#' # Data frame
+#' df <- data.frame(vec, vec2)
+#' geomean(df)
+#' geomean(df, na.rm = FALSE)
+geomean <- function(object, na.rm = TRUE) {
+    if (is.vector(object)) {
+        exp(mean(log(object), na.rm = TRUE))
+    } else if (is.data.frame(object) | is.matrix(object)) {
+        # `2` denotes columnwise calculation
+        exp(apply(log(object), 2, mean, na.rm = na.rm))
+    }
+}
+
+
+
 #' Convert numeric to percentage
 #'
 #' @param number Number.
@@ -23,7 +61,7 @@ pct <- function(number) {
 #' Convert log ratio normalized values to fold change. Based on the approach
 #' used in `gtools::logratio2foldchange()`.
 #'
-#' @param logRatio Numeric vector of log ratio values.
+#' @param lr Numeric vector of log ratio values.
 #' @param base Logarithm base. Defaults to `2`, for compatibility with RNA-Seq
 #'   differential expression output.
 #'
@@ -32,10 +70,8 @@ pct <- function(number) {
 #'
 #' @examples
 #' logRatioToFoldChange(seq(-3, 3, 0.5))
-logRatioToFoldChange <- function(logRatio, base = 2) {
-    x <- base^(logRatio)
-    x <- ifelse(x < 1, -1/x, x)
-    x
+logRatioToFoldChange <- function(lr, base = 2) {
+    base^(lr) %>% ifelse(. < 1, -1/., .)
 }
 
 #' @rdname aliases
