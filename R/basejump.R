294231c97ba26380a43d992e3b6804737cfc697e
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-13 10:30:36 -0400

    Simplify dependencies, downgrade r-base requirement to 3.3.1

--- R/basejump.R
+++ R/basejump.R
@@ -32,7 +32,8 @@
 #' @importFrom magrittr %>% set_colnames set_rownames
 #' @importFrom parallel mclapply mcmapply
 #' @importFrom pbmcapply pbmclapply
-#' @importFrom pkgdown build_site
+# [fix] Add back once pkgdown is on CRAN
+# @importFrom pkgdown build_site
 #' @importFrom R.utils gzip gunzip
 #' @importFrom RCurl getURL
 #' @importFrom readxl read_excel
