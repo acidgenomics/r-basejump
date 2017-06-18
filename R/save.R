617d4061208310261ac15274c7394515230777f0
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-18 10:28:55 -0400

    Simplify `saveData()`

--- R/save.R
+++ R/save.R
@@ -19,11 +19,11 @@ saveData <- function(..., dir = "data", compress = "xz") {
     if (!dir.exists(dir)) {
         dir.create(dir, recursive = TRUE, showWarnings = FALSE)
     }
-    names <- as.character(substitute(list(...)))[-1L]
     objs <- get_objs_from_dots(dots(...))
     paths <- file.path(dir, paste0(objs, ".rda"))
-    paste("Saving", toString(names), "to", dir) %>%
-        paste0("...") %>% message
+    paste("Saving", toString(objs), "to", dir) %>%
+        paste0("...") %>%
+        message
     mapply(save, list = objs, file = paths, compress = compress)
     invisible()
 }
