a8df0fed3c34f5c93b1e65ed0a7ca0b309252bf9
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-28 22:34:07 -0400

    Simplify documentation for tidyverse generics

--- R/all_generics.R
+++ R/all_generics.R
@@ -1,21 +1,25 @@
 #' @rdname tidy
+#' @usage NULL
 #' @export
 setGeneric("arrange", function(object, ...) standardGeneric("arrange"))
 
 
 
 #' @rdname tidy
+#' @usage NULL
 #' @export
 setGeneric("as_tibble", function(object, ...) standardGeneric("as_tibble"))
 
 
 
 #' @rdname tidy
+#' @usage NULL
 #' @export
 setGeneric("filter", function(object, ...) standardGeneric("filter"))
 
 
 
 #' @rdname tidy
+#' @usage NULL
 #' @export
 setGeneric("mutate", function(object, ...) standardGeneric("mutate"))
