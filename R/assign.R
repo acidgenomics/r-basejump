9a54e316de6b6351170ec8800a398524d9e85a97
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-10 14:48:19 -0400

    Export new assign function

--- R/assign.R
+++ R/assign.R
@@ -3,6 +3,8 @@
 #' @param name Desired variable name.
 #' @param object Object.
 #' @param compress File compression argument from [save()].
+#'
+#' @export
 assign_data <- function(name, object, compress = "xz") {
     assign(name, object, envir = parent.frame())
     save(list = name,
