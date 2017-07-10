e4a5da89339bbf3146da59ffcee8e0cd972dc73e
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-10 14:42:58 -0400

    Initial commit

---
+++ R/assign.R
@@ -0,0 +1,11 @@
+#' Assign and save data
+#'
+#' @param name Desired variable name.
+#' @param object Object.
+#' @param compress File compression argument from [save()].
+assign_data <- function(name, object, compress = "xz") {
+    assign(name, object, envir = parent.frame())
+    save(list = name,
+         file = file.path("data", str_c(name, ".rda")),
+         compress = compress)
+}
