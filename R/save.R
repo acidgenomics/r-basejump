3825b8b97e0c1489b68b8b1efdaf45c335edb0df
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-12 09:03:24 -0400

    Rename `assignData()` to `assignAndSaveData()` for improved semantic meaning

--- R/save.R
+++ R/save.R
@@ -1,17 +1,22 @@
 #' Save data objects
 #'
-#' Quick save to `data` (default) or `data-raw` directory.
+#' Quickly save objects in a project.
 #'
 #' @rdname save
 #'
-#' @param ... Objects.
-#' @param dir Save directory.
+#' @param ... Dot objects.
+#' @param dir Output directory. Defaults to **data**.
 #' @param compress Compression method, supporting `xz` (**preferred**), `bzip2`,
 #'   or `gzip`. Compression can be disabled by setting as `FALSE`, although this
 #'   is not generally recommended.
 #'
 #' @export
 #' @seealso [base::save()].
+
+
+
+#' @rdname save
+#' @export
 saveData <- function(..., dir = "data", compress = "xz") {
     if (!is_string(dir)) {
         stop("dir must be a string")
@@ -29,7 +34,29 @@ saveData <- function(..., dir = "data", compress = "xz") {
 
 
 #' @rdname save
+#' @description [saveDataRaw()] is a wrapper for [saveData()] that enables quick
+#'   saving of objects to the `data-raw` directory.
 #' @export
 saveDataRaw <- function(...) {
     saveData(..., dir = "data-raw")
+}
+
+
+
+#' @rdname save
+#' @description [assignAndSaveData()] assigns a new object by name to the
+#'   current working environment then saves the newly assigned object, specified
+#'   by `dir`.
+#'
+#' @param name Desired variable name.
+#' @param object Object.
+#'
+#' @export
+assignAndSaveData <- function(name, object, dir = "data", compress = "xz") {
+    envir <- parent.frame()
+    assign(name, object, envir = envir)
+    save(list = name,
+         file = file.path(dir, str_c(name, ".rda")),
+         envir = envir,
+         compress = compress)
 }
