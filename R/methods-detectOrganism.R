2a200c9e712482e97f23264c72ad9fbbc73c85c3
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-28 13:28:54 -0400

    Migrate `detectOrganism()` to S4

--- R/methods-detectOrganism.R
+++ R/methods-detectOrganism.R
@@ -1,8 +1,8 @@
-#' Detect the Organism from String
+#' Detect Organism
 #'
 #' Supports organism detection from genome build or Ensembl identifier.
 #'
-#' @param string String.
+#' @rdname detectOrganism
 #'
 #' @return Full latin scientific organism name.
 #' @export
@@ -23,39 +23,39 @@
 #' # D. melanogaster
 #' detectOrganism("BDGP6")
 #' detectOrganism("FBgn0000003")
-detectOrganism <- function(string) {
-    if (str_detect(string,
+setMethod("detectOrganism", "character", function(object) {
+    if (str_detect(object,
                    regex("^(grch|hg)\\d{2}$", ignore_case = TRUE)) |
-        str_detect(string, "^ENSG\\d{11}$")) {
+        str_detect(object, "^ENSG\\d{11}$")) {
         c(human = "Homo sapiens")
-    } else if (str_detect(string,
+    } else if (str_detect(object,
                           regex("^(grcm|mm)\\d{2}$", ignore_case = TRUE)) |
-               str_detect(string, "^ENSMUSG\\d{11}$")) {
+               str_detect(object, "^ENSMUSG\\d{11}$")) {
         c(mouse = "Mus musculus")
-    } else if (str_detect(string,
+    } else if (str_detect(object,
                           regex("^wbcel\\d{3}$", ignore_case = TRUE)) |
-               str_detect(string, "^WBGene\\d{8}$")) {
+               str_detect(object, "^WBGene\\d{8}$")) {
         c(roundworm = "Caenorhabditis elegans")
-    } else if (str_detect(string,
+    } else if (str_detect(object,
                           regex("^bdgp\\d$", ignore_case = TRUE)) |
-               str_detect(string, "^FBgn\\d{7}$")) {
+               str_detect(object, "^FBgn\\d{7}$")) {
         c(fruitfly = "Drosophila melanogaster")
-    } else if (str_detect(string, "^ENSGALG\\d{11}$")) {
+    } else if (str_detect(object, "^ENSGALG\\d{11}$")) {
         c(chicken = "Gallus gallus")
-    } else if (str_detect(string, "^ENSRNOG\\d{11}$")) {
+    } else if (str_detect(object, "^ENSRNOG\\d{11}$")) {
         c(rat = "Rattus norvegicus")
-    } else if (str_detect(string,
+    } else if (str_detect(object,
                           regex("^(mb|mb)\\d", ignore_case = TRUE))) {
         warning("Unsupported genome")
         c(bacteria = "Escherichia coli")
-    } else if (str_detect(string, "^ASM\\d")) {
+    } else if (str_detect(object, "^ASM\\d")) {
         warning("Unsupported annotations")
         c(yeast = "Schizosaccharomyces pombe")
-    } else if (str_detect(string,
+    } else if (str_detect(object,
                           regex("^zv\\d", ignore_case = TRUE))) {
         warning("Unsupported annotations")
         c(zebrafish = "Danio rerio")
     } else {
         stop("Failed to detect organism")
     }
-}
+})
