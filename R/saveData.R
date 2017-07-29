3cb56e5eda3d9c363f912b597bc016b9a79aed95
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-29 05:25:04 -0400

    Update documentation

--- R/saveData.R
+++ R/saveData.R
@@ -1,8 +1,10 @@
-#' Save Data Objects
+#' Save Data
 #'
-#' Quickly save objects in a project.
+#' Wrapper for [base::save()] supporting quick saving of object names passed as
+#' symbols. This function saves each object into a separate `.rda` file rather
+#' than combining into a single file.
 #'
-#' @rdname save
+#' @family Save Utilities
 #'
 #' @param ... Object names as symbols.
 #' @param dir Output directory. Defaults to **data**.
@@ -10,32 +12,23 @@
 #'   or `gzip`. Compression can be disabled by setting as `FALSE`, although this
 #'   is not generally recommended.
 #'
-#' @note These functions will *overwrite* existing saved data, following the
+#' @note These function will *overwrite* existing saved data, following the
 #'   same conventions as [base::save()]. Conversely, [devtools::use_data()] does
 #'   not overwrite by default if that behavior is preferred.
 #'
 #' @seealso
-#' - [base::save()].
+#' - [base::save()]
 #' - [devtools::use_data()].
-#' - [devtools::use_data_raw()].
-
-
-
-#' @rdname save
-#' @details
-#' - [saveData()]: Wrapper for [base::save()] supporting quick saving of object
-#'   names passed as symbols. This function saves each object into a separate
-#'   `.rda` file rather than combining into a single file.
+#'
+#' @return No value.
 #' @export
 saveData <- function(..., dir = "data", compress = TRUE) {
-    if (!is_string(dir)) {
-        stop("dir must be a string")
-    }
+    if (!is_string(dir)) stop("dir must be a string")
     if (!dir.exists(dir)) {
         dir.create(dir, recursive = TRUE, showWarnings = FALSE)
     }
     objs <- getObjsFromDots(dots(...))
-    paths <- file.path(dir, str_c(objs, ".rda"))
+    paths <- file.path(dir, paste0(objs, ".rda"))
     message(paste("Saving", toString(objs), "to", dir))
     mapply(save, list = objs, file = paths, compress = compress)
     invisible()
@@ -43,10 +36,16 @@ saveData <- function(..., dir = "data", compress = TRUE) {
 
 
 
-#' @rdname save
-#' @details
-#' - [saveDataRaw()]: Wrapper for [saveData()] that enables quick saving of
-#'   objects to the `data-raw` directory.
+#' Save Raw Data
+#'
+#' Wrapper for [saveData()] that enables quick saving of objects to the
+#' `data-raw/` directory.
+#'
+#' @family Save Utilities
+#' @inherit saveData
+#'
+#' @seealso [devtools::use_data_raw()].
+#'
 #' @export
 saveDataRaw <- function(...) {
     saveData(..., dir = "data-raw")
