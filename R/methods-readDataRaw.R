15010a2d5b42cfc2fefe5a9df15f943f21c3e79e
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-04 14:27:16 -0400

    Finish migration to S4 methods

--- R/methods-readDataRaw.R
+++ R/methods-readDataRaw.R
@@ -3,11 +3,20 @@
 #' This function automatically loads raw CSVs using the readr package and
 #' saves the data as R binary data into the `data/` directory.
 #'
+#' @rdname readDataRaw
+#' @name readDataRaw
+#'
 #' @family Data Import and Project Utilities
 #'
 #' @return No value.
+NULL
+
+
+
+# Methods ====
+#' @rdname readDataRaw
 #' @export
-readDataRaw <- function() {
+setMethod("readDataRaw", "missing", function() {
     csv <- list.files("data-raw", pattern = "*.csv", full.names = TRUE)
     lapply(seq_along(csv), function(a) {
         name <- basename(csv[a]) %>% file_path_sans_ext
@@ -17,4 +26,4 @@ readDataRaw <- function() {
         save(list = name, file = file.path("data", paste0(name, ".rda")))
     }) %>%
         invisible
-}
+})
