f7780db6df7cc15f1ac48f7afa09195ed25d6005
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-05 10:09:32 -0400

    Improve unit testing

---
+++ tests/testthat/test_assign.R
@@ -0,0 +1,18 @@
+context("Object Assignment Utilities")
+
+test_that("assignAndSaveData", {
+    expect_message(
+        assignAndSaveData("test", mtcars),
+        "Saving test to data")
+})
+
+
+
+test_that("multiassignAsNewEnv", {
+    expect_message(
+        multiassignAsNewEnv(mtcars, starwars, newEnv = "test"),
+        "Assigning mtcars, starwars as test")
+    expect_equal(
+        multiassignAsNewEnv(mtcars, starwars, newEnv = "test"),
+        c("mtcars", "starwars"))
+})
