f6d5e078af501d111515cbd3a1e0e9a56988ee68
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-13 07:55:01 -0400

    Improve report/markdown unit testing

---
+++ tests/testthat/test_markdown.R
@@ -0,0 +1,22 @@
+context("Markdown Utilities")
+
+test_that("mdHeader", {
+    expect_output(
+        mdHeader("Header"),
+        "\\n\\n## Header\\n")
+    expect_output(
+        mdHeader("Header", level = 4L),
+        "\\n\\n#### Header\\n")
+})
+
+
+
+test_that("mdList", {
+    groceries <- c("milk", "eggs")
+    expect_output(
+        mdList(groceries),
+        "\\n- milk\\n- eggs\\n")
+    expect_output(
+        mdList(groceries, ordered = TRUE),
+        "\\n1. milk\\n2. eggs\\n")
+})
