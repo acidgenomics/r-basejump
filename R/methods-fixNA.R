7d612435cf71ea41e8f0818798b8172eebd53c2f
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-21 17:15:00 -0400

    Improve `fixNA()` to work on data frames

--- R/methods-fixNA.R
+++ R/methods-fixNA.R
@@ -11,6 +11,20 @@ NULL
 
 
 
+# Constructors ====
+.fixNAVec <- function(object) {
+    gsub("^$|^\\s+$|^NA$", NA, object)
+}
+
+
+
+.fixNADim <- function(object) {
+    apply(object, 2L, .fixNAVec)
+}
+
+
+
+# Methods ====
 #' @rdname fixNA
 #' @export
 setMethod("fixNA", "ANY", function(object) {
@@ -21,6 +35,22 @@ setMethod("fixNA", "ANY", function(object) {
 
 #' @rdname fixNA
 #' @export
-setMethod("fixNA", "character", function(object) {
-    gsub("^$|^\\s+$|^NA$", NA, object)
-})
+setMethod("fixNA", "character", .fixNAVec)
+
+
+
+#' @rdname fixNA
+#' @export
+setMethod("fixNA", "data.frame", .fixNADim)
+
+
+
+#' @rdname fixNA
+#' @export
+setMethod("fixNA", "DataFrame", .fixNADim)
+
+
+
+#' @rdname fixNA
+#' @export
+setMethod("fixNA", "tbl_df", .fixNADim)
