76a570a25c345baac16c9cd8ba4e96e2a805bc21
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-22 14:45:53 -0400

    Move testDataURL to main package and export

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,12 +3,4 @@ options(stringsAsFactors = FALSE)
 library(testthat)
 library(basejump)
 
-testDataURL <- file.path(
-    "https://raw.githubusercontent.com",
-    "steinbaugh",
-    "basejump",
-    "dev",
-    "docs",
-    "tests")
-
 test_check("basejump")
