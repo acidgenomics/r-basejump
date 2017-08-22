3dc8ac726fc3a9943a87f7c859f4bb2d3a8a656a
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-22 14:06:10 -0400

    Move dataset exports to 'datasets.R' file

---
+++ R/datasets.R
@@ -0,0 +1,13 @@
+#' @inherit datasets::mtcars
+#' @keywords internal
+#' @return [data.frame].
+#' @export
+datasets::mtcars -> mtcars
+
+
+
+#' @inherit dplyr::starwars
+#' @keywords internal
+#' @return [tibble].
+#' @export
+dplyr::starwars -> starwars
