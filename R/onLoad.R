83b7434b6ce84d128197a94d23c06ede1d415172
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-23 07:48:24 -0400

    lintr fix

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-    pkgs <- c("annotables")
+    pkgs <- "annotables"
     lapply(seq_along(pkgs), function(a) {
         if (!pkgs[a] %in% (.packages())) {
             attachNamespace(pkgs[a])
