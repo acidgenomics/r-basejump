0fa5ae573420df568b22825e218a8703ab34cad3
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-24 15:08:09 -0400

    Fix documentation to pass lintr check

--- R/methods-prepareTemplate.R
+++ R/methods-prepareTemplate.R
@@ -10,8 +10,8 @@
 #'
 #' @param object *Optional*. File name. If `NULL` (default), download the
 #'   default dependency files for a new experiment.
-#' @param sourceDir Source directory, typically a URL, where the dependency files are
-#'   located.
+#' @param sourceDir Source directory, typically a URL, where the dependency
+#'   files are located.
 #'
 #' @return No value.
 #'
