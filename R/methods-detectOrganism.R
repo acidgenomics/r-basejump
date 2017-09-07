002e02fde670526b8c33380596718ac12c2be7b4
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-07 11:22:42 -0400

    Improve case insensitive detection

--- R/methods-detectOrganism.R
+++ R/methods-detectOrganism.R
@@ -13,24 +13,9 @@
 #' detectOrganism("GRCh38")
 #' detectOrganism("hg38")
 #' detectOrganism("ENSG00000000003")
-#' detectOrganism("human")
-#' detectOrganism("hsapiens")
 #' detectOrganism("Homo sapiens")
-#'
-#' # M. musculus
-#' detectOrganism("GRCm38")
-#' detectOrganism("mm10")
-#' detectOrganism("ENSMUSG00000002459")
-#'
-#' # C. elegans
-#' detectOrganism("WBcel235")
-#' detectOrganism("ce11")
-#' detectOrganism("WBGene00000001")
-#'
-#' # D. melanogaster
-#' detectOrganism("BDGP6")
-#' detectOrganism("dm6")
-#' detectOrganism("FBgn0000003")
+#' detectOrganism("Hsapiens")
+#' detectOrganism("human")
 NULL
 
 
@@ -39,47 +24,47 @@ NULL
 #' @rdname detectOrganism
 #' @export
 setMethod("detectOrganism", "character", function(object) {
-    if (object %in% c("human",
-                      "hsapiens",
-                      "Homo sapiens") |
+    if (tolower(object) %in% c("human",
+                               "hsapiens",
+                               "homo sapiens") |
         str_detect(object,
                    regex("^(grch|hg)\\d+$", ignore_case = TRUE)) |
         str_detect(object, "^ENS(G|T)\\d{11}$")) {
         c(human = "Homo sapiens")
     } else if (
-        object %in% c("mouse",
-                      "mmusculus",
-                      "Mus musculus") |
+        tolower(object) %in% c("mouse",
+                               "mmusculus",
+                               "mus musculus") |
         str_detect(object,
                    regex("^(grcm|mm)\\d+$", ignore_case = TRUE)) |
         str_detect(object, "^ENSMUS(G|T)\\d{11}$")) {
         c(mouse = "Mus musculus")
     } else if (
-        object %in% c("roundworm",
-                      "celegans",
-                      "Caenorhabditis elegans") |
+        tolower(object) %in% c("roundworm",
+                               "celegans",
+                               "caenorhabditis elegans") |
         str_detect(object,
                    regex("^(ce|wbcel)\\d+$", ignore_case = TRUE)) |
         str_detect(object, "^WBGene\\d{8}$")) {
         c(roundworm = "Caenorhabditis elegans")
     } else if (
-        object %in% c("fruitfly",
-                      "dmelanogaster",
-                      "Drosophila melanogaster") |
+        tolower(object) %in% c("fruitfly",
+                               "dmelanogaster",
+                               "drosophila melanogaster") |
         str_detect(object,
                    regex("^(bdgp|dm)\\d+$", ignore_case = TRUE)) |
         str_detect(object, "^FB(gn|tr)\\d{7}$")) {
         c(fruitfly = "Drosophila melanogaster")
     } else if (
-        object %in% c("chicken",
-                      "ggallus",
-                      "Gallus gallus") |
+        tolower(object) %in% c("chicken",
+                               "ggallus",
+                               "gallus gallus") |
         str_detect(object, "^ENSGAL(G|T)\\d{11}$")) {
         c(chicken = "Gallus gallus")
     } else if (
-        object %in% c("rat",
-                      "rnorvegicus",
-                      "Rattus norvegicus") |
+        tolower(object) %in% c("rat",
+                               "rnorvegicus",
+                               "rattus norvegicus") |
         str_detect(object, "^ENSRNO(G|T)\\d{11}$")) {
         c(rat = "Rattus norvegicus")
     } else {
