98d2e5b9722c492fed31f913a2ac5693dd5673df
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-09 19:14:55 -0400

    Remove onLoad requirement of annotables

--- R/zzz.R
+++
@@ -1,9 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-    pkgs <- "annotables"
-    lapply(seq_along(pkgs), function(a) {
-        if (!pkgs[a] %in% (.packages())) {
-            attachNamespace(pkgs[a])
-        }
-    })
-    invisible()
-}
