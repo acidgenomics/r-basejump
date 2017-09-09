7e1ff629fc0f908c9cb8e853668e614eef8c9923
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-09 13:22:39 -0400

    Resave synonyms

--- data-raw/synonyms.R
+++ data-raw/synonyms.R
@@ -1,13 +1,15 @@
+library(devtools)
+library(stringr)
 library(tidyverse)
 
+# Support for C. elegans is poor here. Use WormBase instead.
 genomes <- list(
-    celegans = c("Invertebrates", "Caenorhabditis_elegans"),
     dmelanogaster = c("Invertebrates", "Drosophila_melanogaster"),
     hsapiens = c("Mammalia", "Homo_sapiens"),
     mmusculus = c("Mammalia", "Mus_musculus"))
 
 synonyms <- lapply(seq_along(genomes), function(a) {
-    file.path("ftp://ftp.ncbi.nih.gov",
+    tbl <- file.path("ftp://ftp.ncbi.nih.gov",
               "gene",
               "DATA",
               "GENE_INFO",
@@ -15,13 +17,29 @@ synonyms <- lapply(seq_along(genomes), function(a) {
               paste0(genomes[[a]][[2L]], ".gene_info.gz")) %>%
         read_tsv %>%
         camel %>%
-        filter(synonyms != "-") %>%
-        rename(ncbi = geneId) %>%
-        mutate(ensgene = str_extract(.data[["dbXrefs"]],
-                                     "\\bENS[A-Z]+[0-9]{11}\\b")) %>%
-        select(symbol, synonyms, description, ncbi, ensgene) %>%
+        select(symbol, synonyms, dbXrefs) %>%
+        filter(synonyms != "-",
+               dbXrefs != "-") %>%
+        mutate(synonyms = str_replace_all(synonyms, "\\|", ", ")) %>%
         arrange(symbol)
+
+    # Extract the gene identifiers
+    organism <- names(genomes)[[a]]
+    if (organism == "dmelanogaster") {
+        tbl <- tbl %>%
+            mutate(ensgene = str_extract(
+                .data[["dbXrefs"]], "\\bFBgn[0-9]{7}\\b"))
+    } else if (organism %in% c("hsapiens", "mmusculus")) {
+        tbl <- tbl %>%
+            mutate(ensgene = str_extract(
+                .data[["dbXrefs"]], "\\bENS[A-Z]+[0-9]{11}\\b"))
+    }
+
+    tbl %>%
+        filter(!is.na(ensgene)) %>%
+        select(ensgene, symbol, synonyms) %>%
+        arrange(ensgene)
 }) %>%
     set_names(names(genomes))
 
-devtools::use_data(synonyms, compress = "xz", overwrite = TRUE)
+use_data(synonyms, compress = "xz", overwrite = TRUE)
