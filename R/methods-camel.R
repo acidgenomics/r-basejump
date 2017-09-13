fca2bcdfc578b001678489b3f7068d9ae1df3d4d
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-13 13:32:01 -0400

    Break out camel and snake methods into separate .R files

---
+++ R/methods-camel.R
@@ -0,0 +1,71 @@
+# Constructors ====
+.makeNamesCamel <- function(object, strict = TRUE) {
+    object %>%
+        # Special word exceptions
+        str_replace_all("RNAi", "Rnai") %>%
+        .makeNamesDotted(strict = strict) %>%
+        gsub("\\.(\\w?)", "\\U\\1", ., perl = TRUE)
+}
+
+
+
+.setNamesCamel <- function(object, rownames = FALSE, strict = TRUE) {
+    if (.checkNames(object)) {
+        object <- setNames(
+            object,
+            .makeNamesCamel(names(object), strict = strict))
+    }
+    if (isTRUE(rownames) & .checkRownames(object)) {
+        object <- set_rownames(
+            object,
+            .makeNamesCamel(rownames(object), strict = strict))
+    }
+    object
+}
+
+
+
+# Methods ====
+#' @rdname makeNames
+#' @export
+setMethod("camel", "ANY", .setNamesCamel)
+
+
+
+#' @rdname makeNames
+#' @export
+setMethod("camel", "character", function(object, strict = TRUE) {
+    if (isTRUE(.checkNames(object))) {
+        .setNamesCamel(object, rownames = FALSE, strict = strict)
+    } else {
+        .makeNamesCamel(object, strict = strict)
+    }
+})
+
+
+
+#' @rdname makeNames
+#' @export
+setMethod("camel", "data.frame", .setNamesCamel)
+
+
+
+#' @rdname makeNames
+#' @export
+setMethod("camel", "list", function(object, strict = TRUE) {
+    .setNamesCamel(object, rownames = FALSE, strict = strict)
+})
+
+
+
+#' @rdname makeNames
+#' @export
+setMethod("camel", "matrix", .setNamesCamel)
+
+
+
+#' @rdname makeNames
+#' @export
+setMethod("camel", "tbl_df", function(object, strict = TRUE) {
+    .setNamesCamel(object, rownames = FALSE, strict = strict)
+})
