52c85679244078612929b114bb6ff4b9731aa230
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-19 08:24:02 -0400

    Don't mess with environments for the time being

--- R/loadData.R
+++ R/loadData.R
@@ -4,9 +4,6 @@
 #' corresponding script from the `data-raw/` directory.
 #'
 #' @param dir Directory where the RData (`.rda`) files are located.
-#' @param envir Environment where the objects will be loaded. Defaults to
-#'   `parent.frame()`, also referred to as the calling environment. This
-#'   argument supports new environment creation when a character string is used.
 #'
 #' @return No value.
 #' @export
@@ -16,16 +13,17 @@
 #' loadData(geneIDs, oligo)
 #' loadData(geneIDs, envir = "newenv")
 #' }
-loadData <- function(..., dir = "data", envir = parent.frame()) {
-    dots <- dots(..., character = TRUE)
-    message(paste("Loading", toString(dots), "from", dir))
-    lapply(seq_along(dots), function(a) {
-        file <- file.path(dir, paste0(dots[a], ".rda"))
+loadData <- function(..., dir = "data") {
+    envir <- parent.frame()
+    names <- dots(..., character = TRUE)
+    message(paste("Loading", toString(names), "from", dir))
+    lapply(seq_along(names), function(a) {
+        file <- file.path(dir, paste0(names[a], ".rda"))
         if (file.exists(file)) {
             load(file, envir = envir)
         } else {
             # Skip and warn
-            warning(paste(dots[a], "missing"))
+            warning(paste(names[a], "missing"), call. = FALSE)
         }
     }) %>%
         invisible
