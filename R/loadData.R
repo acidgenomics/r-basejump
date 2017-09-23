007e071d1bb0e9b610ee74df6f423b07ac730d77
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-23 09:23:53 -0400

    Add silent return of file paths

--- R/loadData.R
+++ R/loadData.R
@@ -4,26 +4,32 @@
 #' file paths.
 #'
 #' @inheritParams saveData
+#' @param envir Environment to use for assignment. Defaults to `parent.frame()`,
+#' which will assign into the calling environment.
 #'
-#' @return No value.
+#' @return Silent named character vector of file paths.
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' loadData(geneIDs, oligo)
 #' }
-loadData <- function(..., dir = "data") {
-    envir <- parent.frame()
+loadData <- function(..., dir = "data", envir = parent.frame()) {
+    # The dots method will error at this step because the objects as symbols
+    # aren't present in the calling environment.
     names <- as.character(substitute(list(...)))[-1L]
     message(paste("Loading", toString(names), "from", dir))
-    lapply(seq_along(names), function(a) {
+    files <- sapply(seq_along(names), function(a) {
         file <- file.path(dir, paste0(names[a], ".rda"))
         if (file.exists(file)) {
-            load(file, envir = envir)
+            file <- normalizePath(file)
+            name <- load(file, envir = envir)
+            names(file) <- name
+            file
         } else {
             # Skip and warn
             warning(paste(names[a], "missing"), call. = FALSE)
         }
-    }) %>%
-        invisible
+    })
+    invisible(files)
 }
