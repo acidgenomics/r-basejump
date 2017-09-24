5044ca6e4b6da79e0d28cf8194ee8264a4eade4f
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-24 18:24:51 -0400

    Rename `multiassignAsNewEnv()` to `multiassignAsNewEnvir()`

---
+++ R/multiassignAsNewEnvir.R
@@ -0,0 +1,37 @@
+#' Assign Multiple Objects as a New Environment
+#'
+#' @family Object Assignment Utilities
+#'
+#' @inheritParams dots
+#' @param envirName New environment name (string).
+#' @param parentEnvir Parent environment of new environment.
+#'
+#' @return Character vector of assigned objects in the new environment.
+#' @export
+#'
+#' @seealso [Biobase::multiassign()].
+#'
+#' @examples
+#' multiassignAsNewEnvir(mtcars, starwars, envirName = "test")
+multiassignAsNewEnvir <- function(
+    ...,
+    envirName,
+    parentEnvir = parent.frame()) {
+    if (!is_string(envirName)) {
+        stop("'envirName' must be a string", call. = FALSE)
+    }
+    if (!is.environment(parentEnvir)) {
+        stop("'parentEnvir' must be an environment", call. = FALSE)
+    }
+    dots <- dots(...)
+    dotsNames <- dots(..., character = TRUE)
+    envir <- new.env(parent = parentEnvir)
+    lapply(seq_along(dots), function(a) {
+        assign(dotsNames[[a]], eval(dots[[a]]), envir = envir)
+    }) %>%
+        invisible
+    message(paste("Assigning", toString(dotsNames), "as", envirName))
+    assign(envirName, value = envir, envir = parentEnvir)
+    # Silently return a list of the objects in the new environment
+    invisible(objects(envir))
+}
