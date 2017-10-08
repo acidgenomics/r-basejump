d43b48a73cfcd822e20cd5366c177cdeb1172059
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-08 09:12:28 -0400

    Consolidate all `readSampleMetadataFile()` code

--- R/methods-readSampleMetadataFile.R
+++ R/methods-readSampleMetadataFile.R
@@ -26,45 +26,105 @@ setMethod("readSampleMetadataFile", "character", function(
     patternCol = "sampleName",
     lanes = 1) {
     meta <- readFileByExtension(object)
-    # Rename legacy `samplename` column, if set
+
+    # Warn on legacy `samplename` column. We need to work on improving the
+    # consistency in examples or the internal handlng of file and sample
+    # names in a future update.
     if ("samplename" %in% colnames(meta)) {
+        warning(paste(
+            "'samplename' is used in some bcbio examples for FASTQ file names,",
+            "and 'description' for sample names. Here we are using 'fileName'",
+            "for FASTQ file names (e.g. 'control_replicate_1.fastq.gz'),",
+            "'description' for multiplexed per file sample names",
+            "(e.g. 'control replicate 1', and 'sampleName' for multiplexed",
+            "sample names (i.e. inDrop barcoded samples)."
+        ), call. = FALSE)
         meta <- dplyr::rename(meta, fileName = .data[["samplename"]])
     }
 
-    # Rename `description` to `sampleName`, if set
-    if ("description" %in% colnames(meta)) {
-        meta <- dplyr::rename(meta, sampleName = .data[["description"]])
+    # Check for basic required columns
+    requiredCols <- c("fileName", "description")
+    if (!all(requiredCols %in% colnames(meta))) {
+        stop(paste("Required columns:", toString(requiredCols)))
     }
+
+    # Prepare metadata for lane split replicates. This step will expand rows
+    # into the number of desired replicates.
+    if (lanes > 1) {
+        meta <- meta %>%
+            group_by(!!sym("description")) %>%
+            # Expand by lane (e.g. "L001")
+            expand(lane = paste0("L", str_pad(1:lanes, 3, pad = "0"))) %>%
+            left_join(meta, by = "description") %>%
+            ungroup() %>%
+            mutate(description = paste(
+                .data[["description"]], .data[["lane"]], sep = "_"))
+    }
+
+    # Determine whether the samples are multiplexed, based on the presence
+    # of duplicate values in the `description` column
+    if (any(duplicated(meta[["description"]]))) {
+        multiplexedFASTQ <- TRUE
+    } else {
+        multiplexedFASTQ <- FALSE
+    }
+
+    if (isTRUE(multiplexedFASTQ)) {
+        requiredCols <- c("fileName", "description", "sampleName", "sequence")
+        if (!all(requiredCols %in% colnames(meta))) {
+            stop(paste("Required columns:", toString(requiredCols)))
+        }
+    } else {
+        # Check for duplicate `description` and `sampleName`
+        if (all(c("description", "sampleName"))) {
+            stop(paste(
+                "Specify only 'description' and omit 'sampleName' for",
+                "demultiplexed FASTQ file metadata"
+            ))
+        }
+        meta[["sampleName"]] <- meta[["description"]]
+    }
+
+    # Check that sample names are unique
+    if (any(duplicated(meta[["sampleName"]]))) {
+        stop("Sample names are not unique", call. = FALSE)
+    }
+
     meta <- meta %>%
+        # Valid rows must contain `description` and `sampleName`. Imported Excel
+        # files can contain empty rows, so this helps correct that problem.
+        dplyr::filter(!is.na(.data[["description"]])) %>%
+        dplyr::filter(!is.na(.data[["sampleName"]])) %>%
         # Strip all NA rows and columns
         removeNA() %>%
         # Make colnames camelCase
-        camel(strict = FALSE) %>%
-        # Remove rows with no sample name. Sometimes Excel files will add
-        # empty rows, so this helps correct that problem as well.
-        .[!is.na(.[["sampleName"]]), , drop = FALSE]
-
-    # Lane split, if desired
-    if (lanes > 1) {
-        meta <- meta %>%
-            group_by(!!sym("sampleName")) %>%
-            # Expand by lane (e.g. "L001")
-            expand(lane = paste0("L", str_pad(1:lanes, 3, pad = "0"))) %>%
-            left_join(meta, by = "sampleName") %>%
-            ungroup() %>%
-            mutate(sampleName = paste(
-                .data[["sampleName"]], .data[["lane"]], sep = "_"))
-    }
+        camel(strict = FALSE)
 
     # Subset by pattern, if desired
     if (!is.null(pattern)) {
         meta <- meta %>%
-            .[str_detect(.[[patternCol]], pattern), , drop = FALSE]
+            dplyr::filter(str_detect(.data[[patternCol]], pattern))
     }
 
-    meta %>%
-        # Sanitize `sampleID` into valid names
-        mutate(sampleID = make.names(.data[["sampleName"]])) %>%
-        .arrangeMetadataByPriorityCols() %>%
-        .setMetadataFactors()
+    # Set the `sampleID` column
+    if (isTRUE(multiplexedFASTQ)) {
+        # The per sample directories are created by combining the
+        # `sampleName` column with the reverse complement (`revcomp`) of the
+        # index barcode sequence (`sequence`)
+        meta <- meta %>%
+            mutate(
+                revcomp = vapply(.data[["sequence"]], revcomp, character(1)),
+                # Match the sample directories exactly here, using the hyphen.
+                # We'll sanitize into valid names using `make.names()` in
+                # the final return chain.
+                sampleID = paste(
+                    .data[["description"]],
+                    .data[["revcomp"]],
+                    sep = "-"))
+    } else {
+        # For demultiplexed samples, we can just use the `description`
+        meta[["sampleID"]] <- meta[["description"]]
+    }
+
+    .sampleMetadata(meta)
 })
