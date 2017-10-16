3e77d74b990410bb358261db5a5cd814acb2076c
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-16 00:27:21 -0400

    Improve metadata, including imports

--- R/internal-prepareSampleMetadata.R
+++ R/internal-prepareSampleMetadata.R
@@ -1,31 +1,44 @@
 #' Sample Metadata Constructor
 #'
 #' @keywords internal
-#' @noRd
+#'
+#' @importFrom dplyr arrange everything mutate mutate_if select
+#' @importFrom magrittr set_rownames
+#' @importFrom rlang .data syms !!!
 #'
 #' @param metadata Metadata [data.frame].
 #' @param factors Set columns that don't apply to sample names as factors.
 #'
 #' @return [data.frame].
 .prepareSampleMetadata <- function(metadata, factors = TRUE) {
-    meta <- metadata %>%
-        as.data.frame() %>%
+    metadata <- as.data.frame(metadata)
+    # `description` must already be defined
+    if (!"description" %in% colnames(metadata)) {
+        stop("'description' column is required", .call = FALSE)
+    }
+    # Set `sampleName`, if necessary
+    if (!"sampleName" %in% colnames(metadata)) {
+        metadata[["sampleName"]] <- metadata[["description"]]
+    }
+    # Set `sampleID`, if necessary
+    if (!"sampleID" %in% colnames(metadata)) {
+        metadata[["sampleID"]] <- metadata[["sampleName"]]
+    }
+    if (isTRUE(factors)) {
+        # Set all non-priority columns as factor
+        metadata <- metadata %>%
+            mutate_if(!colnames(.) %in% metadataPriorityCols, factor)
+    }
+    metadata %>%
         # Ensure `sampleID` has valid names. This allows for input of samples
         # beginning with numbers or containing hyphens for example, which aren't
         # valid names in R.
-        mutate(
-            sampleName = .data[["description"]],
-            sampleID = make.names(
-                str_replace_all(.data[["sampleName"]], "-", "_"))
-        )
-    if (isTRUE(factors)) {
-        # Set all non-priority columns as factor
-        meta <- meta %>%
-            mutate_if(!colnames(.) %in% metadataPriorityCols, factor)
-    }
-    meta %>%
-        # Put the priority columns first and arrange rows
-        dplyr::select(metadataPriorityCols, everything()) %>%
+        mutate(sampleID = make.names(
+            gsub(x = .data[["sampleID"]],
+                 pattern = "-",
+                 replacement = "_"))) %>%
+        # Put the priority columns first and arrange the rows by them
+        select(metadataPriorityCols, everything()) %>%
         arrange(!!!syms(metadataPriorityCols)) %>%
         set_rownames(.[["sampleID"]])
 }
