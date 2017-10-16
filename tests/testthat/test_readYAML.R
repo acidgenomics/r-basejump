88382e811601b275850f03532809ed95b75c2d45
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-16 09:38:38 -0400

    Break out into separate test calls

--- tests/testthat/test_readYAML.R
+++ tests/testthat/test_readYAML.R
@@ -1,6 +1,6 @@
 context("readYAML")
 
-test_that("readYAML", {
+test_that("bcbio project summary", {
     yaml <- file.path(testDataURL, "project-summary.yaml") %>%
         readYAML(quiet = TRUE)
     expect_equal(
@@ -11,30 +11,36 @@ test_that("readYAML", {
         names(yaml),
         c("date", "upload", "bcbio_system", "samples")
     )
+})
 
-    # Check '.yml' file support
-    yaml <- file.path(
-        "https://raw.githubusercontent.com",
-        "steinbaugh",
-        "basejump",
-        "master",
-        ".travis.yml") %>%
-        readYAML(quiet = TRUE)
+test_that("'.yml' file support", {
+    # Use the package `.travis.yml` configuration as an example
+    yaml <- readYAML(
+        file.path(
+            "https://raw.githubusercontent.com",
+            "steinbaugh",
+            "basejump",
+            "master",
+            ".travis.yml"),
+        quiet = TRUE)
     expect_true("language" %in% names(yaml))
+})
 
-    # Unsupported file type
+test_that("unsupported file type", {
     expect_error(
         readYAML(file.path(testDataURL, "mtcars.csv"), quiet = TRUE),
         "YAML file must have '.yaml' or '.yml' extension"
     )
+})
 
-    # Warn and return `NULL` on missing file
+# Warn and return `NULL` on missing file
+test_that("missing file", {
+    yaml <- suppressWarnings(
+        readYAML("foobar.yaml", quiet = TRUE)
+    )
     expect_warning(
         readYAML("foobar.yaml", quiet = TRUE),
         "foobar.yaml file missing"
     )
-    expect_equal(
-        readYAML("foobar.yaml", quiet = TRUE),
-        NULL
-    )
+    expect_equal(yaml, NULL)
 })
