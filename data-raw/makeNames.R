eeeadf382ddf8002b64b705d3a8efecb2d8575c8
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-15 22:20:22 -0400

    Resave makeNames example data

--- data-raw/makeNames.R
+++ data-raw/makeNames.R
@@ -1,24 +1,34 @@
-unnamedVec <- c(
+vec <- c(
     "hello world",
     "HELLO WORLD",
     "RNAi clones",
+    "tx2gene",
+    "TX2GeneID",
+    "G2M.Score",
     "worfdbHTMLRemap",
     123,
     NA)
+
 namedVec <- c(
     Item.A = "hello world",
     Item.B = "HELLO WORLD")
+
 df <- head(mtcars)
+
+tbl <- head(starwars)
+
 lst <- list(
     Item.A = c(1, 2),
     Item.B = c(3, 4))
 
 makeNames <- list(
-    unnamedVec = unnamedVec,
+    vec = vec,
     namedVec = namedVec,
     df = df,
+    tbl = tbl,
     lst = lst
 )
+
 save(makeNames,
      file = file.path(testDataDir, "makeNames.rda"),
      compress = "xz")
