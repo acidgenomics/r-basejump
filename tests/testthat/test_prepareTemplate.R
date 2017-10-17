d509731c0c50014233006de547c3b4b47877efc2
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-17 09:25:27 -0400

    Simplify the `prepareTemplate()` unit test

--- tests/testthat/test_prepareTemplate.R
+++ tests/testthat/test_prepareTemplate.R
@@ -1,15 +1,16 @@
 context("prepareTemplate")
 
 test_that("prepareTemplate", {
+    files <- c(
+        "_footer.Rmd",
+        "_header.Rmd",
+        "_output.yaml",
+        "bibliography.bib",
+        "setup.R")
     expect_silent(prepareTemplate())
     expect_message(
         prepareTemplate("setup.R", overwrite = TRUE),
         "Overwriting setup.R"
     )
+    unlink(files)
 })
-
-unlink("_footer.Rmd")
-unlink("_header.Rmd")
-unlink("_output.yaml")
-unlink("bibliography.bib")
-unlink("setup.R")
