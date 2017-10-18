dc8e21ada0150db772d2ad61f1ea222bec973850
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-17 21:05:05 -0400

    Add testing for missing method support

--- tests/testthat/test_bcbioGenerics.R
+++ tests/testthat/test_bcbioGenerics.R
@@ -1,6 +1,6 @@
 context("bcbioGenerics")
 
-test_that("bcbioGenerics", {
+test_that("generic definition", {
     generics <- list(
         aggregateReplicates,
         bcbio,
@@ -14,4 +14,21 @@ test_that("bcbioGenerics", {
         selectSamples)
     classes <- vapply(generics, class, "character")
     expect_true(all(classes == "nonstandardGenericFunction"))
+})
+
+test_that("no methods defined", {
+    error <- "unable to find an inherited method"
+    expect_error(aggregateReplicates(), error)
+    expect_error(bcbio(), error)
+    expect_error(
+        bcbio() <- "xxx",
+        "invalid \\(NULL\\) left side of assignment"
+    )
+    expect_error(flatFiles(), error)
+    expect_error(interestingGroups(), error)
+    expect_error(metrics(), error)
+    expect_error(plotGene(), error)
+    expect_error(sampleDirs(), error)
+    expect_error(sampleMetadata(), error)
+    expect_error(selectSamples(), error)
 })
