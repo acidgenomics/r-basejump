503f8a2a365bf93a52e9879cfc14003210d188f3
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-18 09:04:40 -0400

    Fix `is_tibble()` call

--- tests/testthat/test_readDataVersions.R
+++ tests/testthat/test_readDataVersions.R
@@ -4,7 +4,7 @@ test_that("readDataVersions", {
     versions <- readDataVersions(
         file.path(testDataURL, "data_versions.csv"),
         quiet = TRUE)
-    expect_true(is_tibble(versions))
+    expect_true(tibble::is_tibble(versions))
     expect_equal(
         colnames(versions),
         c("genome", "resource", "version")
