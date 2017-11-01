e591501b361d3e09dd189ec74f6eedfad0912249
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-01 16:43:11 -0400

    Download the `loadData` examples only once

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,4 +3,17 @@ options(stringsAsFactors = FALSE)
 library(testthat)
 library(basejump)
 
+# Download files required to test `loadData()` and `loadDataAsName()`
+utils::download.file(
+    url = "http://basejump.seq.cloud/mtcars.rda",
+    destfile = "mtcars.rda",
+    quiet = TRUE)
+utils::download.file(
+    url = "http://basejump.seq.cloud/starwars.rda",
+    destfile = "starwars.rda",
+    quiet = TRUE)
+
 test_check("basejump")
+
+# Clean up the downloaded R data files
+unlink(c("mtcars.rda", "starwars.rda"))
