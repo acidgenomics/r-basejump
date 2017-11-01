42be908bf24b6e68c25fbddd4516204496296158
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-01 15:34:46 -0400

    Don't write out documentation on internal functions

--- R/internal-localOrRemoteFile.R
+++ R/internal-localOrRemoteFile.R
@@ -1,6 +1,7 @@
 #' Dynamically Handle a Local or Remote File Path
 #'
 #' @keywords internal
+#' @noRd
 #'
 #' @importFrom utils download.file
 #'
@@ -10,11 +11,6 @@
 #' @return Named character vector containing the original file name as the
 #'   name and local file path as the string. Returns `NULL` on a misisng
 #'   local file.
-#'
-#' @examples
-#' \dontrun{
-#' .localOrRemoteFile("http://basejump.seq.cloud/mtcars.csv")
-#' }
 .localOrRemoteFile <- function(
     object,
     quiet = FALSE) {
