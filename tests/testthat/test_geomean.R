40b74eff09e60d7af9c295e5628552cda69313c3
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-01 17:26:06 -0400

    Improve unit testing

--- tests/testthat/test_geomean.R
+++ tests/testthat/test_geomean.R
@@ -1,6 +1,6 @@
 context("geomean")
 
-vec1 <- seq(1L, 5L, 1L)
+vec1 <- seq(from = 1L, to = 5L, by = 1L)
 vec2 <- vec1 ^ 2L
 means <- c(vec1 = 2.605171, vec2 = 6.786916)
 
@@ -11,11 +11,38 @@ test_that("numeric", {
     expect_equal(geo2, means[["vec2"]])
 })
 
-test_that("column data", {
+test_that("Column data", {
     df <- data.frame(vec1, vec2)
     geo1 <- round(geomean(df), digits = 6L)
     expect_identical(geo1, means)
     mat <- as.matrix(df)
     geo2 <- round(geomean(mat), digits = 6L)
     expect_identical(geo2, means)
+})
+
+test_that("Non-numeric column data", {
+    expect_error(
+        geomean(starwars),
+        paste("Non-numeric columns: name, hair_color,")
+    )
+})
+
+test_that("NaN on negative numbers", {
+    vec <- seq(from = -5L, to = 5L, by = 1L)
+    expect_equal(
+        geomean(vec),
+        NaN
+    )
+})
+
+test_that("Zero propagation", {
+    vec <- seq(from = 0, to = 5L, by = 1L)
+    expect_equal(
+        geomean(vec, zeroPropagate = TRUE),
+        0L
+    )
+    expect_equal(
+        round(geomean(vec, zeroPropagate = FALSE), digits = 6L),
+        2.220906
+    )
 })
