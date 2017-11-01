615e706bd5f60066d99088a598fa6d10e2f41a24
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-01 16:42:57 -0400

    Add option to change extension. Improve documentation.

--- R/loadDataAsName.R
+++ R/loadDataAsName.R
@@ -8,6 +8,7 @@
 #'   defines the new name of the object in the environment, whereas the value
 #'   (string) denotes the original object name. For example, `newName1 =
 #'   "oldName1", newName2 = "oldName2"`.
+#' @param ext R data file extension. Defaults to `rda`.
 #' @param envir The environment where the data should be loaded.
 #'
 #' @return Silently return named character vector of file paths.
@@ -15,17 +16,36 @@
 #'
 #' @examples
 #' # New dots method (>= 0.1.1)
+#' # Simply specify the key-value pairs as arguments, and set the directory path
+#' # (`dir`) where the R data files saved. This example will load `mtcars.rda`
+#' # and `starwars.rda` files saved at `data/` as `newName1` and `newName2` in
+#' # the working environment.
 #' \dontrun{
-#' loadDataAsName(foo = "mtcars", bar = "starwars")
+#' loadDataAsName(
+#'     newName1 = "mtcars",
+#'     newName2 = "starwars",
+#'     dir = "data"
+#' )
 #' }
 #'
-#' # Legacy named character vector mappings method
+#' # Requesting the file path directly also works. In this case, the `dir`
+#' # argument wil be ignored.
 #' \dontrun{
-#' loadDataAsName(c(foo = "mtcars", bar = "starwars"))
+#' loadDataAsName(newName = "data/mtcars.rda")
+#' }
+#'
+#' # Legacy mappings method using a named character vector is still supported.
+#' \dontrun{
+#' loadDataAsName(
+#'     c(newName1 = "mtcars",
+#'       newName2 = "starwars"),
+#'     dir = "data"
+#' )
 #' }
 loadDataAsName <- function(
     ...,
     dir = "data",
+    ext = "rda",
     envir = parent.frame()) {
     mappings <- list(...)
     # Check for legacy mappings method, used prior to v0.1.1
@@ -37,12 +57,12 @@ loadDataAsName <- function(
     if (!is.environment(envir)) {
         stop("'envir' must be an environment", call. = FALSE)
     }
-    tmpenv <- new.env()
+    tmpEnv <- new.env()
     loaded <- sapply(seq_along(mappings), function(a) {
         object <- mappings[[a]]
         name <- names(mappings)[[a]]
         # Check to see if full file path was passed
-        fileExtPattern <- "\\.[A-Za-z0-9]+$"
+        fileExtPattern <- paste0("\\.", ext, "$")
         if (grepl(x = object, pattern = fileExtPattern)) {
             file <- object
             # Extract the object name from the file name
@@ -51,20 +71,15 @@ loadDataAsName <- function(
                 pattern = fileExtPattern,
                 replacement = "")
         } else {
-            file <- file.path(dir, paste0(object, ".rda"))
+            file <- file.path(dir, paste0(object, paste0(".", ext)))
         }
         if (!file.exists(file)) {
             stop(paste(object, "missing"), call. = FALSE)
         }
         file <- normalizePath(file)
-        loaded <- load(file, envir = tmpenv)
-        if (!identical(as.character(object), loaded)) {
-            stop(paste(
-                name, "file and saved object names are not identical"
-            ), call. = FALSE)
-        }
+        tmpName <- load(file, envir = tmpEnv)
         assign(x = name,
-               value = get(object, envir = tmpenv, inherits = FALSE),
+               value = get(tmpName, envir = tmpEnv, inherits = FALSE),
                envir = envir)
         names(file) <- name
         file
