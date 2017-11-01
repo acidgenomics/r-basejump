da1fb1478cee7948615721884421ef99b5f675ce
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-01 16:43:46 -0400

    Intial commit

---
+++ tests/testthat/test_loadDataAsName.R
@@ -0,0 +1,48 @@
+context("loadDataAsName")
+
+test_that("Dot object key-value pair method", {
+    # Short-hand method, using `dir` argument (preferred)
+    loaded <- loadDataAsName(
+        newName1 = "mtcars",
+        newName2 = "starwars",
+        dir = getwd()
+    )
+    # Variable file paths (more flexible, but requires more typing)
+    loaded2 <- loadDataAsName(
+        newName1 = "mtcars.rda",
+        newName2 = "starwars.rda",
+        dir = "XXX"  # ignored
+    )
+    expect_equal(
+        loaded,
+        c(newName1 = file.path(getwd(), "mtcars.rda"),
+          newName2 = file.path(getwd(), "starwars.rda"))
+    )
+    expect_identical(loaded, loaded2)
+    # Error on first missing file
+    expect_error(
+        loadDataAsName(
+            newName1 = "XXXXXX",
+            newName2 = "YYYYYY",
+            dir = getwd()
+        ),
+        "XXXXXX missing"
+    )
+})
+
+test_that("Legacy named character method", {
+    loaded <- loadDataAsName(
+        mappings = c(test = "mtcars"),
+        dir = getwd())
+    expect_equal(
+        loaded,
+        c(test = file.path(getwd(), "mtcars.rda"))
+    )
+    expect_error(
+        loadDataAsName(
+            mappings = c(test = "foobar"),
+            dir = getwd()
+        ),
+        "foobar missing"
+    )
+})
