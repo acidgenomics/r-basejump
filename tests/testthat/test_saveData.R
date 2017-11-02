e57a5bb0ff5da7d0f95cd0caa0ffbbae78c7dc8f
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-01 23:37:40 -0400

    Invisibly return NULL if all files skipped

--- tests/testthat/test_saveData.R
+++ tests/testthat/test_saveData.R
@@ -1,9 +1,24 @@
 context("saveData")
 
 test_that("saveData", {
+    expect_equal(
+        saveData(mtcars, starwars, dir = "data",
+                 overwrite = TRUE, quiet = TRUE),
+        c(mtcars = file.path(getwd(), "data", "mtcars.rda"),
+          starwars = file.path(getwd(), "data", "starwars.rda"))
+    )
     expect_message(
-        saveData(mtcars, starwars, dir = "data"),
-        paste("Saving mtcars, starwars to", file.path(getwd(), "data"))
+        saveData(mtcars, starwars, dir = "data", overwrite = TRUE),
+        paste("Saving mtcars.rda, starwars.rda to", file.path(getwd(), "data"))
+    )
+    expect_equal(
+        saveData(mtcars, starwars, dir = "data",
+                 overwrite = FALSE, quiet = TRUE),
+        NULL
+    )
+    expect_message(
+        saveData(mtcars, starwars, dir = "data", overwrite = FALSE),
+        "Skipping mtcars.rda, starwars.rda"
     )
     expect_error(
         saveData(XXX),
@@ -13,6 +28,13 @@ test_that("saveData", {
     expect_error(
         saveData("mtcars"),
         "Dot objects cannot contain arguments"
+    )
+})
+
+test_that("Invalid arguments", {
+    expect_error(
+        saveData(mtcars, dir = NULL),
+        "'dir' must be a string"
     )
 })
 
