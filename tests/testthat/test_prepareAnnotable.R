14e697e3dae2408bf93651c7ee1c0515bc29631c
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-04 21:42:33 -0400

    Update unit tests to match improved annotable format

--- tests/testthat/test_prepareAnnotable.R
+++ tests/testthat/test_prepareAnnotable.R
@@ -3,38 +3,26 @@ context("prepareAnnotable")
 # Use the pre-compiled grch37 annotable from annotables package
 loadRemoteData("http://basejump.seq.cloud/grch37.rda", quiet = TRUE)
 
-test_that("Drop extra columns", {
+test_that("grch37", {
     # This is fast but will drop extra columns
-    drop <- prepareAnnotable(grch37, dropExtraCols = TRUE)
+    human <- prepareAnnotable(grch37)
     expect_equal(
-        dim(drop),
-        c(63677, 5)
+        dim(human),
+        c(63677L, 9L)
     )
     expect_equal(
-        colnames(drop),
-        c("ensgene",
-          "symbol",
-          "description",
-          "biotype",
-          "broadClass")
-    )
-})
-
-test_that("Nest extra columns", {
-    # This is slow but will keep the extra columns by nesting
-    nest <- prepareAnnotable(grch37, dropExtraCols = FALSE)
-    expect_equal(
-        dim(nest),
-        c(63677, 6)
-    )
-    expect_equal(
-        colnames(nest),
-        c("ensgene",
-          "symbol",
-          "description",
-          "biotype",
-          "nestedData",
-          "broadClass")
+        lapply(human, class),
+        list(
+            ensgene = "character",
+            symbol = "character",
+            description = "character",
+            biotype = "character",
+            broadClass = "character",
+            chr = "character",
+            start = "integer",
+            end = "integer",
+            strand = "integer"
+        )
     )
 })
 
