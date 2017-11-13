e1cdb3c0db3dc3a31621a39b6aa7e413a14fe16a
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-13 10:49:31 -0500

    Update PANTHER script

--- inst/R_scripts/panther/hsapiens.R
+++ inst/R_scripts/panther/hsapiens.R
@@ -1,24 +1,20 @@
 #' Homo sapiens PANTHER annotations
 #' Michael Steinbaugh
-#' 2017-09-27
-
+#' 2017-11-13
 library(basejump)
-library(stringr)
-library(readr)
 library(tidyverse)
 
 # HGNC annotations ====
-hgncTXT <-
-    file.path("ftp://ftp.ebi.ac.uk",
-              "pub",
-              "databases",
-              "genenames",
-              "new",
-              "tsv",
-              "hgnc_complete_set.txt")
+hgncTXT <- file.path(
+    "ftp://ftp.ebi.ac.uk",
+    "pub",
+    "databases",
+    "genenames",
+    "new",
+    "tsv",
+    "hgnc_complete_set.txt")
 hgnc <- read_tsv(hgncTXT)
 saveData(hgnc)
-
 hgnc <- hgnc %>%
     select(hgnc_id, ensembl_gene_id) %>%
     mutate(hgnc_id = str_replace(hgnc_id, "^HGNC\\:", ""))
@@ -30,11 +26,11 @@ pantherFTP <-
               "sequence_classifications",
               "current_release",
               "PANTHER_Sequence_Classification_files")
-pantherFile <-
-    transmit(pantherFTP,
-             pattern = organism,
-             compress = TRUE,
-             localDir = "annotations")
+pantherFile <- transmit(
+    pantherFTP,
+    pattern = organism,
+    compress = TRUE,
+    localDir = "annotations")
 panther <- pantherFile %>%
     as.character() %>%
     read_tsv(col_names = c(
@@ -62,16 +58,16 @@ colnames(panther) <- camel(paste("panther", colnames(panther), sep = "."))
 # First extract the annotations that match Ensembl
 ensemblMatched <- panther %>%
     # First extract the Ensembl ID
-    mutate(ensgene = str_extract(pantherId, "Ensembl=ENSG[0-9]{11}"),
+    mutate(ensgene = str_extract(pantherID, "Ensembl=ENSG[0-9]{11}"),
            ensgene = str_replace(ensgene, "^Ensembl=", "")) %>%
     filter(!is.na(ensgene))
 
 # Most of the annotations are mapped to HGNC for human
 hgncMatched <- panther %>%
-    filter(!pantherId %in% ensemblMatched[["pantherId"]]) %>%
+    filter(!pantherID %in% ensemblMatched[["pantherID"]]) %>%
     # Extract the HGNC ID, which we will use for join with HGNC annotations
     # to match the Ensembl ID.
-    mutate(hgnc_id = str_extract(pantherId, "HGNC=[0-9]+"),
+    mutate(hgnc_id = str_extract(pantherID, "HGNC=[0-9]+"),
            hgnc_id = str_replace(hgnc_id, "^HGNC=", "")) %>%
     left_join(hgnc, by = "hgnc_id") %>%
     filter(!is.na(ensembl_gene_id)) %>%
@@ -80,6 +76,6 @@ hgncMatched <- panther %>%
 
 # Now combine PANTHER annotations that are matched to Ensembl ID
 pantherWithEnsembl <- bind_rows(ensemblMatched, hgncMatched) %>%
-    select(-pantherId) %>%
+    select(-pantherID) %>%
     select(ensgene, everything())
 saveData(pantherWithEnsembl)
