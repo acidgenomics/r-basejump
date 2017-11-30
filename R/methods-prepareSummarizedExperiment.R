884a0e436732ab1d653cf7d3ab178cfb9ae6a14e
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-30 09:21:34 -0500

    Fix percentage calculation typo

--- R/methods-prepareSummarizedExperiment.R
+++ R/methods-prepareSummarizedExperiment.R
@@ -152,11 +152,9 @@ NULL
                 sort()
             warning(paste(
                 "Unannotated genes detected in counts matrix",
-                paste0("(",
-                       percent(length(unannotatedGenes)),
-                       " / ",
-                       nrow(assay),
-                       ")")
+                paste0(
+                    "(", percent(length(unannotatedGenes) / nrow(assay)), ")"
+                )
             ), call. = FALSE)
         } else {
             unannotatedGenes <- NULL
