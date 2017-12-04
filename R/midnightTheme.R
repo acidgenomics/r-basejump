8bd997ebcb81efa3a4adfb0792725879577432c3
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-04 13:30:47 -0500

    Rename `darkTheme()` to `midnightTheme()`. Use `minimal_theme()` as the base theme.

---
+++ R/midnightTheme.R
@@ -0,0 +1,37 @@
+#' Midnight Theme for `ggplot` Objects
+#'
+#' Blackout palette that sets the plot background as black, with white text.
+#'
+#' @importFrom ggplot2 alpha element_blank element_line element_rect
+#'   element_text margin theme theme_minimal
+#'
+#' @param ... Passthrough arguments to [ggplot2::theme()].
+#'
+#' @seealso
+#' - [ggplot2::theme()].
+#' - `Seurat::DarkTheme()`.
+#'
+#' @return ggplot2 theme.
+#' @export
+midnightTheme <- function(...) {
+    gray <- "gray12"
+    blackBackground <- element_rect(color = NA, fill = "black")
+    grayBackground <- element_rect(color = NA, fill = gray)
+    grayLine <- element_line(color = gray)
+    whiteText <- element_text(color = "white")
+    theme_minimal() +
+        theme(
+            text = whiteText,
+            axis.line = element_blank(),
+            axis.text = whiteText,
+            axis.ticks = element_blank(),
+            legend.key = grayBackground,
+            panel.background = element_blank(),
+            panel.border = element_blank(),
+            panel.grid.major = grayLine,
+            panel.grid.minor = element_blank(),
+            plot.background = blackBackground,
+            complete = TRUE,
+            validate = TRUE,
+            ...)
+}
