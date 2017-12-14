539dec5a26caaa4697b1d65d181168533cb8dc46
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-14 10:58:36 -0500

    Simplify script

--- data-raw/grch37.R
+++ data-raw/grch37.R
@@ -1,9 +1,5 @@
-library(devtools)
-install_github("stephenturner/annotables")
-library(annotables)
-
-load_all()
-
+devtools::install_github("stephenturner/annotables")
+devtools::load_all()
 grch37 <- annotable(annotables::grch37)
 lapply(grch37, class)
-use_data(grch37, compress = "xz", overwrite = TRUE)
+devtools::use_data(grch37, compress = "xz", overwrite = TRUE)
