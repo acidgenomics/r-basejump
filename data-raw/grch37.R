6cd9f765b499acc443eb0557f193d05f33ca5589
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-15 09:58:45 -0500

    Add internal GRCh37/hg19 transcript to gene mappings

--- data-raw/grch37.R
+++ data-raw/grch37.R
@@ -1,5 +1,14 @@
-devtools::install_github("stephenturner/annotables")
+# devtools::install_github("stephenturner/annotables")
+
 devtools::load_all()
+library(magrittr)
+
 grch37 <- annotable(annotables::grch37)
 lapply(grch37, class)
-devtools::use_data(grch37, compress = "xz", overwrite = TRUE)
+
+grch37Tx2gene <- annotables::grch37_tx2gene %>%
+    as.data.frame() %>%
+    set_rownames(.[["enstxp"]])
+lapply(grch37Tx2gene, class)
+
+devtools::use_data(grch37, grch37Tx2gene, compress = "xz", overwrite = TRUE)
