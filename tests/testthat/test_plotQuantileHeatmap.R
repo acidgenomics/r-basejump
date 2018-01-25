c5440ca0a0bf33b35bbcfbee03f753366f5c8160
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-25 17:32:59 -0500

    Add unit test

--- tests/testthat/test_plotQuantileHeatmap.R
+++ tests/testthat/test_plotQuantileHeatmap.R
@@ -3,4 +3,12 @@ context("plotQuantileHeatmap")
 test_that("Simple matrix", {
     mat <- matrix(1:10000, nrow = 100, ncol = 100)
     p <- plotQuantileHeatmap(mat)
+    expect_identical(
+        names(p),
+        c("quantiles", "plot")
+    )
+    expect_identical(
+        names(p[["plot"]]),
+        c("tree_row", "tree_col", "kmeans", "gtable")
+    )
 })
