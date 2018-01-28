0d7901e4c83e1433a4a6796981169801e268b5a9
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-28 18:30:10 -0500

    Update S3 bucket scripts

---
+++ data-raw/s3-plotlist.R
@@ -0,0 +1,21 @@
+devtools::load_all()
+
+library(ggplot2)
+library(cowplot)
+
+# Examples from the ggplot2 cheatsheet
+# https://www.rstudio.com/wp-content/uploads/2015/03/ggplot2-cheatsheet.pdf
+continuous <- ggplot(mpg, aes(hwy)) +
+    geom_histogram(binwidth = 5)
+discrete <- ggplot(mpg, aes(fl)) +
+    geom_bar()
+
+plotlist <- list(
+    continuous = continuous,
+    discrete = discrete
+)
+
+# Plotting example using the plotlist with cowplot
+plot_grid(plotlist = plotlist)
+
+saveData(plotlist, dir = "~/Desktop")
