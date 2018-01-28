53b9bc7850fdbd66a7eed5cce4a91b1cb4c3f99b
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-28 11:25:26 -0500

    Update unit test

--- tests/testthat/test_writeCounts.R
+++ tests/testthat/test_writeCounts.R
@@ -2,9 +2,22 @@ context("writeCounts")
 
 test_that("writeCounts", {
     df <- as.data.frame(mtcars)
-    mat <- as.matrix(mtcars)
     dgc <- as(mat, "dgCMatrix")
-    expect_message(writeCounts(df, mat, dgc))
+    mat <- as.matrix(mtcars)
+    expect_message(
+        writeCounts(df, dgc, mat, dir = "counts"),
+        "Writing df, dgc, mat"
+    )
+    expect_identical(
+        dir("counts"),
+        c(
+            "df.csv.gz",
+            "dgc.mtx.colnames",
+            "dgc.mtx.gz",
+            "dgc.mtx.rownames",
+            "mat.csv.gz"
+        )
+    )
     expect_error(
         writeCounts(XXX),
         "object 'XXX' not found"
@@ -13,6 +26,5 @@ test_that("writeCounts", {
         writeCounts(seq(1L:10L)),
         "Object must support `dim\\(\\)`"
     )
+    unlink("counts", recursive = TRUE)
 })
-
-unlink("results", recursive = TRUE)
