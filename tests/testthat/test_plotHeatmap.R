4387af0e65b28a00b241d7bbf45415c4baa2d087
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-28 15:35:59 -0500

    Add unit testing for `annotationCol` parameter

--- tests/testthat/test_plotHeatmap.R
+++ tests/testthat/test_plotHeatmap.R
@@ -1,11 +1,27 @@
 context("plotHeatmap")
 
+plotNames <- c("tree_row", "tree_col", "kmeans", "gtable")
+
 test_that("matrix", {
     mat <- as.matrix(mtcars)
     p <- plotHeatmap(mat)
     expect_is(p, "list")
-    expect_identical(
-        names(p),
-        c("tree_row", "tree_col", "kmeans", "gtable")
+    expect_identical(names(p), plotNames)
+})
+
+test_that("Annotation columns support", {
+    loadRemoteData("http://basejump.seq.cloud/counts.rda", quiet = TRUE)
+    annotationCol <- data.frame(
+        genotype = c(
+            "wildtype",
+            "wildtype",
+            "mutant",
+            "mutant"
+        ),
+        row.names = colnames(counts)
     )
+    p <- plotHeatmap(counts, annotationCol = annotationCol)
+    expect_identical(names(p), plotNames)
+    # TODO Add a method here to extract the annotation columns from the gtable
+    # grobs. Not sure how to do this yet.
 })
--- tests/testthat/test_plotQuantileHeatmap.R
+++ tests/testthat/test_plotQuantileHeatmap.R
@@ -1,5 +1,7 @@
 context("plotQuantileHeatmap")
 
+plotNames <- c("tree_row", "tree_col", "kmeans", "gtable")
+
 test_that("Simple matrix", {
     mat <- matrix(1:10000, nrow = 100, ncol = 100)
     p <- plotQuantileHeatmap(mat)
@@ -9,12 +11,26 @@ test_that("Simple matrix", {
     )
     expect_identical(
         names(p[["plot"]]),
-        c("tree_row", "tree_col", "kmeans", "gtable")
+        plotNames
     )
 })
 
 test_that("Annotation columns support", {
-
-
-
+    loadRemoteData("http://basejump.seq.cloud/counts.rda", quiet = TRUE)
+    annotationCol <- data.frame(
+        genotype = c(
+            "wildtype",
+            "wildtype",
+            "mutant",
+            "mutant"
+        ),
+        row.names = colnames(counts)
+    )
+    p <- plotQuantileHeatmap(counts, annotationCol = annotationCol)
+    expect_identical(
+        names(p[["plot"]]),
+        plotNames
+    )
+    # TODO Add a method here to extract the annotation columns from the gtable
+    # grobs. Not sure how to do this yet.
 })
