dabec231d2729a9f5f04fc3d8a11df3f1d044b6d
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-28 19:26:44 -0500

    Add additional unit testing

--- tests/testthat/test_plotQuantileHeatmap.R
+++ tests/testthat/test_plotQuantileHeatmap.R
@@ -1,22 +1,17 @@
 context("plotQuantileHeatmap")
 
+loadRemoteData("http://basejump.seq.cloud/counts.rda", quiet = TRUE)
 plotNames <- c("tree_row", "tree_col", "kmeans", "gtable")
 
 test_that("Simple matrix", {
     mat <- matrix(1:10000, nrow = 100, ncol = 100)
     p <- plotQuantileHeatmap(mat)
-    expect_identical(
-        names(p),
-        c("quantiles", "plot")
-    )
-    expect_identical(
-        names(p[["plot"]]),
-        plotNames
-    )
+    expect_identical(names(p), c("quantiles", "plot"))
+    expect_identical(names(p[["plot"]]), plotNames)
+    expect_length(p[["plot"]][["gtable"]], 5L)
 })
 
 test_that("Annotation columns support", {
-    loadRemoteData("http://basejump.seq.cloud/counts.rda", quiet = TRUE)
     annotationCol <- data.frame(
         genotype = c(
             "wildtype",
@@ -27,10 +22,21 @@ test_that("Annotation columns support", {
         row.names = colnames(counts)
     )
     p <- plotQuantileHeatmap(counts, annotationCol = annotationCol)
-    expect_identical(
-        names(p[["plot"]]),
-        plotNames
+    expect_identical(names(p[["plot"]]), plotNames)
+    expect_length(p[["plot"]][["gtable"]], 9L)
+})
+
+test_that("Default color palette", {
+    expect_silent(plotQuantileHeatmap(counts, color = NULL))
+})
+
+test_that("Matrix dimensions are too small", {
+    expect_error(
+        plotQuantileHeatmap(matrix(seq(1L:10L), ncol = 1L)),
+        "Need at least 2 columns to plot heatmap"
     )
-    # TODO Add a method here to extract the annotation columns from the gtable
-    # grobs. Not sure how to do this yet.
+    expect_error(
+        plotQuantileHeatmap(matrix(seq(1L:10L), nrow = 1L)),
+        "Need at least 2 rows to plot heatmap"
+    )
 })
