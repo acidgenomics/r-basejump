aacaca9c4f0627579c7e4c18fdcce52aeef1e55b
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-31 17:28:19 -0500

    Improve comment

--- R/methods-annotable.R
+++ R/methods-annotable.R
@@ -377,7 +377,7 @@ NULL
             distinct() %>%
             left_join(entrez, by = "ensgene")
     }
-    # Collapse rows by Ensembl ID, if necessary
+    # Collapse any remaining duplicated rows by Ensembl ID, if necessary
     if (any(duplicated(object[["ensgene"]]))) {
         object <- object %>%
             group_by(!!!syms(requiredCols)) %>%
