77216a3ee8532fdc4ad5dff03fafcc9f9645e4e7
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-07 12:38:39 -0500

    Update unit tests

---
+++ tests/testthat/test_convertGenesToSymbols.R
@@ -0,0 +1,104 @@
+context("convertGenesToSymbols")
+
+test_that("character", {
+    g2s <- convertGenesToSymbols(
+        c("ENSMUSG00000000001",
+          "ENSMUSG00000000003"),
+        release = 88L,
+        quiet = TRUE)
+    expect_identical(
+        g2s,
+        c(ENSMUSG00000000001 = "Gnai3",
+          ENSMUSG00000000003 = "Pbsn")
+    )
+})
+
+test_that("matrix", {
+    mat <- matrix(
+        data = seq(1L:4L),
+        byrow = TRUE,
+        nrow = 2L,
+        ncol = 2L,
+        dimnames = list(
+            c("ENSMUSG00000000001",
+              "ENSMUSG00000000003"),
+            c("sample1", "sample2")
+        )
+    )
+    g2s <- convertGenesToSymbols(
+        mat,
+        release = 88L,
+        quiet = TRUE)
+    expect_identical(
+        rownames(g2s),
+        c(ENSMUSG00000000001 = "Gnai3",
+          ENSMUSG00000000003 = "Pbsn")
+    )
+})
+
+# Specify organism (to handle FASTA spike-ins (e.g. EGFP)
+test_that("FASTA spike-in support", {
+    vec <- c("EGFP", "ENSMUSG00000000001")
+    g2s <- suppressWarnings(
+        convertGenesToSymbols(
+            vec,
+            organism = "Mus musculus",
+            release = 88L,
+            quiet = TRUE)
+    )
+    expect_identical(
+        g2s,
+        c(EGFP = "EGFP",
+          ENSMUSG00000000001 = "Gnai3")
+    )
+
+    expect_warning(
+        convertGenesToSymbols(
+            vec,
+            organism = "Mus musculus",
+            release = 88L,
+            quiet = TRUE),
+        "Failed to match all gene IDs to symbols: EGFP"
+    )
+
+    expect_warning(
+        convertGenesToSymbols(vec),
+        "Failed to detect supported organism"
+    )
+    expect_warning(
+        convertGenesToSymbols(vec),
+        "Returning unmodified gene identifiers"
+    )
+    expect_identical(
+        suppressWarnings(convertGenesToSymbols(vec)),
+        c(EGFP = "EGFP",
+          ENSMUSG00000000001 = "ENSMUSG00000000001")
+    )
+})
+
+test_that("invalid identifiers", {
+    expect_warning(
+        convertGenesToSymbols(
+            c("ENSMUSG00000000000",
+              "ENSMUSG00000000001"),
+            release = 88L,
+            quiet = TRUE),
+        "Failed to match all gene IDs to symbols"
+    )
+    expect_warning(
+        convertGenesToSymbols(
+            c("ENSMUSG00000000001",
+              "ENSMUSG00000000001"),
+            release = 88L,
+            quiet = TRUE),
+        "Duplicate gene identifiers detected"
+    )
+    expect_error(
+        convertGenesToSymbols(c("ENSMUSG00000000001", NA)),
+        "NA identifier detected"
+    )
+    expect_error(
+        convertGenesToSymbols(c("ENSMUSG00000000001", "")),
+        "Empty string identifier detected"
+    )
+})
---
+++ tests/testthat/test_convertTranscriptsToGenes.R
@@ -0,0 +1,60 @@
+context("convertTranscriptsToGenes")
+
+test_that("convertTranscriptsToGenes", {
+    # character
+    expect_equal(
+        convertTranscriptsToGenes(
+            c("ENSMUST00000000001", "ENSMUST00000000003"),
+            release = 88L,
+            quiet = TRUE),
+        c("ENSMUST00000000001" = "ENSMUSG00000000001",
+          "ENSMUST00000000003" = "ENSMUSG00000000003")
+    )
+    expect_error(
+        convertTranscriptsToGenes(
+            c("ENSMUST00000000000",
+              "ENSMUST00000000001"),
+            release = 88L,
+            quiet = TRUE),
+        "Unmatched transcripts present. Try using a GFF file instead."
+    )
+    expect_error(
+        convertTranscriptsToGenes(c("ENSMUSG00000000001", NA)),
+        "NA identifier detected"
+    )
+    expect_error(
+        convertTranscriptsToGenes(c("ENSMUSG00000000001", "")),
+        "Empty string identifier detected"
+    )
+
+    # matrix
+    mat <- matrix(
+        data = seq(1L:8L),
+        byrow = TRUE,
+        nrow = 4L,
+        ncol = 2L,
+        dimnames = list(
+            c("ENSMUST00000000000",
+              "ENSMUST00000000001",
+              "ENSMUST00000000003",
+              "ENSMUST00000114041"),
+            c("sample1", "sample2")
+        )
+    )
+    expect_error(
+        convertTranscriptsToGenes(
+            mat,
+            release = 88L,
+            quiet = TRUE),
+        "Unmatched transcripts present. Try using a GFF file instead."
+    )
+    expect_equal(
+        mat[2L:4L, ] %>%
+            convertTranscriptsToGenes(quiet = TRUE) %>%
+            dimnames() %>%
+            .[[1L]],
+        c("ENSMUST00000000001" = "ENSMUSG00000000001",
+          "ENSMUST00000000003" = "ENSMUSG00000000003",
+          "ENSMUST00000114041" = "ENSMUSG00000000003")
+    )
+})
