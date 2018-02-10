18d74fb927eff82155222eebe242c3b8817b4ed9
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-10 17:33:30 -0500

    Make function stricter using assertive checks

--- R/localOrRemoteFile.R
+++ R/localOrRemoteFile.R
@@ -6,28 +6,27 @@
 #' @inheritParams saveData
 #'
 #' @return Named character vector containing the original file name as the
-#'   name and local file path as the string. Returns `NULL` on a misisng
-#'   local file.
+#'   name and local file path as the string. Aborts on a missing file.
 #' @export
-localOrRemoteFile <- function(
-    object,
-    quiet = FALSE) {
-    fileName <- basename(object)
+#'
+#' @examples
+#' localOrRemoteFile("http://basejump.seq.cloud/mtcars.rda")
+localOrRemoteFile <- function(object, quiet = FALSE) {
+    assert_is_a_string(object)
+    assert_is_a_bool(quiet)
+    basename <- basename(object)
+
+    # Download remote file, if necessary
     if (grepl("\\://", object)) {
         # Remote file
-        filePath <- tempfile()
-        download.file(object, filePath, quiet = quiet)
+        file <- tempfile()
+        download.file(url = object, destfile = file, quiet = quiet)
     } else {
-        # Local file
-        # Check to see if file exists, otherwise return `NULL`
-        if (file.exists(object)) {
-            filePath <- normalizePath(object)
-        } else {
-            warn(paste(basename(object), "missing"))
-            return(invisible())
-        }
+        file <- object
     }
-    file <- filePath
-    names(file) <- fileName
+
+    assert_all_are_existing_files(file)
+    file <- normalizePath(file)
+    names(file) <- basename
     file
 }
