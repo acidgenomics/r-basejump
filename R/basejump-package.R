38ac21f117690861e28c7eec1e357ddd77ef2255
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-11 08:05:33 -0500

    Improve assertive imports

--- R/basejump-package.R
+++ R/basejump-package.R
@@ -2,15 +2,46 @@
 #'
 #' Base functions for bioinformatics and R package development.
 #'
-#' @importFrom assertive assert_all_are_existing_files
-#'   assert_all_are_matching_regex assert_any_are_matching_regex
-#'   assert_are_identical assert_has_dims assert_has_dimnames assert_is_a_bool
-#'   assert_is_a_string assert_is_any_of assert_is_data.frame
-#'   assert_is_environment assert_is_not_null assert_is_subset is_scalar
 #' @import methods
-#' @importFrom rlang !! !!! .data abort dots_list eval_bare inform sym syms warn
-#' @importFrom utils globalVariables
+#'
+#' @importFrom assertive assert_all_are_existing_files
+#' @importFrom assertive assert_all_are_matching_regex
+#' @importFrom assertive assert_all_are_non_empty_character
+#' @importFrom assertive assert_all_are_not_na
+#' @importFrom assertive assert_any_are_matching_regex
+#' @importFrom assertive assert_are_identical
+#' @importFrom assertive assert_has_colnames
+#' @importFrom assertive assert_has_dimnames
+#' @importFrom assertive assert_has_dims
+#' @importFrom assertive assert_has_names
+#' @importFrom assertive assert_has_names
+#' @importFrom assertive assert_has_no_duplicates
+#' @importFrom assertive assert_has_rownames
+#' @importFrom assertive assert_is_a_bool
+#' @importFrom assertive assert_is_a_string
+#' @importFrom assertive assert_is_any_of
+#' @importFrom assertive assert_is_character
+#' @importFrom assertive assert_is_data.frame
+#' @importFrom assertive assert_is_environment
+#' @importFrom assertive assert_is_factor
+#' @importFrom assertive assert_is_factor
+#' @importFrom assertive assert_is_list
+#' @importFrom assertive assert_is_matrix
+#' @importFrom assertive assert_is_name
+#' @importFrom assertive assert_is_non_empty
+#' @importFrom assertive assert_is_not_null
+#' @importFrom assertive assert_is_numeric
+#' @importFrom assertive assert_is_scalar
+#' @importFrom assertive assert_is_subset
+#' @importFrom assertive assert_is_vector
+#' @importFrom assertive is_a_string
+#' @importFrom assertive is_scalar
+#'
+#' @importFrom rlang !! !!! .data abort inform sym syms warn
 "_PACKAGE"
 
+
+
+#' @importFrom utils globalVariables
 globalVariables(".")
 annotableCols <- c("ensgene", "symbol", "description", "biotype")
--- R/internal-assertions.R
+++ R/internal-assertions.R
@@ -1,8 +1,28 @@
 # nolint begin
 
+#' @importFrom assertive assert_has_colnames
+#' @importFrom assertive assert_has_rownames
+#' @importFrom assertive assert_is_any_of
+#' @importFrom assertive assert_is_identical_to_na
+.assert_arg_annotation_col <- function(x) {
+    assert_is_any_of(x, c("data.frame", "logical"))
+    if (is.data.frame(x)) {
+        assert_has_colnames(x)
+        assert_has_rownames(x)
+    }
+    if (is.logical(x)) {
+        assert_is_identical_to_na(x)
+    }
+}
+
+
+
+#' @importFrom assertive assert_is_a_string
+#' @importFrom assertive assert_is_any_of
+#' @importFrom assertive assert_is_subset
 #' @seealso base::save
 #' @noRd
-.assert_compress <- function(x) {
+.assert_formal_compress <- function(x) {
     assert_is_any_of(x, c("character", "logical"))
     if (is.character(x)) {
         assert_is_a_string(x)
@@ -12,7 +32,10 @@
 
 
 
-.assert_markdown_header_level <- function(x) {
+#' @importFrom assertive assert_is_numeric
+#' @importFrom assertive assert_is_scalar
+#' @importFrom assertive assert_is_subset
+.assert_formal_header_level <- function(x) {
     assert_is_numeric(x)
     assert_is_scalar(x)
     assert_is_subset(as.integer(x), seq(1L:7L))
@@ -20,6 +43,8 @@
 
 
 
+#' @importFrom assertive assert_is_a_string
+#' @importFrom assertive assert_is_any_of
 .assert_is_a_string_or_null <- function(x) {
     assert_is_any_of(x, c("character", "NULL"))
     if (is.character(x)) {
@@ -29,6 +54,8 @@
 
 
 
+#' @importFrom assertive assert_is_any_of
+#' @importFrom assertive assert_is_scalar
 .assert_is_numeric_scalar_or_null <- function(x) {
     assert_is_any_of(x, c("numeric", "NULL"))
     if (is.numeric(x)) {
