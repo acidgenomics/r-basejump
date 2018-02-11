9a44114a7aab696bb1be1449dbeba354e85963f6
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-11 09:11:09 -0500

    Fix dropping of rownames in mtcars examle CSV

--- data-raw/internal-mtcars.R
+++ data-raw/internal-mtcars.R
@@ -1,9 +1,16 @@
 extdataDir <- file.path("inst", "extdata")
 dir.create(extdataDir, recursive = TRUE, showWarnings = FALSE)
 
-save(mtcars,
-     file = file.path(extdataDir, "mtcars.rda"),
-     compress = "xz")
+save(
+    mtcars,
+    file = file.path(extdataDir, "mtcars.rda"),
+    compress = "xz"
+)
 
-readr::write_csv(mtcars, file.path(extdataDir, "mtcars.csv"))
-readr::write_csv(mtcars, file.path(extdataDir, "mtcars.csv.gz"))
+# Note that `readr::write_csv()` never writes rownames
+write.csv(mtcars, file.path(extdataDir, "mtcars.csv"))
+R.utils::gzip(
+    filename = file.path(extdataDir, "mtcars.csv"),
+    overwrite = TRUE,
+    remove = FALSE
+)
