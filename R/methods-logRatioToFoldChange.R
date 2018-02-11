f0a01865abd9fbd5cf72274d4e05194c34a78ec0
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-11 13:11:27 -0500

    Break out constructor and add method support for integer

--- R/methods-logRatioToFoldChange.R
+++ R/methods-logRatioToFoldChange.R
@@ -1,11 +1,26 @@
+# Constructors =================================================================
+.logRatioToFoldChange <- function(object, base = 2L) {
+    assert_is_numeric(base)
+    object <- base ^ object
+    object <- ifelse(object < 1L, -1L / object, object)
+    object
+}
+
+
+
+# Methods ======================================================================
+#' @rdname logRatio
+#' @export
+setMethod(
+    "logRatioToFoldChange",
+    signature("integer"),
+    .logRatioToFoldChange)
+
+
+
 #' @rdname logRatio
 #' @export
 setMethod(
     "logRatioToFoldChange",
     signature("numeric"),
-    function(object, base = 2L) {
-        assert_is_numeric(base)
-        object <- base ^ object
-        object <- ifelse(object < 1L, -1L / object, object)
-        object
-    })
+    .logRatioToFoldChange)
