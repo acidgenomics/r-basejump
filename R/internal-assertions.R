a4142dc4966873bf484366cf4420c05359cc8e41
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-10 19:56:07 -0500

    Improve assertive checks for `compress` parameter across functions

--- R/internal-assertions.R
+++ R/internal-assertions.R
@@ -1,5 +1,17 @@
 # nolint begin
 
+#' @seealso base::save
+#' @noRd
+.assert_compress <- function(x) {
+    assert_is_any_of(x, c("character", "logical"))
+    if (is.character(x)) {
+        assert_is_a_string(x)
+        assert_is_subset(x, c("bzip", "gzip", "xz"))
+    }
+}
+
+
+
 .assert_is_a_string_or_null <- function(x) {
     assert_is_any_of(x, c("character", "NULL"))
     if (is.character(x)) {
