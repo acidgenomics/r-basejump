3a3c344599d788d02278b263ea4f5b763f2430e1
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-10 23:08:55 -0500

    Enforce input as string

--- R/methods-grepString.R
+++ R/methods-grepString.R
@@ -18,24 +18,30 @@ NULL
 
 
 
+# Constructors =================================================================
+.grepString <- function(object) {
+    assert_is_a_string(object)
+    object %>%
+        paste0(
+            # Unique
+            "^", ., "$",
+            "|",
+            # Beginning of list
+            "^", ., ",",
+            "|",
+            # Middle of list
+            "\\s", ., ",",
+            "|",
+            # End of list
+            "\\s", ., "$")
+}
+
+
+
 # Methods ======================================================================
 #' @rdname grepString
 #' @export
 setMethod(
     "grepString",
     signature("character"),
-    function(object) {
-        object %>%
-            paste0(
-                # Unique
-                "^", ., "$",
-                "|",
-                # Beginning of list
-                "^", ., ",",
-                "|",
-                # Middle of list
-                "\\s", ., ",",
-                "|",
-                # End of list
-                "\\s", ., "$")
-    })
+    .grepString)
