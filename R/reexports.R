67cb83a6fdfbbe3b98673a4b9dfc50eabde90d5e
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-11 21:24:23 -0500

    Update reexports

--- R/reexports.R
+++ R/reexports.R
@@ -8,9 +8,23 @@ dplyr::pull
 
 
 
+#' @importFrom ggplot2 aes
+#' @export
+ggplot2::aes
+
+#' @importFrom ggplot2 ggplot
+#' @export
+ggplot2::ggplot
+
+
+
 #' @importFrom magrittr %>%
 #' @export
 magrittr::`%>%`
+
+#' @importFrom magrittr set_rownames
+#' @export
+magrittr::set_rownames
 
 
 
