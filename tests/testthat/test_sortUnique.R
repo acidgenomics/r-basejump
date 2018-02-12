24c64067f9ced0a294514dbe5a41fdcaa3dbd0ec
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-12 14:39:29 -0500

    Don't omit NA

--- tests/testthat/test_sortUnique.R
+++ tests/testthat/test_sortUnique.R
@@ -3,6 +3,6 @@ context("sortUnique")
 test_that("sortUnique", {
     expect_identical(
         sortUnique(c("milk", "eggs", "eggs", NA)),
-        c("eggs", "milk")
+        c("eggs", "milk", NA)
     )
 })
