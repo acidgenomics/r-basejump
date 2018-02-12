18a3abd93e56b6d06ca99dce90c1a4a88aca242c
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-11 21:09:34 -0500

    Break out to separate files

---
+++ tests/testthat/test_markdownHeader.R
@@ -0,0 +1,29 @@
+context("markdownHeader")
+
+test_that("String", {
+    md <- markdownHeader("Header")
+    expect_identical(
+        class(md),
+        "knit_asis"
+    )
+    md <- markdownHeader("Header", level = 4L) %>%
+        as.character()
+    expect_identical(
+        md,
+        "#### Header\n"  # nolint
+    )
+    md <- markdownHeader("Header", tabset = TRUE) %>%
+        as.character()
+    expect_identical(
+         md,
+        "## Header {.tabset}\n"  # nolint
+    )
+    expect_output(
+        markdownHeader("Header", asis = TRUE),
+        "\\n\\n## Header\\n"  # nolint
+    )
+    expect_error(
+        markdownHeader("Header", level = 8L),
+        "Markdown supports 1-7 header levels"
+    )
+})
