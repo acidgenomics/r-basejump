fcaad1d62752113e1cd8d9bd27ce0789991669eb
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-12 17:08:26 -0500

    Update unit tests

--- tests/testthat/test_toStringUnique.R
+++ tests/testthat/test_toStringUnique.R
@@ -1,9 +1,14 @@
 context("toStringUnique")
 
-test_that("toStringUnique", {
+test_that("Atomic", {
     vec <- c("hello", "world", NA, "hello", "world", NA)
     expect_identical(
         toStringUnique(vec),
         "hello, world"
+    )
+
+    expect_error(
+        toStringUnique(mtcars),
+        "is_atomic"
     )
 })
