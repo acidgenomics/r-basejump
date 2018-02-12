eb4c3ac4579f6ded7114ad1b72b40ef66f996a4b
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-11 23:31:47 -0500

    Improve unit tests

--- tests/testthat/test_plotQuantileHeatmap.R
+++ tests/testthat/test_plotQuantileHeatmap.R
@@ -11,6 +11,7 @@ test_that("Simple matrix", {
     )
     colnames(mat) <- paste0("col", seq_len(ncol(mat)))
     p <- plotQuantileHeatmap(mat)
+    expect_is(p, "list")
     expect_identical(names(p), c("quantiles", "plot"))
     expect_identical(names(p[["plot"]]), plotNames)
     expect_length(p[["plot"]][["gtable"]], 5L)
@@ -32,16 +33,17 @@ test_that("Annotation columns support", {
 })
 
 test_that("Default color palette", {
-    expect_silent(plotQuantileHeatmap(counts, color = NULL))
+    p <- plotQuantileHeatmap(counts, color = NULL)
+    expect_is(p, "list")
 })
 
 test_that("Matrix dimensions are too small", {
     expect_error(
         plotQuantileHeatmap(matrix(seq(1L:10L), ncol = 1L)),
-        "Need at least 2 columns to plot heatmap"
+        "is_greater_than : ncol"
     )
     expect_error(
         plotQuantileHeatmap(matrix(seq(1L:10L), nrow = 1L)),
-        "Need at least 2 rows to plot heatmap"
+        "is_greater_than : nrow"
     )
 })
