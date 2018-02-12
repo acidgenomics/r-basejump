b2f5b4fd87dbdc1dcb09cf096a76e081581cb726
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-11 22:07:06 -0500

    Improve unit testing

--- tests/testthat/test_plotQuantileHeatmap.R
+++ tests/testthat/test_plotQuantileHeatmap.R
@@ -5,6 +5,11 @@ plotNames <- c("tree_row", "tree_col", "kmeans", "gtable")
 
 test_that("Simple matrix", {
     mat <- matrix(1:10000, nrow = 100, ncol = 100)
+    expect_error(
+        plotQuantileHeatmap(mat),
+        "has_colnames"
+    )
+    colnames(mat) <- paste0("col", seq_len(ncol(mat)))
     p <- plotQuantileHeatmap(mat)
     expect_identical(names(p), c("quantiles", "plot"))
     expect_identical(names(p[["plot"]]), plotNames)
