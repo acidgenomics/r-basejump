89c4fa2fb5a0787ab0934ae660d7e61835902824
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-11 23:43:58 -0500

    Enforce positive integer base

--- R/methods-logRatioToFoldChange.R
+++ R/methods-logRatioToFoldChange.R
@@ -1,6 +1,8 @@
 # Constructors =================================================================
 .logRatioToFoldChange <- function(object, base = 2L) {
-    assert_is_numeric(base)
+    assert_is_integer(base)
+    assert_is_scalar(base)
+    assert_all_are_positive(base)
     object <- base ^ object
     object <- ifelse(object < 1L, -1L / object, object)
     object
--- tests/testthat/test_logRatio.R
+++ tests/testthat/test_logRatio.R
@@ -4,9 +4,19 @@ fc <- c(-8L, -4L, -2L, 1L, 2L, 4L, 8L)
 lr <- seq(-3L, 3L, 1L)
 
 test_that("foldChangeToLogRatio", {
-    expect_identical(foldChangeToLogRatio(fc), lr)
+    expect_equal(foldChangeToLogRatio(fc), lr)
+    expect_error(
+        foldChangeToLogRatio(lr, base = 0L),
+        "is_positive : base"
+    )
 })
 
 test_that("logRatioToFoldChange", {
-    expect_identical(logRatioToFoldChange(lr), fc)
+    expect_equal(logRatioToFoldChange(lr), fc)
+    expect_error(
+        logRatioToFoldChange(lr, base = 0L),
+        "is_positive : base"
+    )
 })
+
+
