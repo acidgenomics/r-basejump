3bc2d9b0bf3ed1ae9c052c0c81aad38b81e3b60f
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-15 15:46:38 -0500

    Update assertions

--- R/assert_is_implicit_integer.R
+++ R/assert_is_implicit_integer.R
@@ -1,14 +1,36 @@
-# TODO Need to use `assert_engine()` to generate error message
-
-
-
 #' Implict Integer Assert Check
 #'
 #' @family Assert Checks
+#' @rdname assert_is_implicit_integer
+#' @name assert_is_implicit integer
 #'
 #' @inherit assert
 #' @inheritParams general
-#'
+NULL
+
+
+
+#' @rdname assert_is_implicit_integer
+#' @export
+assert_is_an_implicit_integer <- function(x) {  # nolint
+    assert_is_implicit_integer(x)
+    assert_is_scalar(x)
+}
+
+
+
+#' @rdname assert_is_implicit_integer
+#' @export
+assert_is_an_implicit_integer_or_null <- function(x) {  # nolint
+    assert_is_implicit_integer_or_null(x)
+    if (is_implicit_integer(x)) {
+        assert_is_a_number(x)
+    }
+}
+
+
+
+#' @rdname assert_is_implicit_integer
 #' @export
 assert_is_implicit_integer <- function(x) {  # nolint
     stopifnot(is_implicit_integer(x))
@@ -20,26 +42,6 @@ assert_is_implicit_integer <- function(x) {  # nolint
 #' @export
 assert_is_implicit_integer_or_null <- function(x) {  # nolint
     stopifnot(any(is_implicit_integer(x), is.null(x)))
-}
-
-
-
-#' @rdname assert_is_implicit_integer
-#' @export
-assert_is_implicit_integer_scalar <- function(x) {  # nolint
-    assert_is_implicit_integer(x)
-    assert_is_scalar(x)
-}
-
-
-
-#' @rdname assert_is_implicit_integer
-#' @export
-assert_is_implicit_integer_scalar_or_null <- function(x) {  # nolint
-    assert_is_implicit_integer_or_null(x)
-    if (is_implicit_integer(x)) {
-        assert_is_scalar(x)
-    }
 }
 
 
