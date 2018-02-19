f7ef69c2657d37bc9a5ac3ee2e113ef11e1cdbb2
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-19 15:31:43 -0500

    Add `assertIsColorScaleDiscreteOrNULL()` assert check

---
+++ R/assertIsFillScaleContinuousOrNULL.R
@@ -0,0 +1,41 @@
+# TODO Need to add a check to distinguish `color/fill`
+
+#' Is Fill Palette Scale Continuous Assert Check
+#'
+#' @family Assert Checks
+#' @inherit assert
+#'
+#' @export
+#'
+#' @examples
+#' # Success
+#' fill <- scale_fill_viridis(discrete = TRUE)
+#' class(fill)
+#' assertIsFillScaleContinuousOrNULL(fill)
+#' assertIsFillScaleContinuousOrNULL(NULL)
+#'
+#' # Failure
+#' fill <- scale_color_viridis(discrete = FALSE)
+#' class(fill)
+#' tryCatch(
+#'     assertIsFillScaleContinuousOrNULL(color),
+#'     error = function(e) e)
+assertIsFillScaleContinuousOrNULL <- function(x, severity = "stop") {
+    assert_is_any_of(
+        x = x,
+        classes = c("ScaleContinuous", "NULL"),
+        severity = severity)
+    if (!is.null(x)) {
+        assert_is_all_of(
+            x = x,
+            classes = c("ggproto", "Scale", "ScaleContinuous"),
+            severity = severity)
+    }
+}
+
+
+
+# Aliases ======================================================================
+#' @rdname assertIsFillScaleContinuousOrNULL
+#' @export
+assertIsFillScaleContinuousOrNULL -> assert_is_fill_scale_continuous_or_null
