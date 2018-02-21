ef698403fab61b0b6709506d48029912501b63d9
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-21 11:36:11 -0500

    Drop dependency requirements and update `annotable()` error message

--- tests/testthat/test_assertFormalGene2symbol.R
+++ tests/testthat/test_assertFormalGene2symbol.R
@@ -1,6 +1,6 @@
 context("assertFormalGene2symbol")
 
-gene2symbol <- gene2symbol("Homo sapiens", release = 90L)
+gene2symbol <- gene2symbol("Homo sapiens", release = 87L)
 genes <- head(rownames(gene2symbol), 2L)
 x <- data.frame(
     sample1 = c(1L, 2L),
