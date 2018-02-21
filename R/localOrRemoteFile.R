2e355bd36c200f5d4a6a6356c3ac01b0d90144c7
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-21 14:37:22 -0500

    Vectorize `localOrRemoteFile()`

--- R/localOrRemoteFile.R
+++ R/localOrRemoteFile.R
@@ -8,37 +8,43 @@
 #' @param severity Return `stop` (default), `warning`, or `message` if file
 #'   doesn't exist.
 #'
-#' @return Named character vector containing the original file name as the
-#'   name and local file path as the string. Aborts on a missing file.
+#' @return Named list containing the original basename as the name and local
+#'   file path (i.e. tempfile) as the string. Aborts on a missing file.
 #' @export
 #'
 #' @examples
-#' localOrRemoteFile("http://basejump.seq.cloud/mtcars.rda")
+#' files <- localOrRemoteFile(c(
+#'     "http://basejump.seq.cloud/mtcars.csv",
+#'     "http://basejump.seq.cloud/mtcars.rda"
+#' ))
+#' glimpse(files)
 localOrRemoteFile <- function(
     object,
     severity = "stop",
     quiet = FALSE) {
-    assert_is_a_string(object)
+    assert_is_character(object)
     assert_is_a_string(severity)
     assert_is_subset(severity, c("message", "stop", "warning"))
     assert_is_a_bool(quiet)
-    basename <- basename(object)
 
-    # Download remote file, if necessary
-    if (grepl("\\://", object)) {
-        # Remote file
-        file <- tempfile()
-        download.file(url = object, destfile = file, quiet = quiet)
-    } else {
-        file <- object
-    }
+    files <- mapply(
+        FUN = function(path, quiet) {
+            # Download remote file, if necessary
+            if (grepl("\\://", path)) {
+                # Remote file
+                file <- file_temp()
+                download.file(url = path, destfile = file, quiet = quiet)
+            } else {
+                file <- path
+            }
+            file
+        },
+        path = object,
+        MoreArgs = list(quiet = quiet),
+        SIMPLIFY = FALSE,
+        USE.NAMES = FALSE)
+    names(files) <- basename(object)
 
-    assert_all_are_existing_files(file, severity = severity)
-    if (!file.exists(file)) {
-        return(invisible())
-    }
-
-    file <- path_real(file)
-    names(file) <- basename
-    file
+    assert_all_are_existing_files(files, severity = severity)
+    files
 }
