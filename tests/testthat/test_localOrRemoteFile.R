47b65f442e493bd7c283b84d326a6eff3c646a54
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-21 15:38:21 -0500

    Update vectorized unit test

--- tests/testthat/test_localOrRemoteFile.R
+++ tests/testthat/test_localOrRemoteFile.R
@@ -1,14 +1,10 @@
 context("localOrRemoteFile")
 
 test_that("Vectorized", {
-    files <- localOrRemoteFile(c(
+    urls <- c(
         "http://basejump.seq.cloud/mtcars.csv",
-        "http://basejump.seq.cloud/mtcars.rda"
-    ))
-    expect_is(files, "list")
-    expect_true(all(vapply(
-        X = files,
-        FUN = function(x) is(x, "fs_path"),
-        FUN.VALUE = logical(1L)
-    )))
+        "http://basejump.seq.cloud/mtcars.rda")
+    files <- localOrRemoteFile(urls)
+    expect_is(files, "character")
+    expect_identical(names(files), basename(urls))
 })
