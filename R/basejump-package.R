cfe1208013a0ad12fa2b2b1cd04425c0a3cdbe34
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-22 16:31:33 -0500

    Add `ensembldbGeneCols` as a global variable

--- R/basejump-package.R
+++ R/basejump-package.R
@@ -10,4 +10,21 @@
 
 #' @importFrom utils globalVariables
 globalVariables(".")
-annotableCols <- c("ensgene", "symbol", "description", "biotype")
+# FIXME Rework this?
+annotableCols <- c(
+    "ensgene",
+    "symbol",
+    "description",
+    "biotype")
+ensembldbGeneCols <- c(
+    "gene_id",
+    "gene_name",
+    "gene_biotype",
+    "gene_seq_start",
+    "gene_seq_end",
+    "seq_name",
+    "seq_strand",
+    "seq_coord_system",
+    "description",
+    "symbol",
+    "entrezid")
