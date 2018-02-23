061112ab977133326c6591337aebce96ee632b77
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-23 06:22:10 -0500

    Use helper files

--- tests/testthat/test_aggregateFeatures.R
+++ tests/testthat/test_aggregateFeatures.R
@@ -1,19 +1,5 @@
 context("aggregateFeatures")
 
-mat <- data.frame(
-    "sample1" = c(0L, 1L, 2L, 3L),
-    "sample2" = c(1L, 2L, 3L, 4L),
-    row.names = c("gene1.1", "gene1.2", "gene2.1", "gene2.2")) %>%
-    as.matrix()
-aggmat <- data.frame(
-    "sample1" = c(1L, 5L),
-    "sample2" = c(3L, 7L),
-    row.names = c("gene1", "gene2")) %>%
-    as.matrix()
-
-groupings <- factor(c("gene1", "gene1", "gene2", "gene2"))
-names(groupings) <- rownames(mat)
-
 test_that("Matrix", {
    data <- aggregateFeatures(mat, groupings = groupings)
    expect_is(data, "matrix")
