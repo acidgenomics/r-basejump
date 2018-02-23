da77ea963714b967768884ad08ce90c8c0c06099
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-23 15:10:36 -0500

    Update unit tests

--- tests/testthat/test_saveData.R
+++ tests/testthat/test_saveData.R
@@ -1,27 +1,17 @@
 context("saveData")
 
 test_that("saveData", {
-    paths <- path(path_real("."), "data", c("mtcars.rda", "starwars.rda"))
+    paths <- path(path_real("."), "savetest", c("mtcars.rda", "starwars.rda"))
     names(paths) <- c("mtcars", "starwars")
     expect_identical(
-        saveData(
-            mtcars, starwars, dir = "data",
-            overwrite = TRUE),
+        saveData(mtcars, starwars, dir = "savetest", overwrite = TRUE),
         paths
     )
-    expect_message(
-        saveData(mtcars, starwars, dir = "data", overwrite = TRUE),
-        paste(
-            "Saving mtcars.rda, starwars.rda to",
-            path_join(c(path_real("."), "data"))
-        )
-    )
     expect_warning(
-        saveData(
-            mtcars, starwars, dir = "data",
-            overwrite = FALSE),
-        "No files were saved"
+        saveData(mtcars, starwars, dir = "savetest", overwrite = FALSE),
+        "Skipping"
     )
+    dir_delete("savetest")
     expect_error(
         saveData(XXX),
         "object 'XXX' not found"
@@ -30,13 +20,8 @@ test_that("saveData", {
         saveData("mtcars"),
         "is_name : X"
     )
-})
-
-test_that("Invalid arguments", {
     expect_error(
         saveData(mtcars, dir = NULL),
         "is_a_string : dir"
     )
 })
-
-dir_delete("data")
