51660f3ce9428480fd76acca94dc609a48a1fcc6
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-22 23:33:18 -0500

    Break out `.uniqueSymbol()` to separate file

--- R/methods-genes.R
+++ R/methods-genes.R
@@ -20,25 +20,6 @@ NULL
 
 
 
-# Constructors =================================================================
-.uniqueSymbol <- function(object) {
-    if (is(object, "GRanges")) {
-        data <- mcols(object)
-    } else {
-        data <- object
-    }
-    assert_is_subset("symbol", colnames(data))
-    data[["symbol"]] <- make.unique(data[["symbol"]])
-    if (is(object, "GRanges")) {
-        mcols(object) <- data
-    } else {
-        object <- data
-    }
-    object
-}
-
-
-
 # Methods ======================================================================
 #' @rdname genes
 #' @export
