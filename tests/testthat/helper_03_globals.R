9fbd8d40a87b26fef28d55ad8b885e3ad28281df
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-24 17:39:56 -0500

    Update globals

--- tests/testthat/helper_03_globals.R
+++ tests/testthat/helper_03_globals.R
@@ -5,19 +5,14 @@ mat <- matrix(
     byrow = FALSE,
     dimnames = list(
         c("gene_1", "gene_2", "gene_3", "gene_4"),
-        c("sample_1", "sample_2", "sample_3", "sample_4")
-    )
-)
+        c("sample_1", "sample_2", "sample_3", "sample_4")))
 df <- as.data.frame(mat)
 tbl <- as(df, "tibble")
 dgc <- as(mat, "dgCMatrix")
-
 coldata <- DataFrame(
     "genotype" = factor(c("wt", "ko", "wt", "ko")),
     "batch" = factor(c(1L, 1L, 2L, 2L)),
-    row.names = c("sample_1", "sample_2", "sample_3", "sample_4")
-)
-
+    row.names = c("sample_1", "sample_2", "sample_3", "sample_4"))
 ensemblURL <- "ftp://ftp.ensembl.org/pub/release-89"
-
 heatmapList <- c("tree_row", "tree_col", "kmeans", "gtable")
+release <- 87L
