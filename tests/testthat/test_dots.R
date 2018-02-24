5ea725ab7505102fa877ff7294a58a3c75ae392e
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-24 10:48:54 -0500

    Update unit tests

--- tests/testthat/test_dots.R
+++ tests/testthat/test_dots.R
@@ -1,21 +1,18 @@
-context("dots")
+context("Dots")
 
-test_that("Dots", {
-    expect_identical(
-        dots(mtcars, starwars),
-        list(rlang::sym("mtcars"),
-             rlang::sym("starwars"))
-    )
-    expect_identical(
-        dots(mtcars, starwars, character = TRUE),
-        c("mtcars", "starwars")
-    )
-    expect_error(
-        dots(mtcars, mtcars),
-        "has_no_duplicates"
-    )
-    expect_error(
-        dots(),
-        "is_non_empty"
-    )
-})
+expect_identical(
+    dots(mtcars, starwars),
+    list(sym("mtcars"), sym("starwars"))
+)
+expect_identical(
+    dots(mtcars, starwars, character = TRUE),
+    c("mtcars", "starwars")
+)
+expect_error(
+    dots(mtcars, mtcars),
+    "has_no_duplicates :"
+)
+expect_error(
+    dots(),
+    "is_non_empty :"
+)
