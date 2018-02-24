de6f16b7dd5d140bdfe68a1791d2ae78c617f6b2
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-24 06:58:08 -0500

    Consolidate unit tests

--- tests/testthat/test_geometricMean.R
+++ tests/testthat/test_geometricMean.R
@@ -1,38 +1,52 @@
 context("geometricMean")
 
-test_that("Numeric", {
-    geo1 <- round(geometricMean(vec1), digits = 6L)
-    expect_identical(geo1, means[["vec1"]])
-    geo2 <- round(geometricMean(vec2), digits = 6L)
-    expect_identical(geo2, means[["vec2"]])
-})
+test_that("geometricMean", {
+    int <- seq(from = 1L, to = 5L, by = 1L)
+    num <- int ^ 2L
+    df <- data.frame(int, num)
+    mat <- as.matrix(df)
+    mean <- c(int = 2.605171, num = 6.786916)
 
-test_that("Column data", {
-    data <- data.frame(vec1, vec2)
-    geo1 <- round(geometricMean(data), digits = 6L)
-    expect_identical(geo1, means)
-    mat <- as.matrix(data)
-    geo2 <- round(geometricMean(mat), digits = 6L)
-    expect_identical(geo2, means)
-})
+    # integer ==================================================================
+    expect_identical(
+        round(geometricMean(int), digits = 6L),
+        mean[["int"]]
+    )
 
-test_that("Non-numeric column data", {
+    # numeric ==================================================================
+    expect_identical(
+        round(geometricMean(num), digits = 6L),
+        mean[["num"]]
+    )
+
+    # data.frame ===============================================================
+    expect_identical(
+        round(geometricMean(df), digits = 6L),
+        mean
+    )
     expect_error(
         geometricMean(starwars),
-        "is_numeric"
+        "is_numeric :"
     )
-})
 
-test_that("NaN on negative numbers", {
-    vec <- seq(from = -5L, to = 5L, by = 1L)
-    expect_identical(geometricMean(vec), NaN)
-})
-
-test_that("Zero propagation", {
-    vec <- seq(from = 0L, to = 5L, by = 1L)
-    expect_identical(geometricMean(vec, zeroPropagate = TRUE), 0L)
+    # matrix ===================================================================
     expect_identical(
-        round(geometricMean(vec, zeroPropagate = FALSE), digits = 6L),
+        round(geometricMean(mat), digits = 6L),
+        mean
+    )
+
+    # NaN on negative numbers ==================================================
+    x <- seq(from = -5L, to = 5L, by = 1L)
+    expect_identical(geometricMean(x), NaN)
+
+    # Zero propagation =========================================================
+    x <- seq(from = 0L, to = 5L, by = 1L)
+    expect_identical(
+        geometricMean(x, zeroPropagate = TRUE),
+        0L
+    )
+    expect_identical(
+        round(geometricMean(x, zeroPropagate = FALSE), digits = 6L),
         2.220906
     )
 })
