98d32a5b220d20c046cd8e55ea5516a92aaa3029
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-24 07:08:53 -0500

    Reorganize unit tests

--- tests/testthat/test_readFileByExtension.R
+++ tests/testthat/test_readFileByExtension.R
@@ -1,36 +1,36 @@
 context("readFileByExtension")
 
-test_that("Comma separated value (.csv) file", {
+test_that("Comma separated value file (.csv)", {
     csv <- readFileByExtension("mtcars.csv")
     expect_is(csv, "tbl_df")
 })
 
-test_that("MatrixMarket (.mtx) file", {
-    sparse <- readFileByExtension("sparse.mtx")
-    expect_is(sparse, "ngTMatrix")
-
-    colnames <- readFileByExtension("test.colnames")
-    expect_identical(colnames, c("foo", "bar"))
+test_that("MatrixMarket file (.mtx)", {
+    mtx <- readFileByExtension("sparse.mtx")
+    expect_is(mtx, "ngTMatrix")
+    col <- readFileByExtension("test.colnames")
+    expect_identical(col, c("foo", "bar"))
     # rownames use the same code base as colnames
 })
 
-test_that("Tab separated values (.tsv) file", {
+test_that("Tab separated values file (.tsv)", {
     tsv <- readFileByExtension("mtcars.tsv")
     expect_is(tsv, "tbl_df")
 })
 
-test_that("Table format (.txt) file", {
+test_that("Table format file (.txt)", {
     txt <- readFileByExtension("mtcars.txt")
     expect_is(txt, "data.frame")
     # txt has integer columns whereas mtcars doesn't
     expect_equal(txt, mtcars)
 })
 
-test_that("Excel (.xlsx) file", {
+test_that("Excel file (.xlsx)", {
     xlsx <- readFileByExtension("mtcars.xlsx")
     expect_is(xlsx, "tbl_df")
+})
 
-    # Counts (.counts) file
+test_that("Counts file (.counts)", {
     counts <- readFileByExtension("test.counts")
     expect_is(counts, "matrix")
     expect_identical(
@@ -43,22 +43,15 @@ test_that("Excel (.xlsx) file", {
     )
 })
 
-test_that("R Data (.rda) file (unsupported)", {
+test_that("Unsupported files", {
+    # Missing extension
+    expect_error(
+        readFileByExtension("DESCRIPTION"),
+        "is_matching_regex :"
+    )
+    # R Data
     expect_error(
         readFileByExtension("mtcars.rda"),
         "Unsupported file extension"
-    )
-
-    # Missing extension
-    expect_error(
-        readFileByExtension(
-            paste(
-                "https://cran.r-project.org",
-                "web",
-                "packages",
-                "testthat",
-                "LICENSE",
-                sep = "/")),
-        "is_matching_regex"
     )
 })
