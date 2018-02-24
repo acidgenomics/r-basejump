f34daad81f8314df5fd571db10983409c68f8b3a
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-24 07:02:00 -0500

    Use `df` global

--- tests/testthat/test_setAs.R
+++ tests/testthat/test_setAs.R
@@ -1,10 +1,13 @@
 context("setAs")
 
 test_that("Coerce to tibble", {
-    tbl <- as(data, "tibble")
-    expect_is(tbl, "tbl_df")
+    x <- as(df, "tibble")
+    expect_is(x, "tbl_df")
     # Expect that rownames are automatically moved to first column
-    expect_identical(colnames(tbl)[[1L]], "rowname")
+    expect_identical(
+        colnames(x)[[1L]],
+        "rowname"
+    )
     expect_error(
          as(list("foo", "bar"), "tibble"),
         "has_dims"
