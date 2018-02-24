b3daef71e9b976a22e4860fa96b78aaf659bb144
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-24 09:19:21 -0500

    Update unit tests

--- tests/testthat/test_aggregation.R
+++ tests/testthat/test_aggregation.R
@@ -1,17 +1,26 @@
 context("Aggregation Utilities")
 
 test_that("aggregateFeatures", {
+    aggMat <- data.frame(
+        "sample_1" = c(3L, 7L),
+        "sample_2" = c(11L, 15L),
+        "sample_3" = c(19L, 23L),
+        "sample_4" = c(27L, 31L),
+        row.names = c("gene_1", "gene_2")
+    ) %>%
+        as.matrix()
+
     groupings <- factor(c("gene_1", "gene_1", "gene_2", "gene_2"))
     names(groupings) <- rownames(df)
 
     # matrix ===================================================================
     x <- aggregateFeatures(mat, groupings = groupings)
     expect_is(x, "matrix")
-    expect_identical(x, matAggFeat)
+    expect_identical(x, aggMat)
 
     # dgCMatrix ================================================================
     x <- aggregateFeatures(dgc, groupings = groupings)
-    y <- as(matAggFeat, "dgCMatrix")
+    y <- as(aggMat, "dgCMatrix")
     expect_is(x, "dgCMatrix")
     expect_identical(as.matrix(x), as.matrix(y))
 
@@ -27,17 +36,24 @@ test_that("aggregateFeatures", {
 })
 
 test_that("aggregateReplicates", {
+    aggMat <- data.frame(
+        "sample_1" = c(6L, 8L, 10L, 12L),
+        "sample_2" = c(22L, 24L, 26L, 28L),
+        row.names = c("gene_1", "gene_2", "gene_3", "gene_4")
+    ) %>%
+        as.matrix()
+
     groupings <- factor(c("sample_1", "sample_1", "sample_2", "sample_2"))
     names(groupings) <- colnames(df)
 
     # matrix ===================================================================
     x <- aggregateReplicates(mat, groupings = groupings)
     expect_is(x, "matrix")
-    expect_identical(x, matAggRep)
+    expect_identical(x, aggMat)
 
     # dgCMatrix ================================================================
     x <- aggregateReplicates(dgc, groupings = groupings)
-    y <- as(matAggRep, "dgCMatrix")
+    y <- as(aggMat, "dgCMatrix")
     expect_is(x, "dgCMatrix")
     expect_identical(x, y)
 
