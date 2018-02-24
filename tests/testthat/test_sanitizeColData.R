2083975252fac17943fe8811fac456e6ee079de0
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-24 06:39:15 -0500

    Improve unit test

--- tests/testthat/test_sanitizeColData.R
+++ tests/testthat/test_sanitizeColData.R
@@ -1,9 +1,11 @@
 context("sanitizeColData")
 
-test_that("DataFrame", {
-    x <- sanitizeColData(coldata)
+test_that("sanitizeColData", {
+    x <- coldata
+    x[["day"]] <- c(14L, 14L, 30L, 30L)
+    x <- sanitizeColData(x)
     expect_is(x, "DataFrame")
-    expect_identical(dimnames(x), dimnames(coldata))
+    expect_identical(rownames(x), rownames(coldata))
     expect_true(all(vapply(
         X = x,
         FUN = is.factor,
