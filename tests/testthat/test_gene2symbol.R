68df10bfec85e865ea8e7678ef5f8cbcc81c0b47
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-25 17:39:21 -0500

    Consolidate gene functions

--- tests/testthat/test_gene2symbol.R
+++
@@ -1,9 +0,0 @@
-context("gene2symbol")
-
-test_that("Character", {
-    organism <- "Homo sapiens"
-    expect_identical(
-        gene2symbol(organism),
-        annotable(organism, format = "gene2symbol")
-    )
-})
