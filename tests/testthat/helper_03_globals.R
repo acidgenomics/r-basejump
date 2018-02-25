1e2b109bfc1dad6a6e52f7b7771822a0e94d335e
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-25 12:26:45 -0500

    Update globals

--- tests/testthat/helper_03_globals.R
+++ tests/testthat/helper_03_globals.R
@@ -9,10 +9,14 @@ mat <- matrix(
 df <- as.data.frame(mat)
 tbl <- as(df, "tibble")
 dgc <- as(mat, "dgCMatrix")
+
 coldata <- DataFrame(
     "genotype" = factor(c("wt", "ko", "wt", "ko")),
     "batch" = factor(c(1L, 1L, 2L, 2L)),
     row.names = c("sample_1", "sample_2", "sample_3", "sample_4"))
+
 ensemblURL <- "ftp://ftp.ensembl.org/pub/release-89"
+groceries <- c(NA, NA, "milk", "eggs", "eggs", "veggies")
 heatmapList <- c("tree_row", "tree_col", "kmeans", "gtable")
+mpgString <- "18.1, 18.7, 21, 21.4, 22.8"
 release <- 87L
