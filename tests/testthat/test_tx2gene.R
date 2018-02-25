19c5947ab39be72c65760f8172c5819e0222038a
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-25 17:39:21 -0500

    Consolidate gene functions

--- tests/testthat/test_tx2gene.R
+++
@@ -1,9 +0,0 @@
-context("tx2gene")
-
-test_that("Character", {
-    organism <- "Homo sapiens"
-    expect_identical(
-        tx2gene(organism),
-        annotable(organism, format = "tx2gene")
-    )
-})
