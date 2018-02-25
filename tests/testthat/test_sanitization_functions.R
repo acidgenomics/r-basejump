5bc5683f5258752ecadd02d3f25209dee47b869a
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-25 18:08:43 -0500

    Reclassify `collapseToString()` as a sanitization function

--- tests/testthat/test_sanitization_functions.R
+++ tests/testthat/test_sanitization_functions.R
@@ -1,5 +1,98 @@
 context("Sanitize Functions")
 
+# collapseToString =============================================================
+test_that("collapseToString : atomic", {
+    expect_identical(
+        collapseToString(
+            groceries,
+            sort = TRUE,
+            removeNA = FALSE,
+            unique = FALSE),
+        "eggs, eggs, milk, veggies, NA, NA"
+    )
+    expect_identical(
+        collapseToString(
+            groceries,
+            sort = TRUE,
+            removeNA = TRUE,
+            unique = FALSE),
+        "eggs, eggs, milk, veggies"
+    )
+    expect_identical(
+        collapseToString(
+            groceries,
+            sort = TRUE,
+            removeNA = TRUE,
+            unique = TRUE),
+        "eggs, milk, veggies"
+    )
+})
+
+test_that("colllapseToString : data.frame", {
+    expect_identical(
+        mtcars %>%
+            head() %>%
+            collapseToString(sort = TRUE, unique = TRUE) %>%
+            .[, "mpg", drop = TRUE],
+        mpgString
+    )
+})
+
+test_that("collapseToString : DataFrame", {
+    expect_identical(
+        mtcars %>%
+            as("DataFrame") %>%
+            head() %>%
+            collapseToString(sort = TRUE, unique = TRUE) %>%
+            .[, "mpg"],
+        mpgString
+    )
+})
+
+test_that("collapseToString : integer", {
+    expect_identical(
+        collapseToString(seq(1L:5L)),
+        "1, 2, 3, 4, 5"
+    )
+})
+
+test_that("collapseToString : logical", {
+    expect_identical(
+        collapseToString(c(TRUE, FALSE), sort = TRUE),
+        "FALSE, TRUE"
+    )
+    # NA and NaN should stay fixed even when sorting is enabled
+    expect_identical(
+        collapseToString(c(NA, NaN), sort = TRUE),
+        "NA, NaN"
+    )
+    expect_identical(
+        collapseToString(c(NaN, NA), sort = TRUE),
+        "NaN, NA"
+    )
+})
+
+test_that("collapseToString : matrix", {
+    expect_identical(
+        mtcars %>%
+            as("matrix") %>%
+            head() %>%
+            collapseToString(sort = TRUE, unique = TRUE) %>%
+            .[1L, "mpg", drop = TRUE] %>%
+            as.character(),
+        mpgString
+    )
+})
+
+test_that("collapseToString : numeric", {
+    expect_identical(
+        collapseToString(c(3.141593, 6.0221409e+23)),
+        "3.141593, 6.0221409e+23"
+    )
+})
+
+
+
 # fixNA ========================================================================
 test_that("fixNA", {
     # character vector
