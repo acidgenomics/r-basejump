62bdb7abd23bc5c84692a79ea54583ab2a0f9ef9
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-01 18:58:42 -0500

    Fix `loadDataAsName()` to work using non-standard evaluation

--- R/loadDataAsName.R
+++ R/loadDataAsName.R
@@ -1,5 +1,8 @@
 #' Load Data File as Name
 #'
+#' @note This function is desired for interactive use and interprets object
+#' names using non-standard evaluation.
+#'
 #' @family Read Functions
 #'
 #' @importFrom fs path path_real
@@ -16,48 +19,40 @@
 #'
 #' @examples
 #' loadDataAsName(
-#'     annotable = "grch37",
-#'     tx2gene = "grch37Tx2gene",
+#'     hg19 = grch37,
+#'     hg19_tx2gene = grch37Tx2gene,
 #'     dir = system.file("extdata", package = "basejump")
 #' )
-#' glimpse(annotable)
+#' glimpse(hg19)
 loadDataAsName <- function(
     ...,
     dir = ".",
     envir = parent.frame()) {
-    dots <- list(...)
+    dots <- dots(..., character = TRUE)
+    assert_is_character(dots)
+    assert_has_names(dots)
+    invisible(lapply(dots, assert_is_a_string))
     assert_all_are_dirs(dir)
     dir <- path_real(dir)
     assert_is_environment(envir)
 
-    # Check for legacy mappings method, used prior to v0.1.1
-    if (length(dots) == 1L & !is.null(names(dots[[1L]]))) {
-        # Convert the named character vector to a named list, for consistency
-        dots <- as.list(dots[[1L]])
-    }
-
-    assert_is_list(dots)
-    assert_has_names(dots)
-    invisible(lapply(dots, assert_is_a_string))
+    files <- path(dir, paste0(dots, ".rda"))
+    names(files) <- dots
+    assert_all_are_existing_files(files)
 
     # Load into a temporary environment
-    fileNames <- as.character(dots)
-    filePaths <- path(dir, paste0(fileNames, ".rda"))
-    assert_all_are_existing_files(filePaths)
     tmpEnvir <- new.env()
     invisible(mapply(
         FUN = .safeLoad,
-        file = filePaths,
+        file = files,
         MoreArgs = list(envir = tmpEnvir),
         SIMPLIFY = FALSE,
         USE.NAMES = FALSE))
-    # Check to ensure everything loaded up correctly
-    assert_are_set_equal(fileNames, ls(tmpEnvir))
+    assert_are_set_equal(dots, ls(tmpEnvir))
 
     # Now assign to the desired object names
-    objectNames <- names(dots)
     # Check to see if any of the new names already exist in environment
-    assertAllAreNonExisting(objectNames, envir = envir, inherits = FALSE)
+    assertAllAreNonExisting(names(dots), envir = envir, inherits = FALSE)
     invisible(mapply(
         FUN = function(old, new, tmpEnvir, envir) {
         assign(
@@ -65,12 +60,12 @@ loadDataAsName <- function(
             value = get(old, envir = tmpEnvir, inherits = FALSE),
             envir = envir)
         },
-        new = objectNames,
-        old = fileNames,
+        new = names(dots),
+        old = dots,
         MoreArgs = list(tmpEnvir = tmpEnvir, envir = envir),
         SIMPLIFY = FALSE,
         USE.NAMES = FALSE
     ))
 
-    invisible(filePaths)
+    invisible(files)
 }
