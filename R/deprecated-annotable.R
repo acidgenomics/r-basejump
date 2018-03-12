1d5cb79908cdc94a5cf556d49f41f4276d3ffff1
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-12 16:49:46 -0400

    Improve annotable deprecation methods

---
+++ R/deprecated-annotable.R
@@ -0,0 +1,26 @@
+#' @rdname deprecated
+#' @export
+setGeneric("annotable", function(object, ...) {
+    standardGeneric("annotable")
+})
+
+#' @rdname deprecated
+#' @export
+setMethod(
+    "annotable",
+    signature("character"),
+    function(object, ...) {
+        .Deprecated("ensembl")
+        ensembl(organism = object, ...)
+    }
+)
+
+#' @rdname deprecated
+#' @export
+setMethod(
+    "annotable",
+    signature("data.frame"),
+    function(object) {
+        .Defunct()
+    }
+)
