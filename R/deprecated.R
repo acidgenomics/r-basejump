463f5f9cca0eb2ab808911a41f3d7f415827875a
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-19 06:29:31 -0400

    Consolidate deprecations

--- R/deprecated.R
+++ R/deprecated.R
@@ -164,6 +164,33 @@ initializeDir <- function(...) {
  # v0.4.0 ======================================================================
 #' @rdname deprecated
 #' @export
+setGeneric("annotable", function(object, ...) {
+    standardGeneric("annotable")
+})
+
+#' @rdname deprecated
+#' @export
+setMethod(
+    "annotable",
+    signature("character"),
+    function(object, ...) {
+        .Deprecated("ensembl")
+        ensembl(organism = object, ...)
+    }
+)
+
+#' @rdname deprecated
+#' @export
+setMethod(
+    "annotable",
+    signature("data.frame"),
+    function(object) {
+        .Defunct()
+    }
+)
+
+#' @rdname deprecated
+#' @export
 assertIsAnnotable <- function(...) {
      .Deprecated("assertAreGeneAnnotations")
      assertAreGeneAnnotations(...)
