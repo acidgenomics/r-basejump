63e57c22b19daa881535d43238183e2738fce9a2
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-22 08:07:22 -0400

    Improve assert check and ensure coercion to character

--- R/makeNames.R
+++ R/makeNames.R
@@ -7,11 +7,14 @@
 #'
 #' @inherit base::make.names
 #'
+#' @seealso [make.names()].
+#'
 #' @return Character vector.
 #' @export
 makeNames <- function(names, unique = FALSE) {
-    assert_is_character(names)
+    assert_is_atomic(names)
     assert_is_a_bool(unique)
+    names <- as.character(names)
     names <- make.names(names, unique = unique)
     names <- gsub("\\.", "_", names)
     names
