4492073b62c7d3c493fa7961b3ee8dc92a18aabc
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-28 15:32:19 -0400

    Ensure local file always contains an extension...some functions won't work otherwise

--- R/localOrRemoteFile.R
+++ R/localOrRemoteFile.R
@@ -35,25 +35,35 @@ localOrRemoteFile <- function(
 
     files <- mapply(
         FUN = function(path) {
-            # Download remote file, if necessary
-            if (grepl("\\://", path)) {
-                # Remote file
-                file <- tempfile()
+            if (file.exists(path)) {
+                # Local file mode
+                path
+            } else if (grepl("\\://", path)) {
+                # Remote file mode
+                tempfile <- tempfile()
+
+                # Make sure local tempfile always has an extension
+                extPattern <- "\\.([A-Za-z0-9]+)$"
+                assert_all_are_matching_regex(path, extPattern)
+                ext <- str_match(path, extPattern)[, 2L]
+
                 # Fix for Excel files on Windows
                 # https://github.com/tidyverse/readxl/issues/374
                 # Otherwise, `read_excel()` errors in `readFileByExtension()`
-                if (grepl("\\.xlsx$", path)) {
+                if (ext == "xlsx") {
                     # Write binary
                     mode <- "wb"
                 } else {
                     # Write (default)
                     mode <- "w"
                 }
-                download.file(url = path, destfile = file, mode = mode)
+
+                destfile <- paste(tempfile, ext, sep = ".")
+                download.file(url = path, destfile = destfile, mode = mode)
+                destfile
             } else {
-                file <- path
+                abort(paste("Invalid path:", path))
             }
-            file
         },
         path = object,
         SIMPLIFY = TRUE,
