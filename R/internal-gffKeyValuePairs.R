0085e901af258c17820714b0190fe51af46b5801
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-28 13:09:17 -0400

    Improve internal GFF key value pair utility to return as data.frame

--- R/internal-gffKeyValuePairs.R
+++ R/internal-gffKeyValuePairs.R
@@ -1,7 +1,27 @@
-.gffKeyValuePairs <- function(object) {
-    assert_is_data.frame(object)
-    assert_is_subset("keyValuePairs", colnames(object))
-    object[["keyValuePairs"]] %>%
-        as.character() %>%
-        unique()
+.gffKeyValuePairs <- function(gff, unique = FALSE) {
+    assertIsGFF(gff)
+    strings <- gff %>%
+        .[["keyValuePairs"]] %>%
+        as.character()
+
+    # This can be significantly faster for large GFF files
+    if (isTRUE(unique)) {
+        strings <- unique(strings)
+    }
+
+    list <- strings %>%
+        # Remove trailing semicolon
+        gsub(";$", "", .) %>%
+        strsplit("; ")
+    list <- mclapply(list, function(string) {
+        match <- str_match(string, "(.+)\\s(.+)")
+        keys <- match[, 2L]
+        # Enforce camel case
+        keys <- camel(keys)
+        values <- match[, 3L]
+        names(values) <- keys
+        values
+    })
+
+    ldply(list, rbind)
 }
