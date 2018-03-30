26ad37c82f4c5e0da3b29089f4a2a23bb5fb2379
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-29 23:30:21 -0400

    Remove link

--- R/makeNames.R
+++ R/makeNames.R
@@ -1,6 +1,6 @@
 #' Make Syntactically Valid Names
 #'
-#' @note Sanitizes names using "`_`" instead of "`.`".
+#' @note Sanitizes names using underscore instead of dot.
 #'
 #' @author Michael Steinbaugh
 #'
@@ -8,10 +8,11 @@
 #'   coerced to `character` if necessary.
 #' @param unique `logical`; if `TRUE`, the resulting elements are unique.
 #'
-#' @seealso `base::make.names()`.
-#'
 #' @return Character vector.
 #' @export
+#'
+#' @examples
+#' makeNames(c(123L, "hello world"))
 makeNames <- function(names, unique = FALSE) {
     assert_is_atomic(names)
     assert_is_a_bool(unique)
