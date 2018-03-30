96c418cd8f9d35270e756686d25d277dedaecebd
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-29 21:17:54 -0400

    Improve consistency of annotation functions fetching from Ensembl or GFF

---
+++ R/makeGene2symbolFromEnsembl.R
@@ -0,0 +1,22 @@
+#' Gene-to-Symbol Mappings from Ensembl
+#'
+#' @inheritParams makeGRangesFromEnsembl
+#'
+#' @return `data.frame`
+#' @export
+makeGene2symbolFromEnsembl <- function(
+    organism,
+    genomeBuild = NULL,
+    release = NULL
+) {
+    gr <- makeGRangesFromEnsembl(
+        organism = organism,
+        format = "genes",
+        genomeBuild = genomeBuild,
+        release = NULL
+    )
+    mcols(gr) %>%
+        .[, c("geneID", "geneName")] %>%
+        as.data.frame() %>%
+        set_rownames(.[[1L]])
+}
---
+++ R/makeTx2geneFromGFF.R
@@ -0,0 +1,45 @@
+#' Transcript-to-Gene Mappings from GFF File
+#'
+#' The GFF (General Feature Format) format consists of one line per feature,
+#' each containing 9 columns of data, plus optional track definition lines. The
+#' GTF (General Transfer Format) is identical to GFF version 2.
+#'
+#' @family GFF Functions
+#'
+#' @inheritParams general
+#'
+#' @return `data.frame`.
+#' @export
+#'
+#' @examples
+#' makeTx2geneFromGFF("http://basejump.seq.cloud/mmusculus.gtf") %>%
+#'     glimpse()
+makeTx2geneFromGFF <- function(file) {
+    data <- parseGFFAttributes(
+        file = file,
+        select = c("gene_id", "transcript_id")
+    ) %>%
+        as.data.frame() %>%
+        set_colnames(c("geneID", "txID")) %>%
+        # Put transcripts first
+        .[, c("txID", "geneID")] %>%
+        # Drop rows containing an NA value
+        .[complete.cases(.), , drop = FALSE] %>%
+        set_rownames(.[["txID"]])
+
+    inform(paste(
+        "tx2gene mappings:",
+        length(unique(data[["txID"]])), "transcripts,",
+        length(unique(data[["geneID"]])), "genes"
+    ))
+
+    data
+}
+
+
+
+
+# Aliases ======================================================================
+#' @rdname makeTx2geneFromGFF
+#' @export
+makeTx2geneFromGFF -> makeTx2geneFromGTF
