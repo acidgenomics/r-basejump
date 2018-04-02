4305243d2db4d0e902955b005e4a4192990723dc
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-02 06:14:30 -0400

    Reduce external links in documentation - error messages on AppVeyor

--- R/dots.R
+++ R/dots.R
@@ -13,17 +13,11 @@
 #' @export
 #'
 #' @seealso
-#' - `help("dotsMethods", "methods")`
-#' - `browseVignettes("rlang")`.
-#' - [rlang](http://rlang.tidyverse.org) functions:
-#'       [rlang::quos()],
-#'       [rlang::dots_definitions()],
-#'       [rlang::dots_values()],
-#'       [rlang::eval_tidy()],
-#'       [rlang::eval_bare()].
+#' - `help("dotsMethods", "methods")`.
 #' - [tidyverse](http://tidyverse.org) code:
-#'       [dplyr utils](https://goo.gl/fhAuak),
-#'       [devtools infrastructure](https://goo.gl/bM5TrP)
+#'   [rlang](http://rlang.tidyverse.org)
+#'   [dplyr utils](https://goo.gl/fhAuak),
+#'   [devtools infrastructure](https://goo.gl/bM5TrP).
 #'
 #' @examples
 #' # names
