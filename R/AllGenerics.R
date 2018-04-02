ad512d580612f1380c949fdea0d718dca584e3a4
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-02 12:23:38 -0400

    Improve line break consistency [ci skip]

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,124 +1,172 @@
 #' @rdname aggregateFeatures
 #' @export
-setGeneric("aggregateFeatures", function(object, ...) {
-    standardGeneric("aggregateFeatures")
-})
+setGeneric(
+    "aggregateFeatures",
+    function(object, ...) {
+        standardGeneric("aggregateFeatures")
+    }
+)
 
 
 
 #' @rdname aggregateReplicates
 #' @export
-setGeneric("aggregateReplicates", function(object, ...) {
-    standardGeneric("aggregateReplicates")
-})
+setGeneric(
+    "aggregateReplicates",
+    function(object, ...) {
+        standardGeneric("aggregateReplicates")
+    }
+)
 
 
 
 #' @rdname camel
 #' @export
-setGeneric("camel", function(object, ...) {
-    standardGeneric("camel")
-})
+setGeneric(
+    "camel",
+    function(object, ...) {
+        standardGeneric("camel")
+    }
+)
 
 
 
 #' @rdname collapseToString
 #' @export
-setGeneric("collapseToString", function(object, ...) {
-    standardGeneric("collapseToString")
-})
+setGeneric(
+    "collapseToString",
+    function(object, ...) {
+        standardGeneric("collapseToString")
+    }
+)
 
 
 
 #' @rdname convertGenesToSymbols
 #' @export
-setGeneric("convertGenesToSymbols", function(object, ...) {
-    standardGeneric("convertGenesToSymbols")
-})
+setGeneric(
+    "convertGenesToSymbols",
+    function(object, ...) {
+        standardGeneric("convertGenesToSymbols")
+    }
+)
 
 
 
 #' @rdname convertTranscriptsToGenes
 #' @export
-setGeneric("convertTranscriptsToGenes", function(object, ...) {
-    standardGeneric("convertTranscriptsToGenes")
-})
+setGeneric(
+    "convertTranscriptsToGenes",
+    function(object, ...) {
+        standardGeneric("convertTranscriptsToGenes")
+    }
+)
 
 
 
 #' @rdname detectOrganism
 #' @export
-setGeneric("detectOrganism", function(object, ...) {
-    standardGeneric("detectOrganism")
-})
+setGeneric(
+    "detectOrganism",
+    function(object, ...) {
+        standardGeneric("detectOrganism")
+    }
+)
 
 
 
 #' @rdname dotted
 #' @export
-setGeneric("dotted", function(object, ...) {
-    standardGeneric("dotted")
-})
+setGeneric(
+    "dotted",
+    function(object, ...) {
+        standardGeneric("dotted")
+    }
+)
 
 
 
 #' @rdname logRatio
 #' @export
-setGeneric("foldChangeToLogRatio", function(object, ...) {
-    standardGeneric("foldChangeToLogRatio")
-})
+setGeneric(
+    "foldChangeToLogRatio",
+    function(object, ...) {
+        standardGeneric("foldChangeToLogRatio")
+    }
+)
 
 
 
 #' @rdname fixNA
 #' @export
-setGeneric("fixNA", function(object, ...) {
-    standardGeneric("fixNA")
-})
+setGeneric(
+    "fixNA",
+    function(object, ...) {
+        standardGeneric("fixNA")
+    }
+)
 
 
 
 #' @rdname geometricMean
-setGeneric("geometricMean", function(object, ...) {
-    standardGeneric("geometricMean")
-})
+setGeneric(
+    "geometricMean",
+    function(object, ...) {
+        standardGeneric("geometricMean")
+    }
+)
 
 
 
 #' @rdname logRatio
 #' @export
-setGeneric("logRatioToFoldChange", function(object, ...) {
-    standardGeneric("logRatioToFoldChange")
-})
+setGeneric(
+    "logRatioToFoldChange",
+    function(object, ...) {
+        standardGeneric("logRatioToFoldChange")
+    }
+)
 
 
 
 #' @rdname removeNA
 #' @export
-setGeneric("removeNA", function(object, ...) {
-    standardGeneric("removeNA")
-})
+setGeneric(
+    "removeNA",
+    function(object, ...) {
+        standardGeneric("removeNA")
+    }
+)
 
 
 
 #' @rdname snake
 #' @export
-setGeneric("snake", function(object, ...) {
-    standardGeneric("snake")
-})
+setGeneric(
+    "snake",
+    function(object, ...) {
+        standardGeneric("snake")
+    }
+)
 
 
 
 #' @rdname stripTranscriptVersions
 #' @export
-setGeneric("stripTranscriptVersions", function(object, ...) {
-    standardGeneric("stripTranscriptVersions")
-})
+setGeneric(
+    "stripTranscriptVersions",
+    function(object, ...) {
+        standardGeneric("stripTranscriptVersions")
+    }
+)
 
 
 
 #' @rdname camel
 #' @export
-setGeneric("upperCamel", function(object, ...) {
-    standardGeneric("upperCamel")
-})
+setGeneric(
+    "upperCamel",
+    function(object, ...) {
+        standardGeneric("upperCamel")
+    }
+)
