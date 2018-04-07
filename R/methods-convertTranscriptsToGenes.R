0a9ecde641bd6bdd73ce820ae551abc415abfa20
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-07 07:09:47 -0400

    Simplify methods

--- R/methods-convertTranscriptsToGenes.R
+++ R/methods-convertTranscriptsToGenes.R
@@ -38,131 +38,64 @@ NULL
 
 
 
-# Constructors =================================================================
-.convertTranscriptsToGenes.vec <- function(  # nolint
-    object,
-    tx2gene = NULL,
-    organism = NULL,
-    genomeBuild = NULL,
-    release = NULL
-) {
-    # Passthrough: genomeBuild, release
-    assert_is_character(object)
-    assert_all_are_non_missing_nor_empty_character(object)
-    assert_has_no_duplicates(object)
-    assert_is_any_of(tx2gene, c("data.frame", "NULL"))
-    assertIsAStringOrNULL(organism)
-    assertIsAnImplicitIntegerOrNULL(release)
-
-    # If no tx2gene is provided, fall back to using Ensembl annotations
-    if (!is.data.frame(tx2gene)) {
-        # Generate tx2gene from Ensembl
-        inform("Obtaining transcript-to-gene mappings from Ensembl")
-        if (is.null(organism)) {
-            organism <- detectOrganism(object, unique = TRUE)
-        } else if (is_a_string(organism)) {
-            organism <- detectOrganism(organism)
-        }
-        assert_is_a_string(organism)
-        tx2gene <- makeTx2geneFromEnsembl(
-            organism = organism,
-            genomeBuild = genomeBuild,
-            release = release
-        )
-    } else {
-        assertIsTx2gene(tx2gene)
-    }
-
-    tx2gene <- tx2gene %>%
-        .[object, , drop = FALSE] %>%
-        .[!is.na(.[["geneID"]]), , drop = FALSE]
-
-    geneID <- tx2gene[["geneID"]]
-    names(geneID) <- tx2gene[["txID"]]
-
-    if (!all(object %in% names(geneID))) {
-        abort(paste(
-            "Unmatched transcripts present.",
-            "Try using a GFF file instead."
-        ))
-    }
-
-    assert_is_character(geneID)
-    assert_has_names(geneID)
-
-    geneID[object]
-}
-
-
-
-.convertTranscriptsToGenes.dim <- function(  # nolint
-    object,
-    tx2gene = NULL,
-    organism = NULL,
-    genomeBuild = NULL,
-    release = NULL
-) {
-    # Passthrough: tx2gene, organism, genomeBuild, release
-    rownames <- rownames(object)
-    rownames <- convertTranscriptsToGenes(
-        object = rownames,
-        tx2gene = tx2gene,
-        organism = organism,
-        genomeBuild = genomeBuild,
-        release = release
-    )
-    rownames(object) <- rownames
-    object
-}
-
-
-
 # Methods ======================================================================
 #' @rdname convertTranscriptsToGenes
 setMethod(
     "convertTranscriptsToGenes",
     signature("character"),
-    .convertTranscriptsToGenes.vec
-)
+    function(
+        object,
+        tx2gene = NULL,
+        organism = NULL,
+        genomeBuild = NULL,
+        release = NULL
+    ) {
+        # Passthrough: genomeBuild, release
+        assert_is_character(object)
+        assert_all_are_non_missing_nor_empty_character(object)
+        assert_has_no_duplicates(object)
+        assert_is_any_of(tx2gene, c("data.frame", "NULL"))
+        assertIsAStringOrNULL(organism)
+        assertIsAnImplicitIntegerOrNULL(release)
 
+        # If no tx2gene is provided, fall back to using Ensembl annotations
+        if (!is.data.frame(tx2gene)) {
+            # Generate tx2gene from Ensembl
+            inform("Obtaining transcript-to-gene mappings from Ensembl")
+            if (is.null(organism)) {
+                organism <- detectOrganism(object, unique = TRUE)
+            } else if (is_a_string(organism)) {
+                organism <- detectOrganism(organism)
+            }
+            assert_is_a_string(organism)
+            tx2gene <- makeTx2geneFromEnsembl(
+                organism = organism,
+                genomeBuild = genomeBuild,
+                release = release
+            )
+        } else {
+            assertIsTx2gene(tx2gene)
+        }
 
+        tx2gene <- tx2gene %>%
+            .[object, , drop = FALSE] %>%
+            .[!is.na(.[["geneID"]]), , drop = FALSE]
 
-#' @rdname convertTranscriptsToGenes
-#' @export
-setMethod(
-    "convertTranscriptsToGenes",
-    signature("data.frame"),
-    .convertTranscriptsToGenes.dim
-)
+        geneID <- tx2gene[["geneID"]]
+        names(geneID) <- tx2gene[["txID"]]
 
+        if (!all(object %in% names(geneID))) {
+            abort(paste(
+                "Unmatched transcripts present.",
+                "Try using a GFF file instead."
+            ))
+        }
 
+        assert_is_character(geneID)
+        assert_has_names(geneID)
 
-#' @rdname convertTranscriptsToGenes
-#' @export
-setMethod(
-    "convertTranscriptsToGenes",
-    signature("DataFrame"),
-    .convertTranscriptsToGenes.dim
-)
-
-
-
-#' @rdname convertTranscriptsToGenes
-#' @export
-setMethod(
-    "convertTranscriptsToGenes",
-    signature("dgCMatrix"),
-    .convertTranscriptsToGenes.dim
-)
-
-
-
-#' @rdname convertTranscriptsToGenes
-#' @export
-setMethod(
-    "convertTranscriptsToGenes",
-    signature("dgTMatrix"),
-    .convertTranscriptsToGenes.dim
+        geneID[object]
+    }
 )
 
 
@@ -172,5 +105,49 @@ setMethod(
 setMethod(
     "convertTranscriptsToGenes",
     signature("matrix"),
-    .convertTranscriptsToGenes.dim
+    function(object, ...) {
+        rownames <- convertTranscriptsToGenes(rownames(object), ...)
+        rownames(object) <- rownames
+        object
+    }
+)
+
+
+
+#' @rdname convertTranscriptsToGenes
+#' @export
+setMethod(
+    "convertTranscriptsToGenes",
+    signature("data.frame"),
+    getMethod("convertTranscriptsToGenes", "matrix")
+)
+
+
+
+#' @rdname convertTranscriptsToGenes
+#' @export
+setMethod(
+    "convertTranscriptsToGenes",
+    signature("DataFrame"),
+    getMethod("convertTranscriptsToGenes", "data.frame")
+)
+
+
+
+#' @rdname convertTranscriptsToGenes
+#' @export
+setMethod(
+    "convertTranscriptsToGenes",
+    signature("dgCMatrix"),
+    getMethod("convertTranscriptsToGenes", "matrix")
+)
+
+
+
+#' @rdname convertTranscriptsToGenes
+#' @export
+setMethod(
+    "convertTranscriptsToGenes",
+    signature("dgTMatrix"),
+    getMethod("convertTranscriptsToGenes", "matrix")
 )
