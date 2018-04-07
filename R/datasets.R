1a8fd47b5103e3357a75816605b2b00fcb82a4f4
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-07 18:34:52 -0400

    Document rnaseqCounts and singleCellCounts

--- R/datasets.R
+++ R/datasets.R
@@ -1,7 +1,31 @@
-#' Gene Symbol Synonyms
+#' Example RNA-Seq Counts
+#'
+#' Generated with `DESeq2::makeExampleDESeqDataSet()`.
 #'
 #' @author Michael Steinbaugh
-#' @keywords internal
+#'
+#' @examples
+#' summary(rnaseqCounts)
+"rnaseqCounts"
+
+
+
+#' Example Single-Cell RNA-Seq Counts
+#'
+#' Modified version of `scater::sc_example_counts`, with dimnames formatted
+#' in camel case for package consistency.
+#'
+#' @examples
+#' summary(singleCellCounts)
+"singleCellCounts"
+
+
+
+#' Gene Name Synonyms
+#'
+#' A list of tibbles per organism containing gene name (a.k.a. symbol) synonyms.
+#'
+#' @author Michael Steinbaugh
 #'
 #' @examples
 #' names(synonyms)
