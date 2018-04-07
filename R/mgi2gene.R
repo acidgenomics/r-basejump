46679e07ba940bbffed027ca42f94ed20369f3a7
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-07 19:47:34 -0400

    Add HGNC and MGI to Ensembl mapping utility functions

---
+++ R/mgi2gene.R
@@ -0,0 +1,31 @@
+#' MGI to Ensembl Gene ID Mappings
+#'
+#' @family Gene Ontology Functions
+#'
+#' @return `data.frame`.
+#' @export
+#'
+#' @examples
+#' x <- mgi2gene()
+#' glimpse(x)
+mgi2gene <- function() {
+    raw <- read_tsv(
+        file = paste(
+            "http://www.informatics.jax.org",
+            "downloads",
+            "reports",
+            "MGI_Gene_Model_Coord.rpt",
+            sep = "/"
+        ),
+        # Note the use of `null` here
+        na = c("", "NA", "null"),
+        col_names = FALSE,
+        skip = 1L
+    )
+    data <- as.data.frame(raw[, c(1L, 11L)])
+    colnames(data) <- c("mgiID", "geneID")
+    data[["mgiID"]] <- as.integer(gsub("^MGI\\:", "", data[["mgiID"]]))
+    data <- data[order(data[["mgiID"]]), ]
+    rownames(data) <- data[["mgiID"]]
+    data
+}
