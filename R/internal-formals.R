11f82722184c8642029472b9479e82a2b501a92d
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-08 10:02:47 -0400

    Need to use `seq_along()`

--- R/internal-formals.R
+++ R/internal-formals.R
@@ -2,7 +2,7 @@
     fun = sys.function(sys.parent())
 ) {
     f <- formals(fun = fun)
-    for (i in seq_len(f)) {
+    for (i in seq_along(f)) {
         assign(x = names(f)[[i]], value = f[[i]])
     }
 }
