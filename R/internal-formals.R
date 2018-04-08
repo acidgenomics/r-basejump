8dd10ede0dc0adab460f63de8d95fc609d5586cc
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-08 10:40:36 -0400

    Add support for snake_case variant with matching formals

--- R/internal-formals.R
+++ R/internal-formals.R
@@ -1,7 +1,10 @@
-.assignArgs <- function(
+# Utility functions for providing snake case alias support
+
+.assignCamelArgs <- function(
     envir = parent.frame()
 ) {
     x <- as.list(envir)
+    x <- camel(x)
     for (i in seq_along(x)) {
         assign(
             x = names(x)[[i]],
@@ -12,13 +15,12 @@
     x
 }
 
-
-
-.assignFormals <- function(
+.assignCamelFormals <- function(
     fun = sys.function(sys.parent()),
     envir = parent.frame()
 ) {
     x <- formals(fun = fun)
+    x <- camel(x)
     for (i in seq_along(x)) {
         assign(
             x = names(x)[[i]],
