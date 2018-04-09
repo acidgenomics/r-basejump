7a1898f729ad84c1987127670d2a2650ba62edf6
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-08 20:05:48 -0400

    Add tibble coercion suport for GRanges

--- R/methods-coerce-tibble.R
+++ R/methods-coerce-tibble.R
@@ -29,9 +29,8 @@ NULL
     if (is_tibble(from)) {
         return(from)
     }
-    assert_has_dims(from)
-    assert_has_colnames(from)
     from <- as.data.frame(from)
+    assert_has_colnames(from)
     if (has_rownames(from)) {
         from <- rownames_to_column(from)
     }
@@ -44,7 +43,9 @@ NULL
 setAs(from = "matrix", to = "tbl_df", def = .tibble)
 setAs(from = "data.frame", to = "tbl_df", def = .tibble)
 setAs(from = "DataFrame", to = "tbl_df", def = .tibble)
+setAs(from = "GRanges", to = "tbl_df", def = .tibble)
 
 setAs(from = "matrix", to = "tibble", def = .tibble)
 setAs(from = "data.frame", to = "tibble", def = .tibble)
 setAs(from = "DataFrame", to = "tibble", def = .tibble)
+setAs(from = "GRanges", to = "tibble", def = .tibble)
