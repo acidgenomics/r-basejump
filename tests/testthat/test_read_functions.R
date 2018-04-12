45e92d1c3cba52a19a63f30d12f2bbea5f9c5222
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-12 12:44:26 -0400

    Update unit tests

--- tests/testthat/test_read_functions.R
+++ tests/testthat/test_read_functions.R
@@ -111,10 +111,10 @@ test_that("loadRemoteData", {
 })
 
 test_that("loadRemoteData : Already loaded", {
-    mtcars <- datasets::mtcars
+    example <- TRUE
     expect_error(
-        loadRemoteData(paste(cacheURL, "mtcars.rda", sep = "/")),
-        "Already exists in environment: mtcars"
+        loadRemoteData(paste(cacheURL, "example.rda", sep = "/")),
+        "Already exists in environment: example"
     )
 })
 
@@ -128,7 +128,10 @@ test_that("loadRemoteData : Invalid arguments", {
         "foobar.rda does not match '\\^http"
     )
     expect_error(
-        loadRemoteData(paste(cacheURL, "mtcars.rda", sep = "/"), envir = "XXX"),
+        loadRemoteData(
+            paste(cacheURL, "example.rda", sep = "/"),
+            envir = "XXX"
+        ),
         "is_environment : envir"
     )
 })
@@ -137,7 +140,7 @@ test_that("loadRemoteData : Invalid arguments", {
 
 # localOrRemoteFile ============================================================
 test_that("localOrRemoteFile : Vectorized", {
-    urls <- paste(cacheURL, c("mtcars.csv", "mtcars.rda"), sep = "/")
+    urls <- paste(cacheURL, c("example.csv", "example.rda"), sep = "/")
     files <- localOrRemoteFile(urls)
     expect_is(files, "character")
     expect_identical(basename(urls), basename(files))
@@ -154,7 +157,7 @@ test_that("localOrRemoteFile : Missing file", {
 
 # readFileByExtension ==========================================================
 test_that("readFileByExtension : Comma separated value file (.csv)", {
-    x <- readFileByExtension("mtcars.csv")
+    x <- readFileByExtension("example.csv")
     expect_is(x, "tbl_df")
 })
 
@@ -170,15 +173,14 @@ test_that("readFileByExtension : MatrixMarket file (.mtx)", {
 })
 
 test_that("readFileByExtension : Tab separated values file (.tsv)", {
-    tsv <- readFileByExtension("mtcars.tsv")
+    tsv <- readFileByExtension("example.tsv")
     expect_is(tsv, "tbl_df")
 })
 
 test_that("readFileByExtension : Table format file (.txt)", {
-    expect_warning(readFileByExtension("mtcars.txt"))
-    txt <- suppressWarnings(readFileByExtension("mtcars.txt"))
+    expect_warning(readFileByExtension("example.txt"))
+    txt <- suppressWarnings(readFileByExtension("example.txt"))
     expect_is(txt, "data.frame")
-    # txt has integer columns whereas mtcars doesn't
     expect_equal(txt, mtcars)
 })
 
@@ -191,7 +193,7 @@ test_that("readFileByExtension : Excel file (.xlsx)", {
 })
 
 test_that("readFileByExtension : Counts file (.counts)", {
-    x <- readFileByExtension("test.counts")
+    x <- readFileByExtension("example.counts")
     expect_is(x, "matrix")
     expect_identical(
         rownames(x)[1L:5L],
@@ -250,27 +252,6 @@ test_that("readGFF : Unsupported file type", {
 
 # readYAML =====================================================================
 test_that("readYAML : bcbio project summary", {
-    yaml <- readYAML("summary.yaml")
-    expect_identical(
-        class(yaml),
-        "list"
-    )
-    expect_identical(
-        names(yaml),
-        c("date", "upload", "bcbio_system", "samples")
-    )
-})
-
-test_that("readYAML : Unsupported file type", {
-    expect_error(
-        readYAML("mtcars.csv"),
-        "is_matching_regex : file"
-    )
-})
-
-test_that("readYAML : Missing file", {
-    expect_error(
-        readYAML("foobar.yaml"),
-        "is_existing_file :"
-    )
+    x <- readYAML("example.yaml")
+    expect_is(x, "list")
 })
