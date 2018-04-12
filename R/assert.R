da912f4b42e262d29d803df3492a5d756c0b15ba
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-12 15:40:26 -0400

    Migrate to bcbioBase

--- R/assert.R
+++ R/assert.R
@@ -114,59 +114,6 @@ assertAreTranscriptAnnotations <- function(
 
 
 
-#' Assert Formal Annotation Column
-#'
-#' @family Assert Check Functions
-#' @author Michael Steinbaugh
-#' @inherit assert
-#'
-#' @param colData Column data.
-#'
-#' @export
-#'
-#' @examples
-#' x <- data.frame(
-#'     "sample_1" = c(1L, 2L),
-#'     "sample_2" = c(3L, 4L),
-#'     row.names = c("gene_1", "gene_2"),
-#'     stringsAsFactors = FALSE
-#' )
-#' colData <- data.frame(
-#'     "genotype" = c("wt", "ko"),
-#'     row.names = c("sample_1", "sample_2"),
-#'     stringsAsFactors = TRUE
-#' )
-#' assertFormalAnnotationCol(x, colData)
-assertFormalAnnotationCol <- function(
-    x,
-    colData,
-    severity = getOption("assertive.severity", "stop")
-) {
-    assert_has_dimnames(x, severity = severity)
-    assert_is_any_of(
-        x = colData,
-        classes = c("data.frame", "DataFrame", "logical", "NULL"),
-        severity = severity
-    )
-    if (has_dims(colData)) {
-        assert_has_dimnames(colData, severity = severity)
-        assert_are_identical(
-            x = colnames(x),
-            y = rownames(colData),
-            severity = severity
-        )
-        # All columns must be factors
-        lapply(colData, function(x) {
-            assert_is_factor(x, severity = severity)
-        })
-    }
-    if (is.logical(colData)) {
-        assert_is_identical_to_na(colData, severity = severity)
-    }
-}
-
-
-
 #' Assert Formal Compression
 #'
 #' @family Assert Check Functions
--- tests/testthat/test_sanitization_functions.R
+++ tests/testthat/test_sanitization_functions.R
@@ -223,11 +223,16 @@ test_that("sanitizeRowData", {
 
 # sanitizeSampleData ===========================================================
 test_that("sanitizeSampleData", {
-    x <- coldata
-    x[["day"]] <- c(14L, 14L, 30L, 30L)
+    x <- DataFrame(
+        "genotype" = factor(c("wt", "ko", "wt", "ko")),
+        "batch" = factor(c(1L, 1L, 2L, 2L)),
+        # not a factor yet
+        "day" = c(14L, 14L, 30L, 30L),
+        row.names = c("sample_1", "sample_2", "sample_3", "sample_4")
+    )
     x <- sanitizeSampleData(x)
     expect_is(x, "DataFrame")
-    expect_identical(rownames(x), rownames(coldata))
+    expect_identical(rownames(x), rownames(cd))
     expect_true(all(vapply(
         X = x,
         FUN = is.factor,
