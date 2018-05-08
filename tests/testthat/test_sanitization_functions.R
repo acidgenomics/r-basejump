1c2ca38d60b17e9629a74fee748944cec8542421
Michael Steinbaugh <mike@steinbaugh.com>
2018-05-08 11:28:58 -0400

    Move `sanitizeSampleData()` to bcbioBase package

--- tests/testthat/test_sanitization_functions.R
+++ tests/testthat/test_sanitization_functions.R
@@ -235,17 +235,3 @@ test_that("sanitizeRowData", {
         )
     )
 })
-
-
-
-# sanitizeSampleData ===========================================================
-test_that("sanitizeSampleData", {
-    x <- sanitizeSampleData(sd)
-    expect_is(x, "DataFrame")
-    expect_identical(rownames(x), rownames(sd))
-    expect_true(all(vapply(
-        X = x,
-        FUN = is.factor,
-        FUN.VALUE = logical(1L)
-    )))
-})
