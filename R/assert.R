80d5bc8e88ab824c785ab1447ea65c328c15b640
Michael Steinbaugh <mike@steinbaugh.com>
2018-05-14 15:57:02 -0400

    Make `assertIsGFF()` defunct

--- R/assert.R
+++ R/assert.R
@@ -566,34 +566,6 @@ assertIsGene2symbol <- function(
 
 
 
-#' Assert Is GFF
-#'
-#' @family Assert Check Functions
-#' @author Michael Steinbaugh
-#' @inherit assert
-#'
-#' @param x `data.frame`.
-#'
-#' @export
-assertIsGFF <- function(
-    x,
-    severity = getOption("assertive.severity", "stop")
-) {
-    assert_is_data.frame(x, severity = severity)
-    assert_is_of_length(
-        x = colnames(x),
-        n = 9L,
-        severity = severity
-    )
-    assert_are_identical(
-        x = colnames(x),
-        y = gffCols,
-        severity = severity
-    )
-}
-
-
-
 #' Assert Is Hex Color Function or NULL
 #'
 #' @family Assert Check Functions
