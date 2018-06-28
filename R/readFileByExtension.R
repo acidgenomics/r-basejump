722d07a3b7409dcf773829ab1b1ec49407b9aee4
Michael Steinbaugh <mike@steinbaugh.com>
2018-06-28 19:29:08 -0400

    Attempt to simply internal code for `readFileByExtension()`, to rely more on `rio::import()`

--- R/readFileByExtension.R
+++ R/readFileByExtension.R
@@ -6,11 +6,11 @@
 #' - `gff`/`gff3`/`gtf`: General Feature Format.
 #' - `mtx`: MatrixMarket sparse matrix.
 #' - `rda`/`RData`: R Data. Must contain a single object. Doesn't require
-#'   internal object name to match, like in the [loadData()] function.
+#'   internal object name to match, like with the [loadData()] function.
 #' - `rds`: R Data Serialized.
 #' - `tsv` Tab Separated Values.
 #' - `xlsx`: Excel workbook.
-#' - `yaml`/`yml`: YAML.
+#' - `yml`: YAML.
 #'
 #' Also supports some additional extensions commonly used with the
 #' [bcbio](https://bcbio-nextgen.readthedocs.io) pipeline:
@@ -33,7 +33,7 @@
 #'
 #' @inheritParams general
 #'
-#' @return `tbl_df` by default, or a sparse matrix for `.mtx` files.
+#' @return Varies depending on the file extension.
 #' @export
 #'
 #' @seealso
@@ -42,131 +42,79 @@
 #' - [Matrix](https://cran.r-project.org/web/packages/Matrix/index.html).
 #'
 #' @examples
-#' # Comma Separated Values
-#' x <- readFileByExtension("http://basejump.seq.cloud/mtcars.csv")
-#' glimpse(x)
-#'
-#' # Counts Table (bcbio)
-#' x <- readFileByExtension("http://basejump.seq.cloud/example.counts")
-#' glimpse(x)
-#'
 #' # R Data
 #' x <- readFileByExtension("http://basejump.seq.cloud/rnaseqCounts.rda")
+#' glimpse(x)
+#'
+#' # Comma Separated Values
+#' x <- readFileByExtension("http://basejump.seq.cloud/mtcars.csv")
 #' glimpse(x)
 #'
 #' # Microsoft Excel Worksheet
 #' x <- readFileByExtension("http://basejump.seq.cloud/mtcars.xlsx")
 #' glimpse(x)
 #'
-#' # Text Table (ambiguous; not recommended)
-#' x <- suppressWarnings(
-#'     readFileByExtension("http://basejump.seq.cloud/example.txt")
-#' )
+#' # bcbio Counts Table
+#' x <- readFileByExtension("http://basejump.seq.cloud/example.counts")
 #' glimpse(x)
-readFileByExtension <- function(file, ...) {
+readFileByExtension <- function(file) {
     assert_is_a_string(file)
     file <- localOrRemoteFile(file)
-
-    ext <- str_match(basename(file), extPattern)[1L, 2L]
-    exti <- tolower(ext)  # case insensitive
-    unsupported <- paste("Unsupported extension", ":", ext)
-
-    # File import, based on extension
     message(paste("Reading", basename(file)))
 
-    # Sanitize NA values
-    na <- c("", "NA", "#N/A", "NULL", "null")
+    ext <- basename(file) %>%
+        str_match(extPattern) %>%
+        .[1L, 2L] %>%
+        tolower()
 
-    blacklist <- c(
-        "doc",
-        "docx",
-        "ppt",
-        "pptx"
-    )
-    source <- c(
-        "md",
-        "py",
-        "r",
-        "rmd",
-        "sh"
-    )
-    if (exti %in% blacklist) {
+    na <- c("", "NA", "#N/A", "NULL", "null")
+    blacklist <- c("doc", "docx", "ppt", "pptx", "txt")
+    source <- c("md", "py", "r", "rmd", "sh")
+    unsupported <- paste("Unsupported extension", ":", deparse(ext))
+
+    if (ext %in% blacklist) {
         stop(unsupported)  # nocov
-    } else if (exti %in% source) {
+    } else if (ext %in% source) {
         message("Importing as source code lines")
         data <- read_lines(file)
-    } else if (exti %in% c("colnames", "rownames")) {
-        data <- read_lines(file = file, na = na, ...)
-    } else if (exti == "counts") {
+    } else if (ext %in% c("colnames", "rownames")) {
+        data <- read_lines(file = file, na = na)
+    } else if (ext == "counts") {
         # bcbio counts output
         data <- read_tsv(
             file = file,
             na = na,
-            progress = FALSE,
-            ...
+            progress = FALSE
         ) %>%
             as.data.frame() %>%
             column_to_rownames("id") %>%
             as.matrix()
-    } else if (exti == "csv") {
-        # Comma separated values
-        data <- readr::read_csv(
-            file = file,
-            na = na,
-            progress = FALSE,
-            ...
-        )
-    } else if (exti %in% c("gff", "gff3", "gtf")) {
+    } else if (ext %in% c("gff", "gff3", "gtf")) {
         data <- suppressMessages(readGFF(file))
-    } else if (exti == "mtx") {
+    } else if (ext == "mtx") {
         # MatrixMarket
         # Require `.rownames` and `.colnames` files
-        data <- readMM(file = file, ...)
+        data <- readMM(file = file)
         rownames <- localOrRemoteFile(paste(file, "rownames", sep = ".")) %>%
             read_lines(na = na)
         colnames <- localOrRemoteFile(paste(file, "colnames", sep = ".")) %>%
             read_lines(na = na)
         rownames(data) <- rownames
         colnames(data) <- colnames
-    } else if (exti %in% c("rda", "rdata")) {
+    } else if (ext %in% c("rda", "rdata")) {
         safe <- new.env()
         object <- load(file, envir = safe)
         if (length(safe) != 1L) {
-            stop("File does not contain a single object")
+            stop("File does not contain a single object.")
         }
         data <- get(object, envir = safe, inherits = FALSE)
-    } else if (exti == "rds") {
+    } else if (ext == "rds") {
         data <- readRDS(file)
-    } else if (exti == "tsv") {
-        # Tab separated values
-        data <- read_tsv(file = file, na = na, progress = FALSE, ...)
-    } else if (exti == "txt") {
-        # Table?
-        warning(paste(
-            paste(deparse(ext), "is ambiguous and not recommended."),
-            "Assuming table output from `utils::write.table()`.",
-            sep = "\n"
-        ))
-        data <- read.table(
-            file = file,
-            header = TRUE,
-            na.strings = na,
-            ...
-        )
-    } else if (exti == "xlsx") {
-        # Excel workbook
-        data <- read_excel(path = file, na = na, ...)
-    } else if (exti %in% c("yaml", "yml")) {
-        data <- suppressMessages(readYAML(file))
     } else if (requireNamespace("rio", quietly = TRUE)) {
-        # nocov start
         message(paste(
-            paste(deparse(ext), "isn't natively supported."),
-            "Attempting to read using `rio::import()`.",
-            sep = "\n"
+            "Using `rio::import()` to read", deparse(ext), "file extension"
         ))
         data <- rio::import(file)
-        # nocov end
     } else {
         # nocov start
         stop(paste(
@@ -175,11 +123,6 @@ readFileByExtension <- function(file, ...) {
             sep = "\n"
         ))
         # nocov end
-    }
-
-    # Remove all NA rows/columns for data frames
-    if (is.data.frame(data)) {
-        data <- removeNA(data)
     }
 
     data
