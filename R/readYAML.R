c4cbcd1022aec2ad38865c8d05e63f893b7d1e34
Michael Steinbaugh <mike@steinbaugh.com>
2018-06-29 08:18:47 -0400

    Fix NAMESPACE imports for `read*()` family of functions

--- R/readYAML.R
+++ R/readYAML.R
@@ -12,10 +12,9 @@
 #' x <- readYAML("http://basejump.seq.cloud/example.yml")
 #' names(x)
 readYAML <- function(file) {
-    requireNamespace("yaml")
     assert_is_a_string(file)
     assert_all_are_matching_regex(file, "\\.ya?ml$")
     message(paste("Reading", basename(file)))
     file <- localOrRemoteFile(file)
-    yaml::yaml.load_file(file)
+    yaml.load_file(file)
 }
