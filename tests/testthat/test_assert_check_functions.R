3adc286d46cb97c2ecb17dc5b295bd3ed7feff9e
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-10 23:02:39 -0400

    Update unit tests

--- tests/testthat/test_assert_check_functions.R
+++ tests/testthat/test_assert_check_functions.R
@@ -53,6 +53,43 @@ test_that("assertFormalGene2symbol", {
 
 
 
+test_that("assertFormalInterestingGroups", {
+    expect_silent(
+        assertFormalInterestingGroups(
+            x = rse_bcb,
+            interestingGroups = c("tissue", "treatment")
+        )
+    )
+    # Must exist as columns in sampleData
+    expect_error(
+        assertFormalInterestingGroups(
+            x = rse_bcb,
+            interestingGroups = "XXX"
+        ),
+        paste(
+            "The interesting groups \"XXX\" are not defined"
+        )
+    )
+    # Require interesting groups to be defined as factor columns
+    expect_error(
+        assertFormalInterestingGroups(
+            x = rse_bcb,
+            interestingGroups = c("totalReads", "exonicRate")
+        ),
+        "The interesting groups \"totalReads, exonicRate\" are not factor"
+    )
+    # Error on blacklisted columns
+    expect_error(
+        assertFormalInterestingGroups(
+            x = rse_bcb,
+            interestingGroups = "genomeBuild"
+        ),
+        "The interesting groups \"genomeBuild\" are blacklisted."
+    )
+})
+
+
+
 test_that("assertHasRownames", {
     expect_silent(assertHasRownames(df))
     rownames(df) <- NULL
