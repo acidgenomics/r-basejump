b0e68f27edfa90ed94c9c8a24d975e951f570927
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-10 23:02:39 -0400

    Update unit tests

--- tests/testthat/test_sanitization_functions.R
+++ tests/testthat/test_sanitization_functions.R
@@ -235,3 +235,24 @@ test_that("sanitizeRowData", {
         )
     )
 })
+
+
+
+# sanitizeSampleData ===========================================================
+test_that("sanitizeSampleData", {
+    sd <- DataFrame(
+        "genotype" = factor(c("wt", "ko", "wt", "ko")),
+        "batch" = factor(c(1L, 1L, 2L, 2L)),
+        # not a factor yet
+        "day" = c(14L, 14L, 30L, 30L),
+        row.names = c("sample_1", "sample_2", "sample_3", "sample_4")
+    )
+    x <- sanitizeSampleData(sd)
+    expect_is(x, "DataFrame")
+    expect_identical(rownames(x), rownames(sd))
+    expect_true(all(vapply(
+        X = x,
+        FUN = is.factor,
+        FUN.VALUE = logical(1L)
+    )))
+})
