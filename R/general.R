dbf30d7b9c7c144129f4b66dd5750c8a272ad1b2
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-12 12:09:43 -0400

    Improve documentation

--- R/general.R
+++ R/general.R
@@ -11,13 +11,15 @@
 #' @param y Secondary object.
 #' @param ... Additional arguments.
 #'
-#' @param dir Output directory. Defaults to the current working directory.
-#' @param envir Environment to use for assignment. Defaults to `parent.frame()`,
-#'   which will assign into the calling environment.
-#' @param file File path.
-#' @param headerLevel Markdown header level.
+#' @param dir `string`. Directory path. Defaults to the current working
+#'   directory.
+#' @param envir `environment` to use for assignment. Defaults to
+#'   `parent.frame()`, which will assign into the calling environment.
+#' @param file `string`. File path.
+#' @param headerLevel `scalar integer`. Markdown header level (1-7).
 #' @param interestingGroups `character`. Groups of interest.
-#' @param organism Full latin organism name (e.g. "`Homo sapiens`").
+#' @param organism `character`. Full latin organism name
+#'   (e.g. "`Homo sapiens`").
 #' @param plotlist `list` containing `ggplot` objects.
 #' @param return Object class to return. Uses [match.arg()] internally and picks
 #'   the first item in the vector by default.
