6fe6b63aa79233f61ae2308e76faf36f95ef6ae2
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-16 16:13:27 -0400

    Break out unit tests

--- tests/testthat/test_gene_annotation_functions.R
+++ tests/testthat/test_gene_annotation_functions.R
@@ -396,37 +396,6 @@ test_that("makeGene2symbolFromEnsembl", {
 
 
 
-# makeGene2symbolFromGFF =======================================================
-test_that("makeGene2symbolFromGFF : Mus musculus", {
-    x <- makeGene2symbolFromGFF("mmusculus.gtf")
-    expect_identical(dim(x), c(17L, 2L))
-    expect_identical(
-        head(x, 2L),
-        data.frame(
-            "geneID" = c("ENSMUSG00000025900", "ENSMUSG00000051951"),
-            "geneName" = c("Rp1", "Xkr4"),
-            row.names = c("ENSMUSG00000025900", "ENSMUSG00000051951"),
-            stringsAsFactors = FALSE
-        )
-    )
-})
-
-test_that("makeGene2symbolFromGFF : Drosophila melanogaster", {
-    x <- makeGene2symbolFromGFF("dmelanogaster.gtf")
-    expect_identical(dim(x), c(5L, 2L))
-    expect_identical(
-        head(x, 2L),
-        data.frame(
-            "geneID" = c("FBgn0031081", "FBgn0031085"),
-            "geneName" = c("Nep3", "CG9570"),
-            row.names = c("FBgn0031081", "FBgn0031085"),
-            stringsAsFactors = FALSE
-        )
-    )
-})
-
-
-
 # makeGRangesFromEnsembl =======================================================
 test_that("makeGRangesFromEnsembl : genes", {
     x <- makeGRangesFromEnsembl(
@@ -547,55 +516,6 @@ test_that("makeGRangesFromEnsembl : metadata", {
 
 
 
-# makeGRangesFromGFF ===========================================================
-test_that("makeGRangesFromGFF : genes", {
-    # Expected warning about `phase` metadata column
-    x <- suppressWarnings(
-        makeGRangesFromGFF("mmusculus.gtf", format = "genes")
-    )
-    expect_identical(length(x), 17L)
-    expect_identical(names(x)[[1L]], "ENSMUSG00000025900")
-    expect_identical(
-        colnames(mcols(x)),
-        c(
-            "geneID",
-            "geneName",
-            "geneBiotype",
-            "geneVersion",
-            "geneSource",
-            "broadClass"
-        )
-    )
-})
-
-test_that("makeGRangesFromGFF : transcripts", {
-    # Expected warning about `phase` metadata column
-    x <- suppressWarnings(
-        makeGRangesFromGFF("mmusculus.gtf", format = "transcripts")
-    )
-    expect_identical(length(x), 20L)
-    expect_identical(names(x)[[1L]], "ENSMUST00000070533")
-    expect_identical(
-        colnames(mcols(x)),
-        c(
-            "transcriptID",
-            "transcriptName",
-            "transcriptBiotype",
-            "geneID",
-            "geneName",
-            "geneBiotype",
-            "geneVersion",
-            "geneSource",
-            "transcriptVersion",
-            "transcriptSource",
-            "transcriptSupportLevel",
-            "broadClass"
-        )
-    )
-})
-
-
-
 # makeTx2geneFromEnsembl =======================================================
 test_that("makeTx2geneFromEnsembl", {
     x <- makeTx2geneFromEnsembl(
@@ -604,59 +524,6 @@ test_that("makeTx2geneFromEnsembl", {
     )
     expect_identical(colnames(x), c("transcriptID", "geneID"))
     expect_identical(nrow(x), 216741L)
-})
-
-
-
-# makeTx2geneFromGFF ===========================================================
-test_that("makeTx2geneFromGFF : Drosophila melanogaster", {
-    x <- makeTx2geneFromGFF("dmelanogaster.gtf")
-    expect_identical(dim(x), c(7L, 2L))
-    expect_identical(
-        head(x, 2L),
-        data.frame(
-            transcriptID = c("FBtr0070000", "FBtr0070001"),
-            geneID = c("FBgn0031081", "FBgn0052826"),
-            row.names = c("FBtr0070000", "FBtr0070001"),
-            stringsAsFactors = FALSE
-        )
-    )
-})
-
-test_that("makeTx2geneFromGFF : Mus musculus", {
-    x <- makeTx2geneFromGFF("mmusculus.gtf")
-    expect_identical(dim(x), c(20L, 2L))
-    expect_identical(
-        head(x, 2L),
-        data.frame(
-            transcriptID = c("ENSMUST00000070533", "ENSMUST00000082908"),
-            geneID = c("ENSMUSG00000051951", "ENSMUSG00000064842"),
-            row.names = c("ENSMUST00000070533", "ENSMUST00000082908"),
-            stringsAsFactors = FALSE
-        )
-    )
-    expect_message(
-        makeTx2geneFromGFF("mmusculus.gtf"),
-        "tx2gene mappings: 20 transcripts, 17 genes"
-    )
-})
-
-
-
-# panther ======================================================================
-test_that("panther", {
-    organisms <- c(
-        "Homo sapiens",
-        "Mus musculus",
-        "Drosophila melanogaster",
-        "Caenorhabditis elegans"
-    )
-    list <- lapply(organisms, function(organism) {
-        invisible(capture.output(
-            x <- panther(organism)
-        ))
-        expect_is(x, "data.frame")
-    })
 })
 
 
