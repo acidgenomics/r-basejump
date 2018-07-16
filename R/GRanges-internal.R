9a5e0cc2281bec3a658c4552d84075b736f427a6
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-16 16:08:12 -0400

    Simplify internal mcols sorting, using alphabetical

--- R/GRanges-internal.R
+++ R/GRanges-internal.R
@@ -2,37 +2,55 @@
     assert_is_all_of(object, "GRanges")
     assert_has_names(object)
 
-    # Sanitize mcols to camel case
-    object <- camel(object)
-
-    # Use `transcript` instead of `tx` consistently
-    colnames(mcols(object)) <- gsub(
-        pattern = "^tx",
-        replacement = "transcript",
-        x = colnames(mcols(object))
-    )
-
     # Ensure GRanges is sorted by names
+    message("Sorting GRanges by identifier")
     object <- object[sort(names(object))]
 
     # Standardize the metadata columns
     message("Standardizing the metadata columns")
     mcols <- mcols(object)
+    # Sanitize to camel case
+    mcols <- camel(mcols)
+    # Ensure "ID" is always capitalized (e.g. "entrezid")
+    colnames(mcols) <- gsub("id$", "ID", colnames(mcols))
+    # Use `transcript` instead of `tx` consistently
+    colnames(mcols) <- gsub(
+        pattern = "^tx",
+        replacement = "transcript",
+        x = colnames(mcols)
+    )
     # Remove columns that are all NA
     mcols <- removeNA(mcols)
-    # Rename the columns
-    colnames(mcols) <- colnames(mcols) %>%
-        camel() %>%
-        # Ensure "ID" is capitalized (e.g. entrezid)
-        gsub("id$", "ID", .)
 
-    # Always use geneName instead of symbol
+    # Missing `geneName`
+    if (!"geneName" %in% colnames(mcols)) {
+        warning(paste(
+            "`geneName` is missing.",
+            "Using `geneID` in place."
+        ))
+        assert_is_subset("geneID", colnames(mcols))
+        mcols[["geneName"]] <- mcols[["geneID"]]
+    }
+
+    # Missing `transcriptName`
+    if (
+        "transcriptID" %in% colnames(mcols) &&
+        !"transcriptName" %in% colnames(mcols)
+    ) {
+        warning(paste(
+            "`transcriptName` is missing.",
+            "Using `transcriptID` in place."
+        ))
+        mcols[["transcriptName"]] <- mcols[["transcriptID"]]
+    }
+
+    # Always use `geneName` instead of `symbol`
     if (all(c("geneName", "symbol") %in% colnames(mcols))) {
-        message("Using geneName instead of symbol")
+        message("Using `geneName` instead of `symbol`")
         mcols[["symbol"]] <- NULL
     } else if ("symbol" %in% colnames(mcols)) {
         # nocov start
-        message("Renaming symbol to geneName")
+        message("Renaming `symbol` to `geneName`")
         mcols[["geneName"]] <- mcols[["symbol"]]
         mcols[["symbol"]] <- NULL
         # nocov end
@@ -40,34 +58,38 @@
 
     # Set strings as factors
     if (is.character(mcols[["geneBiotype"]])) {
-        message("Setting geneBiotype as factor")
+        message("Setting `geneBiotype` as factor")
         mcols[["geneBiotype"]] <- as.factor(mcols[["geneBiotype"]])
     }
     if (is.character(mcols[["seqCoordSystem"]])) {
-        message("Setting seqCoordSystem as factor")
+        message("Setting `seqCoordSystem` as factor")
         mcols[["seqCoordSystem"]] <- as.factor(mcols[["seqCoordSystem"]])
     }
     if (is.character(mcols[["txBiotype"]])) {
-        message("Setting txBiotype as factor")
+        message("Setting `txBiotype` as factor")
         mcols[["txBiotype"]] <- as.factor(mcols[["txBiotype"]])
     }
     if (is.integer(mcols[["txSupportLevel"]])) {
-        message("Setting txSupportLevel as factor")
+        message("Setting `txSupportLevel` as factor")
         mcols[["txSupportLevel"]] <- as.factor(mcols[["txSupportLevel"]])
     }
 
-    # Put the priority columns first
+    # Require that names match the identifier column
+    # Check `transcriptID` then `geneID`
     assert_are_intersecting_sets(annotationCols, colnames(mcols))
-    priorityCols <- intersect(annotationCols, colnames(mcols))
-    mcols <- mcols %>%
-        .[, unique(c(priorityCols, colnames(.))), drop = FALSE]
+    if ("transcriptID" %in% colnames(mcols)) {
+        idCol <- "transcriptID"
+    } else {
+        idCol <- "geneID"
+    }
+    assert_are_identical(names(object), mcols[[idCol]])
     mcols(object) <- mcols
 
-    # Require that the first column contains the names (e.g. `geneID`)
-    assert_are_identical(names(object), mcols(object)[[1L]])
+    # Ensure broad class definitions are included
+    mcols[["broadClass"]] <- broadClass(object)
 
-    # Add broad class definitions
-    mcols(object)[["broadClass"]] <- broadClass(object)
+    # Sort metadata columns alphabetically
+    mcols(object) <- mcols(object)[, sort(colnames(mcols(object)))]
 
     assert_is_all_of(object, "GRanges")
     object
