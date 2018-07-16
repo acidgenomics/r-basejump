fca673774ec77394d4f5a4d76047d31e80875526
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-16 08:41:23 -0400

    draft update

--- R/broadClass-methods.R
+++ R/broadClass-methods.R
@@ -55,7 +55,7 @@ setMethod(
             biotypeCol <- biotypeCol[[1L]]
             biotype <- object[[biotypeCol]]
         } else {
-            warning("biotype missing")
+            warning("biotype missing", call. = FALSE)
             biotype <- NA
         }
 
@@ -70,7 +70,7 @@ setMethod(
             seqnameCol <- seqnameCol[[1L]]
             seqname <- object[[seqnameCol]]
         } else {
-            warning("seqname missing")
+            warning("seqname missing", call. = FALSE)
             seqname <- NA
         }
 
