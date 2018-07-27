6909ca64f3eb31c82c8f006541a2a12fc2c1b761
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-27 08:14:11 -0400

    Improve check for deprecated `clean` and `return` arguments

--- R/sampleData-methods.R
+++ R/sampleData-methods.R
@@ -18,9 +18,6 @@
 #'
 #' @inheritParams general
 #'
-#' @param clean `boolean`. Deprecated argument, providing only factor column
-#'   return.
-#'
 #' @return `DataFrame` containing metadata that describes the samples.
 #'
 #' @examples
@@ -42,16 +39,27 @@ NULL
 setMethod(
     "sampleData",
     signature("SummarizedExperiment"),
-    function(object, clean = FALSE) {
+    function(object, ...) {
+        # Legacy arguments =========================================================
         # nocov start
-        if (isTRUE(clean)) {
+        call <- match.call(expand.dots = TRUE)
+        # clean
+        if (isTRUE(call[["clean"]])) {
             warning(paste(
-                "`clean = TRUE` argument is deprecated for",
-                "`SummarizedExperiment`. `bcbioRNASeq` method support is",
-                "provided in the upcoming release."
+                "`clean` argument is deprecated for `SummarizedExperiment`.",
+                "Improved `bcbioRNASeq` method is provided in v0.2.6."
+            ))
+        }
+        # return
+        if ("return" %in% names(call)) {
+            stop(paste(
+                "`return` argument is defunct.",
+                "Use a separation coercion call after the return",
+                "(e.g. `as.data.frame()`)."
             ))
         }
         # nocov end
+
         colData(object)
     }
 )
