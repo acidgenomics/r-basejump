40b0d24648a1ad55a4581c28123104c97cff0af3
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-01 06:30:37 -0400

    Migrate `separatorBar` and `updateMessage` from bcbioBase

--- R/globals.R
+++ R/globals.R
@@ -1,5 +1,7 @@
 globalVariables(".")
 
+
+
 # Note optional matching of gzip
 extPattern <- "\\.([a-zA-Z0-9]+)(\\.gz)?$"
 
@@ -7,3 +9,22 @@ extPattern <- "\\.([a-zA-Z0-9]+)(\\.gz)?$"
 rdataExtPattern <- "\\.(rd[a|ata|s])$"
 
 rdataError <- "R data files must contain `.rda`, `.rds`, or `.RData` extension."
+
+
+
+#' Separator Bar
+#' @export
+#' @examples
+#' separatorBar
+separatorBar <- paste0(
+    rep(x = "=", times = options("width")),
+    collapse = ""
+)
+
+
+
+#' Update Message
+#' @export
+#' @examples
+#' updateMessage
+updateMessage <- "Run `updateObject()` to update your object"
