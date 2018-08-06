77d45caad4b2bd32aa554b91595da61ccfa68836
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-06 07:08:05 -0400

    Update unit tests

--- tests/testthat/test_data_functions.R
+++ tests/testthat/test_data_functions.R
@@ -111,7 +111,39 @@ test_that("interestingGroups : Assignment method", {
 
 
 
-# makeSummarizedExperiment ==================================================
+# makeSummarizedExperiment =====================================================
+genes <- paste0("gene", seq_len(4L))
+samples <- paste0("sample", seq_len(4L))
+
+mat <- matrix(
+    data = seq(1L:16L),
+    nrow = 4L,
+    ncol = 4L,
+    byrow = FALSE,
+    dimnames = list(genes, samples)
+)
+dgc <- as(mat, "dgCMatrix")
+
+rr <- GRanges(
+    seqnames = replicate(n = 4L, expr = "1"),
+    ranges = IRanges(
+        start = c(1L, 101L, 201L, 301L),
+        end = c(100L, 200L, 300L, 400L)
+    )
+)
+names(rr) <- genes
+
+cd <- data.frame(
+    genotype = c(
+        "wildtype",
+        "wildtype",
+        "knockout",
+        "knockout"
+    ),
+    age = c(3L, 6L, 3L, 6L),
+    row.names = samples
+)
+
 test_that("makeSummarizedExperiment : RangedSummarizedExperiment", {
     rse <- makeSummarizedExperiment(
         assays = list(counts = mat),
