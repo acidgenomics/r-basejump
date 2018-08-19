66efa3af1f04ac2825b07f5eb644947be5bf2e29
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-19 13:30:27 -0400

    Simplify internal code to just check for R 3.5 instead of R 3.5.1

--- R/cleanSystemLibrary.R
+++ R/cleanSystemLibrary.R
@@ -6,6 +6,7 @@
 #' @author Michael Steinbaugh
 #'
 #' @return `logical`. Is system library clean?
+#' @export
 #'
 #' @examples
 #' cleanSystemLibrary()
@@ -19,17 +20,20 @@ cleanSystemLibrary <- function() {
     syslib <- unique(base[, "LibPath"])
     stopifnot(length(syslib) == 1)
 
-    # Now let's check for outdated packages in the system library
+    # Subset packages in the system library
     system <- x[which(x[, "LibPath"] == syslib), ]
-    rversion <- getRversion()
-    outdated <- system[which(system[, "Built"] != rversion), ]
-    if (length(outdated)) {
-        warning(paste(
-            "Build mismatch in system library:",
-            toString(rownames(outdated))
-        ))
-        FALSE
-    } else {
-        TRUE
-    }
+    stopifnot(!any(is.na(system[, "Priority"])))
+
+    # Check for packages built against a different point release
+    # e.g. 3.5.1
+    version <- getRversion()
+    stopifnot(grepl("^\\d\\.\\d\\.\\d$", version))
+    # e.g. 3.5
+    version <- gsub("\\.\\d$", "", version)
+    stopifnot(all(grepl(
+        pattern = paste0("^", version),
+        x = system[, "Built"]
+    )))
+
+    TRUE
 }
