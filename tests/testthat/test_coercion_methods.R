eb2945493ff07fa22aac2e6c862262a95e5c6b73
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-23 06:57:11 -0400

    Fix `tbl_df` assert check

--- tests/testthat/test_coercion_methods.R
+++ tests/testthat/test_coercion_methods.R
@@ -8,14 +8,7 @@ test_that("tbl_df", {
     expect_is(x, "tbl_df")
 
     # Expect that rownames are automatically moved to first column.
-    expect_identical(
-        colnames(x)[[1L]],
-        "rowname"
-    )
-
-    # Also support use of `tibble` instead of `tbl_df`.
-    y <- as(df, "tibble")
-    expect_identical(x, y)
+    expect_identical(colnames(x)[[1L]], "rowname")
 
     # Early return if already tibble.
     x <- tibble()
