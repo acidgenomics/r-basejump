ba27852638a7ef36c2438026f9b1fdaead2051bd
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-26 10:23:46 -0400

    Improve `makeNames` unit tests

--- tests/testthat/test_make_names_functions.R
+++ tests/testthat/test_make_names_functions.R
@@ -9,15 +9,15 @@ funs <- list(
 
 
 
-# ANY ==========================================================================
-test_that("makeNames : ANY", {
-    # Atomic
+# atomic =======================================================================
+test_that("makeNames : atomic", {
+    # Unnamed
     x <- 1L
     lapply(funs, function(fun) {
         expect_identical(fun(x), x)
     })
 
-    # Named atomic
+    # Named
     x <- c("hello.world" = 1L)
     expect <- c(
         "helloWorld",
@@ -27,41 +27,6 @@ test_that("makeNames : ANY", {
     )
     lapply(seq_along(funs), function(i) {
         expect_identical(names(funs[[i]](x)), expect[[i]])
-    })
-
-    # Sparse matrix (dimnames)
-    x <- Matrix(
-        data = 1L:4L,
-        nrow = 2L,
-        ncol = 2L,
-        dimnames = list(
-            c("gene.id.1", "gene.id.2"),
-            c("sample.id.1", "sample.id.2")
-        )
-    )
-    expect <- list(
-        camel = list(
-            c("geneID1", "geneID2"),
-            c("sampleID1", "sampleID2")
-        ),
-        dotted = list(
-            c("gene.ID.1", "gene.ID.2"),
-            c("sample.ID.1", "sample.ID.2")
-        ),
-        snake = list(
-            c("gene_id_1", "gene_id_2"),
-            c("sample_id_1", "sample_id_2")
-        ),
-        upperCamel = list(
-            c("GeneID1", "GeneID2"),
-            c("SampleID1", "SampleID2")
-        )
-    )
-    lapply(seq_along(funs), function(i) {
-        expect_identical(
-            dimnames(funs[[i]](x, rownames = TRUE, colnames = TRUE)),
-            expect[[i]]
-        )
     })
 })
 
@@ -129,7 +94,7 @@ test_that("makeNames : character", {
     })
 })
 
-test_that("makeNames : character : named", {
+test_that("makeNames : character (named)", {
     x <- mn[["namedCharacter"]]
     expect <- list(
         camel = c(itemA = "helloWorld", itemB = "helloWORLD"),
@@ -142,48 +107,54 @@ test_that("makeNames : character : named", {
     })
 })
 
-test_that("makeNames : character : camel-specific", {
-    # Delimited numbers in strings
-    expect_identical(camel("2018-01-01"), "x2018x01x01")
-    expect_identical(camel("0.01"), "x0x01")
-    expect_identical(camel("1,000,000"), "x1x000x000")
-    expect_identical(upperCamel("2018-01-01"), "X2018x01x01")
-    expect_identical(upperCamel("0.01"), "X0x01")
-    expect_identical(upperCamel("1,000,000"), "X1x000x000")
-
-    # Strict mode
-    x <- mn[["character"]]
+test_that("makeNames : character : Delimited numbers", {
     expect_identical(
-        camel(x, strict = TRUE),
-        c(
-            "helloWorld",
-            "helloWorld",
-            "rnaiClones",
-            "nCount",
-            "tx2gene",
-            "tx2GeneId",
-            "g2mScore",
-            "worfdbHtmlRemap",
-            "mazdaRx4",
-            "x123",
-            NA
-        )
+        camel("1,000,000", strict = FALSE),
+        "x1000000"
     )
     expect_identical(
-        upperCamel(x, strict = TRUE),
-        c(
-            "HelloWorld",
-            "HelloWorld",
-            "RnaiClones",
-            "NCount",
-            "Tx2gene",
-            "Tx2GeneId",
-            "G2mScore",
-            "WorfdbHtmlRemap",
-            "MazdaRx4",
-            "X123",
-            NA
-        )
+        camel("1,000,000", strict = TRUE),
+        "x1000000"
+    )
+
+    expect_identical(
+        camel("2018-01-01", strict = FALSE),
+        "x2018.01.01"
+    )
+    expect_identical(
+        camel("2018-01-01", strict = TRUE),
+        "x2018x01x01"
+    )
+
+    expect_identical(
+        camel("0.01", strict = FALSE),
+        "x0.01"
+    )
+    expect_identical(
+        camel("0.01", strict = TRUE),
+        "x0x01"
+    )
+
+    expect_identical(
+        camel("res.0.1", strict = FALSE),
+        "res0.1"
+    )
+    expect_identical(
+        camel("res.0.1", strict = TRUE),
+        "res0x1"
+    )
+
+    expect_identical(
+        upperCamel("1,000,000"),
+        "X1000000"
+    )
+    expect_identical(
+        upperCamel("2018-01-01", strict = TRUE),
+        "X2018x01x01"
+    )
+    expect_identical(
+        upperCamel("0.01", strict = TRUE),
+        "X0x01"
     )
 })
 
@@ -244,7 +215,7 @@ test_that("makeNames : matrix", {
 
 
 # GRanges ======================================================================
-test_that("camel : GRanges", {
+test_that("makeNames : GRanges", {
     # gr object is already camel formatted!
     expect <- list(
         camel = c(
@@ -295,7 +266,7 @@ test_that("camel : GRanges", {
 
 
 # list =========================================================================
-test_that("camel : list", {
+test_that("makeNames : list", {
     x <- mn[["list"]]
     expect <- list(
         camel = c("itemA", "itemB"),
@@ -306,4 +277,82 @@ test_that("camel : list", {
     lapply(seq_along(funs), function(i) {
         expect_identical(names(funs[[i]](x)), expect[[i]])
     })
+})
+
+
+
+# ANY ==========================================================================
+test_that("makeNames : ANY (e.g. Matrix)", {
+    x <- Matrix(
+        data = 1L:4L,
+        nrow = 2L,
+        ncol = 2L,
+        dimnames = list(
+            c("gene.id.1", "gene.id.2"),
+            c("sample.id.1", "sample.id.2")
+        )
+    )
+    expect <- list(
+        camel = list(
+            c("geneID1", "geneID2"),
+            c("sampleID1", "sampleID2")
+        ),
+        dotted = list(
+            c("gene.ID.1", "gene.ID.2"),
+            c("sample.ID.1", "sample.ID.2")
+        ),
+        snake = list(
+            c("gene_id_1", "gene_id_2"),
+            c("sample_id_1", "sample_id_2")
+        ),
+        upperCamel = list(
+            c("GeneID1", "GeneID2"),
+            c("SampleID1", "SampleID2")
+        )
+    )
+    lapply(seq_along(funs), function(i) {
+        expect_identical(
+            dimnames(funs[[i]](x, rownames = TRUE, colnames = TRUE)),
+            expect[[i]]
+        )
+    })
+})
+
+
+
+# camel-specific ===============================================================
+test_that("camel : Strict mode", {
+    x <- mn[["character"]]
+    expect_identical(
+        camel(x, strict = TRUE),
+        c(
+            "helloWorld",
+            "helloWorld",
+            "rnaiClones",
+            "nCount",
+            "tx2gene",
+            "tx2geneId",
+            "g2mScore",
+            "worfdbHtmlRemap",
+            "mazdaRx4",
+            "x123",
+            NA
+        )
+    )
+    expect_identical(
+        upperCamel(x, strict = TRUE),
+        c(
+            "HelloWorld",
+            "HelloWorld",
+            "RnaiClones",
+            "NCount",
+            "Tx2gene",
+            "Tx2geneId",
+            "G2mScore",
+            "WorfdbHtmlRemap",
+            "MazdaRx4",
+            "X123",
+            NA
+        )
+    )
 })
