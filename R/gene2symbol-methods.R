2e0ddc3c95a75e6b76a10e1beef58eb3ebd00cb6
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-08-28 13:48:21 -0400

    Inform the user if `gene2symbol()` is sanitizing symbols

--- R/gene2symbol-methods.R
+++ R/gene2symbol-methods.R
@@ -1,5 +1,8 @@
 #' Gene to Symbol Mappings
 #'
+#' @note This function will make any duplicated symbols unique by applying
+#' [base::make.unique()], which will add ".1" to the end of the gene name.
+#'
 #' @name gene2symbol
 #' @family Data Functions
 #' @author Michael Steinbaugh
@@ -29,10 +32,21 @@ setMethod(
             warning("Object does not contain gene-to-symbol mappings")
             return(NULL)
         }
+        # Inform the user if there are duplicate symbols.
+        if (any(duplicated(data[["geneName"]]))) {
+            x <- data[["geneName"]]
+            n <- length(unique(x[duplicated(x)]))
+            message(paste(
+                "Sanitizing", n, "duplicated symbols using `make.unique()`"
+            ))
+        }
         data <- data %>%
-            as.data.frame() %>%
+            as("tbl_df") %>%
             select(!!!syms(cols)) %>%
             mutate_all(as.character) %>%
+            mutate(geneName = make.unique(!!sym("geneName"))) %>%
+            as.data.frame() %>%
+            # Note that we're matching the object rownames here.
             set_rownames(rownames(object))
         assertIsGene2symbol(data)
         data
