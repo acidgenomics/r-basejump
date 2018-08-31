6e5e7ae467285ac8145f0ec9ff40b2a6709873c6
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-08-31 15:46:12 -0400

    Improve consistency of `makeTx2gene` functions and return as a DataFrame instead of `data.frame`

--- R/makeTx2gene.R
+++ R/makeTx2gene.R
@@ -1,3 +1,7 @@
+# FIXME Improve the formals here.
+
+
+
 #' Make Transcript-to-Gene Mappings
 #'
 #' @name makeTx2gene
@@ -7,7 +11,7 @@
 #' @inheritParams makeGRanges
 #' @param ... Passthrough to [makeGRangesFromEnsembl()].
 #'
-#' @return `data.frame`.
+#' @return `DataFrame`.
 #'
 #' @seealso `help(topic = "makeGRanges", package = "basejump")`.
 #'
@@ -28,15 +32,33 @@ NULL
 
 
 
+.makeTx2gene <- function(data) {
+    data <- data %>%
+        as("tbl_df") %>%
+        select(!!!syms(c("transcriptID", "geneID"))) %>%
+        .[complete.cases(.), , drop = FALSE] %>%
+        unique() %>%
+        mutate_all(as.character) %>%
+        arrange(!!sym("transcriptID")) %>%
+        as("DataFrame") %>%
+        set_rownames(.[["transcriptID"]])
+    message(paste(
+        "tx2gene mappings:",
+        length(unique(data[["transcriptID"]])), "transcripts,",
+        length(unique(data[["geneID"]])), "genes"
+    ))
+    data
+}
+
+
+
 #' @rdname makeTx2gene
 #' @export
 makeTx2geneFromEnsembl <- function(...) {
     gr <- makeGRangesFromEnsembl(..., format = "transcripts")
-    mcols(gr) %>%
-        as.data.frame() %>%
-        select(!!!syms(c("transcriptID", "geneID"))) %>%
-        mutate_all(as.character) %>%
-        set_rownames(.[[1L]])
+    data <- .makeTx2gene(gr)
+    assert_are_identical(names(gr), rownames(data))
+    data
 }
 
 
@@ -46,17 +68,22 @@ makeTx2geneFromEnsembl <- function(...) {
 makeTx2geneFromGFF <- function(file) {
     message("Making tx2gene from GFF")
     gff <- readGFF(file)
+    assert_is_all_of(gff, "GRanges")
 
+    # Get information on the type of GFF file.
     source <- .gffSource(gff)
     type <- .gffType(gff)
     message(paste(source, type, "detected"))
 
-    data <- mcols(gff) %>%
-        as.data.frame() %>%
-        camel()
+    # Coerce GRanges to tibble.
+    data <- camel(as(gff, "tbl_df"))
     assert_is_subset("transcriptID", colnames(data))
-    transcriptIDs <- sort(unique(na.omit(data[["transcriptID"]])))
+
+    # Remove rows that don't contain transcript annotations.
     data <- filter(data, !is.na(!!sym("transcriptID")))
+
+    # Get a vector of unique transcript IDs.
+    transcriptIDs <- sort(unique(data[["transcriptID"]]))
 
     if (type == "GFF") {
         assert_is_subset("parent", colnames(data))
@@ -69,19 +96,7 @@ makeTx2geneFromGFF <- function(file) {
         )
     }
 
-    data <- data %>%
-        select(!!!syms(c("transcriptID", "geneID"))) %>%
-        .[complete.cases(.), , drop = FALSE] %>%
-        unique() %>%
-        arrange(!!sym("transcriptID")) %>%
-        set_rownames(.[["transcriptID"]])
-
-    message(paste(
-        "tx2gene mappings:",
-        length(unique(data[["transcriptID"]])), "transcripts,",
-        length(unique(data[["geneID"]])), "genes"
-    ))
-
+    data <- .makeTx2gene(data)
     assert_are_identical(transcriptIDs, rownames(data))
     data
 }
