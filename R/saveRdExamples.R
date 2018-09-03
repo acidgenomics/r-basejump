43e95dc15c387c2bf98f45f8ce00005ec5d13232
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-03 11:30:50 -0400

    Rename `saveFunctionExamples()` to `saveRdExamples()`

---
+++ R/saveRdExamples.R
@@ -0,0 +1,99 @@
+#' Save R Documentation Examples
+#'
+#' Parse the documentation for a function and save the working examples to an R
+#' script. Note that the `f` argument is parameterized and can handle multiple
+#' requests in a single call.
+#'
+#' @family Developer Functions
+#' @author Michael Steinbaugh
+#' @export
+#'
+#' @inheritParams general
+#' @param Rd `character` or `NULL`. R documentation name(s) from which to parse
+#'   and save the working examples. If `NULL`, all documentation files
+#'   containing examples will be saved.
+#' @param package `string`. Package name.
+#'
+#' @return Invisible `character`. File path(s).
+#'
+#' @seealso
+#' - [parseRd()].
+#' - [tools::Rd_db()].
+#'
+#' @examples
+#' saveRdExamples(
+#'     Rd = c("do.call", "droplevels"),
+#'     package = "base",
+#'     dir = "XXX"
+#' )
+#'
+#' # Clean up
+#' unlink("XXX", recursive = TRUE)
+saveRdExamples <- function(
+    Rd,
+    package,
+    dir = "."
+) {
+    assert_is_any_of(
+        x = Rd,
+        classes = c("character", "NULL")
+    )
+    assert_is_a_string(package)
+    dir <- initializeDirectory(dir)
+
+    # Get a database of the Rd files available in the requested package.
+    db <- Rd_db(package)
+    names(db) <- gsub("\\.Rd", "", names(db))
+
+    # If no function is specified, save everything.
+    if (is.null(Rd)) {
+        Rd <- names(db)
+        message(paste(
+            "Saving", length(Rd), "functions from", package, "to", dir
+        ))
+    }
+
+    # Check that the requiested function(s) are valid.
+    assert_is_subset(Rd, names(db))
+
+    # Parse the Rd files and return the working examples as a character.
+    list <- mapply(
+        Rd = Rd,
+        MoreArgs = list(
+            package = package,
+            dir = dir
+        ),
+        FUN = function(Rd, package, dir) {
+            x <- tryCatch(
+                expr = parseRd(db[[Rd]], kind = "examples"),
+                error = function(e) character()
+            )
+
+            # Early return if there are no examples.
+            if (!length(x)) {
+                message(paste("Skipping", Rd))
+                return(invisible())
+            }
+
+            # Save to an R script.
+            path <- file.path(dir, paste0(Rd, ".R"))
+            message(paste("Saving", Rd))
+            unlink(path)
+            write_lines(x = x, path = path)
+            path
+        },
+        SIMPLIFY = FALSE,
+        USE.NAMES = TRUE
+    )
+
+    return(list)
+
+    # Coerce to character and remove NULL items.
+    paths <- Filter(Negate(is.null), list)
+    names <- names(paths)
+    paths <- as.character(paths)
+    names(paths) <- names
+
+    # Return file paths of saved R scripts.
+    invisible(paths)
+}
