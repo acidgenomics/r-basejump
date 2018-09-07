79b790f1ecc436e4a51e2a27b18a8c0927746dbe
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-07 10:30:16 -0400

    Draft update, exporting `gene2symbol` class as a specific type of `DataFrame`

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,4 +1,20 @@
 #' @include basejump-package.R
 
+
+
 # tibble
 setOldClass(Classes = class(tibble()))
+
+
+
+#' @rdname gene2symbol
+#' @aliases NULL
+#' @exportClass gene2symbol
+#' @usage NULL
+gene2symbol <- setClass(
+    Class = "gene2symbol",
+    contains = "DataFrame"
+)
+
+# TODO Add working example
+# new("gene2symbol", g2s)
