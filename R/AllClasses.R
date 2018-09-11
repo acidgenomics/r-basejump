b8ea73c78f05db3dea1ac8c38261c1cae91eaddd
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-10 22:03:02 -0400

    Improve class documentation

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,31 +1,32 @@
-#' S4 Classes
-#'
-#' @name AllClasses
-#' @keywords internal
-#' @include basejump-package.R
-#'
-#' @inheritParams general
-#'
-#' @return Varies, depending upon the method.
-#'
-#' @seealso
-#' - [methods::getClass()].
-#' - [methods::isClass()].
-NULL
-
-
-
-# tibble =======================================================================
+# tbl_df =======================================================================
 setOldClass(Classes = class(tibble()))
 
 
 
 # gene2symbol ==================================================================
-#' @rdname AllClasses
-#' @aliases NULL
-#' @exportClass gene2symbol
-#' @usage NULL
-gene2symbol <- setClass(
+#' `gene2symbol` Class
+#'
+#' Gene-to-symbol mappings.
+#'
+#' Contains a `DataFrame` with `geneID` and `geneName` columns.
+#'
+#' @family S4 Object
+#' @author Michael Steinbaugh
+#' @export
+#'
+#' @inheritParams general
+#'
+#' @examples
+#' x <- new(
+#'     "gene2symbol",
+#'     DataFrame(
+#'         geneID = "ENSG00000000003",
+#'         geneName = "TSPAN6",
+#'         row.names = "ENSG00000000003"
+#'     )
+#' )
+#' print(x)
+setClass(
     Class = "gene2symbol",
     contains = "DataFrame"
 )
@@ -41,11 +42,25 @@ setValidity(
 
 
 # tx2gene ==================================================================
-#' @rdname AllClasses
-#' @aliases NULL
-#' @exportClass tx2gene
-#' @usage NULL
-tx2gene <- setClass(
+#' `tx2gene` Class
+#'
+#' Transcript-to-gene mappings.
+#'
+#' Contains a `DataFrame` with `transcriptID` and `geneID` columns.
+#'
+#' @export
+#'
+#' @examples
+#' x <- new(
+#'     "tx2gene",
+#'     DataFrame(
+#'         transcriptID = "ENST00000000233",
+#'         geneID = "ENSG00000004059",
+#'         row.names = "ENST00000000233"
+#'     )
+#' )
+#' print(x)
+setClass(
     Class = "tx2gene",
     contains = "DataFrame"
 )
