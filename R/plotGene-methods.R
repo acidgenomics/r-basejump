0f47bf0ecedaee8b3d83acdca8c9b47711f505e0
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-11 12:37:59 -0400

    Migrate `SummarizedExperiment` method from bcbioRNASeq here

---
+++ R/plotGene-methods.R
@@ -0,0 +1,195 @@
+# TODO Keep the gene vector ordered as an option.
+# TODO Consider renaming `return` argument.
+
+
+
+#' Plot Gene Expression
+#'
+#' @name plotGene
+#' @family Plot Functions
+#' @author Michael Steinbaugh
+#' @export
+#'
+#' @inheritParams general
+#' @param countsAxisLabel `string`. Label to use for the counts axis.
+#' @param medianLine `boolean`. Include median line for each group. Disabled if
+#'   samples are colored by sample name.
+#'
+#' @return
+#' - "`facet`": `ggplot` grouped by `sampleName`, with [ggplot2::facet_wrap()]
+#'   applied to panel the samples.
+#' - "`wide`": Show `ggplot` in wide format, with genes on the x-axis.
+#'
+#' @seealso [DESeq2::plotCounts()].
+#'
+#' @examples
+#' object <- rse_small
+#'
+#' rownames <- head(rownames(object))
+#' print(rownames)
+#' g2s <- gene2symbol(object)
+#' geneIDs <- head(g2s[["geneID"]])
+#' print(geneIDs)
+#' geneNames <- head(g2s[["geneName"]])
+#' print(geneNames)
+#'
+#' # Rownames, gene IDs, and gene names (symbols) are supported.
+#' plotGene(object, genes = geneIDs, return = "facet")
+#' plotGene(object, genes = geneNames, return = "wide")
+NULL
+
+
+
+.plotGeneFacet <- function(
+    object,
+    countsAxisLabel = "counts",
+    medianLine = TRUE,
+    color = NULL,
+    legend = TRUE
+) {
+    assert_is_all_of(object, "SummarizedExperiment")
+    interestingGroups <- interestingGroups(object)
+
+    # Coerce the data to a melted tibble.
+    data <- meltCounts(object)
+
+    p <- ggplot(
+        data = data,
+        mapping = aes(
+            x = !!sym("interestingGroups"),
+            y = !!sym("counts"),
+            color = !!sym("interestingGroups")
+        )
+    ) +
+        .genePoint(show.legend = legend) +
+        facet_wrap(facets = sym("rowname"), scales = "free_y") +
+        labs(
+            x = NULL,
+            y = countsAxisLabel,
+            color = paste(interestingGroups, collapse = ":\n")
+        )
+
+    if (
+        isTRUE(medianLine) &&
+        !identical(interestingGroups, "sampleName")
+    ) {
+        p <- p + .geneMedianLine
+    }
+
+    if (is(color, "ScaleDiscrete")) {
+        p <- p + color
+    }
+
+    if (identical(interestingGroups, "sampleName")) {
+        p <- p + guides(color = FALSE)
+    }
+
+    p
+}
+
+
+
+.plotGeneWide <- function(
+    object,
+    countsAxisLabel = "counts",
+    medianLine = TRUE,
+    color = NULL,
+    legend = TRUE
+) {
+    assert_is_all_of(object, "SummarizedExperiment")
+    interestingGroups <- interestingGroups(object)
+
+    # Coerce the data to a melted tibble.
+    data <- meltCounts(object)
+
+    p <- ggplot(
+        data = data,
+        mapping = aes(
+            x = !!sym("rowname"),
+            y = !!sym("counts"),
+            color = !!sym("interestingGroups")
+        )
+    ) +
+        .genePoint(show.legend = legend) +
+        labs(
+            x = NULL,
+            y = countsAxisLabel,
+            color = paste(interestingGroups, collapse = ":\n")
+        )
+
+    if (
+        isTRUE(medianLine) &&
+        !identical(interestingGroups, "sampleName")
+    ) {
+        p <- p + .geneMedianLine
+    }
+
+    if (is(color, "ScaleDiscrete")) {
+        p <- p + color
+    }
+
+    p
+}
+
+
+
+#' @rdname plotGene
+#' @export
+setMethod(
+    "plotGene",
+    signature("SummarizedExperiment"),
+    function(
+        object,
+        genes,
+        interestingGroups = NULL,
+        countsAxisLabel = "counts",
+        medianLine = TRUE,
+        color = getOption("basejump.discrete.color", NULL),
+        legend = getOption("basejump.legend", TRUE),
+        headerLevel = 2L,
+        return = c("facet", "wide")
+    ) {
+        validObject(object)
+        # Coerce to `SummarizedExperiment`, for fast subsetting below.
+        if (is(object, "RangedSummarizedExperiment")) {
+            object <- as(object, "RangedSummarizedExperiment")
+        }
+        object <- as(object, "SummarizedExperiment")
+        assert_is_character(genes)
+        # Limit the number of genes that can be plotted at once.
+        assert_all_are_in_closed_range(length(genes), lower = 1L, upper = 20L)
+        genes <- mapGenesToRownames(object, genes)
+        interestingGroups <- matchInterestingGroups(
+            object = object,
+            interestingGroups = interestingGroups
+        )
+        interestingGroups(object) <- interestingGroups
+        assert_is_a_bool(medianLine)
+        assertIsColorScaleDiscreteOrNULL(color)
+        assert_is_a_bool(legend)
+        assertIsAHeaderLevel(headerLevel)
+        return <- match.arg(return)
+
+        # Subset to match the genes, which have been mapped to the rownames.
+        object <- object[genes, , drop = FALSE]
+        # Now convert the rownames to symbols, for visualization.
+        object <- convertGenesToSymbols(object)
+
+        # Return
+        if (return == "facet") {
+            fxn <- .plotGeneFacet
+        } else if (return == "wide") {
+            fxn <- .plotGeneWide
+        }
+        do.call(
+            what = fxn,
+            args = list(
+                object = object,
+                countsAxisLabel = countsAxisLabel,
+                medianLine = medianLine,
+                color = color,
+                legend = legend
+            )
+        )
+    }
+)
