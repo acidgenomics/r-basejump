59e0c146e6018049f662aa63705f91e9a83bb5fd
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-13 16:29:38 -0400

    Need to rethink how we're approaching S4 call expansion...

--- R/call-internal.R
+++ R/call-internal.R
@@ -1,8 +1,18 @@
-.sysCallWithS4 <- function(which, verbose = FALSE) {
+# FIXME This isn't working because we need to get the formals from .local
+# Think about a different approach using the one we have in `methodFormals()`.
+
+# function (x, ...)
+# {
+#     .local <- function (x, file, format, ...)
+#     {
+
+.sysCallWithS4 <- function(
+    which,
+    verbose = FALSE
+) {
     # Print the call stack, for debugging.
     if (isTRUE(verbose)) {
         print(sys.status())
-        print(which)
     }
 
     # Get the call.
@@ -10,21 +20,38 @@
 
     # Check for S4 `.local()` and recurse up an extra level, if necessary.
     if (identical(call[[1L]], as.symbol(".local"))) {
-        which <- which - 1L
-        call <- sys.call(which = which)
+        if (isTRUE(verbose)) {
+            message(".local detected")
+        }
+        call <- sys.call(which = which - 1L)
+        # Standardize the call.
+        # We need to be able to get the `.local` method expanded here.
+        f <- eval(expr = call[[1L]], envir = sys.frame(which = which - 2L))
+        if (is.primitive(f)) {
+            return(call)
+        }
+        print(formals(f))
+        call <- match.call(definition = f, call = call)
+        print(call)
+        stop()
+    } else {
+        # Standardize the call.
+        f <- eval(expr = call[[1L]], envir = sys.frame(which = which))
+        if (is.primitive(f)) {
+            return(call)
+        }
+        call <- match.call(definition = f, call = call)
     }
-
-    # Standardize the call with pryr package.
-    # This will expand all formals, like `match.call()`.
-    call <- standardise_call(call = call, env = sys.frame(which = which))
 
     # Print the matched call, for debugging.
     if (isTRUE(verbose)) {
+        print(which)
         print(call)
     }
 
     # Check call integrity before returning.
     assert_is_call(call)
+
     # Require that all arguments are named before returning.
     assert_all_are_non_missing_nor_empty_character(names(as.list(call)[-1L]))
 
