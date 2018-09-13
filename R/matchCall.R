cfb64b07daeb898d897a639f07512366655773dd
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-13 17:15:31 -0400

    Rework our match.call approach with S4

--- R/matchCall.R
+++ R/matchCall.R
@@ -1,3 +1,8 @@
+# FIXME Need to rethink this for S4...
+# Can we get the generic and the method from the call stack?
+
+
+
 #' Functions to Access the Function Call Stack
 #'
 #' @note
@@ -16,18 +21,18 @@
 #' - [base::sys.calls()].
 #' - [base::sys.call()].
 #' - [base::match.call()].
-#' - [pryr::standardise_call()].
 #'
 #' @return `call`.
 #'
 #' @examples
 #' x <- "XXX"
+#' y <- "YYY"
 #'
 #' # Standard call
 #' testing <- function(object, ...) {
-#'     matchCall()
+#'     matchS4Call()
 #' }
-#' testing(x)
+#' testing(x, y)
 #'
 #' # S4 mode
 #' setGeneric(
@@ -41,19 +46,62 @@
 #'     f = "testing",
 #'     signature = signature("character"),
 #'     definition = function(object, ...) {
-#'         matchCall()
+#'         matchS4Call()
 #'     }
 #' )
-#' testing(x)
+#' testing(x, y)
 #'
 #' setMethod(
 #'     f = "testing",
 #'     signature = signature("character"),
 #'     definition = function(object, xxx, ...) {
-#'         matchCall()
+#'         matchS4Call()
 #'     }
 #' )
-#' testing(x)
-matchCall <- function(verbose = FALSE) {
-    .sysCallWithS4(which = sys.parent(), verbose = verbose)
+#' testing(x, y)
+matchCallS4 <- function(
+    f,
+    signature,
+    verbose = FALSE
+) {
+    which <- sys.parent()
+
+    # Print the call stack, for debugging.
+    if (isTRUE(verbose)) {
+        print(sys.status())
+    }
+
+    # Get the call.
+    call <- sys.call(which = which)
+
+    # Check for S4 `.local()` and recurse up an extra level, if necessary.
+    if (identical(call[[1L]], as.symbol(".local"))) {
+        if (isTRUE(verbose)) {
+            message(".local detected")
+        }
+        which <- which - 1L
+        call <- sys.call(which = which)
+    }
+
+    # Match (standardize) the call.
+    # We need to be able to get the `.local` method expanded here.
+    f <- eval(expr = call[[1L]], envir = sys.frame(which = which))
+    if (is.primitive(f)) {
+        return(call)
+    }
+    call <- match.call(definition = f, call = call)
+
+    # Print the matched call, for debugging.
+    if (isTRUE(verbose)) {
+        print(which)
+        print(call)
+    }
+
+    # Check call integrity before returning.
+    assert_is_call(call)
+
+    # Require that all arguments are named before returning.
+    assert_all_are_non_missing_nor_empty_character(names(as.list(call)[-1L]))
+
+    call
 }
