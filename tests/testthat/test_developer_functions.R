e7ca858b8359027e1459fcef89ce6205c1bcc75b
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-14 15:52:11 -0400

    Add code coverage for `standardizeCall()`

--- tests/testthat/test_developer_functions.R
+++ tests/testthat/test_developer_functions.R
@@ -131,3 +131,74 @@ test_that("printString", {
         expected = "[1] \"hello\" \"world\""
     )
 })
+
+
+
+# standardizeCall ==============================================================
+aaa <- "AAA"
+bbb <- "BBB"
+ccc <- "CCC"
+
+test_that("standardizeCall : Standard function", {
+    testing <- function(a, b) {
+        standardizeCall()
+    }
+    object <- testing(aaa, bbb)
+    expect_is(object, "call")
+    expect_identical(
+        object = object,
+        expected = call(
+            name = "testing",
+            a = as.symbol("aaa"),
+            b = as.symbol("bbb")
+        )
+    )
+})
+
+test_that("standardizeCall : Inside S4 method", {
+    setGeneric(
+        name = "testing",
+        def = function(a, b, ...) {
+            standardGeneric("testing")
+        }
+    )
+
+    # Method with formals identical to the generic.
+    setMethod(
+        f = "testing",
+        signature = signature("character"),
+        definition = function(a, b, ...) {
+            standardizeCall()
+        }
+    )
+    object <- testing(aaa, bbb)
+    expect_is(object, "call")
+    expect_identical(
+        object = object,
+        expected = call(
+            name = "testing",
+            a = as.symbol("aaa"),
+            b = as.symbol("bbb")
+        )
+    )
+
+    # Method with formals that differ from generic.
+    setMethod(
+        f = "testing",
+        signature = signature("character"),
+        definition = function(a, b, c) {
+            standardizeCall()
+        }
+    )
+    object <- testing(aaa, bbb, ccc)
+    expect_is(object, "call")
+    expect_identical(
+        object = object,
+        expected = call(
+            name = "testing",
+            a = as.symbol("aaa"),
+            b = as.symbol("bbb"),
+            c = as.symbol("ccc")
+        )
+    )
+})
