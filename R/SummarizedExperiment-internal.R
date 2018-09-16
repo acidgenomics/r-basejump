3c097ed5afea5d91f7f78f88d53528d1b41b06fd
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-15 20:37:20 -0400

    Move this code to `coerce-methods.R`

--- R/SummarizedExperiment-internal.R
+++
@@ -1,7 +0,0 @@
-.coerceToSummarizedExperiment <- function(object) {
-    assert_is_all_of(object, "SummarizedExperiment")
-    if (is(object, "RangedSummarizedExperiment")) {
-        object <- as(object, "RangedSummarizedExperiment")
-    }
-    as(object, "SummarizedExperiment")
-}
