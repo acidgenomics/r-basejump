ec94104fa6881cd4e3a6dcdc263ca19f1e8cddcb
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-24 14:36:49 -0400

    Simplify the SE return method

--- R/metrics-methods.R
+++ R/metrics-methods.R
@@ -1,23 +1,36 @@
+# FIXME Improve documentation for SE vs. SCE.
+
+
+
 #' Metrics
 #'
 #' By default, [metrics()] returns [colData()] with an `interestingGroups`
 #' column automatically added. This is useful for `ggplot2` scripting. Column
 #' names are always sorted alphabetically.
 #'
-#' @note [metrics()] Requires that the `sampleName` column is defined, and will
-#' error otherwise.
+#' @section SingleCellExperiment:
+#'
+#' Returns `data.frame` with cellular barcodes as rows. If `recalculate = TRUE`,
+#' returns the original object class (e.g. `SingleCellExperiment`), with updated
+#' metrics in [colData()].
 #'
 #' @name metrics
 #' @family Data Functions
-#' @author Michael Steinbaugh
+#' @author Michael Steinbaugh, Rory Kirchner
 #' @export
 #'
 #' @inheritParams general
 #'
-#' @return `DataFrame`.
+#' @return `grouped_df`. Grouped by `sampleID` column, which corresponds to the
+#'   column names for `SummarizedExperiment`.
 #'
 #' @examples
+#' # SummarizedExperiment ====
 #' x <- metrics(rse_small)
+#' glimpse(x)
+#'
+#' # SingleCellExperiment ====
+#' x <- metrics(sce_small)
 #' glimpse(x)
 NULL
 
@@ -26,21 +39,44 @@ NULL
 .metrics.SE <-  # nolint
     function(object) {
         validObject(object)
-        data <- colData(object)
-        assert_is_non_empty(data)
-        # Add the `interestingGroups` column.
-        # Use `sampleName` column by default, if undefined.
-        interestingGroups <- interestingGroups(object)
-        if (is.null(interestingGroups)) {
-            interestingGroups <- "sampleName"
+        sampleData(object) %>%
+            as_tibble(rownames = "sampleID") %>%
+            group_by(!!sym("sampleID"))
+    }
+
+
+
+.metrics.SCE <-  # nolint
+    function(object) {
+        validObject(object)
+
+        colData <- colData(object)
+
+        data <- as.data.frame(colData)
+        if (!all(metricsCols %in% colnames(data))) {
+            stop(paste(
+                "Missing metrics:",
+                setdiff(metricsCols, colnames(data)),
+                "Run `metrics(object, recalculate = TRUE)`."
+            ))
         }
-        data <- uniteInterestingGroups(data, interestingGroups)
+        if (!"sampleName" %in% colnames(data)) {
+            data[["sampleID"]] <- factor("unknown")
+            data[["sampleName"]] <- factor("unknown")
+        }
+        interestingGroups <- matchInterestingGroups(
+            object = object,
+            interestingGroups = interestingGroups
+        )
+        data <- uniteInterestingGroups(
+            object = data,
+            interestingGroups = interestingGroups
+        )
         assert_is_subset(
             x = c("sampleName", "interestingGroups"),
             y = colnames(data)
         )
-        # Require that the columns are sorted alphabetically.
-        data <- data[, sort(colnames(data)), drop = FALSE]
+
         data
     }
 
@@ -52,4 +88,14 @@ setMethod(
     f = "metrics",
     signature = signature("SummarizedExperiment"),
     definition = .metrics.SE
+)
+
+
+
+#' @rdname metrics
+#' @export
+setMethod(
+    f = "metrics",
+    signature = signature("SingleCellExperiment"),
+    definition = .metrics.SCE
 )
