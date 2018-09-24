164bdb8d96ce13235b1b5870a54793128a403882
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-24 14:11:47 -0400

    Simplify this method. Require that `sampleID` is defined in `colData()`

---
+++ R/cell2sample-methods.R
@@ -0,0 +1,38 @@
+#' Cell to Sample Mappings
+#'
+#' @name cell2sample
+#' @family Data Functions
+#' @author Michael Steinbaugh
+#'
+#' @inheritParams general
+#'
+#' @return Named `factor` containing sample IDs as the levels and cellular
+#'   barcode IDs as the names.
+#'
+#' @examples
+#' x <- cell2sample(sce_small)
+#' table(x)
+NULL
+
+
+
+.cell2sample.SCE <-  # nolint
+    function(object) {
+        validObject(object)
+        assert_is_subset("sampleID", colnames(colData(object)))
+        sample <- colData(object)[["sampleID"]]
+        assert_is_factor(sample)
+        cell <- colnames(object)
+        names(sample) <- cell
+        sample
+    }
+
+
+
+#' @rdname cell2sample
+#' @export
+setMethod(
+    f = "cell2sample",
+    signature = signature("SingleCellExperiment"),
+    definition = .cell2sample.SCE
+)
