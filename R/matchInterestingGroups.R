2424807f460177c80db32d8ca0d9052de13985af
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-25 10:06:54 -0400

    Improve documentation

--- R/matchInterestingGroups.R
+++ R/matchInterestingGroups.R
@@ -16,14 +16,9 @@
 #'
 #' @examples
 #' matchInterestingGroups(rse_small)
-#' matchInterestingGroups(rse_small, interestingGroups = NULL)
-#' matchInterestingGroups(rse_small, interestingGroups = "sampleName")
-matchInterestingGroups <- function(object, interestingGroups) {
+matchInterestingGroups <- function(object, interestingGroups = NULL) {
     assert_is_all_of(object, "SummarizedExperiment")
-    if (
-        missing(interestingGroups) ||
-        is.null(interestingGroups)
-    ) {
+    if (is.null(interestingGroups)) {
         interestingGroups <- basejump::interestingGroups(object)
         if (is.null(interestingGroups)) {
             interestingGroups <- "sampleName"
