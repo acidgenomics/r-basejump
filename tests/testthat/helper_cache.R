75deec029ba78e726f5b6c9c471a1443f2fd5bc3
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-26 09:56:57 -0400

    Export basejumpCacheURL

--- tests/testthat/helper_cache.R
+++ tests/testthat/helper_cache.R
@@ -1,34 +1,33 @@
-files <- c(
-    "example.counts",
-    "example.csv",
-    "example.gtf",
-    "example.gff3",
-    "example.json",
-    "example.rda",
-    "example.rds",
-    "example.tsv",
-    "example.txt",
-    "example.yml",
-    "gr.rda",
-    "mn.rda",
-    "multi.rda",
-    "plotlist.rda",
-    "renamed.rda",
-    "rnaseq_counts.csv.gz",
-    "serialized.rds",
-    "single_cell_counts.mtx.gz",
-    "single_cell_counts.mtx.gz.colnames",
-    "single_cell_counts.mtx.gz.rownames"
-)
-mapply(
-    FUN = function(cacheURL, file, envir) {
+invisible(lapply(
+    X = c(
+        "example.counts",
+        "example.csv",
+        "example.gtf",
+        "example.gff3",
+        "example.json",
+        "example.rda",
+        "example.rds",
+        "example.tsv",
+        "example.txt",
+        "example.yml",
+        "gr.rda",
+        "mn.rda",
+        "multi.rda",
+        "plotlist.rda",
+        "renamed.rda",
+        "rnaseq_counts.csv.gz",
+        "serialized.rds",
+        "single_cell_counts.mtx.gz",
+        "single_cell_counts.mtx.gz.colnames",
+        "single_cell_counts.mtx.gz.rownames"
+    ),
+    FUN = function(file, url) {
         if (!file.exists(file)) {
             utils::download.file(
-                url = paste(cacheURL, file, sep = "/"),
+                url = paste(url, file, sep = "/"),
                 destfile = file
             )
         }
     },
-    file = files,
-    MoreArgs = list(cacheURL = cacheURL, envir = environment())
-)
+    url = basejumpCacheURL
+))
