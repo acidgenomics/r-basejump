62d4c7dbecc79c28794f8162ebc1353d9758c007
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-28 15:36:48 -0400

    Add back SE method.

--- R/selectSamples-methods.R
+++ R/selectSamples-methods.R
@@ -29,12 +29,49 @@
 #' - [S4Vectors::split()].
 #'
 #' @examples
+#' # SummarizedExperiment ====
+#' object <- rse_small
+#' sample <- sampleNames(object) %>% head(1L)
+#' print(sample)
+#' subset <- selectSamples(object, sampleName = sample)
+#' print(subset)
+#'
+#' # SingleCellExperiment ====
 #' object <- sce_small
 #' sample <- sampleNames(object) %>% head(1L)
 #' print(sample)
 #' subset <- selectSamples(object, sampleName = sample)
 #' print(subset)
 NULL
+
+
+
+.selectSamples.SE <-  # nolint
+    function(object, ...) {
+        validObject(object)
+        args <- list(...)
+        invisible(lapply(args, assert_is_atomic))
+
+        # Match the arguments against the sample metadata.
+        sampleData <- sampleData(object)
+        assert_is_subset(names(args), colnames(sampleData))
+
+        # Obtain the sample identifiers.
+        list <- mapply(
+            col = names(args),
+            arg = args,
+            MoreArgs = list(data = sampleData),
+            FUN = function(col, arg, data) {
+                rownames(data[data[[col]] %in% arg, , drop = FALSE])
+            },
+            SIMPLIFY = FALSE,
+            USE.NAMES = TRUE
+        )
+        samples <- sort(as.character(Reduce(f = intersect, x = list)))
+        assert_is_non_empty(samples)
+
+        object[, samples]
+    }
 
 
 
@@ -117,6 +154,16 @@ NULL
         metadata(object)[["selectSamples"]] <- TRUE
         object
     }
+
+
+
+#' @rdname selectSamples
+#' @export
+setMethod(
+    "selectSamples",
+    signature("SummarizedExperiment"),
+    definition = .selectSamples.SE
+)
 
 
 
