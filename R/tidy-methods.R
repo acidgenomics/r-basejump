ddd46ce227b7325797f686e487f114a89add22b8
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-10-04 12:08:11 -0400

    Draft update adding dplyr support for DataFrame

--- R/tidy-methods.R
+++ R/tidy-methods.R
@@ -1,49 +1,65 @@
-# Modified from:
-# https://gist.github.com/DarwinAwardWinner/9efb85856616c042874d
-
-# This will allow you to use dplyr functions with Bioconductor's
-# S4Vectors::DataFrame class. Note that this entails conversion of S4 vector
-# columns to primitive vectors.
+# bind_cols
+# bind_rows
+# slice
 
 # Single table verbs ===========================================================
-single.table.verbs <- c(
-    "arrange",
-    "distinct",
-    "do",
-    "filter",
-    "group_by",
-    "mutate",
-    "rename",
-    "select",
-    "summarise",
-    "transmute"
-)
-for (verb in single.table.verbs) {
-    assign(
-        x = paste0(verb, ".DataFrame"),
-        value = local({
-            generic <- get(verb, envir = as.environment("package:dplyr"))
-            function(.data, ...) {
-                .data %>%
-                    as("tbl_df") %>%
-                    generic(...) %>%
-                    as("DataFrame")
-            }
-        })
-    )
-}
+# arrange_all
+# arrange_at
+# arrange_if
+# distinct
+# do
+# filter
+# filter_all
+# filter_at
+# filter_if
+# group_by
+# mutate
+# mutate_all
+# mutate_at
+# mutate_if
+# rename
+# rename_all
+# mutate_at
+# mutate_if
+# select
+# select_all
+# select_at
+# select_if
+# summarise
+# summarise_all
+# summarise_at
+# summarise_if
+# summarize variants
+# transmute
+# transmute_all
+# transmute_at
+# trasmute_if
 
-# arrange ======================================================================
+
+
 #' @importFrom dplyr arrange
 #' @export
 dplyr::arrange
-
-
 
 #' @method arrange DataFrame
 #' @export
 arrange.DataFrame <- function(.data, ...) {
     .data %>%
+        as("tbl_df") %>%
+        arrange(...) %>%
+        as("DataFrame")
+}
+
+
+
+#' @importFrom dplyr filter
+#' @export
+dplyr::filter
+
+#' @method filter DataFrame
+#' @export
+filter.DataFrame <- function(.data, ...) {
+    ..data %>%
         as("tbl_df") %>%
         filter(...) %>%
         as("DataFrame")
@@ -51,29 +67,58 @@ arrange.DataFrame <- function(.data, ...) {
 
 
 
-# filter =======================================================================
-#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
 #' @export
-dplyr::filter
+dplyr::mutate
 
-
-
-#' @method filter DataFrame
+#' @method mutate DataFrame
 #' @export
-filter.DataFrame <- function(.data, ...) {
-    data <- as(.data, "tbl_df")
-    data <- filter(data, ...)
-    as(data, "DataFrame")
+mutate.DataFrame <- function(.data, ...) {
+    .data %>%
+        as("tbl_df") %>%
+        mutate(...) %>%
+        as("DataFrame")
 }
 
 
 
-# left_join ====================================================================
+#' @importFrom dplyr rename
+#' @export
+dplyr::rename
+
+#' @method rename DataFrame
+#' @export
+rename.DataFrame <- function(.data, ...) {
+    .data %>%
+        as("tbl_df") %>%
+        rename(...) %>%
+        as("DataFrame")
+}
+
+
+
+#' @importFrom dplyr select
+#' @export
+dplyr::select
+
+#' @method select DataFrame
+#' @export
+select.DataFrame <- function(.data, ...) {
+    rownames <- rownames(.data)
+    data <- .data %>%
+        as("tbl_df") %>%
+        select(...) %>%
+        as("DataFrame")
+    rownames(data) <- rownames
+    data
+}
+
+
+
+# Two table verbs ==============================================================
 #' @importFrom dplyr left_join
 #' @export
 dplyr::left_join
-
-
 
 #' @method left_join DataFrame
 #' @export
@@ -86,58 +131,4 @@ left_join.DataFrame <- function(x, y, ...) {
         ...
     )
     as(data, "DataFrame")
-}
-
-
-
-# mutate =======================================================================
-#' @importFrom dplyr mutate
-#' @export
-dplyr::mutate
-
-
-
-#' @method mutate DataFrame
-#' @export
-mutate.DataFrame <- function(.data, ...) {
-    data <- as(.data, "tbl_df")
-    data <- mutate(data, ...)
-    as(data, "DataFrame")
-}
-
-
-
-# rename =======================================================================
-#' @importFrom dplyr rename
-#' @export
-dplyr::rename
-
-
-
-#' @method rename DataFrame
-#' @export
-rename.DataFrame <- function(.data, ...) {
-    data <- as(.data, "tbl_df")
-    data <- rename(data, ...)
-    as(data, "DataFrame")
-}
-
-
-
-# select =======================================================================
-#' @importFrom dplyr select
-#' @export
-dplyr::select
-
-
-
-#' @method select DataFrame
-#' @export
-select.DataFrame <- function(.data, ...) {
-    rownames <- rownames(.data)
-    data <- as(.data, "tbl_df")
-    data <- select(data, ...)
-    data <- as(data, "DataFrame")
-    rownames(data) <- rownames
-    data
 }
