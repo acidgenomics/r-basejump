1089c6c4e9ea8f20219f080cdc86dcb8036622d6
Michael Steinbaugh <mike@steinbaugh.com>
2018-10-07 16:38:17 -0400

    Rename to use `Tx2Gene` instead of `tx2gene` class

--- R/convertTranscriptsToGenes-methods.R
+++ R/convertTranscriptsToGenes-methods.R
@@ -15,10 +15,10 @@
 #' @author Michael Steinbaugh
 #' @export
 #'
-#' @include makeTx2gene.R
+#' @include makeTx2Gene.R
 #' @inherit convertGenesToSymbols
 #'
-#' @param tx2gene `tx2gene` or `NULL`. Transcript-to-gene mappings. If set
+#' @param tx2gene `Tx2Gene` or `NULL`. Transcript-to-gene mappings. If set
 #'   `NULL`, the function will attempt to download the mappings from Ensembl
 #'   automatically.
 #' @param aggregate `boolean`. For objects supporting [dim()], aggregate counts
@@ -74,7 +74,7 @@ NULL
     ) {
         assert_all_are_non_missing_nor_empty_character(object)
         assert_has_no_duplicates(object)
-        assert_is_any_of(tx2gene, c("DataFrame", "NULL"))
+        assert_is_any_of(tx2gene, c("Tx2Gene", "NULL"))
         assertIsAStringOrNULL(organism)
 
         # If no tx2gene is provided, fall back to using Ensembl annotations.
@@ -93,7 +93,7 @@ NULL
                 )
             )
         }
-        assertIsTx2gene(tx2gene)
+        assert_is_all_of(tx2gene, "Tx2Gene")
 
         missing <- setdiff(object, tx2gene[["transcriptID"]])
         if (has_length(missing)) {
@@ -113,7 +113,7 @@ NULL
         return
     }
 f1 <- formals(.convertTranscriptsToGenes.character)
-f2 <- formals(makeTx2geneFromEnsembl)
+f2 <- formals(makeTx2GeneFromEnsembl)
 f2 <- f2[setdiff(names(f2), c(names(f1)))]
 f <- c(f1, f2)
 formals(.convertTranscriptsToGenes.character) <- f
