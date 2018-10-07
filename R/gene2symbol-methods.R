2f7da606afc94a932a5580661e5afe28d39b666a
Michael Steinbaugh <mike@steinbaugh.com>
2018-10-07 19:24:02 -0400

    Improve `gene2symbol()` generator

--- R/gene2symbol-methods.R
+++ R/gene2symbol-methods.R
@@ -4,7 +4,7 @@
 #' [base::make.unique()], which will add ".1" to the end of the gene name.
 #'
 #' @name gene2symbol
-#' @family Annotation Functions
+#' @family S4 Generators
 #' @author Michael Steinbaugh
 #' @export
 #'
@@ -20,19 +20,80 @@ NULL
 
 
 
+#' @rdname gene2symbol
+#' @export
+Gene2Symbol <- function(object, ...) {
+    gene2symbol(object, ...)
+}
+
+
+
+.gene2symbol.tbl_df <-  # nolint
+    function(object) {
+        assert_has_rows(object)
+        cols <- c("geneID", "geneName")
+        if (!all(cols %in% colnames(object))) {
+            stop(
+                "Object does not contain gene-to-symbol mappings.",
+                call. = FALSE
+            )
+        }
+        object %>%
+            select(!!!syms(cols)) %>%
+            mutate_all(as.character) %>%
+            mutate(!!sym("geneName") := make.unique(!!sym("geneName"))) %>%
+            as("DataFrame") %>%
+            new(Class = "Gene2Symbol", .)
+    }
+
+
+
+.gene2symbol.DataFrame <-  # nolint
+    function(object) {
+        object %>%
+            as_tibble(rownames = NULL) %>%
+            gene2symbol() %>%
+            set_rownames(rownames(object))
+    }
+
+
+
 .gene2symbol.GRanges <-  # nolint
     function(object) {
-        .makeGene2symbol(object)
+        object %>%
+            as("DataFrame") %>%
+            gene2symbol()
     }
 
 
 
 .gene2symbol.SE <-  # nolint
     function(object) {
-        data <- rowData(object)
-        rownames(data) <- rownames(object)
-        .makeGene2symbol(data)
+        object %>%
+            rowData() %>%
+            set_rownames(rownames(object)) %>%
+            gene2symbol()
     }
+
+
+
+#' @rdname gene2symbol
+#' @export
+setMethod(
+    f = "gene2symbol",
+    signature = signature("tbl_df"),
+    definition = .gene2symbol.tbl_df
+)
+
+
+
+#' @rdname gene2symbol
+#' @export
+setMethod(
+    f = "gene2symbol",
+    signature = signature("DataFrame"),
+    definition = .gene2symbol.DataFrame
+)
 
 
 
