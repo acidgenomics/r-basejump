ba1bff85544c2bbd3a753ad5d0423802e4c5b3cd
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-10-10 12:37:31 -0400

    Improve documentation

---
+++ R/aggregateCellsToSamples-methods.R
@@ -0,0 +1,52 @@
+#' Aggregate Cells to Samples
+#'
+#' Utilty function that factilites cell-to-sample aggregation. By default, this
+#' function will sum the counts across cells to sample level.
+#'
+#' This function is intended primarily for quality control analysis.
+#'
+#' Internally it automatically obtains the cell-to-sample groupings and then
+#' performs aggregation with the [aggregateCols()] function.
+#'
+#' @name aggregateCellsToSamples
+#' @family Count Matrix Functions
+#' @family SingleCellExperiment Functions
+#' @author Michael Steinbaugh
+#' @export
+#'
+#' @inheritParams general
+#'
+#' @return `SummarizedExperiment`. Object with cell-level counts aggregated
+#'   to sample-level.
+#'
+#' @examples
+#' data(sce_small)
+#' x <- aggregateCellsToSamples(sce_small)
+#' print(x)
+NULL
+
+
+
+.aggregateCellsToSamples.SCE <-  # nolint
+    function(object, fun) {
+        validObject(object)
+        fun <- match.arg(fun)
+        rse <- as(object, "RangedSummarizedExperiment")
+        colData(rse)[["aggregate"]] <- cell2sample(object)
+        aggregateCols(
+            object = rse,
+            col = "aggregate",
+            fun = fun
+        )
+    }
+formals(.aggregateCellsToSamples.SCE)[["fun"]] <- .aggregateFuns
+
+
+
+#' @rdname aggregateCellsToSamples
+#' @export
+setMethod(
+    f = "aggregateCellsToSamples",
+    signature = signature("SingleCellExperiment"),
+    definition = .aggregateCellsToSamples.SCE
+)
