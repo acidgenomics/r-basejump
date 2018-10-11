d7734a2e7260aeba68340e044d46e8625bf5c3bb
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-10-11 10:43:10 -0400

    Draft updates to documentation

--- R/show-methods.R
+++ R/show-methods.R
@@ -2,26 +2,63 @@
 # FIXME PANTHER
 
 
-# # Include the gene lengths per phase.
-# split <- split(x = object, f = object[["phase"]])
-# stopifnot(is(split, "SplitDataFrameList"))
-# lengths <- nrow(split)
+
+#' @inherit methods::show
+#' @importFrom methods show
+#' @name show
+#' @export
+#'
+#' @examples
+#' ## PANTHER ====
+#' x <- panther("Homo sapiens", .test = TRUE)
+#' show(x)
+NULL
 
 
 
-
-
-
-
-.show.PANTHER <-  # nolint
-    function(object) {
-        genes <- length(geneID)
-
+# Internals ====================================================================
+# TODO Add maxwidth handling in a future update.
+.slotInfo <- function(
+    object,
+    name = NULL,
+    unique = FALSE
+) {
+    if (is.null(name)) {
+        name <- deparse(substitute(object))
     }
+    if (isTRUE(unique)) {
+        object <- unique(object)
+    }
+    length <- length(object)
+    headtail <- capture.output(headtail(object))
+    paste0(name, "(", length, "): ", headtail)
+}
 
-# Tx2Gene
+
+
+# Tx2Gene ======================================================================
 # message(paste(
 #     "Mappings:",
 #     length(unique(data[["transcriptID"]])), "transcripts,",
 #     length(unique(data[["geneID"]])), "genes"
 # ))
+
+
+
+# PANTHER ======================================================================
+# FIXME Slot metadata into the data frame that we can show.
+.show.PANTHER <-  # nolint
+    function(object) {
+        genes <- .slotInfo(object[["geneID"]], name = "genes")
+        cat(genes)
+    }
+
+
+
+#' @rdname show
+#' @export
+setMethod(
+    f = "show",
+    signature = signature("PANTHER"),
+    definition = .show.PANTHER
+)
