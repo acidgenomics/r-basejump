e68f130bfb56e162ef54b8751b657520eb1fd69d
Michael Steinbaugh <mike@steinbaugh.com>
2018-10-13 21:27:33 -0400

    Export `checkClasses()` utility function, to be used with `setValidity()`

---
+++ R/checkClasses.R
@@ -0,0 +1,44 @@
+#' Check Classes
+#'
+#' @inheritParams general
+#' @param expected `list`. Named list of expected classes per slot.
+#' @export
+#'
+#' @examples
+#' checkClasses(
+#'     object = metadata(rse_small),
+#'     expected = list(
+#'         version = c("package_version" "numeric_version"),
+#'         date = "Date",
+#'         interestingGroups = "character"
+#'     )
+#' )
+checkClasses <- function(object, expected) {
+    valid <- mapply(
+        slot = names(expected),
+        classes = expected,
+        MoreArgs = list(object = object),
+        FUN = function(slot, classes, object) {
+            intersect <- intersect(
+                x = classes,
+                y = class(object[[slot]])
+            )
+            if (!has_length(intersect)) {
+                FALSE
+            } else {
+                TRUE
+            }
+        },
+        SIMPLIFY = TRUE,
+        USE.NAMES = TRUE
+    )
+    if (!all(valid)) {
+        stop(paste(
+            "Class checks failed.",
+            updateMessage,
+            printString(valid),
+            sep = "\n"
+        ))
+    }
+    TRUE
+}
