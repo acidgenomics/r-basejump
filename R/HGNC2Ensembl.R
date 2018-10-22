0f7b9e05caae3daa236244a7b414bef4845a7be7
Michael Steinbaugh <mike@steinbaugh.com>
2018-10-22 09:39:39 -0400

    Reformat to pass lintr checks

--- R/HGNC2Ensembl.R
+++ R/HGNC2Ensembl.R
@@ -10,34 +10,35 @@
 #' options(basejump.test = TRUE)
 #' x <- HGNC2Ensembl()
 #' print(x)
-HGNC2Ensembl <- function() {
-    stopifnot(has_internet())
+HGNC2Ensembl <-  # nolint
+    function() {
+        stopifnot(has_internet())
 
-    if (isTRUE(getOption("basejump.test"))) {
-        file <- file.path(basejumpCacheURL, "hgnc.txt.gz")
-    } else {
-        file <- .hgncURL
+        if (isTRUE(getOption("basejump.test"))) {
+            file <- file.path(basejumpCacheURL, "hgnc.txt.gz")
+        } else {
+            file <- .hgncURL
+        }
+
+        message("Obtaining HGNC to Ensembl gene ID mappings.")
+        data <- read_tsv(
+            file = file,
+            # Suppress the column messages.
+            col_types = cols(),
+            progress = FALSE
+        )
+        data <- as.data.frame(data)
+        data <- camel(data)
+        data <- data[, c("hgncID", "ensemblGeneID")]
+        colnames(data)[[2L]] <- "geneID"
+        data <- data[!is.na(data[["geneID"]]), ]
+        data[["hgncID"]] <- as.integer(gsub("^HGNC\\:", "", data[["hgncID"]]))
+        data <- data[order(data[["hgncID"]]), ]
+        data <- as(data, "DataFrame")
+        metadata(data) <- .prototypeMetadata
+
+        new(Class = "HGNC2Ensembl", data)
     }
-
-    message("Obtaining HGNC to Ensembl gene ID mappings.")
-    data <- read_tsv(
-        file = file,
-        # Suppress the column messages.
-        col_types = cols(),
-        progress = FALSE
-    )
-    data <- as.data.frame(data)
-    data <- camel(data)
-    data <- data[, c("hgncID", "ensemblGeneID")]
-    colnames(data)[[2L]] <- "geneID"
-    data <- data[!is.na(data[["geneID"]]), ]
-    data[["hgncID"]] <- as.integer(gsub("^HGNC\\:", "", data[["hgncID"]]))
-    data <- data[order(data[["hgncID"]]), ]
-    data <- as(data, "DataFrame")
-    metadata(data) <- .prototypeMetadata
-
-    new(Class = "HGNC2Ensembl", data)
-}
 
 
 
