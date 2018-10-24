123574ff96e51ae694e0e49e8a579dd0d395015f
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-10-24 14:03:04 -0400

    Improve formatting options

--- R/Ensembl2Entrez.R
+++ R/Ensembl2Entrez.R
@@ -4,6 +4,12 @@
 #' @export
 #'
 #' @inheritParams general
+#' @param format `string`. Formatting method to apply:
+#' - `"1:1"`: Return with 1:1 mappings. For Ensembl genes that don't map 1:1
+#'   with Entrez, pick the oldest Entrez ID. Genes that don't map to Entrez
+#'   will contain `NA` in `entrezID` column.
+#' - `"long"`: Return 1:many in long format.
+#' - `"list"`: Return 1:many with `entrezID` as a `list` column.
 #'
 #' @return `Ensembl2Entrez`.
 #'
@@ -15,37 +21,115 @@ NULL
 
 
 
+Ensembl2Entrez.DataFrame <-  # nolint
+    function(
+        object,
+        format = c("1:1", "long", "list")
+    ) {
+        assert_has_rows(object)
+        format <- match.arg(format)
+
+        cols <- c("geneID", "entrezID")
+        if (!all(cols %in% colnames(object))) {
+            stop(paste0(
+                "Object does not contain Ensembl-to-Entrez mappings.\n",
+                "Requires: ", toString(cols)
+            ))
+        }
+
+        data <- object[, cols, drop = FALSE]
+
+        if (format == "1:1") {
+            message("Returning with 1:1 mappings using oldest Entrez ID.")
+            entrez <- object[["entrezID"]]
+            assert_is_list(entrez)
+            names(entrez) <- object[["geneID"]]
+            map <- lapply(
+                X = entrez,
+                FUN = function(x) {
+                    if (all(is.na(x))) {
+                        NA_integer_
+                    } else {
+                        sort(x)[[1L]]
+                    }
+                }
+            )
+            entrez <- unlist(map)
+            data <- DataFrame(
+                geneID = names(entrez),
+                entrezID = as.integer(entrez),
+                row.names = rownames(data)
+            )
+        } else if (format == "long") {
+            message("Returning 1:many in long format.")
+            data <- data %>%
+                as_tibble(rownames = NULL) %>%
+                unnest(!!sym("entrezID")) %>%
+                distinct() %>%
+                arrange(!!!syms(cols)) %>%
+                as("DataFrame")
+        } else if (format == "list") {
+            message("Returning 1:many using nested list column.")
+        }
+
+        metadata(data) <- metadata(object)
+        metadata(data)[["format"]] <- format
+        new(Class = "Ensembl2Entrez", data)
+    }
+
+
+
+Ensembl2Entrez.GRanges <-  # nolint
+    function(object) {
+        data <- as(object, "DataFrame")
+        metadata(data) <- metadata(object)
+        do.call(
+            what = Ensembl2Entrez,
+            args = list(
+                object = data,
+                format = format
+            )
+        )
+    }
+formals(Ensembl2Entrez.GRanges) <- formals(Ensembl2Entrez.DataFrame)
+
+
+
 Ensembl2Entrez.SummarizedExperiment <-  # nolint
     function(object) {
         object <- as.SummarizedExperiment(object)
-        rowData <- rowData(object)
-        assert_is_subset(
-            x = c("entrezID", "geneID"),
-            y = colnames(rowData)
+        do.call(
+            what = Ensembl2Entrez,
+            args = list(
+                object = rowData(object),
+                format = format
+            )
         )
-        entrezID <- rowData[["entrezID"]]
-        assert_is_list(entrezID)
-        names(entrezID) <- rowData[["geneID"]]
-        # For genes that don't map 1:1 with Entrez, use the oldest Entrez ID.
-        map <- lapply(entrezID, function(x) {
-            if (all(is.na(x))) {
-                NULL
-            } else {
-                sort(x)[[1L]]
-            }
-        })
-        # Drop Ensembl genes that don't map to Entrez.
-        map <- Filter(Negate(is.null), map)
-        # Ensembl gene IDs are names. Entrez gene ID are values.
-        vec <- unlist(map)
-        data <- DataFrame(
-            geneID = names(vec),
-            entrezID = vec,
-            row.names = names(vec)
-        )
-        metadata(data) <- metadata(rowData)
-        new(Class = "Ensembl2Entrez", data)
+
+
     }
+formals(Ensembl2Entrez.SummarizedExperiment) <-
+    formals(Ensembl2Entrez.DataFrame)
+
+
+
+#' @rdname Ensembl2Entrez
+#' @export
+setMethod(
+    f = "Ensembl2Entrez",
+    signature = signature("DataFrame"),
+    definition = Ensembl2Entrez.DataFrame
+)
+
+
+
+#' @rdname Ensembl2Entrez
+#' @export
+setMethod(
+    f = "Ensembl2Entrez",
+    signature = signature("GRanges"),
+    definition = Ensembl2Entrez.GRanges
+)
 
 
 
