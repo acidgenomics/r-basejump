3199e4b1d409d397cf500f93cf95c28586e1bace
Michael Steinbaugh <mike@steinbaugh.com>
2018-10-25 09:30:58 -0400

    Improve path normalization for AppVeyor CI in `realpath()`

--- R/realpath.R
+++ R/realpath.R
@@ -1,11 +1,19 @@
 #' @inherit base::normalizePath
 #' @export
-#' @seealso [base::normalizePath()].
+#' @seealso
+#' - [base::file.path()].
+#' - [base::normalizePath()].
 #' @examples
 #' realpath(".")
 #' normalizePath(".")
 realpath <- function(path) {
     assert_is_character(path)
     assert_all_are_existing_files(path)
-    normalizePath(path = path, mustWork = TRUE)
+    # Ensure we're matching the platform conventions.
+    # For example, AppVeyor CI tests on Windows but uses "/" instead of "\\".
+    normalizePath(
+        path = path,
+        winslash = .Platform$file.sep,  # nolint
+        mustWork = TRUE
+    )
 }
