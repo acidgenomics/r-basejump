5c83d6a77b14d41f74683dc4b8785002e6a22920
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-11-02 12:56:53 -0400

    Improve the documentation

--- data-raw/NAMESPACE.R
+++ data-raw/NAMESPACE.R
@@ -13,11 +13,43 @@ ns <- lapply(
         x[!grepl("^.__", x)]
     }
 )
-ns <- unlist(ns)
-ns <- sort(unique(as.character(ns)))
 
-con <- stdout()
-# con <- file("NAMESPACE")
-text <- paste0("export(\"", ns, "\")")
-writeLines(text = text, con = con)
-# close(con)
+
+
+# roxygen2 =====================================================================
+# This method is preferred because it automatically creates reexport
+# documentation in `man/reexports.Rd`.
+# #' @importFrom package function
+# #' @export
+# package::function
+roxygen <- mapply(
+    fun = ns,
+    package = names(ns),
+    FUN = function(fun, package) {
+        vapply(
+            X = fun,
+            FUN = function(fun) {
+                # FIXME Improve backtick escape method here.
+                paste0(
+                    "#' @importFrom ", package, " ", fun, "\n",
+                    "#' @export\n",
+                    package, "::", "`", fun, "`"
+                )
+            },
+            FUN.VALUE = character(1L),
+            USE.NAMES = FALSE
+        )
+    },
+    SIMPLIFY = FALSE,
+    USE.NAMES = FALSE
+)
+cat(unlist(roxygen), sep = "\n\n")
+
+
+
+# Manual addition to NAMESPACE =================================================
+# This method is simple but won't add documentation.
+x <- unlist(ns)
+x <- sort(unique(as.character(x)))
+text <- paste0("export(\"", x, "\")")
+cat(text, sep = "\n")
