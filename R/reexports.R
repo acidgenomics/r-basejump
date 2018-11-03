a53a7712078a04739df5d663dbb02c36aa4d33f4
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-03 07:10:38 -0400

    Improve automatic reexported function generation

---
+++ R/reexports.R
@@ -0,0 +1,752 @@
+#' @importFrom basejump.annotations annotable
+#' @export
+basejump.annotations::`annotable` 
+
+#' @importFrom basejump.annotations convertUCSCBuildToEnsembl
+#' @export
+basejump.annotations::`convertUCSCBuildToEnsembl` 
+
+#' @importFrom basejump.annotations emptyRanges
+#' @export
+basejump.annotations::`emptyRanges` 
+
+#' @importFrom basejump.annotations geneSynonyms
+#' @export
+basejump.annotations::`geneSynonyms` 
+
+#' @importFrom basejump.annotations makeGRangesFromEnsDb
+#' @export
+basejump.annotations::`makeGRangesFromEnsDb` 
+
+#' @importFrom basejump.annotations makeGRangesFromEnsembl
+#' @export
+basejump.annotations::`makeGRangesFromEnsembl` 
+
+#' @importFrom basejump.annotations makeGRangesFromGFF
+#' @export
+basejump.annotations::`makeGRangesFromGFF` 
+
+#' @importFrom basejump.annotations makeGRangesFromGTF
+#' @export
+basejump.annotations::`makeGRangesFromGTF` 
+
+#' @importFrom basejump.annotations makeGene2SymbolFromEnsDb
+#' @export
+basejump.annotations::`makeGene2SymbolFromEnsDb` 
+
+#' @importFrom basejump.annotations makeGene2SymbolFromEnsembl
+#' @export
+basejump.annotations::`makeGene2SymbolFromEnsembl` 
+
+#' @importFrom basejump.annotations makeGene2SymbolFromGFF
+#' @export
+basejump.annotations::`makeGene2SymbolFromGFF` 
+
+#' @importFrom basejump.annotations makeGene2SymbolFromGTF
+#' @export
+basejump.annotations::`makeGene2SymbolFromGTF` 
+
+#' @importFrom basejump.annotations makeTx2GeneFromEnsDb
+#' @export
+basejump.annotations::`makeTx2GeneFromEnsDb` 
+
+#' @importFrom basejump.annotations makeTx2GeneFromEnsembl
+#' @export
+basejump.annotations::`makeTx2GeneFromEnsembl` 
+
+#' @importFrom basejump.annotations makeTx2GeneFromGFF
+#' @export
+basejump.annotations::`makeTx2GeneFromGFF` 
+
+#' @importFrom basejump.annotations makeTx2GeneFromGTF
+#' @export
+basejump.annotations::`makeTx2GeneFromGTF` 
+
+#' @importFrom basejump.assertions assertAllAreNonExisting
+#' @export
+basejump.assertions::`assertAllAreNonExisting` 
+
+#' @importFrom basejump.assertions assertAllAreURL
+#' @export
+basejump.assertions::`assertAllAreURL` 
+
+#' @importFrom basejump.assertions assertAllAreUniqueGeneNames
+#' @export
+basejump.assertions::`assertAllAreUniqueGeneNames` 
+
+#' @importFrom basejump.assertions assertAllAreValidNames
+#' @export
+basejump.assertions::`assertAllAreValidNames` 
+
+#' @importFrom basejump.assertions assertAreGeneAnnotations
+#' @export
+basejump.assertions::`assertAreGeneAnnotations` 
+
+#' @importFrom basejump.assertions assertAreTranscriptAnnotations
+#' @export
+basejump.assertions::`assertAreTranscriptAnnotations` 
+
+#' @importFrom basejump.assertions assertFormalCompress
+#' @export
+basejump.assertions::`assertFormalCompress` 
+
+#' @importFrom basejump.assertions assertFormalGene2Symbol
+#' @export
+basejump.assertions::`assertFormalGene2Symbol` 
+
+#' @importFrom basejump.assertions assertFormalInterestingGroups
+#' @export
+basejump.assertions::`assertFormalInterestingGroups` 
+
+#' @importFrom basejump.assertions assertHasAggregateInfo
+#' @export
+basejump.assertions::`assertHasAggregateInfo` 
+
+#' @importFrom basejump.assertions assertHasRownames
+#' @export
+basejump.assertions::`assertHasRownames` 
+
+#' @importFrom basejump.assertions assertHasValidDimnames
+#' @export
+basejump.assertions::`assertHasValidDimnames` 
+
+#' @importFrom basejump.assertions assertHasValidNames
+#' @export
+basejump.assertions::`assertHasValidNames` 
+
+#' @importFrom basejump.assertions assertIsANumberOrNULL
+#' @export
+basejump.assertions::`assertIsANumberOrNULL` 
+
+#' @importFrom basejump.assertions assertIsAStringOrNULL
+#' @export
+basejump.assertions::`assertIsAStringOrNULL` 
+
+#' @importFrom basejump.assertions assertIsAlpha
+#' @export
+basejump.assertions::`assertIsAlpha` 
+
+#' @importFrom basejump.assertions assertIsAnImplicitInteger
+#' @export
+basejump.assertions::`assertIsAnImplicitInteger` 
+
+#' @importFrom basejump.assertions assertIsAnImplicitIntegerOrNULL
+#' @export
+basejump.assertions::`assertIsAnImplicitIntegerOrNULL` 
+
+#' @importFrom basejump.assertions assertIsAnIntegerOrNULL
+#' @export
+basejump.assertions::`assertIsAnIntegerOrNULL` 
+
+#' @importFrom basejump.assertions assertIsColorScaleContinuousOrNULL
+#' @export
+basejump.assertions::`assertIsColorScaleContinuousOrNULL` 
+
+#' @importFrom basejump.assertions assertIsColorScaleDiscreteOrNULL
+#' @export
+basejump.assertions::`assertIsColorScaleDiscreteOrNULL` 
+
+#' @importFrom basejump.assertions assertIsDir
+#' @export
+basejump.assertions::`assertIsDir` 
+
+#' @importFrom basejump.assertions assertIsFillScaleContinuousOrNULL
+#' @export
+basejump.assertions::`assertIsFillScaleContinuousOrNULL` 
+
+#' @importFrom basejump.assertions assertIsFillScaleDiscreteOrNULL
+#' @export
+basejump.assertions::`assertIsFillScaleDiscreteOrNULL` 
+
+#' @importFrom basejump.assertions assertIsHeaderLevel
+#' @export
+basejump.assertions::`assertIsHeaderLevel` 
+
+#' @importFrom basejump.assertions assertIsHexColorFunctionOrNULL
+#' @export
+basejump.assertions::`assertIsHexColorFunctionOrNULL` 
+
+#' @importFrom basejump.assertions assertIsImplicitInteger
+#' @export
+basejump.assertions::`assertIsImplicitInteger` 
+
+#' @importFrom basejump.assertions assertIsImplicitIntegerOrNULL
+#' @export
+basejump.assertions::`assertIsImplicitIntegerOrNULL` 
+
+#' @importFrom basejump.assertions hasAggregateInfo
+#' @export
+basejump.assertions::`hasAggregateInfo` 
+
+#' @importFrom basejump.assertions hasRownames
+#' @export
+basejump.assertions::`hasRownames` 
+
+#' @importFrom basejump.assertions isAnImplicitInteger
+#' @export
+basejump.assertions::`isAnImplicitInteger` 
+
+#' @importFrom basejump.assertions isImplicitInteger
+#' @export
+basejump.assertions::`isImplicitInteger` 
+
+#' @importFrom basejump.assertions isURL
+#' @export
+basejump.assertions::`isURL` 
+
+#' @importFrom basejump.assertions validDimnames
+#' @export
+basejump.assertions::`validDimnames` 
+
+#' @importFrom basejump.assertions validNames
+#' @export
+basejump.assertions::`validNames` 
+
+#' @importFrom basejump.classes EggNOG
+#' @export
+basejump.classes::`EggNOG` 
+
+#' @importFrom basejump.classes Ensembl2Entrez
+#' @export
+basejump.classes::`Ensembl2Entrez` 
+
+#' @importFrom basejump.classes Gene2Symbol
+#' @export
+basejump.classes::`Gene2Symbol` 
+
+#' @importFrom basejump.classes HGNC2Ensembl
+#' @export
+basejump.classes::`HGNC2Ensembl` 
+
+#' @importFrom basejump.classes MGI2Ensembl
+#' @export
+basejump.classes::`MGI2Ensembl` 
+
+#' @importFrom basejump.classes PANTHER
+#' @export
+basejump.classes::`PANTHER` 
+
+#' @importFrom basejump.classes Tx2Gene
+#' @export
+basejump.classes::`Tx2Gene` 
+
+#' @importFrom basejump.classes show
+#' @export
+basejump.classes::`show` 
+
+#' @importFrom basejump.coercion as.DataFrame
+#' @export
+basejump.coercion::`as.DataFrame` 
+
+#' @importFrom basejump.coercion as.SummarizedExperiment
+#' @export
+basejump.coercion::`as.SummarizedExperiment` 
+
+#' @importFrom basejump.coercion as.data.frame
+#' @export
+basejump.coercion::`as.data.frame` 
+
+#' @importFrom basejump.coercion as_tibble
+#' @export
+basejump.coercion::`as_tibble` 
+
+#' @importFrom basejump.coercion coerce
+#' @export
+basejump.coercion::`coerce` 
+
+#' @importFrom basejump.coercion coerceS4ToList
+#' @export
+basejump.coercion::`coerceS4ToList` 
+
+#' @importFrom basejump.coercion flatFiles
+#' @export
+basejump.coercion::`flatFiles` 
+
+#' @importFrom basejump.developer RdTags
+#' @export
+basejump.developer::`RdTags` 
+
+#' @importFrom basejump.developer checkClasses
+#' @export
+basejump.developer::`checkClasses` 
+
+#' @importFrom basejump.developer detectHPC
+#' @export
+basejump.developer::`detectHPC` 
+
+#' @importFrom basejump.developer dots
+#' @export
+basejump.developer::`dots` 
+
+#' @importFrom basejump.developer extractLocal
+#' @export
+basejump.developer::`extractLocal` 
+
+#' @importFrom basejump.developer hasLocal
+#' @export
+basejump.developer::`hasLocal` 
+
+#' @importFrom basejump.developer matchArgsToDoCall
+#' @export
+basejump.developer::`matchArgsToDoCall` 
+
+#' @importFrom basejump.developer methodFormals
+#' @export
+basejump.developer::`methodFormals` 
+
+#' @importFrom basejump.developer methodFunction
+#' @export
+basejump.developer::`methodFunction` 
+
+#' @importFrom basejump.developer multiassignAsEnvir
+#' @export
+basejump.developer::`multiassignAsEnvir` 
+
+#' @importFrom basejump.developer parseRd
+#' @export
+basejump.developer::`parseRd` 
+
+#' @importFrom basejump.developer printString
+#' @export
+basejump.developer::`printString` 
+
+#' @importFrom basejump.developer separator
+#' @export
+basejump.developer::`separator` 
+
+#' @importFrom basejump.developer showSlotInfo
+#' @export
+basejump.developer::`showSlotInfo` 
+
+#' @importFrom basejump.developer standardizeCall
+#' @export
+basejump.developer::`standardizeCall` 
+
+#' @importFrom basejump.experiment combine
+#' @export
+basejump.experiment::`combine` 
+
+#' @importFrom basejump.experiment counts
+#' @export
+basejump.experiment::`counts` 
+
+#' @importFrom basejump.experiment counts<-
+#' @export
+basejump.experiment::`counts<-` 
+
+#' @importFrom basejump.experiment detectLanes
+#' @export
+basejump.experiment::`detectLanes` 
+
+#' @importFrom basejump.experiment detectOrganism
+#' @export
+basejump.experiment::`detectOrganism` 
+
+#' @importFrom basejump.experiment makeSingleCellExperiment
+#' @export
+basejump.experiment::`makeSingleCellExperiment` 
+
+#' @importFrom basejump.experiment makeSummarizedExperiment
+#' @export
+basejump.experiment::`makeSummarizedExperiment` 
+
+#' @importFrom basejump.experiment mapCellsToSamples
+#' @export
+basejump.experiment::`mapCellsToSamples` 
+
+#' @importFrom basejump.experiment matchInterestingGroups
+#' @export
+basejump.experiment::`matchInterestingGroups` 
+
+#' @importFrom basejump.experiment microplate
+#' @export
+basejump.experiment::`microplate` 
+
+#' @importFrom basejump.experiment minimalSampleData
+#' @export
+basejump.experiment::`minimalSampleData` 
+
+#' @importFrom basejump.experiment organism
+#' @export
+basejump.experiment::`organism` 
+
+#' @importFrom basejump.experiment sampleNames
+#' @export
+basejump.experiment::`sampleNames` 
+
+#' @importFrom basejump.experiment sampleNames<-
+#' @export
+basejump.experiment::`sampleNames<-` 
+
+#' @importFrom basejump.generics aggregateCellsToSamples
+#' @export
+basejump.generics::`aggregateCellsToSamples` 
+
+#' @importFrom basejump.generics aggregateCols
+#' @export
+basejump.generics::`aggregateCols` 
+
+#' @importFrom basejump.generics aggregateRows
+#' @export
+basejump.generics::`aggregateRows` 
+
+#' @importFrom basejump.generics camel
+#' @export
+basejump.generics::`camel` 
+
+#' @importFrom basejump.generics cell2sample
+#' @export
+basejump.generics::`cell2sample` 
+
+#' @importFrom basejump.generics collapseToString
+#' @export
+basejump.generics::`collapseToString` 
+
+#' @importFrom basejump.generics convertGenesToSymbols
+#' @export
+basejump.generics::`convertGenesToSymbols` 
+
+#' @importFrom basejump.generics convertSampleIDsToNames
+#' @export
+basejump.generics::`convertSampleIDsToNames` 
+
+#' @importFrom basejump.generics convertSymbolsToGenes
+#' @export
+basejump.generics::`convertSymbolsToGenes` 
+
+#' @importFrom basejump.generics convertTranscriptsToGenes
+#' @export
+basejump.generics::`convertTranscriptsToGenes` 
+
+#' @importFrom basejump.generics dotted
+#' @export
+basejump.generics::`dotted` 
+
+#' @importFrom basejump.generics export
+#' @export
+basejump.generics::`export` 
+
+#' @importFrom basejump.generics foldChangeToLogRatio
+#' @export
+basejump.generics::`foldChangeToLogRatio` 
+
+#' @importFrom basejump.generics geometricMean
+#' @export
+basejump.generics::`geometricMean` 
+
+#' @importFrom basejump.generics headtail
+#' @export
+basejump.generics::`headtail` 
+
+#' @importFrom basejump.generics interestingGroups
+#' @export
+basejump.generics::`interestingGroups` 
+
+#' @importFrom basejump.generics interestingGroups<-
+#' @export
+basejump.generics::`interestingGroups<-` 
+
+#' @importFrom basejump.generics logRatioToFoldChange
+#' @export
+basejump.generics::`logRatioToFoldChange` 
+
+#' @importFrom basejump.generics mapGenesToIDs
+#' @export
+basejump.generics::`mapGenesToIDs` 
+
+#' @importFrom basejump.generics mapGenesToRownames
+#' @export
+basejump.generics::`mapGenesToRownames` 
+
+#' @importFrom basejump.generics mapGenesToSymbols
+#' @export
+basejump.generics::`mapGenesToSymbols` 
+
+#' @importFrom basejump.generics markdown
+#' @export
+basejump.generics::`markdown` 
+
+#' @importFrom basejump.generics meltCounts
+#' @export
+basejump.generics::`meltCounts` 
+
+#' @importFrom basejump.generics metrics
+#' @export
+basejump.generics::`metrics` 
+
+#' @importFrom basejump.generics metricsPerSample
+#' @export
+basejump.generics::`metricsPerSample` 
+
+#' @importFrom basejump.generics plotCorrelationHeatmap
+#' @export
+basejump.generics::`plotCorrelationHeatmap` 
+
+#' @importFrom basejump.generics plotCountsPerBiotype
+#' @export
+basejump.generics::`plotCountsPerBiotype` 
+
+#' @importFrom basejump.generics plotCountsPerBroadClass
+#' @export
+basejump.generics::`plotCountsPerBroadClass` 
+
+#' @importFrom basejump.generics plotCountsPerGene
+#' @export
+basejump.generics::`plotCountsPerGene` 
+
+#' @importFrom basejump.generics plotGenderMarkers
+#' @export
+basejump.generics::`plotGenderMarkers` 
+
+#' @importFrom basejump.generics plotGene
+#' @export
+basejump.generics::`plotGene` 
+
+#' @importFrom basejump.generics plotGenesDetected
+#' @export
+basejump.generics::`plotGenesDetected` 
+
+#' @importFrom basejump.generics plotHeatmap
+#' @export
+basejump.generics::`plotHeatmap` 
+
+#' @importFrom basejump.generics plotQC
+#' @export
+basejump.generics::`plotQC` 
+
+#' @importFrom basejump.generics plotQuantileHeatmap
+#' @export
+basejump.generics::`plotQuantileHeatmap` 
+
+#' @importFrom basejump.generics plotTotalCounts
+#' @export
+basejump.generics::`plotTotalCounts` 
+
+#' @importFrom basejump.generics plotZerosVsDepth
+#' @export
+basejump.generics::`plotZerosVsDepth` 
+
+#' @importFrom basejump.generics removeNA
+#' @export
+basejump.generics::`removeNA` 
+
+#' @importFrom basejump.generics sampleData
+#' @export
+basejump.generics::`sampleData` 
+
+#' @importFrom basejump.generics sampleData<-
+#' @export
+basejump.generics::`sampleData<-` 
+
+#' @importFrom basejump.generics sanitizeNA
+#' @export
+basejump.generics::`sanitizeNA` 
+
+#' @importFrom basejump.generics sanitizePercent
+#' @export
+basejump.generics::`sanitizePercent` 
+
+#' @importFrom basejump.generics selectSamples
+#' @export
+basejump.generics::`selectSamples` 
+
+#' @importFrom basejump.generics snake
+#' @export
+basejump.generics::`snake` 
+
+#' @importFrom basejump.generics stripTranscriptVersions
+#' @export
+basejump.generics::`stripTranscriptVersions` 
+
+#' @importFrom basejump.generics subsetPerSample
+#' @export
+basejump.generics::`subsetPerSample` 
+
+#' @importFrom basejump.generics topCellsPerSample
+#' @export
+basejump.generics::`topCellsPerSample` 
+
+#' @importFrom basejump.generics tpm
+#' @export
+basejump.generics::`tpm` 
+
+#' @importFrom basejump.generics uniteInterestingGroups
+#' @export
+basejump.generics::`uniteInterestingGroups` 
+
+#' @importFrom basejump.generics upperCamel
+#' @export
+basejump.generics::`upperCamel` 
+
+#' @importFrom basejump.generics zerosVsDepth
+#' @export
+basejump.generics::`zerosVsDepth` 
+
+#' @importFrom basejump.globals barcodePattern
+#' @export
+basejump.globals::`barcodePattern` 
+
+#' @importFrom basejump.globals basejumpCacheURL
+#' @export
+basejump.globals::`basejumpCacheURL` 
+
+#' @importFrom basejump.globals extPattern
+#' @export
+basejump.globals::`extPattern` 
+
+#' @importFrom basejump.globals genomeMetadataNames
+#' @export
+basejump.globals::`genomeMetadataNames` 
+
+#' @importFrom basejump.globals lanePattern
+#' @export
+basejump.globals::`lanePattern` 
+
+#' @importFrom basejump.globals naStrings
+#' @export
+basejump.globals::`naStrings` 
+
+#' @importFrom basejump.globals rdataExtPattern
+#' @export
+basejump.globals::`rdataExtPattern` 
+
+#' @importFrom basejump.globals rdataLoadError
+#' @export
+basejump.globals::`rdataLoadError` 
+
+#' @importFrom basejump.io assignAndSaveData
+#' @export
+basejump.io::`assignAndSaveData` 
+
+#' @importFrom basejump.io import
+#' @export
+basejump.io::`import` 
+
+#' @importFrom basejump.io initDir
+#' @export
+basejump.io::`initDir` 
+
+#' @importFrom basejump.io loadData
+#' @export
+basejump.io::`loadData` 
+
+#' @importFrom basejump.io loadDataAsName
+#' @export
+basejump.io::`loadDataAsName` 
+
+#' @importFrom basejump.io loadRemoteData
+#' @export
+basejump.io::`loadRemoteData` 
+
+#' @importFrom basejump.io localOrRemoteFile
+#' @export
+basejump.io::`localOrRemoteFile` 
+
+#' @importFrom basejump.io realpath
+#' @export
+basejump.io::`realpath` 
+
+#' @importFrom basejump.io saveData
+#' @export
+basejump.io::`saveData` 
+
+#' @importFrom basejump.io transmit
+#' @export
+basejump.io::`transmit` 
+
+#' @importFrom basejump.io writeCounts
+#' @export
+basejump.io::`writeCounts` 
+
+#' @importFrom basejump.markdown markdownHeader
+#' @export
+basejump.markdown::`markdownHeader` 
+
+#' @importFrom basejump.markdown markdownLink
+#' @export
+basejump.markdown::`markdownLink` 
+
+#' @importFrom basejump.markdown markdownList
+#' @export
+basejump.markdown::`markdownList` 
+
+#' @importFrom basejump.markdown markdownPlotlist
+#' @export
+basejump.markdown::`markdownPlotlist` 
+
+#' @importFrom basejump.markdown markdownTables
+#' @export
+basejump.markdown::`markdownTables` 
+
+#' @importFrom basejump.markdown mdHeader
+#' @export
+basejump.markdown::`mdHeader` 
+
+#' @importFrom basejump.markdown mdLink
+#' @export
+basejump.markdown::`mdLink` 
+
+#' @importFrom basejump.markdown mdList
+#' @export
+basejump.markdown::`mdList` 
+
+#' @importFrom basejump.markdown mdPlotlist
+#' @export
+basejump.markdown::`mdPlotlist` 
+
+#' @importFrom basejump.markdown prepareTemplate
+#' @export
+basejump.markdown::`prepareTemplate` 
+
+#' @importFrom basejump.plots basejump_geom_abline
+#' @export
+basejump.plots::`basejump_geom_abline` 
+
+#' @importFrom basejump.plots basejump_geom_label
+#' @export
+basejump.plots::`basejump_geom_label` 
+
+#' @importFrom basejump.plots basejump_geom_label_average
+#' @export
+basejump.plots::`basejump_geom_label_average` 
+
+#' @importFrom basejump.plots basejump_geom_label_repel
+#' @export
+basejump.plots::`basejump_geom_label_repel` 
+
+#' @importFrom basejump.plots theme_midnight
+#' @export
+basejump.plots::`theme_midnight` 
+
+#' @importFrom basejump.plots theme_paperwhite
+#' @export
+basejump.plots::`theme_paperwhite` 
+
+#' @importFrom basejump.sanitization makeDimnames
+#' @export
+basejump.sanitization::`makeDimnames` 
+
+#' @importFrom basejump.sanitization makeNames
+#' @export
+basejump.sanitization::`makeNames` 
+
+#' @importFrom basejump.sanitization sanitizeRowData
+#' @export
+basejump.sanitization::`sanitizeRowData` 
+
+#' @importFrom basejump.sanitization sanitizeSampleData
+#' @export
+basejump.sanitization::`sanitizeSampleData` 
+
+#' @importFrom basejump.sanitization sortUnique
+#' @export
+basejump.sanitization::`sortUnique` 
+
+#' @importFrom basejump.sanitization toStringUnique
+#' @export
+basejump.sanitization::`toStringUnique` 
+
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%` 
+
