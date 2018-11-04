ee9a6bd6db9fdb871205f510c839c8405aa3d92e
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-04 11:49:05 -0500

    Simplify the reexport documentation

--- R/classes.R
+++ R/classes.R
@@ -2,30 +2,45 @@
 # Safe to remove in a future update, once depency packages are updated.
 # Refer to basejump.classes for documentation.
 
-#' @inherit basejump.classes::`EggNOG-class`
+
+
+#' Classes exported from other packages
+#'
+#' These classes are imported from other packages.
+#' Follow the links below to see their documentation.
+#'
+#' - [basejump.classes::`EggNOG-class`].
+#' - [basejump.classes::`Ensembl2Entrez-class`].
+#' - [basejump.classes::`Gene2Symbol-class`].
+#' - [basejump.classes::`HGNC2Ensembl-class`].
+#' - [basejump.classes::`MGI2Ensembl-class`].
+#' - [basejump.classes::`PANTHER-class`].
+#' - [basejump.classes::`Tx2Gene-class`].
+#'
+#' @name classes
+#' @docType import
 #' @keywords internal
+NULL
+
+
+
+#' @rdname classes
 setClass(Class = "EggNOG", contains = "SimpleDataFrameList")
 
-#' @inherit basejump.classes::`Ensembl2Entrez-class`
-#' @keywords internal
+#' @rdname classes
 setClass(Class = "Ensembl2Entrez", contains = "DataFrame")
 
-#' @inherit basejump.classes::`Gene2Symbol-class`
-#' @keywords internal
+#' @rdname classes
 setClass(Class = "Gene2Symbol", contains = "DataFrame")
 
-#' @inherit basejump.classes::`HGNC2Ensembl-class`
-#' @keywords internal
+#' @rdname classes
 setClass(Class = "HGNC2Ensembl", contains = "DataFrame")
 
-#' @inherit basejump.classes::`MGI2Ensembl-class`
-#' @keywords internal
+#' @rdname classes
 setClass(Class = "MGI2Ensembl", contains = "DataFrame")
 
-#' @inherit basejump.classes::`PANTHER-class`
-#' @keywords internal
+#' @rdname classes
 setClass(Class = "PANTHER", contains = "DataFrame")
 
-#' @inherit basejump.classes::`Tx2Gene-class`
-#' @keywords internal
+#' @rdname classes
 setClass(Class = "Tx2Gene", contains = "DataFrame")
