f9b480ed587ffa9c062f8bed9987db23c0b02c4b
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-06 09:48:53 -0500

    Draft commit of generics

---
+++ R/AllGenerics.R
@@ -0,0 +1,30 @@
+#' @rdname Ensembl2Entrez
+#' @export
+setGeneric(
+    name = "Ensembl2Entrez",
+    def = function(object, ...) {
+        standardGeneric("Ensembl2Entrez")
+    }
+)
+
+
+
+#' @rdname Gene2Symbol
+#' @export
+setGeneric(
+    name = "Gene2Symbol",
+    def = function(object, ...) {
+        standardGeneric("Gene2Symbol")
+    }
+)
+
+
+
+#' @rdname Tx2Gene
+#' @export
+setGeneric(
+    name = "Tx2Gene",
+    def = function(object, ...) {
+        standardGeneric("Tx2Gene")
+    }
+)
