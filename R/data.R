102e64e9464b3f6960e7328215418ce03c4820c1
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-06 09:39:45 -0500

    Add back data

---
+++ R/data.R
@@ -0,0 +1,74 @@
+#' Example S4 DataFrame
+#' @examples
+#' data(df)
+#' df
+"df"
+
+
+
+#' Sexually Dimorphic Gender Marker Genes
+#' @examples
+#' data(gender_markers)
+#' gender_markers
+"gender_markers"
+
+
+
+#' Example Dense Matrix
+#' @examples
+#' data(mat)
+#' mat
+"mat"
+
+
+
+#' Organism Mappings
+#' @examples
+#' data(organism_mappings)
+#' organism_mappings
+"organism_mappings"
+
+
+
+#' Example RNA-Seq Data Set
+#'
+#' Generated with `DESeq2::makeExampleDESeqDataSet()`. Contains actual gene
+#' identifiers and Ensembl annotations.
+#'
+#' @examples
+#' data(rse)
+#' rse
+"rse"
+
+
+
+#' Example Single-Cell RNA-Seq Data Set
+#' @seealso `scater::sc_example_counts`.
+#' @examples
+#' data(sce)
+#' sce
+"sce"
+
+
+
+#' Example Sparse Matrix
+#' @examples
+#' data(sparse)
+#' sparse
+"sparse"
+
+
+
+#' Example Tibble
+#' @examples
+#' data(tbl)
+#' tbl
+"tbl"
+
+
+
+#' Example Transcript-Level Data Set
+#' @examples
+#' data(tx_se)
+#' tx_se
+"tx_se"
