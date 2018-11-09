a321d188f91988f7cf04eb46eb9a61b495b6286e
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-11-09 17:20:27 -0500

    Initial commit of `makeSampleData()` utility function

---
+++ R/makeSampleData.R
@@ -0,0 +1,55 @@
+#' Make Sample Data
+#'
+#' Utility function that prepares metadata to be slotted into
+#' `SummarizedExperiment::colData()`.
+#'
+#' This function adheres to the following conventions:
+#'
+#' - All column names will be converted to lower camel case
+#'   (see [camel()] for details).
+#' - Required columns:
+#'   - `sampleName`: Human readable sample names. Note that this column is
+#'     useful for plots and doesn't have to match the column names of a
+#'     `SummarizedExperiment` object, which should use valid names.
+#' - Blacklisted columns:
+#'   - `filename` (use `fileName`).
+#'   - `rowname`.
+#'   - `sample`. Too vague. Does this represent an ID or human readable name?
+#'   - `samplename` (use `sampleName`),
+#' - `sampleName` column is always placed first.
+#'
+#' @inheritParams params
+#' @export
+#'
+#' @return `DataFrame`.
+#'
+#' @seealso [makeNames].
+#'
+#' @examples
+#' object <- data.frame(
+#'     genotype = rep(c("control", "wildtype"), times = 2L),
+#'     treatment = rep(c("vector", "RNAi"), each = 2L),
+#'     sampleName = paste("sample", seq_len(4L)),
+#'     row.names = paste0("GSM000000", seq_len(4L))
+#' )
+makeSampleData <- function(object) {
+    assert_has_dims(object)
+    assert_is_subset("sampleName", colnames(object))
+    assert_are_disjoint_sets(
+        x = c(
+            "filename",
+            "rowname",
+            "sample",
+            "samplename"
+        ),
+        y = colnames(object)
+    )
+    assertHasRownames(object)
+    assertAreValidNames(rownames(object))
+    object %>%
+        as_tibble(rownames = "rowname") %>%
+        mutate_all(as.factor) %>%
+        mutate_all(droplevels) %>%
+        select(!!sym("sampleName"), everything()) %>%
+        as("DataFrame")
+}
