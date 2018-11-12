e8e970a5f26e03f0557bced601d4e79e9db7bd32
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-11 20:57:06 -0500

    Switch to importing `assertAreURLs()` from goalie package

--- R/package.R
+++ R/package.R
@@ -77,7 +77,7 @@
 #'   guides labs position_jitterdodge scale_x_continuous scale_y_continuous
 #'   stat_ecdf stat_summary theme theme_linedraw
 #' @importFrom ggrepel geom_label_repel
-#' @importFrom goalie assertAreNonExisting
+#' @importFrom goalie assertAreNonExisting assertAreURLs
 #' @importFrom grDevices colorRampPalette
 #' @importFrom grid arrow unit
 #' @importFrom jsonlite read_json
