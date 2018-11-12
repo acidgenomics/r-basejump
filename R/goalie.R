e4f9a942df30b8c70aae7c3605680e74ec501859
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-11 20:29:38 -0500

    Migrate assertive check functions to goalie package

---
+++ R/goalie.R
@@ -0,0 +1,118 @@
+# assertFormalGene2Symbol ======================================================
+#' Check Gene-to-Symbol Mappings
+#'
+#' @inherit params
+#' @export
+#'
+#' @param x x class supporting `rownames`. All rownames in this x
+#'   must intersect with the rownames defined in the `gene2symbol` argument.
+#' @param genes `character`. Gene identifiers. Note that gene names (symbols)
+#'   are also supported, but not recommended if the stable IDs can be easily
+#'   provided instead.
+#' @param gene2symbol `Gene2Symbol`. Gene-to-symbol mappings. Must contain
+#'   `geneID` and `geneName` columns, with rownames defined. All of the `x`
+#'   rownames must be defined here, otherwise the function will error.
+#'
+#' @examples
+#' DataFrame <- S4Vectors::DataFrame
+#' Gene2Symbol <- basejump::Gene2Symbol
+#'
+#' x <- DataFrame(
+#'     "sample1" = c(1L, 2L),
+#'     "sample2" = c(3L, 4L),
+#'     row.names = c("gene1", "gene2")
+#' )
+#' print(x)
+#'
+#' gene2symbol <- Gene2Symbol(
+#'     x = DataFrame(
+#'         geneID = c("ENSG00000000003", "ENSG00000000005"),
+#'         geneName = c("TSPAN6", "TNMD"),
+#'         row.names = rownames(x)
+#'     )
+#' )
+#' print(gene2symbol)
+#'
+#' geneIDs <- gene2symbol[["geneID"]]
+#' print(geneIDs)
+#'
+#' geneNames <- gene2symbol[["geneName"]]
+#' print(geneNames)
+#'
+#' assertFormalGene2Symbol(
+#'     x = x,
+#'     genes = geneIDs,
+#'     gene2symbol = gene2symbol
+#' )
+#' assertFormalGene2Symbol(
+#'     x = x,
+#'     genes = geneNames,
+#'     gene2symbol = gene2symbol
+#' )
+assertFormalGene2Symbol <- function(
+    x,
+    genes,
+    gene2symbol
+) {
+    requireNamespace("basejump", quietly = TRUE)
+    assertHasRownames(x)
+    assert_is_character(genes)
+    assert_is_non_empty(genes)
+    assert_is_all_of(gene2symbol, "Gene2Symbol")
+    assert_are_identical(
+        x = nrow(x),
+        y = nrow(gene2symbol)
+    )
+    if (is.null(rownames(gene2symbol))) {
+        rownames(gene2symbol) <- rownames(x)
+    }
+    # Map genes to x rownames, using gene2symbol.
+    rownames <- basejump::mapGenesToRownames(
+        x = gene2symbol,
+        genes = genes
+    )
+    assert_is_subset(rownames, rownames(x))
+    invisible()
+}
+
+
+
+# assertFormalInterestingGroups ================================================
+#' Check Interesting Groups
+#'
+#' Prevent unwanted downstream behavior when a missing interesting group
+#' is requested by the user.
+#'
+#' @inherit params
+#' @export
+#'
+#' @param x S4 class x.
+#' @param interestingGroups `character`. Interesting groups.
+#'
+#' @examples
+#' data(rse, package = "basejump")
+#' assertFormalInterestingGroups(rse, "treatment")
+#' assertFormalInterestingGroups(rse, NULL)
+assertFormalInterestingGroups <- function(x, interestingGroups) {
+    requireNamespace("basejump", quietly = TRUE)
+    assert_that(isS4(x))
+    data <- basejump::sampleData(x)
+
+    # Check `interestingGroups` argument.
+    if (is.null(interestingGroups)) {
+        # Early return clean on `NULL` value (e.g. DESeqDataSet).
+        return(invisible())
+    } else {
+        # Otherwise, require that `interestingGroups` is a character.
+        assert_is_character(interestingGroups)
+    }
+
+    # Check intersection with sample data.
+    assert_is_subset(interestingGroups, colnames(data))
+
+    # Check that interesting groups columns are factors.
+    invisible(lapply(
+        X = data[, interestingGroups, drop = FALSE],
+        FUN = assert_is_factor
+    ))
+}
