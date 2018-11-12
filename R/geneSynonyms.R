a6fcaba048a09413fcc9dd2aeb4985d6348e8c45
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-11-12 10:12:15 -0500

    Split out functions and update documentation

---
+++ R/geneSynonyms.R
@@ -0,0 +1,99 @@
+#' Gene Synonyms
+#'
+#' Look up gene synonyms from NCBI.
+#'
+#' @note Synonym support for *Caenorhabditis elegans* is poor on NCBI.
+#' Use the [wormbase](https://steinbaugh.com/wormbase) package instead.
+#'
+#' @export
+#'
+#' @inheritParams params
+#' @param organism `string`. Supported organisms: *Homo sapiens*,
+#'   *Mus musculus*, *Drosophila melanogaster*.
+#'
+#' @return `grouped_df`. Grouped by `geneID` column.
+#'
+#' @examples
+#' options(basejump.test = TRUE)
+#' x <- geneSynonyms(organism = "Homo sapiens")
+#' print(x)
+geneSynonyms <- function(organism) {
+    assert_that(has_internet())
+    organism <- match.arg(
+        arg = organism,
+        choices = .geneSynonymsOrganisms
+    )
+
+    # NCBI uses underscore for species name
+    species <- gsub(" ", "_", organism)
+    if (species == "Drosophila_melanogaster") {
+        kingdom <- "Invertebrates"
+    } else {
+        kingdom <- "Mammalia"
+    }
+
+    genome <- c(kingdom = kingdom, species = species)
+
+    if (isTRUE(getOption("basejump.test"))) {
+        assert_that(organism == "Homo sapiens")
+        file <- file.path(
+            basejumpCacheURL,
+            paste0(snake(organism), ".gene_info.gz")
+        )
+    } else {
+        file <- paste(
+            "ftp://ftp.ncbi.nih.gov",
+            "gene",
+            "DATA",
+            "GENE_INFO",
+            genome[["kingdom"]],
+            paste0(genome[["species"]], ".gene_info.gz"),
+            sep = "/"
+        )
+    }
+
+    data <- read_tsv(
+        file = file,
+        col_types = cols(),
+        progress = FALSE
+    )
+    assert_is_non_empty(data)
+
+    data <- data %>%
+        camel() %>%
+        select(!!!syms(c("symbol", "synonyms", "dbXrefs"))) %>%
+        rename(geneName = !!sym("symbol")) %>%
+        filter(
+            !!sym("synonyms") != "-",
+            !!sym("dbXrefs") != "-"
+        ) %>%
+        mutate(synonyms = str_replace_all(!!sym("synonyms"), "\\|", ", "))
+
+    # Sanitize the identifiers.
+    if (organism == "Drosophila melanogaster") {
+        data <- mutate(
+            data,
+            geneID = str_extract(!!sym("dbXrefs"), "\\bFBgn[0-9]{7}\\b")
+        )
+    } else {
+        data <- mutate(
+            data,
+            geneID = str_extract(!!sym("dbXrefs"), "\\bENS[A-Z]+[0-9]{11}\\b")
+        )
+    }
+
+    data %>%
+        filter(!is.na(!!sym("geneID"))) %>%
+        select(!!!syms(c("geneID", "geneName", "synonyms"))) %>%
+        arrange(!!sym("geneID")) %>%
+        group_by(!!sym("geneID"))
+}
+
+
+
+# Using this for parameterized unit testing.
+.geneSynonymsOrganisms <- c(
+    "Homo sapiens",
+    "Mus musculus",
+    "Drosophila melanogaster"
+)
