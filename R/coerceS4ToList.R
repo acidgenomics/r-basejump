cba2cc267e0c1216f0c94833217270ab2b658020
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-11-13 16:26:29 -0500

    Ensure `flatFiles()` is exported as a generic, for backward compatibility

---
+++ R/coerceS4ToList.R
@@ -0,0 +1,45 @@
+#' Coerce Any S4 Object to List
+#'
+#' @inheritParams methods::coerce
+#' @export
+#'
+#' @seealso `methods::coerce`.
+#'
+#' @examples
+#' data(rse)
+#'
+#' x <- coerceS4ToList(rse)
+#' class(x)
+#' names(x)
+#'
+#' x <- flatFiles(rse)
+#' class(x)
+#' names(x)
+coerceS4ToList <- function(from) {
+    stopifnot(isS4(from))
+    to <- lapply(slotNames(from), function(slot) {
+        if (.hasSlot(from, slot)) {
+            slot(from, slot)
+        } else {
+            NULL  # nocov
+        }
+    })
+    names(to) <- slotNames(from)
+    to
+}
+
+
+
+flatFiles.SummarizedExperiment <- function(object) {
+    coerceS4ToList(object)
+}
+
+
+
+#' @rdname coerceS4ToList
+#' @export
+setMethod(
+    f = "flatFiles",
+    signature = signature("SummarizedExperiment"),
+    definition = flatFiles.SummarizedExperiment
+)
