bb511a01adc909cf922b8a910f68ffaceb236a49
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-13 18:19:36 -0500

    Restore deprecated data

--- R/data.R
+++ R/data.R
@@ -1,10 +1,3 @@
-# FIXME rnaseq_counts
-# FIXME rse_bcb
-# FIXME rse_dds
-# FIXME single_cell_counts
-
-
-
 #' Example S4 DataFrame
 #' @examples
 #' data(df)
@@ -37,6 +30,19 @@
 
 
 
+#' DEPRECATED Example RNA-Seq Counts
+#'
+#' Generated with `DESeq2::makeExampleDESeqDataSet()`.
+#'
+#' @aliases rnaseqCounts
+#' @keywords internal
+#' @examples
+#' data(rnaseq_counts)
+#' summary(rnaseq_counts)
+"rnaseq_counts"
+
+
+
 #' Example RNA-Seq Data Set
 #'
 #' Generated with `DESeq2::makeExampleDESeqDataSet()`. Contains actual gene
@@ -49,12 +55,44 @@
 
 
 
+#' DEPRECATED Example RNA-Seq DataSet
+#' @keywords internal
+#' @examples
+#' data(rse_bcb)
+#' rse_bcb
+"rse_bcb"
+
+
+
+#' DEPRECATED Example DESeq2 Data Set
+#' @keywords internal
+#' @examples
+#' data(rse_dds)
+#' rse_dds
+"rse_dds"
+
+
+
 #' Example Single-Cell RNA-Seq Data Set
 #' @seealso `scater::sc_example_counts`.
 #' @examples
 #' data(sce)
 #' sce
 "sce"
+
+
+
+#' DEPRECATED Example Single-Cell RNA-Seq Counts
+#'
+#' Modified version of `scater::sc_example_counts`, with dimnames formatted
+#' in camel case for package consistency.
+#'
+#' @aliases singleCellCounts
+#' @keywords internal
+#' @examples
+#' data(single_cell_counts)
+#' summary(single_cell_counts)
+"single_cell_counts"
 
 
 
