ab752adfa6f5e0d6f424399671c243781f85ef1e
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-13 15:51:03 -0500

    Consolidate assertive check functions

--- R/assertions.R
+++ R/assertions.R
@@ -97,3 +97,67 @@ assertFormalInterestingGroups <- function(x, interestingGroups) {
         FUN = assert_is_factor
     ))
 }
+
+
+
+# Consider deprecating this in a future release.
+#' Check Classes
+#' @export
+#'
+#' @inheritParams params
+#' @param expected `list`. Named list of expected classes per slot.
+#' @param subset `boolean`. Only check a subset of slots in the object.
+#'
+#' @examples
+#' data(rse)
+#' object <- rse
+#' metadata <- S4Vectors::metadata(object)
+#' checkClasses(
+#'     object = metadata,
+#'     expected = list(
+#'         version = c("package_version", "numeric_version"),
+#'         date = "Date",
+#'         interestingGroups = "character"
+#'     )
+#' )
+checkClasses <- function(
+    object,
+    expected,
+    subset = FALSE
+) {
+    assert_is_list(expected)
+    assert_has_names(expected)
+    assert_is_a_bool(subset)
+    if (isTRUE(subset)) {
+        assert_is_subset(names(expected), names(object))
+    } else {
+        assert_are_set_equal(names(expected), names(object))
+    }
+    valid <- mapply(
+        slot = names(expected),
+        classes = expected,
+        MoreArgs = list(object = object),
+        FUN = function(slot, classes, object) {
+            intersect <- intersect(
+                x = classes,
+                y = class(object[[slot]])
+            )
+            if (!has_length(intersect)) {
+                FALSE
+            } else {
+                TRUE
+            }
+        },
+        SIMPLIFY = TRUE,
+        USE.NAMES = TRUE
+    )
+    if (!all(valid)) {
+        stop(paste(
+            "Class checks failed.",
+            "Run `updateObject()` to update your object.",
+            printString(valid),
+            sep = "\n"
+        ))
+    }
+    TRUE
+}
