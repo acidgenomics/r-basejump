5f5ef04426082f012ca0fc9520b2ab65809ba8ac
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-11-13 16:18:23 -0500

    Ensure goalie functions are re-exported

---
+++ R/goalie.R
@@ -0,0 +1,215 @@
+#' @importFrom goalie areDirs
+#' @export
+goalie::`areDirs`
+
+#' @importFrom goalie areFiles
+#' @export
+goalie::`areFiles`
+
+#' @importFrom goalie areNonExisting
+#' @export
+goalie::`areNonExisting`
+
+#' @importFrom goalie areURLs
+#' @export
+goalie::`areURLs`
+
+#' @importFrom goalie areUniqueGeneNames
+#' @export
+goalie::`areUniqueGeneNames`
+
+#' @importFrom goalie assertAllAreNonExisting
+#' @export
+goalie::`assertAllAreNonExisting`
+
+#' @importFrom goalie assertAllAreValidNames
+#' @export
+goalie::`assertAllAreValidNames`
+
+#' @importFrom goalie assertAreDirs
+#' @export
+goalie::`assertAreDirs`
+
+#' @importFrom goalie assertAreFiles
+#' @export
+goalie::`assertAreFiles`
+
+#' @importFrom goalie assertAreGeneAnnotations
+#' @export
+goalie::`assertAreGeneAnnotations`
+
+#' @importFrom goalie assertAreNonExisting
+#' @export
+goalie::`assertAreNonExisting`
+
+#' @importFrom goalie assertAreTranscriptAnnotations
+#' @export
+goalie::`assertAreTranscriptAnnotations`
+
+#' @importFrom goalie assertAreURLs
+#' @export
+goalie::`assertAreURLs`
+
+#' @importFrom goalie assertAreUniqueGeneNames
+#' @export
+goalie::`assertAreUniqueGeneNames`
+
+#' @importFrom goalie assertAreValidNames
+#' @export
+goalie::`assertAreValidNames`
+
+#' @importFrom goalie assertFormalCompress
+#' @export
+goalie::`assertFormalCompress`
+
+#' @importFrom goalie assertHasRownames
+#' @export
+goalie::`assertHasRownames`
+
+#' @importFrom goalie assertHasValidDimnames
+#' @export
+goalie::`assertHasValidDimnames`
+
+#' @importFrom goalie assertHasValidNames
+#' @export
+goalie::`assertHasValidNames`
+
+#' @importFrom goalie assertIsAHeaderLevel
+#' @export
+goalie::`assertIsAHeaderLevel`
+
+#' @importFrom goalie assertIsANumberOrNULL
+#' @export
+goalie::`assertIsANumberOrNULL`
+
+#' @importFrom goalie assertIsAStringOrNULL
+#' @export
+goalie::`assertIsAStringOrNULL`
+
+#' @importFrom goalie assertIsAlpha
+#' @export
+goalie::`assertIsAlpha`
+
+#' @importFrom goalie assertIsAnImplicitInteger
+#' @export
+goalie::`assertIsAnImplicitInteger`
+
+#' @importFrom goalie assertIsAnImplicitIntegerOrNULL
+#' @export
+goalie::`assertIsAnImplicitIntegerOrNULL`
+
+#' @importFrom goalie assertIsAnIntegerOrNULL
+#' @export
+goalie::`assertIsAnIntegerOrNULL`
+
+#' @importFrom goalie assertIsAnnotable
+#' @export
+goalie::`assertIsAnnotable`
+
+#' @importFrom goalie assertIsCharacterOrNULL
+#' @export
+goalie::`assertIsCharacterOrNULL`
+
+#' @importFrom goalie assertIsColorScaleContinuousOrNULL
+#' @export
+goalie::`assertIsColorScaleContinuousOrNULL`
+
+#' @importFrom goalie assertIsColorScaleDiscreteOrNULL
+#' @export
+goalie::`assertIsColorScaleDiscreteOrNULL`
+
+#' @importFrom goalie assertIsDataFrameOrNULL
+#' @export
+goalie::`assertIsDataFrameOrNULL`
+
+#' @importFrom goalie assertIsDir
+#' @export
+goalie::`assertIsDir`
+
+#' @importFrom goalie assertIsFile
+#' @export
+goalie::`assertIsFile`
+
+#' @importFrom goalie assertIsFillScaleContinuousOrNULL
+#' @export
+goalie::`assertIsFillScaleContinuousOrNULL`
+
+#' @importFrom goalie assertIsFillScaleDiscreteOrNULL
+#' @export
+goalie::`assertIsFillScaleDiscreteOrNULL`
+
+#' @importFrom goalie assertIsGFF
+#' @export
+goalie::`assertIsGFF`
+
+#' @importFrom goalie assertIsGene2symbol
+#' @export
+goalie::`assertIsGene2symbol`
+
+#' @importFrom goalie assertIsHeaderLevel
+#' @export
+goalie::`assertIsHeaderLevel`
+
+#' @importFrom goalie assertIsHexColorFunctionOrNULL
+#' @export
+goalie::`assertIsHexColorFunctionOrNULL`
+
+#' @importFrom goalie assertIsImplicitInteger
+#' @export
+goalie::`assertIsImplicitInteger`
+
+#' @importFrom goalie assertIsImplicitIntegerOrNULL
+#' @export
+goalie::`assertIsImplicitIntegerOrNULL`
+
+#' @importFrom goalie assertIsStringOrNULL
+#' @export
+goalie::`assertIsStringOrNULL`
+
+#' @importFrom goalie assertIsTx2gene
+#' @export
+goalie::`assertIsTx2gene`
+
+#' @importFrom goalie assertIsURL
+#' @export
+goalie::`assertIsURL`
+
+#' @importFrom goalie hasRownames
+#' @export
+goalie::`hasRownames`
+
+#' @importFrom goalie isAlpha
+#' @export
+goalie::`isAlpha`
+
+#' @importFrom goalie isAnImplicitInteger
+#' @export
+goalie::`isAnImplicitInteger`
+
+#' @importFrom goalie isDir
+#' @export
+goalie::`isDir`
+
+#' @importFrom goalie isFile
+#' @export
+goalie::`isFile`
+
+#' @importFrom goalie isHeaderLevel
+#' @export
+goalie::`isHeaderLevel`
+
+#' @importFrom goalie isImplicitInteger
+#' @export
+goalie::`isImplicitInteger`
+
+#' @importFrom goalie isURL
+#' @export
+goalie::`isURL`
+
+#' @importFrom goalie validDimnames
+#' @export
+goalie::`validDimnames`
+
+#' @importFrom goalie validNames
+#' @export
+goalie::`validNames`
