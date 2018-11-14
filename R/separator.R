e4abbe0977481a4b644daf7f905cf8301fbe3712
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-11-14 16:28:13 -0500

    Split out `separator()` function

---
+++ R/separator.R
@@ -0,0 +1,24 @@
+#' Separator Bar
+#'
+#' Maximum of 72 characters wide.
+#'
+#' @note Bioconductor HTML vignettes don't render correctly when printing > 76
+#'   characters, even though the default width is set at 80.
+#'
+#' @export
+#'
+#' @param sep `string` of length 1. Separator character.
+#' @param times `scalar integer`. Number of times to repeat.
+#'
+#' @return `string`.
+#'
+#' @examples
+#' cat(separator(sep = "=", times = 10L))
+separator <- function(
+    sep = c("\u2500", "=", "-", "+"),
+    times = min(c(getOption("width", 72L), 72L))
+) {
+    sep <- match.arg(sep)
+    assertIsAnImplicitInteger(times)
+    paste0(rep(x = sep, times = times), collapse = "")
+}
