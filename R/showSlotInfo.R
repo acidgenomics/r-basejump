6cb88698efb4c08ad26313e158912a02dab8b9a9
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-11-14 16:27:10 -0500

    Split out `showSlotInfo()`

---
+++ R/showSlotInfo.R
@@ -0,0 +1,46 @@
+#' Show Slot Information
+#'
+#' @param list `list`. Named list containing slot metadata.
+#'
+#' @export
+#'
+#' @return Invisible `NULL`.
+#'
+#' @examples
+#' showSlotInfo(list(
+#'     dir = "~",
+#'     skip = NULL,
+#'     filtered = TRUE
+#' ))
+showSlotInfo <- function(list) {
+    assert_is_list(list)
+    assert_that(all(vapply(
+        X = list,
+        FUN = is.atomic,
+        FUN.VALUE = logical(1L)
+    )))
+    list <- Filter(f = Negate(is.null), x = list)
+    list <- Filter(f = has_length, x = list)
+    # Standardize to Bioconductor `show()` conventions.
+    # Refer to SummarizedExperiment method for example.
+    out <- mapply(
+        name = names(list),
+        x = list,
+        FUN = function(name, x) {
+            if (has_length(x, n = 1L)) {
+                paste0(name, ": ", x)
+            } else {
+                if (has_names(x) && length(x) <= 4L) {
+                    prefix <- paste0("[", names(x), "]")
+                    info <- paste(prefix, x, sep = " ", collapse = "; ")
+                } else {
+                    info <- capture.output(headtail(x))
+                }
+                paste0(name, "(", length(x), "): ", info)
+            }
+        },
+        SIMPLIFY = TRUE,
+        USE.NAMES = FALSE
+    )
+    cat(out, sep = "\n")
+}
