b67531199328017a41c6557ccd77e2ed17841ccf
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-11-16 16:13:30 -0500

    Initial commit of exported `plotPCA()` method for `SummarizedExperiment`

---
+++ R/plotPCA-methods.R
@@ -0,0 +1,163 @@
+#' Plot PCA
+#'
+#' Principal component analysis (PCA) plot.
+#'
+#' @section Principal component analysis:
+#'
+#' PCA (Jolliffe, et al., 2002) is a multivariate technique that allows us to
+#' summarize the systematic patterns of variations in the data. PCA takes the
+#' expression levels for genes and transforms it in principal component space,
+#' reducing each sample into one point. Thereby, we can separate samples by
+#' expression variation, and identify potential sample outliers. The PCA plot is
+#' a way to look at how samples are clustering.
+#'
+#' @name plotPCA
+#' @inheritParams params
+#'
+#' @param ntop `scalar integer` or `Inf`. Number of most variable genes to plot.
+#'   Use `Inf` to include all genes (*not recommended*).
+#'
+#' @references Jolliffe, et al., 2002.
+#'
+#' @seealso `DESeq2::plotPCA()`.
+#' We're using a modified version of the `DESeqTransform` method here.
+#'
+#' ```
+#' methodFunction(
+#'     f = "plotPCA",
+#'     signature = "DESeqTransform",
+#'     package = "DESeq2"
+#' )
+#' ```
+#'
+#' @return `ggplot` or `DataFrame`.
+#'
+#' @examples
+#' data(rse)
+#' plotPCA(rse, label = FALSE)
+#' plotPCA(rse, interestingGroups = "genotype", label = TRUE)
+NULL
+
+
+
+#' @importFrom BiocGenerics plotPCA
+#' @aliases NULL
+#' @export
+BiocGenerics::plotPCA
+
+
+
+# FIXME Set color and label here globally.
+plotPCA.SummarizedExperiment <-  # nolint
+    function(
+        object,
+        interestingGroups = NULL,
+        ntop = 500L,
+        label = getOption("basejump.label", FALSE),
+        pointColor = getOption("basejump.color.discrete", NULL),
+        pointSize = getOption("basejump.point.size", 4L),
+        title = "PCA",
+        subtitle = NULL,
+        return = c("ggplot", "DataFrame"),
+        ...
+    ) {
+        # nocov start
+        call <- standardizeCall()
+        # genes
+        if ("genes" %in% names(call)) {
+            stop("`genes` is defunct. Use `ntop` argument instead.")
+        }
+        # samples, censorSamples
+        if (any(c("samples", "censorSamples") %in% names(call))) {
+            stop("Sample selection is defunct. Use bracket-based subsetting.")
+        }
+        # returnData
+        if ("returnData" %in% names(call)) {
+            stop("`returnData` is defunct. Use `return` argument instead.")
+        }
+        # nocov end
+
+        validObject(object)
+        interestingGroups <- matchInterestingGroups(
+            object = object,
+            interestingGroups = interestingGroups
+        )
+        interestingGroups(object) <- interestingGroups
+        assert_is_a_number(ntop)
+        assertIsColorScaleDiscreteOrNULL(pointColor)
+        assertIsAnImplicitInteger(pointSize)
+        assert_is_a_bool(label)
+        assertIsStringOrNULL(title)
+        return <- match.arg(return)
+
+        if (identical(ntop, Inf)) {
+            nGene <- nrow(object)
+        } else {
+            nGene <- ntop
+        }
+        message(paste("Plotting PCA using", nGene, "genes."))
+
+        # Using a modified version of DESeq2 DESeqTransform method here.
+        counts <- counts(object)
+        rv <- rowVars(counts)
+        select <- order(rv, decreasing = TRUE)[seq_len(min(ntop, length(rv)))]
+        pca <- prcomp(t(counts[select, , drop = FALSE]))
+        percentVar <- pca[["sdev"]]^2L / sum(pca[["sdev"]]^2L)
+
+        data <- DataFrame(
+            PC1 = pca[["x"]][, 1L],
+            PC2 = pca[["x"]][, 2L],
+            sampleData(object)
+        )
+        # Note that we're assigning the percent variation values used
+        # for the axes into the object attributes.
+        attr(data, "percentVar") <- percentVar[1L:2L]
+
+        if (return == "DataFrame") {
+            return(data)
+        }
+
+        p <- ggplot(
+            data = as_tibble(data),
+            mapping = aes(
+                x = !!sym("PC1"),
+                y = !!sym("PC2"),
+                color = !!sym("interestingGroups")
+            )
+        ) +
+            geom_point(size = 4L) +
+            coord_fixed() +
+            labs(
+                title = title,
+                subtitle = subtitle,
+                x = paste0(
+                    "PC1: ", round(percentVar[[1L]] * 100L), "% variance"
+                ),
+                y = paste0(
+                    "PC2: ", round(percentVar[[2L]] * 100L), "% variance"
+                ),
+                color = paste(interestingGroups, collapse = ":\n")
+            )
+
+        if (is(pointColor, "ScaleDiscrete")) {
+            p <- p + pointColor
+        }
+
+        if (isTRUE(label)) {
+            p <- p + basejump_geom_label_repel(
+                mapping = aes(label = !!sym("sampleName"))
+            )
+        }
+
+        p
+    }
+
+
+
+#' @rdname plotPCA
+#' @export
+setMethod(
+    f = "plotPCA",
+    signature = signature("SummarizedExperiment"),
+    definition = plotPCA.SummarizedExperiment
+)
