38649207a3dfbd02fbfbdf2e0c7e3793e0411601
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-11-28 14:26:40 -0500

    Improve SE padding method to also update `sampleNames` consistently. This works for SCE too.

--- R/autopadZeros-methods.R
+++ R/autopadZeros-methods.R
@@ -29,6 +29,8 @@ NULL
 
 autopadZeros.character <- function(object) {
     assert_is_character(object)
+    names <- names(object)
+    object <- as.character(object)
     assertAreValidNames(object)
     pattern <- "(.*[A-Za-z])([[:digit:]]+)$"
     # Early return if no padding is necessary.
@@ -41,7 +43,9 @@ autopadZeros.character <- function(object) {
     width <- max(str_length(nums))
     nums <- str_pad(string = nums, width = width, side = "left", pad = "0")
     mat <- matrix(data = c(prefix, nums), ncol = 2L)
-    paste0(mat[, 1L], mat[, 2L])
+    out <- paste0(mat[, 1L], mat[, 2L])
+    names(out) <- names
+    out
 }
 
 
@@ -93,10 +97,28 @@ setMethod(
 
 
 
+autopadZeros.SummarizedExperiment <- function(object, rownames = FALSE) {
+    object <- do.call(
+        what = autopadZeros.matrix,
+        args = list(
+            object = object,
+            rownames = rownames,
+            colnames = TRUE,
+            sort = TRUE
+        )
+    )
+    # Ensure sample names, which can be defined in `colData()` as `sampleName`
+    # column, also get updated.
+    sampleNames(object) <- autopadZeros(sampleNames(object))
+    object
+}
+
+
+
 #' @rdname autopadZeros
 #' @export
 setMethod(
     f = "autopadZeros",
     signature = signature("SummarizedExperiment"),
-    definition = autopadZeros.matrix
+    definition = autopadZeros.SummarizedExperiment
 )
