e9fd86975715e3c3b363e1ff17e55618e62e19e6
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-29 15:10:37 +0000

    Import `areSamplesUnique()`

--- R/package.R
+++ R/package.R
@@ -79,15 +79,16 @@
 #'   geom_vline ggplot guides labs position_jitterdodge scale_x_continuous
 #'   scale_y_continuous stat_ecdf stat_summary theme theme_linedraw
 #' @importFrom ggrepel geom_label_repel
-#' @importFrom goalie assertAreNonExisting assertAreUniqueGeneNames
-#'   assertAreURLs assertAreValidNames assertFormalCompress assertHasRownames
-#'   assertHasValidDimnames assertIsAlpha assertIsANumberOrNULL
-#'   assertIsAnImplicitInteger assertIsAnImplicitIntegerOrNULL
-#'   assertIsAnIntegerOrNULL assertIsColorScaleContinuousOrNULL
-#'   assertIsColorScaleDiscreteOrNULL assertIsDir assertIsFile
-#'   assertIsFillScaleContinuousOrNULL assertIsFillScaleDiscreteOrNULL
-#'   assertIsHeaderLevel assertIsHexColorFunctionOrNULL assertIsImplicitInteger
-#'   assertIsStringOrNULL hasRownames isAnImplicitInteger isURL
+#' @importFrom goalie areSamplesUnique assertAreNonExisting
+#'   assertAreUniqueGeneNames assertAreURLs assertAreValidNames
+#'   assertFormalCompress assertHasRownames assertHasValidDimnames assertIsAlpha
+#'   assertIsANumberOrNULL assertIsAnImplicitInteger
+#'   assertIsAnImplicitIntegerOrNULL assertIsAnIntegerOrNULL
+#'   assertIsColorScaleContinuousOrNULL assertIsColorScaleDiscreteOrNULL
+#'   assertIsDir assertIsFile assertIsFillScaleContinuousOrNULL
+#'   assertIsFillScaleDiscreteOrNULL assertIsHeaderLevel
+#'   assertIsHexColorFunctionOrNULL assertIsImplicitInteger assertIsStringOrNULL
+#'   hasRownames isAnImplicitInteger isURL
 #' @importFrom grDevices colorRampPalette
 #' @importFrom grid arrow unit
 #' @importFrom jsonlite read_json
