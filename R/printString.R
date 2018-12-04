a2f20882fdd4f045f807cfc3e66ebae02a396b10
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-04 08:44:11 -0500

    `printString()`: Improve `max` argument support

--- R/printString.R
+++ R/printString.R
@@ -6,7 +6,8 @@
 #' @export
 #'
 #' @param x `atomic`.
-#' @param max `scalar integer`. Maximum length of vector.
+#' @param max `scalar integer`. Maximum length of vector. Works like
+#'   `getOption("max.print")` without having to set globally.
 #'
 #' @return `string`.
 #'
@@ -16,8 +17,11 @@
 #' printString(c("hello", "world"))
 printString <- function(x, max = 100L) {
     assert_is_atomic(x)
+    assertIsAnImplicitInteger(max)
     assert_that(length(x) <= max)
     x <- capture.output(print(x))
+    # Limit the number of lines returned, like `max.print` option.
+    x <- head(x, n = max)
     x <- paste(x, collapse = "\n")
     # Remove leading and trailing line breaks.
     x <- gsub("^[\n]+|[\n]+$", "", x)
