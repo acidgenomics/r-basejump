088645b7181252eae4d88d47e8cdcbbe1b3d30dd
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-11 17:47:49 -0500

    Update unit tests

---
+++ tests/testthat/test_s4_generators.R
@@ -0,0 +1,82 @@
+data(rse, envir = environment())
+
+
+
+context("EggNOG")
+
+test_that("EggNOG", {
+    x <- EggNOG()
+    expect_s4_class(x, "EggNOG")
+})
+
+
+
+context("Ensembl2Entrez")
+
+data(rse, envir = environment())
+
+test_that("Ensembl2Entrez", {
+    x <- Ensembl2Entrez(rse)
+    expect_s4_class(x, "Ensembl2Entrez")
+})
+
+
+
+context("Gene2Symbol")
+
+# TODO Add coverage for `format` arguments.
+
+format <- methodFormals(
+    f = "Gene2Symbol",
+    signature = "SummarizedExperiment",
+    package = "basejump"
+) %>%
+    .[["format"]] %>%
+    eval()
+
+with_parameters_test_that(
+    "Gene2Symbol", {
+        x <- Gene2Symbol(rse, format = format)
+        expect_s4_class(x, "Gene2Symbol")
+    },
+    format = format
+)
+
+
+
+context("HGNC2Ensembl")
+
+test_that("HGNC2Ensembl", {
+    x <- HGNC2Ensembl()
+    expect_s4_class(x, "HGNC2Ensembl")
+})
+
+
+
+
+context("MGI2Ensembl")
+
+test_that("MGI2Ensembl", {
+    x <- MGI2Ensembl()
+    expect_s4_class(x, "MGI2Ensembl")
+})
+
+
+
+context("PANTHER")
+
+test_that("PANTHER", {
+    x <- PANTHER(organism = "Homo sapiens")
+    expect_s4_class(x, "PANTHER")
+})
+
+
+
+context("Tx2Gene")
+
+data(tx_se, envir = environment())
+
+test_that("Tx2Gene", {
+    x <- Tx2Gene(tx_se)
+    expect_s4_class(x, "Tx2Gene")
+})
