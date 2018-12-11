a7055b89f65bea353389526ac6abb50648ef0762
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-11 12:53:03 -0500

    Simplify internal functions

--- R/internal.R
+++ R/internal.R
@@ -3,48 +3,23 @@
 
 
 
-# Prototype metadata ===========================================================
 .prototypeMetadata <- list(
     version = packageVersion("basejump"),
     date = Sys.Date()
 )
 
-.hasPrototypeMetadata <- function(object) {
-    isSubset(
-        x = names(.prototypeMetadata),
-        y = names(metadata(object))
-    )
-}
 
 
-
-# Prototype genome metadata ====================================================
-.prototypeGenomeMetadata <- c(
-    .prototypeMetadata,
-    list(
-        organism = character(),
-        genomeBuild = character(),
-        ensemblRelease = integer()
-    )
-)
-
-.genomeMetadata <- function(object) {
+.slotGenomeMetadata <- function(object) {
     metadata <- metadata(object)
-    prototypeMetadata <- .prototypeGenomeMetadata %>%
-        .[setdiff(names(.), names(metadata))]
-    c(prototypeMetadata, metadata)
-}
-
-.hasGenomeMetadata <- function(object) {
-    all(genomeMetadataNames %in% names(metadata(object)))
-}
-
-.assertHasGenomeMetadata <- function(object) {
-    if (!isTRUE(.hasGenomeMetadata(object))) {
-        stop(paste0(
-            "Object does not contain genome information.\n",
-            "Requires: ", toString(genomeMetadataNames)
-        ))
-    }
-    assert(.hasPrototypeMetadata(object))
+    proto <- c(
+        .prototypeMetadata,
+        list(
+            organism = character(),
+            genomeBuild = character(),
+            ensemblRelease = integer()
+        )
+    )
+    proto <- proto[setdiff(names(proto), names(metadata))]
+    c(proto, metadata)
 }
