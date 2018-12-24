f19e0fc4c78bcb11fe8ba20dd21dba4162a677ab
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-24 15:10:07 -0500

    Simplify formalsList definitions

--- R/globals.R
+++ R/globals.R
@@ -29,39 +29,21 @@ barcodePattern <- ")_([ACGT_]{6,})(_[0-9]+)?$"
 
 
 
-#' File extension pattern
-#'
-#' Note optional matching of gzip.
-#'
+#' @importFrom brio extPattern
 #' @export
-#' @examples
-#' extPattern
-extPattern <- "\\.([a-zA-Z0-9]+)(\\.gz)?$"
+brio::extPattern
 
 
 
-#' Formals list
-#'
-#' Formals that the user can set globally with `base::getOption`.
-#'
-#' @export
-#' @examples
-#' formalsList
 formalsList <- list(
     color.continuous = quote(getOption("basejump.color.continuous", NULL)),
     color.discrete = quote(getOption("basejump.color.discrete", NULL)),
-    data.frame = quote(getOption("basejump.data.frame", "data.frame")),
     fill.continuous = quote(getOption("basejump.fill.continuous", NULL)),
     fill.discrete = quote(getOption("basejump.fill.discrete", NULL)),
     flip = quote(getOption("basejump.flip", TRUE)),
     label = quote(getOption("basejump.label", FALSE)),
     legend = quote(getOption("basejump.legend", TRUE)),
-    load.dir = quote(getOption("basejump.load.dir", ".")),
-    point.size = quote(getOption("basejump.point.size", 3L)),
-    save.dir = quote(getOption("basejump.save.dir", ".")),
-    save.ext = quote(getOption("basejump.save.ext", "rds")),
-    save.overwrite = quote(getOption("basejump.save.overwrite", TRUE)),
-    save.compress = quote(getOption("basejump.save.compress", TRUE))
+    point.size = quote(getOption("basejump.point.size", 3L))
 )
 
 
@@ -82,31 +64,9 @@ lanePattern <- "_L([[:digit:]]{3})"
 
 
 
-#' `NA` strings
+#' @importFrom brio naStrings
 #' @export
-#' @examples
-#' naStrings
-naStrings <- c("", "NA", "#N/A", "NULL", "null")
-
-
-
-#' R data load error
-#' @export
-#' @examples
-#' message(rdataLoadError)
-rdataLoadError <- paste(
-    "Failed to load data.",
-    "R data files must contain `.rda`, `.rds`, or `.RData` extension.",
-    sep = "\n"
-)
-
-
-
-#' R data extension pattern
-#' @export
-#' @examples
-#' rdataExtPattern
-rdataExtPattern <- "\\.(rd[a|ata|s])$"
+brio::naStrings
 
 
 
@@ -116,7 +76,3 @@ rdataExtPattern <- "\\.(rd[a|ata|s])$"
 #' @examples
 #' message(updateMessage)
 updateMessage <- "Run `updateObject` to update your object."
-
-
-
-.dataFrameChoices <- c("data.frame", "DataFrame", "tbl_df", "data.table")
