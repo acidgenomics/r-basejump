de6ea9c23ff61a1eaedf9ce06841672cc16929b0
Michael Steinbaugh <mike@steinbaugh.com>
2019-01-12 18:00:47 -0500

    Simplify the NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -18,24 +18,23 @@
 #'
 #' @importFrom AnnotationHub AnnotationHub query snapshotDate
 #' @importFrom BiocGenerics as.data.frame as.list colSums do.call lapply match
-#'   rowMeans rowSums setdiff t
+#'   rowSums setdiff t
 #' @importFrom GenomeInfoDb seqnames
 #' @importFrom GenomicFeatures genes transcripts
 #' @importFrom GenomicRanges GRanges
 #' @importFrom IRanges IRanges
 #' @importFrom Matrix.utils aggregate.Matrix
 #' @importFrom RColorBrewer brewer.pal
-#' @importFrom S4Vectors DataFrame List Rle %in% aggregate complete.cases cor
-#'   decode expand head mcols mcols<- metadata metadata<- na.omit tail
-#' @importFrom SingleCellExperiment SingleCellExperiment isSpike<-
-#'   reducedDimNames reducedDims spikeNames
+#' @importFrom S4Vectors DataFrame List Rle %in% aggregate cor decode expand
+#'   head mcols mcols<- metadata metadata<- na.omit tail
+#' @importFrom SingleCellExperiment SingleCellExperiment isSpike<- spikeNames
 #' @importFrom SummarizedExperiment SummarizedExperiment assay assayNames
 #'   assayNames<- assays assays<- colData colData<- rowData rowData<- rowRanges
 #'   rowRanges<-
 #' @importFrom cowplot plot_grid
-#' @importFrom dplyr arrange bind_rows case_when desc everything filter funs
-#'   group_by left_join mutate mutate_all mutate_at mutate_if n pull rename
-#'   select select_if slice summarise summarise_all top_n ungroup
+#' @importFrom dplyr arrange bind_rows case_when desc filter funs group_by
+#'   left_join mutate mutate_all mutate_at mutate_if n pull rename select
+#'   select_if slice summarise summarise_all top_n ungroup
 #' @importFrom ensembldb ensemblVersion
 #' @importFrom ggplot2 aes coord_fixed coord_flip element_blank element_line
 #'   element_rect element_text expand_limits facet_wrap geom_bar geom_boxplot
@@ -43,16 +42,13 @@
 #'   geom_vline ggplot guides labs position_jitterdodge scale_x_continuous
 #'   scale_y_continuous stat_ecdf stat_summary theme theme_linedraw
 #' @importFrom ggrepel geom_label_repel
-#' @importFrom goalie allAreExisting allAreHexColors allAreNonExisting
-#'   allAreURLs allHaveAccess areDisjointSets areIntersectingSets areSameLength
-#'   areSetEqual assert false formalCompress getNameInParent hasAccess
-#'   hasColnames hasDimnames hasCols hasDims hasInternet hasLength hasNames
-#'   hasNoDuplicates hasNonZeroRowsAndCols hasRows hasRownames hasUniqueCols
-#'   hasValidDimnames isADirectory isAFile isAURL isAny isCharacter isDirectory
-#'   isExisting isFile isFlag isGGScale isHeaderLevel isHexColorFunction
-#'   isInClosedRange isInRange isInt isMatchingRegex isNonEmpty isNonNegative
-#'   isNotMatchingRegex isNumber isPositive isScalar isString isSubset
-#'   isSuperset isURL matchesUniqueGeneNames validNames validate validateClasses
+#' @importFrom goalie allAreHexColors areDisjointSets areIntersectingSets
+#'   areSameLength areSetEqual assert false getNameInParent hasDims hasInternet
+#'   hasLength hasNames hasNoDuplicates hasNonZeroRowsAndCols hasRows
+#'   hasRownames hasUniqueCols hasValidDimnames isADirectory  isAny isCharacter
+#'   isFlag isGGScale isHeaderLevel isHexColorFunction isInClosedRange isInRange
+#'   isInt isMatchingRegex isNonNegative isNotMatchingRegex isNumber isPositive
+#'   isScalar isString isSubset isSuperset validNames validate validateClasses
 #' @importFrom grDevices colorRampPalette
 #' @importFrom grid arrow unit
 #' @importFrom knitr asis_output kable opts_knit
@@ -61,23 +57,26 @@
 #' @importFrom methods as coerce formalArgs getGeneric getMethod is isGeneric
 #'   new selectMethod setAs setClass setGeneric setMethod setOldClass show
 #'   signature slot slotNames validObject .hasSlot
-#' @importFrom pbapply pblapply
 #' @importFrom pheatmap pheatmap
 #' @importFrom purrr map
 #' @importFrom readr cols read_lines read_tsv
 #' @importFrom reshape2 melt
-#' @importFrom rlang !! !!! := sym syms UQ
+#' @importFrom rlang !! !!! := UQ sym syms
 #' @importFrom sessioninfo session_info
 #' @importFrom stats as.formula dist hclust prcomp quantile
-#' @importFrom stringr regex str_dup str_extract str_length str_match str_pad
-#'   str_replace str_replace_all str_replace_na str_subset str_trunc
-#' @importFrom tidyr gather separate unite
-#' @importFrom tidyselect everything matches starts_with
+#' @importFrom stringr str_dup str_extract str_length str_match str_pad
+#'   str_replace_all str_subset str_trunc
+#' @importFrom tidyr gather separate
+#' @importFrom tidyselect everything matches
 #' @importFrom tibble as_tibble column_to_rownames tibble
-#' @importFrom tools Rd_db
 #' @importFrom utils capture.output data getFromNamespace globalVariables
-#'   installed.packages packageVersion sessionInfo
+#'   installed.packages packageVersion
 NULL
+
+
+
+# FIXME set in enhances
+# @importFrom pbapply pblapply
 
 
 
