1a7d17ebbfc4f278148feb735274ff11ec0ab85e
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-08 07:52:07 -0500

    Split out reexports

---
+++ R/reexports-brio.R
@@ -0,0 +1,91 @@
+#' @importFrom brio assignAndSaveData
+#' @export
+brio::assignAndSaveData
+
+#' @importFrom brio atomize
+#' @export
+brio::atomize
+
+#' @importFrom brio basenameSansExt
+#' @export
+brio::basenameSansExt
+
+#' @importFrom brio decode
+#' @export
+brio::decode
+
+#' @importFrom brio dots
+#' @export
+brio::dots
+
+#' @importFrom brio encode
+#' @export
+brio::encode
+
+#' @importFrom brio export
+#' @export
+brio::export
+
+#' @importFrom brio extPattern
+#' @export
+brio::extPattern
+
+#' @importFrom brio factorize
+#' @export
+brio::factorize
+
+#' @importFrom brio import
+#' @export
+brio::import
+
+#' @importFrom brio initDir
+#' @export
+brio::initDir
+
+#' @importFrom brio loadData
+#' @export
+brio::loadData
+
+#' @importFrom brio loadDataAsName
+#' @export
+brio::loadDataAsName
+
+#' @importFrom brio loadRemoteData
+#' @export
+brio::loadRemoteData
+
+#' @importFrom brio localOrRemoteFile
+#' @export
+brio::localOrRemoteFile
+
+#' @importFrom brio naStrings
+#' @export
+brio::naStrings
+
+#' @importFrom brio pasteURL
+#' @export
+brio::pasteURL
+
+#' @importFrom brio rdataExtPattern
+#' @export
+brio::rdataExtPattern
+
+#' @importFrom brio rdataLoadError
+#' @export
+brio::rdataLoadError
+
+#' @importFrom brio realpath
+#' @export
+brio::realpath
+
+#' @importFrom brio saveData
+#' @export
+brio::saveData
+
+#' @importFrom brio transmit
+#' @export
+brio::transmit
+
+#' @importFrom brio writeCounts
+#' @export
+brio::writeCounts
---
+++ R/reexports-goalie.R
@@ -0,0 +1,35 @@
+#' @importFrom goalie assert
+#' @export
+goalie::assert
+
+#' @importFrom goalie extractLocal
+#' @export
+goalie::extractLocal
+
+#' @importFrom goalie hasLocal
+#' @export
+goalie::hasLocal
+c
+#' @importFrom goalie matchArgsToDoCall
+#' @export
+goalie::matchArgsToDoCall
+
+#' @importFrom goalie methodFormals
+#' @export
+goalie::methodFormals
+
+#' @importFrom goalie methodFunction
+#' @export
+goalie::methodFunction
+
+#' @importFrom goalie printString
+#' @export
+goalie::printString
+
+#' @importFrom goalie standardizeCall
+#' @export
+goalie::standardizeCall
+
+#' @importFrom goalie validate
+#' @export
+goalie::validate
