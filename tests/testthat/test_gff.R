42c1c2f807f9cb756d9fe1851086366000c8578d
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-05 16:38:12 -0500

    Improve WormBase GTF handling

--- tests/testthat/test_gff.R
+++ tests/testthat/test_gff.R
@@ -2,8 +2,8 @@ levels <- c("genes", "transcripts")
 
 
 
-# Minimal ======================================================================
-context("Minimal GFF/GTF file checks")
+# Local ========================================================================
+context("Local GFF file import")
 
 files <- c(
     ensemblGFF  = "ensembl.gff3.gz",
@@ -21,17 +21,7 @@ names(files) <- names
 rm(names)
 stopifnot(all(file.exists(files)))
 
-with_parameters_test_that(
-    "Ensembl GTF", {
-        file <- files[["ensemblGTF"]]
-        object <- makeGRangesFromGFF(file = file, level = level)
-        expect_s4_class(object, "GRanges")
-        expect_length(object, length)
-    },
-    level = levels,
-    length = c(60L, 167L)
-)
-
+# > Ensembl ----
 with_parameters_test_that(
     "Ensembl GFF", {
         file <- files[["ensemblGFF"]]
@@ -43,6 +33,18 @@ with_parameters_test_that(
     length = c(53L, 147L)
 )
 
+with_parameters_test_that(
+    "Ensembl GTF", {
+        file <- files[["ensemblGTF"]]
+        object <- makeGRangesFromGFF(file = file, level = level)
+        expect_s4_class(object, "GRanges")
+        expect_length(object, length)
+    },
+    level = levels,
+    length = c(60L, 167L)
+)
+
+# > FlyBase ----
 with_parameters_test_that(
     "FlyBase GTF", {
         file <- files[["flybaseGTF"]]
@@ -60,6 +62,18 @@ with_parameters_test_that(
     length = c(39L, 72L)
 )
 
+# > GENCODE ----
+with_parameters_test_that(
+    "GENCODE GFF", {
+        file <- files[["gencodeGFF"]]
+        object <- makeGRangesFromGFF(file, level = level)
+        expect_s4_class(object, "GRanges")
+        expect_length(object, length)
+    },
+    level = levels,
+    length = c(60L, 167L)
+)
+
 with_parameters_test_that(
     "GENCODE GTF", {
         file <- files[["gencodeGTF"]]
@@ -71,37 +85,7 @@ with_parameters_test_that(
     length = c(60L, 167L)
 )
 
-# FIXME Transcript level loads aren't supported yet.
-with_parameters_test_that(
-    "GENCODE GFF", {
-        file <- files[["gencodeGFF"]]
-        object <- makeGRangesFromGFF(file, level = level)
-        expect_s4_class(object, "GRanges")
-        expect_length(object, length)
-    },
-    level = levels,
-    length = c(60L, 0L)
-)
-
-with_parameters_test_that(
-    "RefSeq GFF", {
-        file <- files[["refseqGFF"]]
-        expect_warning(
-            makeGRangesFromGFF(file = file, level = level),
-            "RefSeq support is experimental."
-        )
-        suppressWarnings(
-            object <- makeGRangesFromGFF(file = file, level = level)
-        )
-        # FIXME This fails for transcripts.
-        # Need to figure out method to return these uniquely.
-        expect_s4_class(object, "GRanges")
-        expect_length(object, length)
-    },
-    level = levels,
-    length = c(62L, 100L)
-)
-
+# > WormBase ----
 with_parameters_test_that(
     "WormBase GTF", {
         file <- files[["wormbaseGTF"]]
@@ -119,19 +103,44 @@ with_parameters_test_that(
     length = c(66L, 83L)
 )
 
+# > RefSeq ----
+test_that("RefSeq GFF", {
+    file <- files[["refseqGFF"]]
+
+    # Genes
+    expect_warning(
+        makeGRangesFromGFF(file = file, level = "genes"),
+        "RefSeq support is experimental."
+    )
+    suppressWarnings(
+        object <- makeGRangesFromGFF(file = file, level = "genes")
+    )
+    expect_s4_class(object, "GRanges")
+    expect_length(object, 62L)
+
+    # Transcripts
+    suppressWarnings(
+        object <- makeGRangesFromGFF(file = file, level = "transcripts")
+    )
+    expect_s4_class(object, "GRangesList")
+    expect_length(object, 100L)
+})
 
 
-# CPU-intensive extra checks (local only) ======================================
-context("Extra : Remote GFF/GTF files")
+
+# Remote =======================================================================
+context("Remote GFF file import")
 skip_if_not(
     condition = isTRUE(getOption("basejump.testthat.extra")),
     message = paste(
         "Too CPU intensive for CI.",
-        "Enable locally with `options(basejump.testthat.extra = TRUE)`.",
+        "Enable with `options(basejump.testthat.extra = TRUE)`.",
         sep = "\n"
     )
 )
 # skip_on_appveyor(); skip_on_bioc(); skip_on_travis()
+
+
 
 # Run more thorough unit tests when checking locally. These take too long to
 # run on CI and can time out. The full checks on GFF3 files are particularly
@@ -140,20 +149,9 @@ skip_if_not(
 # Wrapping with `localOrRemoteFile()` calls here so we don't download the
 # file repeatedly from the remote server.
 
-# FIXME Document the gene/transcript differences between Ensembl and GENCODE.
 
 
-
-ensembl_lengths <- c(58735L, 206601L)
-ensembl_gtf_file <- localOrRemoteFile(pasteURL(
-    "ftp.ensembl.org",
-    "pub",
-    "release-95",
-    "gtf",
-    "homo_sapiens",
-    "Homo_sapiens.GRCh38.95.gtf.gz",
-    protocol = "ftp"
-))
+# > Ensembl ----
 ensembl_gff3_file <- localOrRemoteFile(pasteURL(
     "ftp.ensembl.org",
     "pub",
@@ -163,20 +161,16 @@ ensembl_gff3_file <- localOrRemoteFile(pasteURL(
     "Homo_sapiens.GRCh38.95.gff3.gz",
     protocol = "ftp"
 ))
-
-with_parameters_test_that(
-    "Ensembl GTF", {
-        object <- makeGRangesFromGFF(
-            file = ensembl_gtf_file,
-            level = level,
-            .checkAgainstTxDb = TRUE
-        )
-        expect_s4_class(object, "GRanges")
-        expect_identical(length(object), length)
-    },
-    level = levels,
-    length = ensembl_lengths
-)
+ensembl_gtf_file <- localOrRemoteFile(pasteURL(
+    "ftp.ensembl.org",
+    "pub",
+    "release-95",
+    "gtf",
+    "homo_sapiens",
+    "Homo_sapiens.GRCh38.95.gtf.gz",
+    protocol = "ftp"
+))
+ensembl_lengths <- c(58735L, 206601L)
 
 # Note that GenomicRanges chokes on Ensembl GFF3 file, so warnings in strict
 # mode here are expected.
@@ -196,101 +190,23 @@ with_parameters_test_that(
     length = ensembl_lengths
 )
 
-
-
-# Note that we're dropping PAR Y chromosome dupes on genes and transcripts.
-gencode_lengths <- c(58676L, 206534L)
-gencode_gtf_file <- localOrRemoteFile(pasteURL(
-    "ftp.ebi.ac.uk",
-    "pub",
-    "databases",
-    "gencode",
-    "Gencode_human",
-    "release_29",
-    "gencode.v29.annotation.gtf.gz",
-    protocol = "ftp"
-))
-gencode_gff3_file = localOrRemoteFile(pasteURL(
-    "ftp.ebi.ac.uk",
-    "pub",
-    "databases",
-    "gencode",
-    "Gencode_human",
-    "release_29",
-    "gencode.v29.annotation.gff3.gz",
-    protocol = "ftp"
-))
-
-# Expecting warning about PAR genes mismatch in strict mode.
 with_parameters_test_that(
-    "GENCODE GTF", {
-        suppressWarnings(
-            object <- makeGRangesFromGFF(
-                file = gencode_gtf_file,
-                level = level,
-                .checkAgainstTxDb = TRUE
-            )
+    "Ensembl GTF", {
+        object <- makeGRangesFromGFF(
+            file = ensembl_gtf_file,
+            level = level,
+            .checkAgainstTxDb = TRUE
         )
         expect_s4_class(object, "GRanges")
         expect_identical(length(object), length)
     },
     level = levels,
-    length = gencode_lengths
-)
-
-with_parameters_test_that(
-    "GENCODE GFF3", {
-        suppressWarnings(
-            object <- makeGRangesFromGFF(
-                file = gencode_gff3_file,
-                level = level,
-                .checkAgainstTxDb = TRUE
-            )
-        )
-        expect_s4_class(object, "GRanges")
-        expect_identical(length(object), length)
-    },
-    level = levels,
-    length = gencode_lengths
+    length = ensembl_lengths
 )
 
 
 
-# FIXME Can we return with a better identifier than the gene/transcript symbol?
-# FIXME Can we get this to return as GRanges instead?
-refseq_lengths <- c(54613L, 0L)
-refseq_gff3_file <- localOrRemoteFile(pasteURL(
-    "ftp.ncbi.nlm.nih.gov",
-    "genomes",
-    "refseq",
-    "vertebrate_mammalian",
-    "Homo_sapiens",
-    "reference",
-    "GCF_000001405.38_GRCh38.p12",
-    "GCF_000001405.38_GRCh38.p12_genomic.gff.gz",
-    protocol = "ftp"
-))
-
-# Expecting GenomicFeatures warnings in strict mode.
-with_parameters_test_that(
-    "RefSeq GFF3", {
-        suppressWarnings(
-            object <- makeGRangesFromGFF(
-                file = refseq_gff3_file,
-                level = level,
-                .checkAgainstTxDb = TRUE
-            )
-        )
-        expect_s4_class(object, "GRangesList")
-        expect_identical(length(object), length)
-    },
-    level = levels,
-    length = refseq_lengths
-)
-
-
-
-flybase_lengths <- c(17772L, 35307L)
+# > FlyBase ----
 flybase_gtf_file <- localOrRemoteFile(pasteURL(
     "ftp.flybase.net",
     "releases",
@@ -300,6 +216,7 @@ flybase_gtf_file <- localOrRemoteFile(pasteURL(
     "dmel-all-r6.24.gtf.gz",
     protocol = "ftp"
 ))
+flybase_lengths <- c(17772L, 35307L)
 
 # Expecting warnings about broad class and GenomicFeatures dropping transcripts.
 with_parameters_test_that(
@@ -320,19 +237,114 @@ with_parameters_test_that(
 
 
 
-wormbase_lengths <- c(0L, 0L)
+# > GENCODE ----
+# Expecting warning about PAR genes mismatch in strict mode.
+gencode_gff3_file = localOrRemoteFile(pasteURL(
+    "ftp.ebi.ac.uk",
+    "pub",
+    "databases",
+    "gencode",
+    "Gencode_human",
+    "release_29",
+    "gencode.v29.annotation.gff3.gz",
+    protocol = "ftp"
+))
+gencode_gtf_file <- localOrRemoteFile(pasteURL(
+    "ftp.ebi.ac.uk",
+    "pub",
+    "databases",
+    "gencode",
+    "Gencode_human",
+    "release_29",
+    "gencode.v29.annotation.gtf.gz",
+    protocol = "ftp"
+))
+# Note that we're dropping PAR Y chromosome dupes on genes and transcripts.
+gencode_lengths <- c(58676L, 206534L)
+
+with_parameters_test_that(
+    "GENCODE GFF3", {
+        suppressWarnings(
+            object <- makeGRangesFromGFF(
+                file = gencode_gff3_file,
+                level = level,
+                .checkAgainstTxDb = TRUE
+            )
+        )
+        expect_s4_class(object, "GRanges")
+        expect_identical(length(object), length)
+    },
+    level = levels,
+    length = gencode_lengths
+)
+
+with_parameters_test_that(
+    "GENCODE GTF", {
+        suppressWarnings(
+            object <- makeGRangesFromGFF(
+                file = gencode_gtf_file,
+                level = level,
+                .checkAgainstTxDb = TRUE
+            )
+        )
+        expect_s4_class(object, "GRanges")
+        expect_identical(length(object), length)
+    },
+    level = levels,
+    length = gencode_lengths
+)
+
+
+
+# > RefSeq ----
+# FIXME Can we return with a better identifier than the gene/transcript symbol?
+# FIXME Can we get this to return as GRanges instead?
+refseq_gff3_file <- localOrRemoteFile(pasteURL(
+    "ftp.ncbi.nlm.nih.gov",
+    "genomes",
+    "refseq",
+    "vertebrate_mammalian",
+    "Homo_sapiens",
+    "reference",
+    "GCF_000001405.38_GRCh38.p12",
+    "GCF_000001405.38_GRCh38.p12_genomic.gff.gz",
+    protocol = "ftp"
+))
+refseq_lengths <- c(54613L, 0L)
+
+# Expecting GenomicFeatures warnings in strict mode.
+with_parameters_test_that(
+    "RefSeq GFF3", {
+        suppressWarnings(
+            object <- makeGRangesFromGFF(
+                file = refseq_gff3_file,
+                level = level,
+                .checkAgainstTxDb = TRUE
+            )
+        )
+        expect_s4_class(object, "GRangesList")
+        expect_identical(length(object), length)
+    },
+    level = levels,
+    length = refseq_lengths
+)
+
+
+
+# > WormBase ----
 wormbase_gtf_file <- localOrRemoteFile(pasteURL(
     "ftp.wormbase.org",
     "pub",
     "wormbase",
     "releases",
-    "WS268",
+    "WS267",
     "species",
     "c_elegans",
     "PRJNA13758",
-    "c_elegans.PRJNA13758.WS268.canonical_geneset.gtf.gz",
+    "c_elegans.PRJNA13758.WS267.canonical_geneset.gtf.gz",
     protocol = "ftp"
 ))
+wormbase_lengths <- c(47169L, 0L)
 
 # Expecting warnings about broad class and GenomicFeatures dropping transcripts.
 with_parameters_test_that(
