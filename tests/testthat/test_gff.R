5c65b15445afb31f3c923304a8824dfb675d6ce8
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-05 14:35:38 -0500

    Improve GFF unit tests

--- tests/testthat/test_gff.R
+++ tests/testthat/test_gff.R
@@ -5,14 +5,133 @@ levels <- c("genes", "transcripts")
 # Minimal ======================================================================
 context("Minimal GFF/GTF file checks")
 
-# Don't run `.checkAgainstTxDb = TRUE` here because it is intended to work only
-# with complete GFF/GTF files.
+files <- c(
+    ensemblGFF  = "ensembl.gff3.gz",
+    ensemblGTF  = "ensembl.gtf.gz",
+    flybaseGTF  = "flybase.gtf.gz",
+    gencodeGFF  = "gencode.gff3.gz",
+    gencodeGTF  = "gencode.gtf.gz",
+    refseqGFF   = "refseq.gff.gz",
+    wormbaseGTF = "wormbase.gtf.gz"
+)
+names <- names(files)
+# Note that `system.file()` call drops names.
+files <- system.file("extdata", files, package = "basejump")
+names(files) <- names
+rm(names)
+stopifnot(all(file.exists(files)))
+
+with_parameters_test_that(
+    "Ensembl GTF", {
+        file <- files[["ensemblGTF"]]
+        object <- makeGRangesFromGFF(file = file, level = level)
+        expect_s4_class(object, "GRanges")
+        expect_length(object, length)
+    },
+    level = levels,
+    length = c(60L, 167L)
+)
+
+with_parameters_test_that(
+    "Ensembl GFF", {
+        file <- files[["ensemblGFF"]]
+        object <- makeGRangesFromGFF(file = file, level = level)
+        expect_s4_class(object, "GRanges")
+        expect_length(object, length)
+    },
+    level = levels,
+    length = c(53L, 147L)
+)
+
+with_parameters_test_that(
+    "FlyBase GTF", {
+        file <- files[["flybaseGTF"]]
+        expect_warning(
+            makeGRangesFromGFF(file, level = level),
+            "GRanges does not contain biotype in mcols\\(\\)."
+        )
+        suppressWarnings(
+            object <- makeGRangesFromGFF(file, level = level)
+        )
+        expect_s4_class(object, "GRanges")
+        expect_length(object, length)
+    },
+    level = levels,
+    length = c(39L, 72L)
+)
+
+with_parameters_test_that(
+    "GENCODE GTF", {
+        file <- files[["gencodeGTF"]]
+        object <- makeGRangesFromGFF(file, level = level)
+        expect_s4_class(object, "GRanges")
+        expect_length(object, length)
+    },
+    level = levels,
+    length = c(60L, 167L)
+)
+
+# FIXME Transcript level loads aren't supported yet.
+with_parameters_test_that(
+    "GENCODE GFF", {
+        file <- files[["gencodeGFF"]]
+        object <- makeGRangesFromGFF(file, level = level)
+        expect_s4_class(object, "GRanges")
+        expect_length(object, length)
+    },
+    level = levels,
+    length = c(60L, 0L)
+)
+
+with_parameters_test_that(
+    "RefSeq GFF", {
+        file <- files[["refseqGFF"]]
+        expect_warning(
+            makeGRangesFromGFF(file = file, level = level),
+            "RefSeq support is experimental."
+        )
+        suppressWarnings(
+            object <- makeGRangesFromGFF(file = file, level = level)
+        )
+        # FIXME This fails for transcripts.
+        # Need to figure out method to return these uniquely.
+        expect_s4_class(object, "GRanges")
+        expect_length(object, length)
+    },
+    level = levels,
+    length = c(62L, 100L)
+)
+
+with_parameters_test_that(
+    "WormBase GTF", {
+        file <- files[["wormbaseGTF"]]
+        expect_warning(
+            makeGRangesFromGFF(file = file, level = level),
+            "geneName"
+        )
+        suppressWarnings(
+            object <- makeGRangesFromGFF(file = file, level = level)
+        )
+        expect_s4_class(object, "GRanges")
+        expect_length(object, length)
+    },
+    level = levels,
+    length = c(66L, 83L)
+)
 
 
 
-# CPU-intensive ================================================================
-context("Remote GFF/GTF file checks (local only)")
-skip_on_appveyor(); skip_on_bioc(); skip_on_travis()
+# CPU-intensive extra checks (local only) ======================================
+context("Extra : Remote GFF/GTF files")
+skip_if_not(
+    condition = isTRUE(getOption("basejump.testthat.extra")),
+    message = paste(
+        "Too CPU intensive for CI.",
+        "Enable locally with `options(basejump.testthat.extra = TRUE)`.",
+        sep = "\n"
+    )
+)
+# skip_on_appveyor(); skip_on_bioc(); skip_on_travis()
 
 # Run more thorough unit tests when checking locally. These take too long to
 # run on CI and can time out. The full checks on GFF3 files are particularly
