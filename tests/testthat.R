a8d1aba7b9a28fa917c7fdb435dedf9378fe47ee
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-06 08:00:32 -0500

    Add support for extra local tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,6 @@ library(testthat)
 library(patrick)
 library(basejump)
 test_check("basejump")
+if (isTRUE(getOption("basejump.tests.extra"))) {
+    test_dir(file.path("tests", "testthat-extra"))
+}
