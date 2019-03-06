5eb7dc77d917995c62d2aa88cd862ebc3bf6aa7c
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-06 18:37:06 -0500

    Add comment about how to enable

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,9 @@ library(testthat)
 library(patrick)
 library(basejump)
 test_check("basejump")
+
 # Run the resource intensive unit tests less frequently (e.g. per month).
+# Currently, I'm setting this in `~/.Rsecrets` for supported machines.
 if (isTRUE(getOption("basejump.test.extra"))) {
     # Disable our useful interactive flag for quick CI tests (see Rprofile).
     options(basejump.test = FALSE)
