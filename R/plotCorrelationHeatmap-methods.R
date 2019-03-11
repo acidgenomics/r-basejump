6f05b9ae744bcac7a55fc9c8db19e0bbd6af00e9
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-11 13:28:48 -0400

    Draft update, switching to local

--- R/plotCorrelationHeatmap-methods.R
+++ R/plotCorrelationHeatmap-methods.R
@@ -76,7 +76,61 @@ plotCorrelationHeatmap.SummarizedExperiment <-  # nolint
 
         # Correlation matrix.
         mat <- as.matrix(assays(object)[[assay]])
+
+        # Inform the user if NA values are present.
+        # Note that we're including `na.rm` in `rowVars()` and `colVars()` calls
+        # below to handle this edge case.
+        if (any(is.na(object))) {
+            message("NA values detected in matrix.")
+        }
+
+        # Ensure we're dropping rows without variation, otherwise the
+        # calculation will fail and return NA.
+        varThreshold <- 0L
+
+        # Check across the columns first (samples) and fail, if necessary.
+        pass <- colVars(mat, na.rm = TRUE) > varThreshold
+        if (!all(pass)) {
+            fail <- !pass
+            n <- sum(fail, na.rm = TRUE)
+            stop(paste(
+                sprintf(ngettext(
+                    n = n,
+                    msg1 = "%s column doesn't",
+                    msg2 = "%s columns don't"
+                ), n),
+                "have enough variance:",
+                toString(colnames(mat)[which(fail)], width = 200L)
+            ))
+        }
+
+        # Then check across the rows (features) and inform then remove the
+        # offenders automatically.
+        pass <- rowVars(mat, na.rm = TRUE) > varThreshold
+        if (!all(pass)) {
+            fail <- !pass
+            n <- sum(fail, na.rm = TRUE)
+            message(paste(
+                sprintf(ngettext(
+                    n = n,
+                    msg1 = "%s row doesn't",
+                    msg2 = "%s rows don't"
+                ), n),
+                "have enough variance:",
+                toString(rownames(mat)[which(fail)], width = 200L),
+                "Dropping from return."
+            ))
+            mat <- mat[pass, , drop = FALSE]
+        }
+
         mat <- cor(mat, method = method)
+
+        # Check to see if hierarchical clustering works on our return.
+        # Disable otherwise. This isn't handled well currently in pheatmap.
+        message("Calculating distance matrix.")
+        d <- dist(mat)
+        assert(is(d, "dist"))
+        hc <- hclust(d = d, method = clusteringMethod)
 
         # Get annotation columns and colors automatically.
         x <- .pheatmapAnnotations(object = object, legendColor = legendColor)
