f135636f71afb12c52a611eb0a5e7d28abf0ffcc
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-11 07:59:56 -0400

    Update GFF example file handling

--- tests/testthat/helper_cache.R
+++ tests/testthat/helper_cache.R
@@ -1,4 +1,6 @@
 files <- c(
+    "ensembl.gff3.gz",
+    "ensembl.gtf.gz",
     "example.counts",
     "example.csv",
     "example.gtf",
@@ -10,16 +12,21 @@ files <- c(
     "example.tsv",
     "example.txt",
     "example.yml",
+    "flybase.gtf.gz",
+    "gencode.gff3.gz",
+    "gencode.gtf.gz",
     "gr.rda",
     "mn.rda",
     "multi.rda",
     "plotlist.rda",
+    "refseq.gff.gz",
     "renamed.rda",
     "rnaseq_counts.csv.gz",
     "serialized.rds",
     "single_cell_counts.mtx.gz",
     "single_cell_counts.mtx.gz.colnames",
-    "single_cell_counts.mtx.gz.rownames"
+    "single_cell_counts.mtx.gz.rownames",
+    "wormbase.gtf.gz"
 )
 mapply(
     FUN = function(cacheURL, file, envir) {
