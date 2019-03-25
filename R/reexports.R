64e251a80a6552d81c2f9e6a30dbed576f8a7a1e
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-25 13:06:59 -0400

    Reexport `set_names()`, `set_rownames()`, `set_colnames()`

--- R/reexports.R
+++ R/reexports.R
@@ -14,7 +14,20 @@ magrittr::`%>%`
 
 # Tee pipe
 #' @importFrom magrittr %T>%
+#' @export
 magrittr::`%T>%`
+
+#' @importFrom magrittr set_colnames
+#' @export
+magrittr::set_colnames
+
+#' @importFrom magrittr set_names
+#' @export
+magrittr::set_names
+
+#' @importFrom magrittr set_rownames
+#' @export
+magrittr::set_rownames
 
 
 
