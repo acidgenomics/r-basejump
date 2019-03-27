8a38c54981e21c69bdd3382e148a3fefd0fca261
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-27 11:59:35 -0400

    Remove the example GFF/GTF files, which are now used by freerange

--- tests/testthat/helper_cache.R
+++ tests/testthat/helper_cache.R
@@ -1,6 +1,4 @@
 files <- c(
-    "ensembl.gff3.gz",
-    "ensembl.gtf.gz",
     "example.counts",
     "example.csv",
     "example.gtf",
@@ -12,21 +10,16 @@ files <- c(
     "example.tsv",
     "example.txt",
     "example.yml",
-    "flybase.gtf.gz",
-    "gencode.gff3.gz",
-    "gencode.gtf.gz",
     "gr.rda",
     "mn.rda",
     "multi.rda",
     "plotlist.rda",
-    "refseq.gff.gz",
     "renamed.rda",
     "rnaseq_counts.csv.gz",
     "serialized.rds",
     "single_cell_counts.mtx.gz",
     "single_cell_counts.mtx.gz.colnames",
-    "single_cell_counts.mtx.gz.rownames",
-    "wormbase.gtf.gz"
+    "single_cell_counts.mtx.gz.rownames"
 )
 mapply(
     FUN = function(remoteDir, file, envir) {
