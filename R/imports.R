55da7dccd3c4d297741d35ee5222940c7a33ec79
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-27 12:07:58 -0400

    Remove imports offloaded to freerange

--- R/imports.R
+++ R/imports.R
@@ -7,7 +7,7 @@
 
 
 
-#' @importClassesFrom GenomicRanges GRanges GRangesList
+#' @importClassesFrom GenomicRanges GRanges
 #' @importClassesFrom IRanges SimpleDataFrameList
 #' @importClassesFrom Matrix sparseMatrix
 #' @importClassesFrom S4Vectors DataFrame List
@@ -22,10 +22,6 @@
 #' @importFrom BiocGenerics as.data.frame as.list colSums do.call lapply match
 #'   rowSums setdiff t
 #' @importFrom GenomeInfoDb seqnames
-#' @importFrom GenomicFeatures genes makeTxDbFromGFF makeTxDbFromGRanges
-#'   transcripts
-#' @importFrom GenomicRanges GRanges ranges
-#' @importFrom IRanges IRanges
 #' @importFrom Matrix.utils aggregate.Matrix
 #' @importFrom RColorBrewer brewer.pal
 #' @importFrom S4Vectors DataFrame List Rle %in% aggregate cor decode expand
@@ -38,7 +34,6 @@
 #' @importFrom dplyr arrange bind_rows case_when desc filter funs group_by
 #'   left_join mutate mutate_all mutate_at mutate_if n pull rename select
 #'   select_if slice summarise summarise_all top_n ungroup
-#' @importFrom ensembldb ensemblVersion
 #' @importFrom ggplot2 aes coord_fixed coord_flip element_blank element_line
 #'   element_rect element_text expand_limits facet_wrap geom_bar geom_boxplot
 #'   geom_density geom_hline geom_jitter geom_label geom_point geom_violin
