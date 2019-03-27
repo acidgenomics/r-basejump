fe700a48b7fdae91288d880f65b2a0b432938b09
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-27 11:39:02 -0400

    Update NAMESPACE to reexport brio and freerange offloaded functions

---
+++ R/reexports-freerange.R
@@ -0,0 +1,31 @@
+#' @importFrom freerange annotable
+#' @export
+freerange::annotable
+
+#' @importFrom freerange convertUCSCBuildToEnsembl
+#' @export
+freerange::convertUCSCBuildToEnsembl
+
+#' @importFrom freerange detectOrganism
+#' @export
+freerange::detectOrganism
+
+#' @importFrom freerange emptyRanges
+#' @export
+freerange::emptyRanges
+
+#' @importFrom freerange makeGRangesFromEnsDb
+#' @export
+freerange::makeGRangesFromEnsDb
+
+#' @importFrom freerange makeGRangesFromEnsembl
+#' @export
+freerange::makeGRangesFromEnsembl
+
+#' @importFrom freerange makeGRangesFromGFF
+#' @export
+freerange::makeGRangesFromGFF
+
+#' @importFrom freerange makeGRangesFromGTF
+#' @export
+freerange::makeGRangesFromGTF
