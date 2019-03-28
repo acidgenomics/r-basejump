5ea12e7965036500fb33a32e0e2494fa5ea47034
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-27 21:49:41 -0400

    Migrate ggplot2 code to minimalism package

--- R/imports.R
+++ R/imports.R
@@ -31,15 +31,9 @@
 #' @importFrom SummarizedExperiment SummarizedExperiment assay assayNames
 #'   assayNames<- assays assays<- colData colData<- rowData rowData<- rowRanges
 #'   rowRanges<-
-#' @importFrom cowplot plot_grid
 #' @importFrom dplyr arrange bind_rows case_when desc filter funs group_by
 #'   left_join mutate mutate_all mutate_at mutate_if n pull rename select
 #'   select_if slice summarise summarise_all top_n ungroup
-#' @importFrom ggplot2 aes coord_fixed coord_flip element_blank element_line
-#'   element_rect element_text expand_limits facet_wrap geom_bar geom_boxplot
-#'   geom_density geom_hline geom_jitter geom_label geom_point geom_violin
-#'   geom_vline ggplot guides labs position_jitterdodge scale_x_continuous
-#'   scale_y_continuous stat_ecdf stat_summary theme theme_linedraw
 #' @importFrom ggrepel geom_label_repel
 #' @importFrom goalie allAreHexColors areDisjointSets areIntersectingSets
 #'   areSameLength areSetEqual assert cause false getNameInParent hasDims
