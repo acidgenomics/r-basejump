e327688a0947270d1feabf3ef470d6694c763f13
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-01 13:58:38 -0400

    Add code coverage

--- R/rankedMatrix.R
+++ R/rankedMatrix.R
@@ -15,22 +15,16 @@
 #' - Not to be confused with [Matrix::rankMatrix()].
 #'
 #' @examples
-#' ## Order was randomized using `sample()`.
-#' mat <- matrix(
-#'     data = c(
-#'         -2.00, -1.25,  0.00,  0.25,  0.25,  0.50,  2.00,  4.00,
-#'          2.00,  0.25, -1.25, -2.00,  0.50,  0.25,  4.00,  0.00,
-#'          0.50,  4.00, -1.25,  0.25,  0.00,  0.25,  2.00, -2.00,
-#'          4.00,  0.50, -2.00,  2.00,  0.25, -1.25,  0.25,  0.00
-#'     ),
-#'     nrow = 8L,
-#'     ncol = 4L,
-#'     byrow = FALSE,
-#'     dimnames = list(
-#'         paste0("gene", seq_len(8L)),
-#'         paste0("contrast", seq_len(4L))
-#'     )
-#' )
+#' data(lfc, package = "acidtest")
+#'
+#' ## Increasing (negative to positive)
+#' rankedMatrix(lfc, method = "increasing")
+#'
+#' ## Decreasing (positive to negative)
+#' rankedMatrix(lfc, method = "decreasing")
+#'
+#' ## Bidirectional
+#' rankedMatrix(lfc, method = "bidirectional")
 rankedMatrix <- function(
     object,
     MARGIN = 2L,  # nolint
@@ -57,7 +51,7 @@ rankedMatrix <- function(
         mat <- .bidirRank.matrix(
             x = object,
             MARGIN = MARGIN,
-            removeZeros = TRUE
+            removeZeros = FALSE
         )
     }
 
@@ -68,26 +62,10 @@ rankedMatrix <- function(
 
 # Consider using `data.table::frank()` instead of `base::rank()` for speed.
 # This adds an additional dependency, so avoid at the moment.
-
-# nolint start
-#
-# x
-# > gene1 gene2 gene3 gene4 gene5 gene6 gene7 gene8
-# >  2.00  0.25 -1.25 -2.00  0.50  0.25  4.00  0.00
-#
-# .rank.numeric(x, decreasing = FALSE)
-# > gene1 gene2 gene3 gene4 gene5 gene6 gene7 gene8
-# >   7.0   4.5   2.0   1.0   6.0   4.5   8.0   3.0
-#
-# .rank.numeric(x, decreasing = TRUE)
-# > gene1 gene2 gene3 gene4 gene5 gene6 gene7 gene8
-# >   2.0   4.5   7.0   8.0   3.0   4.5   1.0   6.0
-#
-# nolint end
-
 .rank.numeric <- function(x, decreasing = FALSE) {
     assert(
         is.numeric(x),
+        isFlag(decreasing),
         isFlag(decreasing)
     )
     r <- x
@@ -96,32 +74,6 @@ rankedMatrix <- function(
 }
 
 
-
-# nolint start
-#
-# x
-# >       contrast1 contrast2 contrast3 contrast4
-# > gene1     -2.00      2.00      0.50      4.00
-# > gene2     -1.25      0.25      4.00      0.50
-# > gene3      0.00     -1.25     -1.25     -2.00
-# > gene4      0.25     -2.00      0.25      2.00
-# > gene5      0.25      0.50      0.00      0.25
-# > gene6      0.50      0.25      0.25     -1.25
-# > gene7      2.00      4.00      2.00      0.25
-# > gene8      4.00      0.00     -2.00      0.00
-#
-# .rank.matrix(x, MARGIN = 2L, decreasing = FALSE)
-# >       contrast1 contrast2 contrast3 contrast4
-# > gene1       1.0       7.0       6.0       8.0
-# > gene2       2.0       4.5       8.0       6.0
-# > gene3       3.0       2.0       2.0       1.0
-# > gene4       4.5       1.0       4.5       7.0
-# > gene5       4.5       6.0       3.0       4.5
-# > gene6       6.0       4.5       4.5       2.0
-# > gene7       7.0       8.0       7.0       4.5
-# > gene8       8.0       3.0       1.0       3.0
-#
-# nolint end
 
 .rank.matrix <- function(
     x,
@@ -133,26 +85,18 @@ rankedMatrix <- function(
         isFlag(decreasing),
         isInt(MARGIN)
     )
-    apply(X = x, MARGIN = MARGIN, FUN = .rank.numeric)
+    apply(
+        X = x,
+        MARGIN = MARGIN,
+        FUN = .rank.numeric,
+        decreasing = decreasing
+    )
 }
 
 
 
 # Note that use of `which()` here will omit `NA` intentionally.
-
-# nolint start
-#
-# x
-# > gene1 gene2 gene3 gene4 gene5 gene6 gene7 gene8
-# >  2.00  0.25 -1.25 -2.00  0.50  0.25  4.00  0.00
-#
-# .bidirRank.numeric(x, removeZeros = TRUE)
-# > gene1 gene2 gene3 gene4 gene5 gene6 gene7 gene8
-# >   4.0   1.5  -1.0  -2.0   3.0   1.5   5.0    NA
-#
-# nolint end
-
-.bidirRank.numeric <- function(x, removeZeros = TRUE) {
+.bidirRank.numeric <- function(x, removeZeros = FALSE) {
     assert(
         is.numeric(x),
         isFlag(removeZeros)
@@ -175,36 +119,10 @@ rankedMatrix <- function(
 
 
 
-# nolint start
-#
-# x
-# >       contrast1 contrast2 contrast3 contrast4
-# > gene1     -2.00      2.00      0.50      4.00
-# > gene2     -1.25      0.25      4.00      0.50
-# > gene3      0.00     -1.25     -1.25     -2.00
-# > gene4      0.25     -2.00      0.25      2.00
-# > gene5      0.25      0.50      0.00      0.25
-# > gene6      0.50      0.25      0.25     -1.25
-# > gene7      2.00      4.00      2.00      0.25
-# > gene8      4.00      0.00     -2.00      0.00
-#
-# .bidirRank.matrix(x, MARGIN = 2L, removeZeroes = TRUE)
-# >       contrast1 contrast2 contrast3 contrast4
-# > gene1      -2.0       4.0       3.0       5.0
-# > gene2      -1.0       1.5       5.0       3.0
-# > gene3        NA      -1.0      -1.0      -2.0
-# > gene4       1.5      -2.0       1.5       4.0
-# > gene5       1.5       3.0        NA       1.5
-# > gene6       3.0       1.5       1.5      -1.0
-# > gene7       4.0       5.0       4.0       1.5
-# > gene8       5.0        NA      -2.0        NA
-#
-# nolint end
-
 .bidirRank.matrix <- function(
     x,
     MARGIN = 2L,  # nolint
-    removeZeros = TRUE
+    removeZeros = FALSE
 ) {
     assert(
         is.matrix(x),
