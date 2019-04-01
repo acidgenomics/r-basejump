3dca7e8ba61d8d8264d2b49163cd1aad307321fb
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-01 08:43:31 -0400

    Draft update, exporting `rankedMatrix`

---
+++ R/rankedMatrix.R
@@ -0,0 +1,17 @@
+#' Calculate a ranked matrix
+#'
+#' @export
+#' @inheritParams params
+#' @inheritParams base::apply
+#'
+#' @seealso Not to be confused with [Matrix::rankMatrix()].
+#'
+rankedMatrix <- function(object, MARGIN = 2L) {
+
+
+
+}
+
+
+
+# FIXME Can't load DESeqAnalysis currently.
