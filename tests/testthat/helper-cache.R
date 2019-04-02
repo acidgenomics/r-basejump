14c50ebb257b328efee32c4259910ffe5a2ecb7b
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-02 14:31:31 -0400

    Improve aggregation code coverage

--- tests/testthat/helper-cache.R
+++ tests/testthat/helper-cache.R
@@ -1,26 +1,8 @@
 dir.create("cache", showWarnings = FALSE)
 files <- c(
-    "example.counts",
-    "example.csv",
     "example.gtf",
     "example.gff3",
-    "example.json",
-    "example.R",
-    "example.rda",
-    "example.rds",
-    "example.tsv",
-    "example.txt",
-    "example.yml",
-    "gr.rda",
-    "mn.rda",
-    "multi.rda",
-    "plotlist.rda",
-    "renamed.rda",
-    "rnaseq_counts.csv.gz",
-    "serialized.rds",
-    "single_cell_counts.mtx.gz",
-    "single_cell_counts.mtx.gz.colnames",
-    "single_cell_counts.mtx.gz.rownames"
+    "sce_lanesplit.rds"
 )
 mapply(
     FUN = function(remoteDir, file, envir) {
