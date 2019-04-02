37e9fd2eec7c4ecd8b033b82afa2fcdfbc400958
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-01 20:43:50 -0400

    Reorganize annotations

--- tests/testthat/helper-globals.R
+++ tests/testthat/helper-globals.R
@@ -1,3 +1,18 @@
+data(
+    lfc, mat, rse, sce,
+    package = "acidtest",
+    envir = environment()
+)
+
+# nolint start
+DataFrame <- S4Vectors::DataFrame
+GRanges <- GenomicRanges::GRanges
+IRanges <- IRanges::IRanges
+# nolint end
+
+organism <- "Homo sapiens"
+release <- 87L
+
 options(
     acid.save.dir = ".",
     acid.save.ext = "rds",
