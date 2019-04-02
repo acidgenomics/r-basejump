c08d91a532c99fa1e731cfb3772c113f4b218b43
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-02 05:32:42 -0400

    Fix typo: `test_that()`

--- tests/testthat/test-detectHPC.R
+++ tests/testthat/test-detectHPC.R
@@ -4,13 +4,13 @@ test_that("Local machine", {
     expect_identical(detectHPC(), FALSE)
 })
 
-test("LSF", {
+test_that("LSF", {
     Sys.setenv("LSF_ENVDIR" = "XXX")
     expect_identical(detectHPC(), "LSF")
     Sys.unsetenv("LSF_ENVDIR")
 })
 
-test("SLURM", {
+test_that("SLURM", {
     Sys.setenv("SLURM_CONF" = "XXX")
     expect_identical(detectHPC(), "SLURM")
     Sys.unsetenv("SLURM_CONF")
