2d198a5243dacf3423f971de92a420ce64930337
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-05 15:57:35 -0400

    Improve code coverage

--- tests/testthat/test-mapCellsToSamples.R
+++ tests/testthat/test-mapCellsToSamples.R
@@ -1,9 +1,9 @@
 context("mapCellsToSamples")
 
-test_that("mapCellsToSamples", {
+test_that("2 samples", {
     samples <- paste0("sample", seq_len(2L))
     cells <- paste(samples, c("AAAAAAAA", "CCCCCCCC"), sep = "_")
-    x <- mapCellsToSamples(cells, samples)
+    x <- mapCellsToSamples(cells = cells, samples = samples)
     expect_is(x, "factor")
     expect_identical(
         object = x,
@@ -11,5 +11,45 @@ test_that("mapCellsToSamples", {
             sample1_AAAAAAAA = "sample1",
             sample2_CCCCCCCC = "sample2"
         ))
+    )
+})
+
+# Support for this is useful when working with example Seurat objects.
+# Consider making this stricter and checking for ACGT specifically.
+test_that("1 sample", {
+    expect_identical(
+        object = mapCellsToSamples(
+            cells = c("AAAAAAAA", "CCCCCCCC"),
+            samples = "sample"
+        ),
+        expected = as.factor(c(
+            AAAAAAAA = "sample",
+            CCCCCCCC = "sample"
+        ))
+    )
+})
+
+test_that("Invalid barcodes", {
+    expect_error(
+        object = mapCellsToSamples(cells = c("aaa", "bbb"), samples = "sample"),
+        regexp = "ACGT"
+    )
+})
+
+test_that("No separator", {
+    samples <- paste0("sample", seq_len(2L))
+    cells <- c("AAAAAAAA", "CCCCCCCC")
+    expect_error(
+        object = mapCellsToSamples(cells = cells, samples = samples),
+        regexp = "allAreMatchingRegex"
+    )
+})
+
+test_that("Match failure", {
+    samples <- paste0("sample", seq_len(2L))
+    cells <- paste("xxx", c("AAAAAAAA", "CCCCCCCC"), sep = "_")
+    expect_error(
+        object = mapCellsToSamples(cells = cells, samples = samples),
+        expected = "\"sample1\" sample failed to match any cells."
     )
 })
