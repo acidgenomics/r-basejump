bf3670c17cbec2e9cdba1b4d95d90c2faebc1bd3
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-05 16:18:03 -0400

    Add back markdown unit tests

---
+++ tests/testthat/test-markdown.R
@@ -0,0 +1,131 @@
+context("markdownHeader")
+
+test_that("default", {
+    md <- markdownHeader("Header")
+    expect_is(md, "knit_asis")
+})
+
+test_that("level", {
+    md <- markdownHeader("Header", level = 4L)
+    expect_identical(
+        object = as.character(md),
+        expected = "#### Header\n"  # nolint
+    )
+})
+
+test_that("invalid level", {
+    expect_error(
+        object = markdownHeader("Header", level = 8L),
+        expected = "Markdown supports header levels 1-7."
+    )
+})
+
+test_that("tabset", {
+    md <- markdownHeader("Header", tabset = TRUE)
+    expect_identical(
+        object = as.character(md),
+        expected = "## Header {.tabset}\n"  # nolint
+    )
+})
+
+test_that("asis", {
+    expect_output(
+        object = markdownHeader("Header", asis = TRUE),
+        regexp = "\\n\\n## Header\\n"  # nolint
+    )
+})
+
+
+
+context("markdownLink")
+
+test_that("markdownLink", {
+    expect_identical(
+        capture.output(markdownLink(
+            text = "R",
+            url = "https://www.r-project.org",
+            title = "The R Project for Statistical Computing"
+        )),
+        paste(
+            "[R](https://www.r-project.org):",
+            "The R Project for Statistical Computing"
+        )
+    )
+})
+
+
+
+
+context("markdownList")
+
+test_that("markdownList", {
+    groceries <- c("milk", "eggs")
+    md <- markdownList(groceries)
+    expect_identical(
+        class(md),
+        "knit_asis"
+    )
+    md <- markdownList(groceries) %>%
+        as.character()
+    expect_identical(
+        md,
+        c("- milk\n", "- eggs\n")
+    )
+    md <- markdownList(groceries, ordered = TRUE) %>%
+        as.character()
+    expect_identical(
+        md,
+        c("1. milk\n", "2. eggs\n")
+    )
+    expect_output(
+        markdownList(groceries, asis = TRUE),
+        "\\n- milk\\n- eggs\\n"
+    )
+})
+
+
+
+test_that("markdownPlots", {
+    load(file = file.path("cache", "plotlist.rda"))
+    output <- capture.output(markdownPlots(list = plotlist))
+    expect_identical(
+        object = output,
+        expected = c(
+            "",
+            "",
+            "## continuous",
+            "",
+            "",
+            "",
+            "## discrete",
+            ""
+        )
+    )
+})
+
+
+
+context("markdownTables")
+
+test_that("Return unmodified in (interactive) R session", {
+    x <- markdownTables(
+        list = list(
+            mpg = head(ggplot2::mpg),
+            mtcars = head(datasets::mtcars)
+        ),
+        captions = c("mpg", "mtcars"),
+        force = FALSE
+    )
+    expect_is(x, "list")
+    expect_identical(names(x), c("mpg", "mtcars"))
+})
+
+test_that("Simulated knit session", {
+    x <- markdownTables(
+        list = list(head(ggplot2::mpg), head(datasets::mtcars)),
+        captions = c("mpg", "mtcars"),
+        force = TRUE
+    )
+    expect_is(x, "knit_asis")
+
+})
