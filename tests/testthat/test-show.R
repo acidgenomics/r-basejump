da00bd4eaac2ea5c50ee2a632bd1aac3f7791d84
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-05 17:13:27 -0400

    Add code coverage

---
+++ tests/testthat/test-show.R
@@ -0,0 +1,27 @@
+test <- getOption("acid.test")
+options(acid.test = TRUE)
+
+
+
+context("show (print)")
+
+test_that("EggNOG", {
+    object <- EggNOG()
+    expect_output(
+        object = show(object),
+        regexp = "EggNOG"
+    )
+})
+
+test_that("PANTHER", {
+    object <- PANTHER("Homo sapiens")
+    expect_output(
+        object = show(object),
+        regexp = "PANTHER"
+    )
+
+})
+
+
+
+options(acid.test = test)
