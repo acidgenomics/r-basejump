b0ea11b075fe6bf3eafc1ce5d892d804c966ef95
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-06 16:49:08 -0400

    Add assignment method checks

--- tests/testthat/test-sampleNames.R
+++ tests/testthat/test-sampleNames.R
@@ -7,6 +7,25 @@ test_that("SummarizedExperiment", {
     expect_identical(object, expected)
 })
 
+test_that("SE assignment", {
+    samples <- letters[seq_len(ncol(rse))]
+    names(samples) <- colnames(rse)
+    sampleNames(rse) <- samples
+    expect_s4_class(rse, "RangedSummarizedExperiment")
+    expect_identical(sampleNames(rse), samples)
+})
+
+test_that("SE assignment failures", {
+    expect_error(
+        sampleNames(rse) <- "xxx",
+        "The names of value are NULL."
+    )
+    expect_error(
+        sampleNames(rse) <- c(aaa = "bbb"),
+        "areSetEqual"
+    )
+})
+
 test_that("sampleName column isn't factor", {
     colData(rse)[["sampleName"]] <- "xxx"
     expect_error(
