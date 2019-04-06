8975548a630c7aa495ec2fe5efb946ef609ad12c
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-06 16:53:07 -0400

    Add code coverage

--- tests/testthat/test-sampleNames.R
+++ tests/testthat/test-sampleNames.R
@@ -36,3 +36,24 @@ test_that("sampleName column isn't factor", {
         )
     )
 })
+
+test_that("SingleCellExperiment", {
+    expect_identical(
+        object = sampleNames(sce),
+        expected = c(
+            sample1 = "sample1",
+            sample2 = "sample2"
+        )
+    )
+})
+
+test_that("SCE assignment", {
+    oldSamples <- sampleNames(sce)
+    newSamples <- letters[seq_along(oldSamples)]
+    names(newSamples) <- names(oldSamples)
+    sampleNames(sce) <- newSamples
+    expect_identical(
+        sampleNames(sce),
+        c(sample1 = "a", sample2 = "b")
+    )
+})
