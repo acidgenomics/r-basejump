3753b434661f9833da1f5bab24b03bcc6993c898
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-30 14:03:50 -0400

    Add alpha and lfcThreshold method support

---
+++ R/lfcThreshold-methods.R
@@ -0,0 +1,70 @@
+#' @name lfcThreshold
+#' @inherit bioverbs::lfcThreshold
+#' @param ... Additional arguments.
+#' @examples
+#' data(rse, package = "acidtest")
+#'
+#' ## Annotated
+#' lfcThreshold(rse) <- 0.5
+#' lfcThreshold(rse)
+NULL
+
+
+
+#' @rdname lfcThreshold
+#' @name lfcThreshold
+#' @importFrom bioverbs lfcThreshold
+#' @usage lfcThreshold(object, ...)
+#' @export
+NULL
+
+#' @rdname lfcThreshold
+#' @name lfcThreshold<-
+#' @importFrom bioverbs lfcThreshold<-
+#' @usage lfcThreshold(object, ...) <- value
+#' @export
+NULL
+
+
+
+lfcThreshold.Annotated <-  # nolint
+    function(object) {
+        validObject(object)
+        metadata(object)[["lfcThreshold"]]
+    }
+
+
+
+#' @rdname lfcThreshold
+#' @export
+setMethod(
+    f = "lfcThreshold",
+    signature = signature("Annotated"),
+    definition = lfcThreshold.Annotated
+)
+
+
+
+`lfcThreshold<-.Annotated,numeric` <-  # nolint
+    function(object, value) {
+        assert(
+            isScalar(value),
+            isNonNegative(value)
+        )
+        metadata(object)[["lfcThreshold"]] <- value
+        validObject(object)
+        object
+    }
+
+
+
+#' @rdname lfcThreshold
+#' @export
+setMethod(
+    f = "lfcThreshold<-",
+    signature = signature(
+        object = "Annotated",
+        value = "numeric"
+    ),
+    definition = `lfcThreshold<-.Annotated,numeric`
+)
