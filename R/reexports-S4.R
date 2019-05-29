e36d1d38986209470704eb45b9a215cbc9bdd184
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-29 09:55:01 -0400

    Improve reexport documentation

---
+++ R/reexports-S4.R
@@ -0,0 +1,16 @@
+#' S4 generics exported from other packages
+#'
+#' These generics are imported from other packages.
+#'
+#' @name reexports-S4
+#' @aliases reexports-S4
+#' @docType import
+#'
+#' @param object,x Object.
+#' @param ... Additional arguments.
+#'
+#' @keywords internal
+#'
+#' @examples
+#' showMethods("camel")
+NULL
