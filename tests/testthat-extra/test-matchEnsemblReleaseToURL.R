5aa510e87e7dffe6bd37213ccaa49c6aa6274b54
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-07 09:02:04 -0400

    Improve extra unit tests that must be run locally

---
+++ tests/testthat-extra/test-matchEnsemblReleaseToURL.R
@@ -0,0 +1,17 @@
+# This code depends on biomaRt and Ensembl, which can time out.
+
+context("extra | matchEnsemblReleaseToURL")
+
+test_that("NULL input", {
+    expect_identical(
+        object = matchEnsemblReleaseToURL(NULL),
+        expected = "http://useast.ensembl.org"
+    )
+})
+
+test_that("integer input", {
+    expect_identical(
+        object = matchEnsemblReleaseToURL(96L),
+        expected = "http://apr2019.archive.ensembl.org"
+    )
+})
