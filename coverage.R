3833fbaf0f6a65501452dfb937b5a93788fff7b1
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-15 16:26:32 -0400

    Update CI config

--- coverage.R
+++ coverage.R
@@ -1,17 +1,23 @@
 #!/usr/bin/env Rscript
 
 # Check package coverage with covr.
-# Updated 2019-07-12.
+# Updated 2019-07-15.
 
 options(
     error = quote(quit(status = 1L)),
     warning = quote(quit(status = 1L))
 )
 
+if (packageVersion("base") < "3.6") {
+    quit()
+}
+
+requireNamespace("covr", quietly = TRUE)
+
 cov <- covr::package_coverage()
 pct <- covr::percent_coverage(cov)
 
 if (pct < 100L) {
     print(cov)
-    stop(paste0("Coverage is ", pct, "%."))
+    stop(sprintf("Coverage is %s.", round(pct, digits = 2L)))
 }
