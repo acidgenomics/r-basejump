da93e09c2bf9a429715352e2fc69d9bca5c444da
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-05 16:26:14 -0400

    Add generics

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -26,3 +26,30 @@ setGeneric(
         standardGeneric("Tx2Gene")
     }
 )
+
+#' @rdname makeSampleData
+#' @export
+setGeneric(
+    name = "makeSampleData",
+    def = function(object, ...) {
+        standardGeneric("makeSampleData")
+    }
+)
+
+#' @rdname makeSingleCellExperiment
+#' @export
+setGeneric(
+    name = "makeSingleCellExperiment",
+    def = function(assays, ...) {
+        standardGeneric("makeSingleCellExperiment")
+    }
+)
+
+#' @rdname makeSummarizedExperiment
+#' @export
+setGeneric(
+    name = "makeSummarizedExperiment",
+    def = function(assays, ...) {
+        standardGeneric("makeSummarizedExperiment")
+    }
+)
