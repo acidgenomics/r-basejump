a14e20002f124ea1714c89a4749549eba0855e1f
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-07 21:53:33 -0400

    Update NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -32,15 +32,16 @@
 #' @importFrom dplyr arrange bind_rows desc filter group_by left_join mutate
 #'   mutate_all mutate_at mutate_if pull rename select select_if slice
 #'   summarise_all top_n ungroup
-#' @importFrom goalie allAreMatchingRegex areDisjointSets areIntersectingSets
-#'   areSameLength areSetEqual assert bapply false getNameInParent hasColnames
-#'   hasCols hasDims hasInternet hasLength hasNames hasNoDuplicates
-#'   hasNonZeroRowsAndCols hasRows hasRownames hasUniqueCols hasValidDimnames
-#'   hasValidNames isADirectory isAlpha isAny isCharacter isFlag isGGScale
-#'   isGreaterThanOrEqualTo isHeaderLevel isHexColorFunction isInClosedRange
-#'   isInRange isInt isMatchingRegex isNonNegative isNotMatchingRegex isNumber
-#'   isPositive isScalar isString isSubset isSuperset matchesUniqueGeneNames
-#'   methodFunction validNames validate validateClasses
+#' @importFrom goalie allAreMatchingRegex appendToBody areDisjointSets
+#'   areIntersectingSets areSameLength areSetEqual assert bapply false
+#'   getNameInParent hasColnames hasCols hasDims hasInternet hasLength hasNames
+#'   hasNoDuplicates hasNonZeroRowsAndCols hasRows hasRownames hasUniqueCols
+#'   hasValidDimnames hasValidNames isADirectory isAlpha isAny isCharacter
+#'   isFlag isGGScale isGreaterThanOrEqualTo isHeaderLevel isHexColorFunction
+#'   isInClosedRange isInRange isInt isMatchingRegex isNonNegative
+#'   isNotMatchingRegex isNumber isPositive isScalar isString isSubset
+#'   isSuperset matchesUniqueGeneNames methodFunction validNames validate
+#'   validateClasses
 #' @importFrom knitr asis_output kable opts_knit
 #' @importFrom magrittr %>% set_colnames set_rownames
 #' @importFrom matrixStats colVars rowVars
