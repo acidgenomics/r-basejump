0221c58c13dbda5088ba55f77a0c1418f6b70f8d
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-13 14:41:13 -0400

    Update deprecations

--- R/deprecated.R
+++ R/deprecated.R
@@ -161,53 +161,12 @@ panther <- function(...) {
     .Defunct("PANTHER")
 }
 
-## Still in use by bcbioRNASeq v0.2.
-#' @rdname deprecated
-#' @export
-readFileByExtension <- function(...) {
-    ## > .Deprecated("import")  # nolint
-    import(...)
-}
-
-## Still in use by bcbioRNASeq v0.2.
-#' @rdname deprecated
-#' @export
-readGFF <- function(...) {
-    ## > .Deprecated("import")  # nolint
-    import(...)
-}
-
-## Still in use by bcbioRNASeq v0.2.
-#' @rdname deprecated
-#' @export
-readGTF <- function(...) {
-    ## > .Deprecated("import")  # nolint
-    import(...)
-}
-
-## Still in use by bcbioRNASeq v0.2.
-#' @rdname deprecated
-#' @export
-readJSON <- function(...) {
-    ## > .Deprecated("import")  # nolint
-    import(...)
-}
-
-## Still in use by bcbioRNASeq v0.2.
-#' @rdname deprecated
-#' @export
-readYAML <- function(...) {
-    ## > .Deprecated("import")  # nolint
-    import(...)
-}
-
 #' @rdname defunct
 #' @export
 setArgsToDoCall <- function(...) {
     .Defunct("matchArgsToDoCall")
 }
 
-## Soft deprecated until we update bcbio R packages.
 #' @rdname defunct
 #' @export
 tx2gene <- function(...) {
@@ -232,218 +191,6 @@ mdPlotlist <- markdownPlotlist
 
 
 
-# v0.9.10 ======================================================================
-# Legacy functions still in use by bcbio R packages that were previously defined
-# in v0.7.2 release. Keep these functions soft deprecated, so a bunch of
-# warnings don't pop up during assert checks in the older package versions.
-
-# Source code is defined here primarily:
-# https://github.com/steinbaugh/basejump/blob/v0.7.2/R/assert.R
-
-# Imported by bcbioRNASeq 0.2.9.
-#' @rdname deprecated
-#' @export
-assertAreGeneAnnotations <- function(x) {
-    ## > .Defunct("goalie::assert")  # nolint
-    x <- as.data.frame(x)
-    assert(
-        isSubset(c("geneID", "geneName"), colnames(x)),
-        hasRows(x)
-    )
-    TRUE
-}
-
-# Imported by bcbioRNASeq 0.2.9.
-#' @rdname deprecated
-#' @export
-assertFormalGene2symbol <- function(x, genes, gene2symbol) {
-    ## > .Defunct("goalie::assert")  # nolint
-    assert(
-        hasRownames(x),
-        isCharacter(genes, nullOK = TRUE),
-        # [2019-02-17] Note that we've added `Gene2Symbol` here.
-        isAny(gene2symbol, classes = c("Gene2Symbol", "data.frame", "NULL"))
-    )
-    if (is.data.frame(gene2symbol)) {
-        # Note that this function will be deprecated.
-        assertIsGene2symbol(gene2symbol)
-        assert(isSubset(rownames(x), gene2symbol[["geneID"]]))
-    }
-    TRUE
-}
-
-# Imported by bcbioBase 0.4.2.
-#' @rdname deprecated
-#' @export
-assertFormalInterestingGroups <- function(x, interestingGroups) {
-    ## > .Defunct("goalie::assert")  # nolint
-
-    # Early return on `NULL` interesting groups (e.g. DESeqDataSet).
-    if (is.null(interestingGroups)) {
-        return(TRUE)
-    }
-
-    assert(isCharacter(interestingGroups))
-
-    # Obtain column data, if S4 object is passed in.
-    if (isS4(x)) {
-        x <- colData(x)
-    }
-    x <- as(x, "DataFrame")
-
-    # Check that interesting groups are slotted into sampleData
-    if (!isTRUE(isSubset(interestingGroups, colnames(x)))) {
-        stop(paste(
-            "The interesting groups",
-            deparse(toString(setdiff(interestingGroups, colnames(x)))),
-            "are not defined as columns in `sampleData()`."
-        ))
-    }
-
-    # Check that interesting groups are factors
-    isFactor <- vapply(
-        X = x[, interestingGroups, drop = FALSE],
-        FUN = is.factor,
-        FUN.VALUE = logical(1L),
-        USE.NAMES = TRUE
-    )
-    if (!all(isFactor)) {
-        invalid <- names(isFactor)[which(!isFactor)]
-        stop(paste(
-            "The interesting groups",
-            deparse(toString(invalid)),
-            "are not factor."
-        ))
-    }
-
-    TRUE
-}
-
-# Imported by bcbioRNASeq 0.2.9.
-#' @rdname deprecated
-#' @export
-assertIsAHeaderLevel <- function(x) {
-    ## > .Defunct("goalie::assert")  # nolint
-    assert(isHeaderLevel(x))
-}
-
-# Imported by bcbioRNASeq 0.2.9.
-#' @rdname deprecated
-#' @export
-assertIsAStringOrNULL <- function(x) {
-    ## > .Defunct("goalie::assert")  # nolint
-    assert(isString(x, nullOK = TRUE))
-}
-
-# Imported by bcbioBase 0.4.2.
-#' @rdname deprecated
-#' @export
-assertIsAnImplicitInteger <- function(x) {
-    ## > .Defunct("goalie::assert")  # nolint
-    assert(isInt(x, nullOK = FALSE))
-}
-
-# Imported by bcbioRNASeq 0.2.9.
-#' @rdname deprecated
-#' @export
-assertIsAnImplicitIntegerOrNULL <- function(x) {
-    ## > .Defunct("goalie::assert")  # nolint
-    assert(isInt(x, nullOK = TRUE))
-}
-
-# Imported by bcbioRNASeq 0.2.9.
-#' @rdname deprecated
-#' @export
-assertIsColorScaleDiscreteOrNULL <- function(x) {
-    ## > .Defunct("goalie::assert")  # nolint
-    assert(
-        isGGScale(
-            x = x,
-            scale = "discrete",
-            aes = "colour",
-            nullOK = TRUE
-        )
-    )
-}
-
-# Imported by bcbioRNASeq 0.2.9.
-#' @rdname deprecated
-#' @export
-assertIsFillScaleDiscreteOrNULL <- function(x) {
-    ## > .Defunct("goalie::assert")  # nolint
-    assert(
-        isGGScale(
-            x = x,
-            scale = "discrete",
-            aes = "fill",
-            nullOK = TRUE
-        )
-    )
-}
-
-# Imported by bcbioBase 0.4.2.
-# Note that we don't want to check for `Gene2Symbol` S4 class here.
-# Older versions of bcbio R packages don't slot gene-to-symbol mappings as our
-# new and improved Gene2Symbol class.
-#' @rdname deprecated
-#' @export
-assertIsGene2symbol <- function(x) {
-    ## > .Defunct("goalie::assert")  # nolint
-    assert(
-        is.data.frame(x),
-        identical(colnames(x), c("geneID", "geneName")),
-        hasRows(x),
-        # Require that all columns are character.
-        all(bapply(
-            X = x,
-            FUN = is.character
-        ))
-    )
-}
-
-# Imported by bcbioRNASeq 0.2.9.
-#' @rdname deprecated
-#' @export
-assertIsHexColorFunctionOrNULL <- function(x) {
-    ## > .Defunct("goalie::assert")  # nolint
-    assert(isHexColorFunction(x, nullOK = TRUE))
-}
-
-# Imported by bcbioRNASeq 0.2.9.
-#' @rdname deprecated
-#' @export
-assertIsImplicitInteger <- function(x) {
-    ## > .Defunct("goalie::assert")  # nolint
-    assert(isInt(x))
-}
-
-# Imported by bcbioBase 0.4.2.
-# Note that we don't want to check for `Tx2Gene` S4 class here. Older versions
-# of bcbio R packages don't slot transcript-to-gene mappings as our new and
-# improved Tx2Gene class.
-#' @rdname deprecated
-#' @export
-assertIsTx2gene <- function(x) {
-    ## > .Defunct("goalie::assert")  # nolint
-    assert(is.data.frame(x))
-    # Rename `txID` to `transcriptID`, if necessary.
-    # Note that in newer code that uses `Tx2Gene` class, this is stricter.
-    if ("txID" %in% colnames(x)) {
-        colnames(x) <- gsub("^txID$", "transcriptID", colnames(x))
-    }
-    assert(
-        identical(colnames(x), c("transcriptID", "geneID")),
-        hasRows(x),
-        # Require that all columns are character.
-        all(bapply(
-            X = x,
-            FUN = is.character
-        ))
-    )
-}
-
-
-
 ## v0.10.4 ======================================================================
 ## Now recommending `plotCounts()` instead of `plotGene()`.
 ## Still in use by some revdeps, so keep re-exported.
@@ -457,12 +204,7 @@ bioverbs::plotGene
 #' @rdname defunct
 #' @export
 assertHasRownames <- function(...) {
-    .Defunct(msg = paste(
-        "'assertHasRownames' is defunct.",
-        "Use 'assert(hasRownames())' in goalie package instead.",
-        "See help(\"Defunct\")",
-        sep = "\n"
-    ))
+    .Defunct("goalie::hasRownames")
 }
 
 #' @rdname defunct
@@ -485,35 +227,148 @@ tx2geneFromGFF <- function(...) {
 
 
 
-# v0.11.0 ======================================================================
+# v0.11.6 ======================================================================
+#' @rdname defunct
+#' @export
+separatorBar <- function(...) {
+    .Defunct("separator")
+}
+
+
+
+# v0.11.8 ======================================================================
+#' @rdname defunct
+#' @export
+assertFormalInterestingGroups <- function(...) {
+    .Defunct("goalie::assert")
+}
+
+#' @rdname defunct
+#' @export
+assertIsAHeaderLevel <- function(...) {
+    .Defunct("goalie::isHeaderLevel")
+}
+
+#' @rdname defunct
+#' @export
+assertIsAStringOrNULL <- function(...) {
+    .Defunct("goalie::isString")
+}
+
+#' @rdname defunct
+#' @export
+assertIsAnImplicitInteger <- function(...) {
+    .Defunct("goalie::isInt")
+}
+
+#' @rdname defunct
+#' @export
+assertIsAnImplicitIntegerOrNULL <- function(...) {
+    .Defunct("goalie::IsInt")
+}
+
+#' @rdname defunct
+#' @export
+assertIsColorScaleDiscreteOrNULL <- function(...) {
+    .Defunct("goalie::isGGScale")
+}
+
+#' @rdname defunct
+#' @export
+assertIsFillScaleDiscreteOrNULL <- function(...) {
+    .Defunct("goalie::isGGScale")
+}
+
+#' @rdname defunct
+#' @export
+assertIsGene2symbol <- function(...) {
+    .Defunct("goalie::assert")
+}
+
+#' @rdname defunct
+#' @export
+assertIsHexColorFunctionOrNULL <- function(...) {
+    .Defunct("goalie::isHexColorFunction")
+}
+
+#' @rdname defunct
+#' @export
+assertIsImplicitInteger <- function(...) {
+    .Defunct("goalie::isIntegerish")
+}
+
+#' @rdname defunct
+#' @export
+assertIsTx2gene <- function(...) {
+    .Defunct("goalie::assert")
+}
+
+#' @rdname defunct
+#' @export
+assertAreGeneAnnotations <- function(...) {
+    .Defunct("goalie::assert")
+}
+
+#' @rdname defunct
+#' @export
+assertFormalGene2symbol <- function(...) {
+    .Defunct("goalie::assert")
+}
+
+#' @rdname deprecated
+#' @export
+readFileByExtension <- function(...) {
+    .Deprecated("import")
+    import(...)
+}
+
+#' @rdname deprecated
+#' @export
+readGFF <- function(...) {
+    .Deprecated("import")
+    import(...)
+}
+
+#' @rdname deprecated
+#' @export
+readGTF <- function(...) {
+    .Deprecated("import")
+    import(...)
+}
+
+#' @rdname deprecated
+#' @export
+readJSON <- function(...) {
+    .Deprecated("import")
+    import(...)
+}
+
+#' @rdname deprecated
+#' @export
+readYAML <- function(...) {
+    .Deprecated("import")
+    import(...)
+}
+
 #' @rdname deprecated
 #' @export
 relevelColData <- function(object) {
-    ## > .Deprecated("relevel")  # nolint
+    .Deprecated("relevel")
     relevel(x = object)
 }
 
 #' @rdname deprecated
 #' @export
 relevelRowData <- function(object) {
-    ## > .Deprecated("relevel")  # nolint
+    .Deprecated("relevel")
     relevel(x = object)
 }
 
 #' @rdname deprecated
 #' @export
 relevelRowRanges <- function(object) {
-    ## > .Deprecated("relevel")  # nolint
+    .Deprecated("relevel")
     relevel(x = object)
-}
-
-
-
-# v0.11.6 ======================================================================
-#' @rdname defunct
-#' @export
-separatorBar <- function(...) {
-    .Defunct("separator")
 }
 
 
