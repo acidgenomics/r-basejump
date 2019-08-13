66fe37738a3f3597c12d77cbcf54f69736b452d8
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-13 11:14:51 -0400

    Update unit tests

--- tests/testthat/helper-cache.R
+++ tests/testthat/helper-cache.R
@@ -7,10 +7,22 @@ if (!isTRUE(hasInternet())) {
 }
 dir.create("cache", showWarnings = FALSE)
 files <- c(
+    "bcbio-metadata-demultiplexed-invalid-duplicated.csv",
+    "bcbio-metadata-demultiplexed-invalid-legacy-samplename.csv",
+    "bcbio-metadata-demultiplexed-invalid-missing-columns.csv",
+    "bcbio-metadata-demultiplexed-invalid-sample-id.csv",
+    "bcbio-metadata-demultiplexed.csv",
+    "bcbio-metadata-invalid-column-name.csv",
+    "bcbio-metadata-invalid-description.csv",
+    "bcbio-metadata-multiplexed-cellranger.csv",
+    "bcbio-metadata-multiplexed-indrops.csv",
+    "bcbio-metadata-multiplexed-invalid-duplicated.csv",
+    "bcbio-metadata-multiplexed-invalid-missing-columns.csv",
     "example.gtf",
     "example.gff3",
     "plotlist.rds",
-    "sce_lanesplit.rds"
+    "sce_lanesplit.rds",
+    "tx2gene.csv"
 )
 mapply(
     FUN = function(remoteDir, file, envir) {
