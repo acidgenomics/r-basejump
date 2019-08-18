b5f87725b62069002bbf61a062783f9147b411fd
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-18 16:42:42 -0400

    Switch to using base R methods

--- R/mapCellsToSamples.R
+++ R/mapCellsToSamples.R
@@ -1,14 +1,10 @@
-## FIXME Remove magrittr pipe.
-
-
-
 #' Map cells to samples
 #'
 #' This function extracts `sampleID` from the `cellID` column using grep
 #' matching.
 #'
 #' @name mapCellsToSamples
-#' @note Updated 2019-08-11.
+#' @note Updated 2019-08-18.
 #' @export
 #'
 #' @inheritParams acidroxygen::params
@@ -27,7 +23,6 @@ mapCellsToSamples <- function(cells, samples) {
         isCharacter(cells), hasNoDuplicates(cells),
         isCharacter(samples), hasNoDuplicates(samples)
     )
-
     ## Early return as simple factor for single sample.
     ## This code is useful for working with some example objects (e.g. PBMC).
     if (isString(samples)) {
@@ -37,37 +32,33 @@ mapCellsToSamples <- function(cells, samples) {
         names(cell2sample) <- cells
         return(cell2sample)
     }
-
     ## Check that all cells contain a separator.
     assert(allAreMatchingRegex(x = cells, pattern = "[_-]"))
-
     list <- lapply(X = samples, FUN = function(sample) {
         pattern <- paste0("^(", sample, barcodePattern)
-        match <- str_match(cells, pattern = pattern)
+        match <- str_match(string = cells, pattern = pattern)
         if (all(is.na(match[, 1L]))) {
             stop(sprintf(
                 "'%s' sample failed to match any cells.",
                 deparse(sample)
             ))
         }
-        match %<>%
-            data.frame() %>%
-            set_colnames(c(
-                "cellID",
-                "sampleID",
-                "cellularBarcode",
-                ## Trailing number is for matching Cell Ranger output.
-                ## This gets used in the Chromium package.
-                "trailingNumber"
-            )) %>%
-            .[, c("cellID", "sampleID")] %>%
-            .[!is.na(.[["sampleID"]]), , drop = FALSE]
-        vec <- match[["sampleID"]]
-        names(vec) <- match[["cellID"]]
-        vec
+        ## Trailing number is for matching Cell Ranger output.
+        ## This gets used in the Chromium package.
+        colnames(match) <- c(
+            "cellID",
+            "sampleID",
+            "cellularBarcode",
+            "trailingNumber"
+        )
+        match <- match[, c("cellID", "sampleID")]
+        keep <- !is.na(match[, "sampleID", drop = TRUE])
+        match <- match[keep, , drop = FALSE]
+        out <- match[, "sampleID", drop = TRUE]
+        names(out) <- match[, "cellID", drop = TRUE]
+        out
     })
-
-    cell2sample <- unlist(list)
+    cell2sample <- unlist(list, recursive = FALSE, use.names = TRUE)
     assert(identical(length(cells), length(cell2sample)))
     as.factor(cell2sample)
 }
--- R/matchHumanOrthologs.R
+++ R/matchHumanOrthologs.R
@@ -1,10 +1,6 @@
-## FIXME Remove magrittr pipe.
-
-
-
 #' Match human gene orthologs
 #'
-#' @note Updated 2019-08-11.
+#' @note Updated 2019-08-18.
 #' @export
 #'
 #' @inheritParams acidroxygen::params
@@ -23,47 +19,41 @@
 #'     "ENSMUSG00000000028", "ENSMUSG00000000031",
 #'     "ENSMUSG00000000037", "ENSMUSG00000000049"
 #' )
-#' matchHumanOrthologs(genes, ensemblRelease = 87L)
+#' matchHumanOrthologs(genes = genes, ensemblRelease = 87L)
 matchHumanOrthologs <- function(
     genes,
     organism = NULL,
     ensemblRelease = NULL
 ) {
     requireNamespace("biomaRt", quietly = TRUE)
+    timeout <- "biomaRt timed out connecting to Ensembl."
     assert(
         isCharacter(genes),
         isString(organism, nullOK = TRUE),
         isInt(ensemblRelease, nullOK = TRUE)
     )
-
     if (is.null(organism)) {
         organism <- detectOrganism(genes)
     }
     ## Don't allow the user to pass in human genes, since this makes no sense.
     assert(!identical(organism, "Homo sapiens"))
-
     ## Match the Ensembl release to the archive host name, required for biomaRt.
     host <- sub(
         pattern = "^http(s)?://([a-z0-9.]+)/?$",
         replacement = "\\2",
         x = matchEnsemblReleaseToURL(ensemblRelease)
     )
-
     marts <- tryCatch(
         expr = biomaRt::listMarts(host = host),
-        error = function(e) {
-            stop("biomaRt timed out connecting to Ensembl.")
-        }
+        error = function(e) stop(timeout)
     )
-    version <- marts[["version"]][
-        match("ENSEMBL_MART_ENSEMBL", marts[["biomart"]])
-    ]
+    which <- match("ENSEMBL_MART_ENSEMBL", marts[["biomart"]])
+    version <- marts[["version"]][which]
     message(sprintf(
-        "Matching orthologs against %s (%s) with biomart %s.",
+        "Matching orthologs against %s (%s) with biomaRt %s.",
         version, host, packageVersion("biomaRt")
     ))
-
-    ## e.g. "mmusculus_gene_ensembl"
+    ## e.g. "mmusculus_gene_ensembl".
     dataset <- paste0(
         tolower(sub(
             pattern = "^([A-Z])([a-z]+)\\s([a-z]+)$",
@@ -72,7 +62,6 @@ matchHumanOrthologs <- function(
         )),
         "_gene_ensembl"
     )
-
     mart <- tryCatch(
         expr = biomaRt::useMart(
             ## "ensembl" also works.
@@ -80,11 +69,8 @@ matchHumanOrthologs <- function(
             dataset = dataset,
             host = host
         ),
-        error = function(e) {
-            stop("biomaRt timed out connecting to Ensembl.")
-        }
+        error = function(e) stop(timeout)
     )
-
     map <- tryCatch(
         expr = biomaRt::select(
             x = mart,
@@ -95,36 +81,30 @@ matchHumanOrthologs <- function(
                 "hsapiens_homolog_ensembl_gene"
             )
         ),
-        error = function(e) {
-            stop("biomaRt timed out connecting to Ensembl.")
-        }
-    ) %>%
-        as_tibble(rownames = NULL) %>%
-        sanitizeNA() %>%
-        set_colnames(c("geneID", "hgncID"))
-
+        error = function(e) stop(timeout)
+    )
+    map <- as(map, "DataFrame")
+    colnames(map) <- c("geneID", "hgncID")
+    map <- sanitizeNA(map)
+    ## Get the corresponding gene-to-symbol mappings.
     message(sprintf("Getting %s gene symbols.", organism))
-    g2s <-
-        makeGene2SymbolFromEnsembl(
-            organism = organism,
-            release = ensemblRelease,
-            format = "unmodified"
-        ) %>%
-        as_tibble(rownames = NULL)
-
+    g2s <- makeGene2SymbolFromEnsembl(
+        organism = organism,
+        release = ensemblRelease,
+        format = "unmodified"
+    )
     message("Getting Homo sapiens gene symbols.")
-    g2shs <-
-        makeGene2SymbolFromEnsembl(
-            organism = "Homo sapiens",
-            release = ensemblRelease,
-            format = "unmodified"
-        ) %>%
-        as_tibble(rownames = NULL) %>%
-        set_colnames(c("hgncID", "hgncName"))
-
-    map %>%
-        left_join(g2s, by = "geneID") %>%
-        left_join(g2shs, by = "hgncID") %>%
-        as("DataFrame") %>%
-        set_rownames(.[["geneID"]])
+    g2shs <- makeGene2SymbolFromEnsembl(
+        organism = "Homo sapiens",
+        release = ensemblRelease,
+        format = "unmodified"
+    )
+    g2shs <- as(g2shs, "DataFrame")
+    colnames(g2shs) <- c("hgncID", "hgncName")
+    ## Return.
+    out <- map
+    out <- left_join(out, g2s, by = "geneID")
+    out <- left_join(out, g2shs, by = "hgncID")
+    rownames(out) <- out[["geneID"]]
+    out
 }
