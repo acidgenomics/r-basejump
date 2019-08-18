358a317fa73ec4991a0b4c697713e97ed61aba59
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-18 11:13:47 -0400

    Include some reexports from goalie

--- R/reexports.R
+++ R/reexports.R
@@ -246,6 +246,26 @@ freerange::makeGRangesFromGFF
 #' @export
 freerange::makeGRangesFromGTF
 
+#' @importFrom goalie matchArgsToDoCall
+#' @export
+goalie::matchArgsToDoCall
+
+#' @importFrom goalie methodFormals
+#' @export
+goalie::methodFormals
+
+#' @importFrom goalie methodFunction
+#' @export
+goalie::methodFunction
+
+#' @importFrom goalie printString
+#' @export
+goalie::printString
+
+#' @importFrom goalie standardizeCall
+#' @export
+goalie::standardizeCall
+
 #' @importFrom magrittr %<>%
 #' @export
 magrittr::`%<>%`
