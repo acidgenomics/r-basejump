4270eda307e585058bf47ee6d8dfac36eb3dfe68
Michael Steinbaugh <mike@steinbaugh.com>
2019-09-25 13:27:17 -0400

    Migrate S4 methods from syntactic here

---
+++ R/upperCamelCase-methods.R
@@ -0,0 +1,304 @@
+#' @name upperCamelCase
+#' @inherit syntactic::upperCamelCase
+#' @note Updated 2019-09-25.
+#' @examples
+#' data(syntactic, package = "acidtest")
+#' lapply(syntactic, upperCamelCase)
+NULL
+
+
+
+`upperCamelCase,atomic` <-  # nolint
+    function(object, names = TRUE, strict = FALSE) {
+        assert(
+            isFlag(names),
+            isFlag(strict)
+        )
+        if (isTRUE(names) && hasNames(object)) {
+            names(object) <- upperCamelCase(names(object), strict = strict)
+        }
+        object
+    }
+
+
+
+#' @rdname upperCamelCase
+#' @export
+setMethod(
+    f = "upperCamelCase",
+    signature = signature("atomic"),
+    definition = `upperCamelCase,atomic`
+)
+
+
+
+`upperCamelCase,factor` <-  # nolint
+    function(object, names = TRUE, strict = FALSE) {
+        assert(
+            isFlag(names),
+            isFlag(strict)
+        )
+        if (isTRUE(names) && hasNames(object)) {
+            names <- upperCamelCase(names(object), strict = strict)
+        } else {
+            names <- names(object)
+        }
+        object <- as.character(object)
+        object <- upperCamelCase(object, strict = strict)
+        object <- as.factor(object)
+        names(object) <- names
+        object
+    }
+
+
+
+#' @rdname upperCamelCase
+#' @export
+setMethod(
+    f = "upperCamelCase",
+    signature = signature("factor"),
+    definition = `upperCamelCase,factor`
+)
+
+
+
+`upperCamelCase,list` <- `upperCamelCase,atomic`  # nolint
+
+
+
+#' @rdname upperCamelCase
+#' @export
+setMethod(
+    f = "upperCamelCase",
+    signature = signature("list"),
+    definition = `upperCamelCase,list`
+)
+
+
+
+`upperCamelCase,matrix` <-  # nolint
+    function(
+        object,
+        rownames = FALSE,
+        colnames = TRUE,
+        strict = FALSE
+    ) {
+        assert(
+            hasDimnames(object),
+            isFlag(rownames),
+            isFlag(colnames),
+            isFlag(strict)
+        )
+        if (isTRUE(rownames) && hasRownames(object)) {
+            rownames(object) <-
+                upperCamelCase(rownames(object), strict = strict)
+        }
+        if (isTRUE(colnames) && hasColnames(object)) {
+            colnames(object) <-
+                upperCamelCase(colnames(object), strict = strict)
+        }
+        object
+    }
+
+
+
+#' @rdname upperCamelCase
+#' @export
+setMethod(
+    f = "upperCamelCase",
+    signature = signature("matrix"),
+    definition = `upperCamelCase,matrix`
+)
+
+
+
+`upperCamelCase,data.frame` <- `upperCamelCase,matrix`  # nolint
+
+
+
+#' @rdname upperCamelCase
+#' @export
+setMethod(
+    f = "upperCamelCase",
+    signature = signature("data.frame"),
+    definition = `upperCamelCase,data.frame`
+)
+
+
+
+`upperCamelCase,Vector` <-  # nolint
+    function(
+        object,
+        names = TRUE,
+        mcols = TRUE,
+        metadata = TRUE,
+        strict = FALSE
+    ) {
+        assert(
+            isFlag(names),
+            isFlag(mcols),
+            isFlag(metadata),
+            isFlag(strict)
+        )
+        if (isTRUE(names) && hasNames(object)) {
+            names(object) <-
+                upperCamelCase(names(object), strict = strict)
+        }
+        if (isTRUE(mcols) && hasNames(mcols(object))) {
+            names(mcols(object)) <-
+                upperCamelCase(names(mcols(object)), strict = strict)
+        }
+        if (isTRUE(metadata) && hasNames(metadata(object))) {
+            names(metadata(object)) <-
+                upperCamelCase(names(metadata(object)), strict = strict)
+        }
+        object
+    }
+
+
+
+#' @rdname upperCamelCase
+#' @export
+setMethod(
+    f = "upperCamelCase",
+    signature = signature("Vector"),
+    definition = `upperCamelCase,Vector`
+)
+
+
+
+`upperCamelCase,DataTable` <-  # nolint
+    function(
+        object,
+        rownames = FALSE,
+        colnames = TRUE,
+        mcols = TRUE,
+        metadata = TRUE,
+        strict = FALSE
+    ) {
+        assert(
+            hasDimnames(object),
+            isFlag(rownames),
+            isFlag(colnames),
+            isFlag(mcols),
+            isFlag(metadata),
+            isFlag(strict)
+        )
+        if (isTRUE(rownames) && hasRownames(object)) {
+            rownames(object) <-
+                upperCamelCase(rownames(object), strict = strict)
+        }
+        if (isTRUE(colnames) && hasColnames(object)) {
+            colnames(object) <-
+                upperCamelCase(colnames(object), strict = strict)
+        }
+        if (isTRUE(mcols) && hasNames(mcols(object))) {
+            names(mcols(object)) <-
+                upperCamelCase(names(mcols(object)), strict = strict)
+        }
+        if (isTRUE(metadata) && hasNames(metadata(object))) {
+            names(metadata(object)) <-
+                upperCamelCase(names(metadata(object)), strict = strict)
+        }
+        object
+
+    }
+
+
+
+#' @rdname upperCamelCase
+#' @export
+setMethod(
+    f = "upperCamelCase",
+    signature = signature("DataTable"),
+    definition = `upperCamelCase,DataTable`
+)
+
+
+
+`upperCamelCase,Ranges` <- `upperCamelCase,Vector`  # nolint
+formals(`upperCamelCase,Ranges`)[c("mcols", "names")] <- c(TRUE, FALSE)
+
+
+
+#' @rdname upperCamelCase
+#' @export
+setMethod(
+    f = "upperCamelCase",
+    signature = signature("Ranges"),
+    definition = `upperCamelCase,Ranges`
+)
+
+
+
+`upperCamelCase,Matrix` <- `upperCamelCase,matrix`  # nolint
+
+
+
+#' @rdname upperCamelCase
+#' @export
+setMethod(
+    f = "upperCamelCase",
+    signature = signature("Matrix"),
+    definition = `upperCamelCase,Matrix`
+)
+
+
+
+`upperCamelCase,SummarizedExperiment` <-  # nolint
+    function(
+        object,
+        rownames = FALSE,
+        colnames = TRUE,
+        assayNames = TRUE,
+        rowData = TRUE,
+        colData = TRUE,
+        metadata = TRUE,
+        strict = FALSE
+    ) {
+        assert(
+            isFlag(rownames),
+            isFlag(colnames),
+            isFlag(assayNames),
+            isFlag(rowData),
+            isFlag(colData),
+            isFlag(metadata),
+            isFlag(strict)
+        )
+        if (isTRUE(rownames) && hasRownames(object)) {
+            rownames(object) <-
+                upperCamelCase(rownames(object), strict = strict)
+        }
+        if (isTRUE(colnames) && hasColnames(object)) {
+            colnames(object) <-
+                upperCamelCase(colnames(object), strict = strict)
+        }
+        if (isTRUE(assayNames) && isCharacter(assayNames(object))) {
+            ## `assayNames<-` assignment method doesn't work reliably.
+            names(assays(object)) <-
+                upperCamelCase(names(assays(object)), strict = strict)
+        }
+        if (isTRUE(rowData) && hasColnames(rowData(object))) {
+            colnames(rowData(object)) <-
+                upperCamelCase(colnames(rowData(object)), strict = strict)
+        }
+        if (isTRUE(colData) && hasColnames(colData(object))) {
+            colnames(colData(object)) <-
+                upperCamelCase(colnames(colData(object)), strict = strict)
+        }
+        if (isTRUE(metadata) && hasNames(metadata(object))) {
+            names(metadata(object)) <-
+                upperCamelCase(names(metadata(object)), strict = strict)
+        }
+        object
+    }
+
+
+
+#' @rdname upperCamelCase
+#' @export
+setMethod(
+    f = "upperCamelCase",
+    signature = signature("SummarizedExperiment"),
+    definition = `upperCamelCase,SummarizedExperiment`
+)
