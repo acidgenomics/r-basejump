22444507a0b1238b85575acb048425609359a7b9
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-24 16:05:24 -0400

    Add more working examples

--- R/makeTx2GeneFromFASTA.R
+++ R/makeTx2GeneFromFASTA.R
@@ -43,22 +43,37 @@
 #'     protocol = "ftp"
 #' )
 #' makeTx2GeneFromFASTA(file, source = "gencode")
+#'
+#' ## WormBase ====
+#' file <- pasteURL(
+#'     "ftp.wormbase.org",
+#'     "pub",
+#'     "wormbase",
+#'     "releases",
+#'     "WS272",
+#'     "species",
+#'     "c_elegans",
+#'     "PRJNA13758",
+#'     "c_elegans.PRJNA13758.WS272.mRNA_transcripts.fa.gz",
+#'     protocol = "ftp"
+#' )
+#' makeTx2GeneFromFASTA(file, source = "wormbase")
 makeTx2GeneFromFASTA <- function(
     file,
     source = c(
         ## "refseq"
-        ## "wormbase"
         "ensembl",
         "gencode",
-        "flybase"
+        "flybase",
+        "wormbase"
     )
 ) {
     x <- import(file, format = "lines")
     source <- match.arg(source)
     x <- grep(pattern = "^>", x = x, value = TRUE)
-    ## Error if the input doesn't contain ">". For example, this happens with
-    ## bcbio "ref-transcripts.fa" file returned by gffutils.
-    assert(hasLength(x))
+    if (!hasLength(x)) {
+        stop("FASTA file does not contain '>' annotations.")
+    }
     x <- substr(x, start = 2L, stop = nchar(x))
     if (identical(source, "ensembl")) {
         ## Ensembl -------------------------------------------------------------
@@ -96,6 +111,17 @@ makeTx2GeneFromFASTA <- function(
             replacement = "\\1",
             x = x[, 2L]
         )
+    } else if (identical(source, "wormbase")) {
+        ## WormBase ------------------------------------------------------------
+        x <- strsplit(x = x, split = " ", fixed = TRUE)
+        x <- lapply(
+            X = x,
+            FUN = function(x) {
+                x[c(1L, 2L)]
+            }
+        )
+        x <- do.call(what = rbind, args = x)
+        x[, 2L] <- gsub(pattern = "^gene=", replacement = "", x = x[, 2L])
     }
     x <- unique(x)
     Tx2Gene(x)
