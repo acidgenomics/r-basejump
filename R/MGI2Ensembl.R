64ba6ed6f468fb191d68c5a1709e352e18199d3c
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-24 12:55:16 -0400

    Split out generators to separate files and rework Tx2Gene

---
+++ R/MGI2Ensembl.R
@@ -0,0 +1,30 @@
+#' @inherit MGI2Ensembl-class title description return
+#' @note Updated 2019-08-08.
+#' @export
+#' @inheritParams acidroxygen::params
+#' @examples
+#' options(acid.test = TRUE)
+#' x <- MGI2Ensembl()
+#' print(x)
+MGI2Ensembl <- function() {  # nolint
+    assert(hasInternet())
+    if (isTRUE(getOption("acid.test"))) {
+        file <- pasteURL(basejumpTestsURL, "mgi.rpt.gz", protocol = "none")
+    } else {
+        file <- pasteURL(
+            "www.informatics.jax.org",
+            "downloads",
+            "reports",
+            "MGI_Gene_Model_Coord.rpt",
+            protocol = "http"
+        )
+    }
+    message("Importing MGI-to-Ensembl gene ID mappings.")
+    data <- import(file, format = "tsv", colnames = FALSE)
+    data <- as(data[, c(1L, 11L)], "DataFrame")
+    colnames(data) <- c("mgiID", "geneID")
+    data[["mgiID"]] <- as.integer(gsub("^MGI\\:", "", data[["mgiID"]]))
+    data <- data[order(data[["mgiID"]]), , drop = FALSE]
+    metadata(data) <- .prototypeMetadata
+    new(Class = "MGI2Ensembl", data)
+}
