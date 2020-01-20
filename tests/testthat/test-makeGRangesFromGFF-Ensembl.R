449cb4fc22e607af4f61d326f25994ed588d0675
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-20 07:51:40 -0500

    Update test data globals

--- tests/testthat/test-makeGRangesFromGFF-Ensembl.R
+++ tests/testthat/test-makeGRangesFromGFF-Ensembl.R
@@ -1,6 +1,9 @@
 context("makeGRangesFromGFF : Ensembl")
 
 skip_if_not(hasInternet())
+
+Rle <- structure("Rle", package = "S4Vectors")  # nolint
+
 file <- file.path("cache", "ensembl.gtf")
 
 test_that("GTF genes", {
