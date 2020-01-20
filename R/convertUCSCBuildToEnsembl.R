7924e45d41a693c9780ea78b76eb21bb5402a9ff
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-19 23:39:47 -0500

    Migrate functions from freerange

---
+++ R/convertUCSCBuildToEnsembl.R
@@ -0,0 +1,69 @@
+#' Convert UCSC build to Ensembl
+#'
+#' @export
+#' @note Updated 2019-08-21.
+#'
+#' @inheritParams acidroxygen::params
+#'
+#' @return `character`. Ensembl genome build as the value, UCSC build as the
+#'   name. Stops on match failure.
+#'
+#' @seealso
+#' - [UCSC hgGateway](https://genome.ucsc.edu/cgi-bin/hgGateway)
+#' - [bcbio genome recipes](https://goo.gl/164J2P)
+#'
+#' @examples
+#' from <- c("hg19", "hg38")
+#' to <- convertUCSCBuildToEnsembl(from)
+#' print(to)
+convertUCSCBuildToEnsembl <- function(object) {
+    assert(isCharacter(object))
+    ucsc <- .ucscMatrix[, "ucsc"]
+    ensembl <- .ucscMatrix[, "ensembl"]
+    match <- match(x = object, table = ucsc)
+    ## Stop on any match failure.
+    if (any(is.na(match))) {
+        stop(sprintf(
+            "Failed to match UCSC to Ensembl: %s.",
+            toString(object[which(is.na(match))], width = 100L)
+        ))
+    }
+    out <- ensembl[match]
+    names(out) <- ucsc[match]
+    out
+}
+
+
+
+## nolint start
+.ucscMatrix <- matrix(
+    dimnames = list(NULL, c(
+        "ucsc",       "ensembl",           "date",    "organism",                 "ensembldb"
+    )),
+    data = c(
+        "araTha1",    "TAIR10",            "2011-02", "Arabidopsis thaliana",     FALSE,
+        "canFam3",    "CanFam3.1",         "2011-09", "Canis familiaris",         TRUE,
+        "ce10",       "WS220",             "2010-10", "Caenorhabditis elegans",   FALSE,
+        "ce11",       "WBcel235",          "2013-02", "Caenorhabditis elegans",   TRUE,
+        "danRer10",   "GRCz10",            "2014-09", "Danio rerio",              TRUE,
+        "danRer11",   "GRCz11",            "2017-05", "Danio rerio",              TRUE,
+        "dm3",        "BDGP5",             "2006-04", "Drosophila melanogaster",  FALSE,
+        "dm6",        "BDGP6",             "2014-08", "Drosophila melanogaster",  TRUE,
+        "galGal4",    "Galgal4",           "2011-11", "Gallus gallus",            FALSE,
+        "galGal5",    "Gallus_gallus-5.0", "2015-12", "Gallus gallus",            TRUE,
+        "galGal6",    "GRCg6a",            "2018-03", "Gallus gallus",            FALSE,
+        "hg19",       "GRCh37",            "2009-02", "Homo sapiens",             TRUE,
+        "hg38",       "GRCh38",            "2013-12", "Homo sapiens",             TRUE,
+        "hg38-noalt", "GRCh38",            "2013-12", "Homo sapiens",             TRUE,
+        "mm9",        "GRCm37",            "2007-07", "Mus musculus",             FALSE,
+        "mm10",       "GRCm38",            "2011-12", "Mus musculus",             TRUE,
+        "rn5",        "Rnor_5.0",          "2012-03", "Rattus norvegicus",        FALSE,
+        "rn6",        "Rnor_6.0",          "2014-07", "Rattus norvegicus",        TRUE,
+        "sacCer3",    "R64-1-1",           "2011-04", "Saccharomyces cerevisiae", TRUE,
+        "susScr11",   "Sscrofa11.1",       "2017-02", "Sus scrofa",               TRUE,
+        "xenTro2",    "JGI_4.1",           "2005-08", "Xenopus tropicalis",       FALSE,
+        "xenTro3",    "JGI_4.2",           "2009-11", "Xenopus tropicalis",       TRUE
+    ),
+    ncol = 5L, byrow = TRUE
+)
+## nolint end
---
+++ R/makeGRanges-internal.R
@@ -0,0 +1,260 @@
+#' Add the transcript version
+#'
+#' Append the transcript version to the identifier (e.g. ENST00000000233.10).
+#'
+#' @note Updated 2019-09-05.
+#' @noRd
+.addTxVersion <- function(object) {
+    message("Adding version to transcript identifiers.")
+    mcolnames <- colnames(mcols(object))
+    assert(
+        is(object, "GRanges"),
+        identical(metadata(object)[["level"]], "transcripts"),
+        isSubset("transcriptID", mcolnames)
+    )
+    if (isSubset("transcriptIDVersion", mcolnames)) {
+        ## `makeGRangesFromEnsembl()` output via ensembldb.
+        id <- mcols(object)[["transcriptIDVersion"]]
+    } else if (isSubset("transcriptVersion", mcolnames)) {
+        ## `makeGRangesFromGFF()` output.
+        id <- mcols(object)[["transcriptID"]]
+        version <- mcols(object)[["transcriptVersion"]]
+        id <- Rle(paste(id, version, sep = "."))
+    } else {
+        stop("Failed to locate transcript version metadata.")  # nolint
+    }
+    mcols(object)[["transcriptID"]] <- id
+    ## Note that names are set by `.makeGRanges()`.
+    object
+}
+
+
+
+## Note that this intentionally prioritizes transcripts over genes.
+.detectGRangesIDs <- function(object) {
+    if (is(object, "GRangesList")) {
+        object <- object[[1L]]
+    }
+    assert(is(object, "GRanges"))
+    mcolnames <- colnames(mcols(object))
+    if ("transcriptID" %in% mcolnames) {
+        "transcriptID"
+    } else if ("transcript_id" %in% mcolnames) {
+        "transcript_id"
+    } else if ("geneID" %in% mcolnames) {
+        "geneID"
+    } else if ("gene_id" %in% mcolnames) {
+        "gene_id"
+    } else {
+        stop("Failed to detect ID column.")
+    }
+}
+
+
+
+## This is the main GRanges final return generator, used by
+## `makeGRangesFromEnsembl()` and `makeGRangesFromGFF()`.
+.makeGRanges <- function(object, ignoreTxVersion = TRUE) {
+    assert(
+        is(object, "GRanges"),
+        hasNames(object),
+        hasLength(object),
+        isFlag(ignoreTxVersion)
+    )
+    ## Stash object length to ensure no dropping is occurring.
+    length <- length(object)
+    ## Minimize the object, removing unnecessary metadata columns.
+    object <- .minimizeGRanges(object)
+    assert(identical(length(object), length))
+    ## Now we're ready to standardize the metadata naming conventions.
+    object <- .standardizeGRanges(object)
+    assert(identical(length(object), length))
+    ## Prepare the metadata.
+    ## Slot organism into metadata.
+    object <- .slotOrganism(object)
+    ## Ensure object contains prototype metadata.
+    metadata(object) <- c(.prototypeMetadata, metadata(object))
+    ## Ensure the transcript identifier includes the version, if desired. This
+    ## is useful for tx2gene handling with tximport.
+    if (identical(ignoreTxVersion, FALSE)) {
+        object <- .addTxVersion(object)
+    }
+    idCol <- .detectGRangesIDs(object)
+    assert(isSubset(idCol, colnames(mcols(object))))
+    names <- as.character(mcols(object)[[idCol]])
+    assert(!any(is.na(names)))
+    ## Inform the user if the object contains invalid names, showing offenders.
+    invalid <- setdiff(names, make.names(names, unique = TRUE))
+    if (hasLength(invalid)) {
+        invalid <- sort(unique(invalid))
+        message(sprintf(
+            "%d invalid %s: %s.",
+            length(invalid),
+            ngettext(
+                n = length(invalid),
+                msg1 = "name",
+                msg2 = "names"
+            ),
+            toString(invalid, width = 100L)
+        ))
+    }
+    rm(invalid)
+    ## Split into GRangesList if object contains multiple ranges per feature.
+    if (hasDuplicates(names)) {
+        message(sprintf(
+            fmt = paste0(
+                "GRanges contains multiple ranges per '%s'.\n",
+                "Splitting into GRangesList."
+            ),
+            idCol
+        ))
+        ## Metadata will get dropped during `split()` call; stash and reassign.
+        metadata <- metadata(object)
+        object <- split(x = object, f = as.factor(names))
+        metadata(object) <- metadata
+        rm(metadata)
+    } else {
+        names(object) <- names
+    }
+    ## Ensure the ranges are sorted by gene identifier.
+    object <- object[sort(names(object))]
+    ## Inform the user about the number of features returned.
+    level <- match.arg(
+        arg = metadata(object)[["level"]],
+        choices = c("genes", "transcripts")
+    )
+    message(sprintf(
+        "%d %s detected.",
+        length(object),
+        ngettext(
+            n = length(object),
+            msg1 = substr(level, 1L, nchar(level) - 1L),  # gene
+            msg2 = level                                  # genes
+        )
+    ))
+    object
+}
+
+
+
+## Remove uninformative metadata columns from GFF3 before return.
+## Always remove columns beginning with a capital letter.
+## - Ensembl: Alias, ID, Name, Parent
+## - GENCODE: ID, Parent
+## - RefSeq: Dbxref, Gap, ID, Name, Note, Parent, Target
+.minimizeGFF3 <- function(object) {
+    assert(is(object, "GRanges"))
+    mcols <- mcols(object)
+    mcolnames <- colnames(mcols)
+    ## Remove all columns beginning with a capital letter.
+    keep <- !grepl("^[A-Z]", mcolnames)
+    mcolnames <- mcolnames[keep]
+    ## Remove additional blacklisted columns.
+    blacklist <- "biotype"
+    mcolnames <- setdiff(mcolnames, blacklist)
+    ## Subset the metadata columns.
+    mcols <- mcols[, mcolnames, drop = FALSE]
+    mcols(object) <- mcols
+    object
+}
+
+
+
+## This step drops extra columns in `mcols()` and applies run-length encoding,
+## to reduce memory overhead.
+##
+## Note that `removeNA()` call currently will error on complex columns.
+## For example, this will error on `CharacterList` columns returned from
+## GENCODE GFF3 file.
+.minimizeGRanges <- function(object) {
+    assert(is(object, "GRanges"))
+    mcols <- mcols(object)
+    ## Drop any complex S4 columns that aren't either atomic or list.
+    keep <- bapply(
+        X = mcols,
+        FUN = function(x) {
+            is.atomic(x) || is.list(x)
+        }
+    )
+    mcols <- mcols[, keep, drop = FALSE]
+    ## Ensure NA values are properly set, prior to `removeNA()` call.
+    mcols <- sanitizeNA(mcols)
+    ## Remove columns that are all `NA`. This step will remove all
+    ## transcript-level columns from gene-level ranges.
+    mcols <- removeNA(mcols)
+    ## Apply run-length encoding on all atomic columns.
+    mcols <- lapply(
+        X = mcols,
+        FUN = function(x) {
+            if (isS4(x) || is(x, "AsIs") || !is.atomic(x)) {
+                ## `I()` inhibits reinterpretation and returns `AsIs` class.
+                ## This keeps complex columns (e.g. Entrez list) intact.
+                ## Recommended in the `DataFrame` documentation.
+                I(x)
+            } else {
+                ## Ensure factor levels get correctly reset, to save memory.
+                if (is.factor(x)) {
+                    x <- droplevels(x)
+                }
+                ## Use S4 run length encoding (Rle) for atomic metadata columns.
+                ## Many of these elements are repetitive, and this makes
+                ## operations faster.
+                Rle(x)
+            }
+        }
+    )
+    ## `lapply()` returns as list, so we need to coerce back to DataFrame.
+    mcols <- as(mcols, "DataFrame")
+    mcols(object) <- mcols
+    object
+}
+
+
+
+## Standardize the GRanges into desired conventions used in basejump. Note that
+## this step makes GRanges imported via `rtracklayer::import()` incompatible
+## with `GenomicFeatures::makeTxDbFromGRanges()`.
+.standardizeGRanges <- function(object) {
+    assert(is(object, "GRanges"))
+    ## Standardize the metadata columns.
+    mcols <- mcols(object)
+    ## Use `transcript` prefix instead of `tx` consistently.
+    colnames(mcols) <- gsub(
+        pattern = "^tx_",
+        replacement = "transcript_",
+        x = colnames(mcols)
+    )
+    ## Ensure "ID" is always capitalized (e.g. "entrezid").
+    colnames(mcols) <- gsub(
+        pattern = "(.+)id$",
+        replacement = "\\1ID",
+        x = colnames(mcols)
+    )
+    ## Always return using camel case, even though GFF/GTF files use snake.
+    colnames(mcols) <- camelCase(colnames(mcols))
+    ## Always use `geneName` instead of `symbol`.
+    ## Note that ensembldb output duplicates these.
+    if (all(c("geneName", "symbol") %in% colnames(mcols))) {
+        mcols[["symbol"]] <- NULL
+    } else if ("symbol" %in% colnames(mcols)) {
+        message("Renaming 'symbol' to 'geneName' in 'mcols()'.")
+        mcols[["geneName"]] <- mcols[["symbol"]]
+        mcols[["symbol"]] <- NULL
+    }
+    ## Re-slot updated mcols back into object before calculating broad class
+    ## biotype and/or assigning names.
+    mcols(object) <- mcols
+    ## Ensure broad class definitions are included, using run-length encoding.
+    ## Don't pass `mcols` to `.broadClass()`, use the GRanges instead because
+    ## we need the corresponding seqnames for calculations.
+    mcols(object)[["broadClass"]] <- Rle(.broadClass(object))
+    ## Finally, sort the metadata columns alphabetically.
+    mcols(object) <-
+        mcols(object)[, sort(colnames(mcols(object))), drop = FALSE]
+    ## Ensure the ranges are sorted by identifier.
+    idCol <- .detectGRangesIDs(object)
+    message(sprintf("Arranging by '%s'.", idCol))
+    names(object) <- mcols(object)[[idCol]]
+    object <- object[sort(names(object))]
+    object
+}
