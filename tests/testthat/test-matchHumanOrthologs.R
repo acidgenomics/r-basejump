c80bf97fca2f751466bd1e92fc80bc0d6de38fc0
Michael Steinbaugh <mike@steinbaugh.com>
2020-03-15 17:44:22 -0400

    Update unit tests

--- tests/testthat/test-matchHumanOrthologs.R
+++ tests/testthat/test-matchHumanOrthologs.R
@@ -4,20 +4,20 @@ skip_if_not(hasInternet())
 
 test_that("Mus musculus", {
     genes <- c(
-        "ENSMUSG00000000001", "ENSMUSG00000000003",
-        "ENSMUSG00000000028", "ENSMUSG00000000031",
-        "ENSMUSG00000000037", "ENSMUSG00000000049"
+        "ENSMUSG00000000001",
+        "ENSMUSG00000000003",
+        "ENSMUSG00000000028"
     )
     ## This depends on biomaRt, and has a tendency to time out.
     map <- tryCatch(
-        expr = matchHumanOrthologs(genes, ensemblRelease = 87L),
+        expr = matchHumanOrthologs(genes, ensemblRelease = NULL),
         error = function(e) e
     )
     ## Skip if connection timed out.
     if (is(map, "error")) {
         msg <- as.character(map)
         skip_if(
-            condition = grepl(pattern = "timed out", x = msg),
+            condition = grepl(pattern = "biomaRt", x = msg),
             message = msg
         )
     }
@@ -29,26 +29,17 @@ test_that("Mus musculus", {
             geneName = c(
                 "Gnai3",
                 "Pbsn",
-                "Cdc45",
-                "H19",
-                "Scml2",
-                "Apoh"
+                "Cdc45"
             ),
             hgncID = c(
                 "ENSG00000065135",
                 NA,
-                "ENSG00000093009",
-                NA,
-                "ENSG00000102098",
-                "ENSG00000091583"
+                "ENSG00000093009"
             ),
             hgncName = c(
                 "GNAI3",
                 NA,
-                "CDC45",
-                NA,
-                "SCML2",
-                "APOH"
+                "CDC45"
             ),
             row.names = genes
         )
