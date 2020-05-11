1ac1eaa71b057b2b1c980a789e9a3cf2dc2c5d21
Michael Steinbaugh <mike@steinbaugh.com>
2020-05-11 12:47:44 -0400

    Unit test updates

--- tests/testthat/test-getURLDirList.R
+++ tests/testthat/test-getURLDirList.R
@@ -1,11 +1,11 @@
 context("getURLDirList")
 
-skip_if_not(hasInternet())
 skip_on_appveyor()
 skip_on_docker()
 
 test_that("NCBI FTP", {
-    url <- "ftp://ftp.ncbi.nlm.nih.gov/genomes/Homo_sapiens/current"
-    x <- getURLDirList(url, pattern = "^GCF_")
+    url <- "ftp://ftp.ncbi.nlm.nih.gov/genomes/"
+    skip_if_not(hasInternet(url))
+    x <- getURLDirList(url, pattern = "^refseq$")
     expect_is(x, "character")
 })
