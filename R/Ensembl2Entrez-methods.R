f6917df3f71cb88574d9ff10cffceeea51b90db3
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-01 19:22:13 -0400

    Rework methods

--- R/Ensembl2Entrez-methods.R
+++ R/Ensembl2Entrez-methods.R
@@ -19,12 +19,13 @@
 #'
 #' ## character ====
 #' ## Ensembl-to-Entrez.
-#' genes <- c("ENSG00000000003", "ENSG00000000005", "ENSG00000000419")
+#' genes <- c("ENSG00000000003", "ENSG00000000005")
 #' x <- Ensembl2Entrez(genes)
 #' print(x)
 #'
-#' ## Entrez-to-Ensembl
-#' genes <- c(1L, 2L, 3L).
+#' ## integer ====
+#' ## Entrez-to-Ensembl.
+#' genes <- c(1L, 2L)
 #' x <- Entrez2Ensembl(genes)
 #' print(x)
 #'
@@ -35,166 +36,142 @@ NULL
 
 
 
-## Updated 2020-10-01.
-.ncbiOrgDb <- function(organism) {
-    assert(isString(organism))
-    ah <- .annotationHub()
-    ahs <- query(ah, pattern = c(organism, "NCBI", "OrgDb"))
-    id <- tail(names(ahs), n = 1L)
-    orgdb <- ah[[id]]
-    assert(is(orgdb, "OrgDb"))
-    orgdb
-}
-
-
-
-## FIXME Add character method.
-
-
-
-## Updated 2020-10-01.
-`Entrez2Ensembl,character` <-
+#' Make an Ensembl2Entrez (or Entrez2Ensembl) object
+#'
+#' @note Updated 2019-08-16.
+#' @noRd
+.makeEnsembl2Entrez <-
     function(
         object,
-        organism,
         format = c("1:1", "long"),
-        strict = TRUE
+        return = c("Ensembl2Entrez", "Entrez2Ensembl")
     ) {
-        assert(isFlag(strict))
         format <- match.arg(format)
-        df <- select(
-            x = .ncbiOrgDb(organism),
-            keys = object,
-            keytype = "ENTREZID",
-            columns = "ENSEMBL", "SYMBOL"
+        return <- match.arg(return)
+        cols <- switch(
+            EXPR = return,
+            "Ensembl2Entrez" = c("ensembl", "entrez"),
+            "Entrez2Ensembl" = c("entrez", "ensembl")
         )
-
-        ## FIXME STRICT MATCH MODE.
-
-        ## FIXME RETURN 1:1 HERE.
-
-        df
-    }
-
-
-
-#' @rdname Ensembl2Entrez
-#' @export
-setMethod(
-    f = "Entrez2Ensembl",
-    signature = signature("character"),
-    definition = `Entrez2Ensembl,character`
-)
-
-
-
-## Updated 2019-08-16.
-`Ensembl2Entrez,DataFrame` <-  # nolint
-    function(object, format = c("1:1", "long")) {
-        assert(hasRows(object))
-        format <- match.arg(format)
-        cols <- c("geneID", "entrezID")
-        if (!all(cols %in% colnames(object))) {
-            stop(sprintf(
-                "Object does not contain Ensembl-to-Entrez mappings: %s.",
-                toString(cols)
-            ))
-        }
-        data <- DataFrame(
-            geneID = as.character(decode(object[["geneID"]])),
-            entrezID = I(object[["entrezID"]]),
-            row.names = rownames(object)
+        assert(
+            is(object, "DataFrame"),
+            hasRows(object),
+            isSubset(cols, colnames(object))
         )
-        ## Expand to long format.
-        data <- expand(data)
-        ## Inform the user about genes that don't map to Entrez.
-        unmapped <- data[["geneID"]][which(is.na(data[["entrezID"]]))]
-        assert(hasNoDuplicates(unmapped))
-        if (length(unmapped) > 0L) {
-            cli_alert_info(sprintf(
-                "%d %s map to Entrez IDs.",
-                length(unmapped),
-                ngettext(
-                    n = length(unmapped),
-                    msg1 = "gene doesn't",
-                    msg2 = "genes don't"
-                )
-            ))
-        }
-        ## Inform the user about how many genes multi-map to Entrez.
-        multimapped <- unique(data[["geneID"]][duplicated(data[["geneID"]])])
-        if (length(multimapped) > 0L) {
-            cli_alert_info(sprintf(
-                "%d %s to multiple Entrez IDs.",
-                length(multimapped),
-                ngettext(
-                    n = length(multimapped),
-                    msg1 = "gene maps",
-                    msg2 = "genes map"
-                )
-            ))
-        }
-        ## Return mode.
-        if (format == "1:1") {
-            cli_alert(
-                "Returning with 1:1 mappings using oldest Entrez ID per gene."
-            )
-            entrez <- object[["entrezID"]]
-            assert(is.list(entrez))
-            names(entrez) <- object[["geneID"]]
-            map <- bplapply(
-                X = entrez,
-                FUN = function(x) {
-                    if (all(is.na(x))) {
-                        NA_integer_
-                    } else {
-                        sort(x)[[1L]]
+        df <- object[, cols]
+        rownames(df) <- NULL
+        df <- decode(expand(df))
+        assert(
+            is.character(df[["ensembl"]]),
+            is.integer(df[["entrez"]])
+        )
+        if (identical(format, "1:1")) {
+            split <- split(x = df, f = df[[1L]])
+            unique <- unlist(
+                x = bplapply(
+                    X = split[, 2L],
+                    FUN = function(x) {
+                        if (all(is.na(x))) {
+                            NA
+                        } else {
+                            head(sort(x), n = 1L)
+                        }
                     }
-                }
+                ),
+                recursive = FALSE,
+                use.names = FALSE
             )
-            entrez <- unlist(map, recursive = FALSE, use.names = TRUE)
-            data <- DataFrame(
-                geneID = names(entrez),
-                entrezID = as.integer(entrez),
-                row.names = rownames(object)
-            )
-        } else if (format == "long") {
-            cli_alert_warning(
-                "Returning 1:many in long format {.emph (not recommended)}."
-            )
+            df <- DataFrame( "a" = names(split), "b" = unique)
+            rownames(df) <- df[[1L]]
+            colnames(df) <- cols
         }
-        metadata(data) <- metadata(object)
-        metadata(data)[["format"]] <- format
-        new(Class = "Ensembl2Entrez", data)
+        df[["entrez"]] <- as.integer(df[["entrez"]])
+        df <- df[complete.cases(df), ]
+        assert(hasRows(df))
+        metadata(df) <- metadata(object)
+        metadata(df)[["format"]] <- format
+        new(Class = return, df)
     }
 
 
+
+## Updated 2020-10-01.
+`Ensembl2Entrez,character` <-  # nolint
+    function(
+        object,
+        organism = NULL,
+        format = c("1:1", "long")
+    ) {
+        if (is.null(organism)) {
+            organism <- detectOrganism(object)
+        }
+        df <- .ncbiOrgDb(
+            keys = object,
+            keytype = "ENSEMBL",
+            columns = "ENTREZID",
+            organism = organism
+        )
+        .makeEnsembl2Entrez(
+            object = df,
+            format = match.arg(format)
+        )
+    }
 
 #' @rdname Ensembl2Entrez
 #' @export
 setMethod(
     f = "Ensembl2Entrez",
-    signature = signature("DataFrame"),
-    definition = `Ensembl2Entrez,DataFrame`
+    signature = signature("character"),
+    definition = `Ensembl2Entrez,character`
 )
 
 
 
-## Updated 2019-07-22.
-`Ensembl2Entrez,GRanges` <-  # nolint
-    function(object) {
-        data <- as(object, "DataFrame")
-        metadata(data) <- metadata(object)
-        do.call(
-            what = Ensembl2Entrez,
-            args = list(
-                object = data,
-                format = format
-            )
+## Updated 2020-10-01.
+`Entrez2Ensembl,integer` <-  # nolint
+    function(
+        object,
+        organism = "Homo sapiens",
+        format = c("1:1", "long")
+    ) {
+        df <- .ncbiOrgDb(
+            keys = as.character(object),
+            keytype = "ENTREZID",
+            columns = "ENSEMBL",
+            organism = organism
+        )
+        .makeEnsembl2Entrez(
+            object = df,
+            format = match.arg(format),
+            return = "Entrez2Ensembl"
         )
     }
 
-formals(`Ensembl2Entrez,GRanges`) <- formals(`Ensembl2Entrez,DataFrame`)
+#' @rdname Ensembl2Entrez
+#' @export
+setMethod(
+    f = "Entrez2Ensembl",
+    signature = signature("integer"),
+    definition = `Entrez2Ensembl,integer`
+)
+
+
+
+## Updated 2020-10-01.
+`Ensembl2Entrez,GRanges` <-  # nolint
+    function(
+        object,
+        format = c("1:1", "long")
+    ) {
+        df <- as(object, "DataFrame")
+        colnames(df)[colnames(df) == "geneID"] <- "ensembl"
+        colnames(df)[colnames(df) == "entrezID"] <- "entrez"
+        metadata(df) <- metadata(object)
+        `Ensembl2Entrez,DataFrame`(
+            object = df,
+            format = match.arg(format)
+        )
+    }
 
 
 
@@ -208,25 +185,11 @@ setMethod(
 
 
 
-## Updated 2019-07-22.
-`Ensembl2Entrez,SummarizedExperiment` <-  # nolint
-    function(object) {
-        object <- as.SummarizedExperiment(object)
-        data <- rowData(object)
-        rownames(data) <- rownames(object)
-        do.call(
-            what = Ensembl2Entrez,
-            args = list(
-                object = data,
-                format = format
-            )
-        )
-
-
+## Updated 2020-10-01.
+`Ensembl2Entrez,RangedSummarizedExperiment` <-  # nolint
+    function(object, ...) {
+        Ensembl2Entrez(rowRanges(object), ...)
     }
-
-formals(`Ensembl2Entrez,SummarizedExperiment`) <-
-    formals(`Ensembl2Entrez,DataFrame`)
 
 
 
@@ -234,6 +197,6 @@ formals(`Ensembl2Entrez,SummarizedExperiment`) <-
 #' @export
 setMethod(
     f = "Ensembl2Entrez",
-    signature = signature("SummarizedExperiment"),
-    definition = `Ensembl2Entrez,SummarizedExperiment`
+    signature = signature("RangedSummarizedExperiment"),
+    definition = `Ensembl2Entrez,RangedSummarizedExperiment`
 )
