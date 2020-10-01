44853177796d76076ac32915aac28a5210c25689
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-01 19:24:54 -0400

    Rework Ensembl2Entrez approach

--- R/internal-AnnotationHub.R
+++ R/internal-AnnotationHub.R
@@ -165,3 +165,70 @@
     .forceDetach(keep = userAttached)
     id
 }
+
+
+
+#' Get Ensembl/Entrez mappings from NCBI OrgDb via AnnotationHub
+#'
+#' @note Updated 2020-10-01.
+#' @noRd
+.getEnsembl2EntrezFromOrgDb <- function(
+    keys,
+    keytype,
+    columns,
+    organism,
+    strict = TRUE
+) {
+    assert(
+        isCharacter(keys),
+        hasNoDuplicates(keys),
+        isString(keytype),
+        isCharacter(columns),
+        isString(organism),
+        isFlag(strict)
+    )
+    cli_alert(sprintf(
+        "Matching identifiers using NCBI {.var %s} via {.pkg %s} %s.",
+        "OrgDb",
+        "AnnotationHub",
+        packageVersion("AnnotationHub")
+    ))
+    userAttached <- .packages()
+    ah <- .annotationHub()
+    ahs <- query(ah, pattern = c(organism, "NCBI", "OrgDb"))
+    id <- tail(names(ahs), n = 1L)
+    suppressMessages({
+        orgdb <- ah[[id]]
+    })
+    assert(is(orgdb, "OrgDb"))
+    cli_alert_info(sprintf(
+        "{.val %s} (%s): %s.",
+        id,
+        mcols(ahs)[id, "title"],
+        mcols(ahs)[id, "description"]
+    ))
+    suppressMessages({
+        df <- select(
+            x = orgdb,
+            keys = keys,
+            keytype = keytype,
+            columns = columns
+        )
+    })
+    df <- as(df, "DataFrame")
+    if (isTRUE(strict)) {
+        df <- df[complete.cases(df), ]
+        if (!areSetEqual(keys, unique(df[[keytype]]))) {
+            setdiff <- setdiff(keys, unique(df[[keytype]]))
+            stop(sprintf(
+                "Match failure: %s.",
+                toString(setdiff, width = 200L)
+            ))
+        }
+    }
+    colnames(df)[colnames(df) == "ENSEMBL"] <- "ensembl"
+    colnames(df)[colnames(df) == "ENTREZID"] <- "entrez"
+    df[["entrez"]] <- as.integer(df[["entrez"]])
+    .forceDetach(keep = userAttached)
+    df
+}
