d86f20013978da03913356afe7c38de5e7b59479
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-05 11:11:06 -0400

    Use BiocFileCache, change column names, return complete cases

--- R/MGI2Ensembl.R
+++ R/MGI2Ensembl.R
@@ -1,29 +1,28 @@
 #' @inherit MGI2Ensembl-class title description return
-#' @note Updated 2020-05-18.
+#' @note Updated 2020-10-05.
 #' @export
 #' @examples
-#' options(acid.test = TRUE)
-#' x <- MGI2Ensembl()
-#' print(x)
+#' object <- MGI2Ensembl()
+#' print(object)
 MGI2Ensembl <- function() {  # nolint
     assert(hasInternet())
-    if (isTRUE(getOption("acid.test"))) {
-        file <- pasteURL(basejumpTestsURL, "mgi.rpt.gz", protocol = "none")
-    } else {
-        file <- pasteURL(
-            "www.informatics.jax.org",
-            "downloads",
-            "reports",
-            "MGI_Gene_Model_Coord.rpt",
-            protocol = "http"
-        )
-    }
     cli_alert("Importing MGI-to-Ensembl gene ID mappings.")
-    data <- import(file = file, format = "tsv", colnames = TRUE)
-    data <- as(data[, c(1L, 11L)], "DataFrame")
-    colnames(data) <- c("mgiID", "geneID")
-    data[["mgiID"]] <- as.integer(gsub("^MGI\\:", "", data[["mgiID"]]))
-    data <- data[order(data[["mgiID"]]), , drop = FALSE]
-    metadata(data) <- .prototypeMetadata
-    new(Class = "MGI2Ensembl", data)
+    url <- pasteURL(
+        "www.informatics.jax.org",
+        "downloads",
+        "reports",
+        "MGI_Gene_Model_Coord.rpt",
+        protocol = "http"
+    )
+    file <- cacheURL(url)
+    df <- import(file = file, format = "tsv", colnames = TRUE)
+    df <- as(df[, c(1L, 11L)], "DataFrame")
+    colnames(df) <- c("mgi", "ensembl")
+    df <- df[complete.cases(df), ]
+    df[["mgi"]] <- as.integer(gsub("^MGI\\:", "", df[["mgi"]]))
+    assert(hasNoDuplicates(df[["mgi"]]))
+    rownames(df) <- df[["mgi"]]
+    df <- df[order(df[["mgi"]]), , drop = FALSE]
+    metadata(df) <- .prototypeMetadata
+    new(Class = "MGI2Ensembl", df)
 }
