b5500f366672bdfe669b9794806efe4d371aa4ec
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-06 09:04:27 -0400

    Migrate genome code to AcidGenomes

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -5,50 +5,6 @@ NULL
 
 
 
-#' @rdname Ensembl2Entrez
-#' @export
-setGeneric(
-    name = "Ensembl2Entrez",
-    def = function(object, ...) {
-        standardGeneric("Ensembl2Entrez")
-    }
-)
-
-
-
-#' @rdname Ensembl2Entrez
-#' @export
-setGeneric(
-    name = "Entrez2Ensembl",
-    def = function(object, ...) {
-        standardGeneric("Entrez2Ensembl")
-    }
-)
-
-
-
-#' @rdname Gene2Symbol
-#' @export
-setGeneric(
-    name = "Gene2Symbol",
-    def = function(object, ...) {
-        standardGeneric("Gene2Symbol")
-    }
-)
-
-
-
-#' @rdname Tx2Gene
-#' @export
-setGeneric(
-    name = "Tx2Gene",
-    def = function(object, ...) {
-        standardGeneric("Tx2Gene")
-    }
-)
-
-
-
 #' @rdname integerCounts
 #' @export
 setGeneric(
