ed7b5e6357b2c8cf1cd33395b2ef37765d4fc236
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-07 11:19:23 -0400

    Migrate unit tests to subpackages

--- tests/testthat/test-select.R
+++
@@ -1,9 +0,0 @@
-context("select")
-
-x <- as(mtcars, "DataFrame")
-
-test_that("selectIf", {
-    x <- selectIf(x, predicate = is.double)
-    expect_s4_class(x, "DataFrame")
-    expect_true(hasRownames(x))
-})
