ba876898f0ffe1702f6a6415acfdc3f001aee822
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-07 13:00:29 -0400

    Improve code coverage

--- tests/testthat/test-headtail.R
+++ tests/testthat/test-headtail.R
@@ -1,28 +1,5 @@
 context("headtail")
 
-test_that("atomic", {
-    expect_output(
-        object = headtail(LETTERS),
-        regexp = "A B ... Y Z"
-    )
-
-})
-
-test_that("matrix", {
-    mat <- assay(rse)
-    expect_output(
-        object = headtail(mat),
-        regexp = "gene001"
-    )
-})
-
-test_that("matrix : No quadrants", {
-    expect_message(
-        object = capture.output(headtail(mat)),
-        regexp = "Object can't be split into quadrants."
-    )
-})
-
 test_that("GRanges", {
     expect_output(
         object = headtail(gr),
