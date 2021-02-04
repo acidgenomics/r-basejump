598b1ca7c7e5a6820f38a1d69e1544b45961f93c
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-04 09:36:20 -0500

    Migrate generics

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,50 +1,8 @@
-#' @rdname coerce-list
-#' @name coerceToList
-#' @importFrom AcidGenerics coerceToList
-#' @usage coerceToList(object, ...)
-#' @export
-NULL
-
-
-
+## FIXME MOVE TO ACIDEXPERIMENT?
 #' @rdname headtail
 #' @name headtail
 #' @importFrom AcidGenerics headtail
 #' @importMethodsFrom AcidBase headtail
 #' @usage headtail(x, ...)
-#' @export
-NULL
-
-
-
-#' @rdname intersectAll
-#' @name intersectAll
-#' @importFrom AcidGenerics intersectAll
-#' @usage intersectAll(object, ...)
-#' @export
-NULL
-
-
-
-#' @rdname intersectionMatrix
-#' @name intersectionMatrix
-#' @importFrom AcidGenerics intersectionMatrix
-#' @usage intersectionMatrix(object, ...)
-#' @export
-NULL
-
-
-
-#' @rdname logRatio
-#' @name foldChangeToLogRatio
-#' @importFrom AcidGenerics foldChangeToLogRatio
-#' @usage foldChangeToLogRatio(object, ...)
-#' @export
-NULL
-
-#' @rdname logRatio
-#' @name logRatioToFoldChange
-#' @importFrom AcidGenerics logRatioToFoldChange
-#' @usage logRatioToFoldChange(object, ...)
 #' @export
 NULL
