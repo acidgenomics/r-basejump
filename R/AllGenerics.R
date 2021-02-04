c59819454180efeb6d39f5f27eb5bb1b56ff52b6
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-04 11:00:28 -0500

    Work toward package simplification

--- R/AllGenerics.R
+++
@@ -1,8 +0,0 @@
-## FIXME MOVE TO ACIDEXPERIMENT?
-#' @rdname headtail
-#' @name headtail
-#' @importFrom AcidGenerics headtail
-#' @importMethodsFrom AcidBase headtail
-#' @usage headtail(x, ...)
-#' @export
-NULL
