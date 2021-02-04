21d7f0e9e44c683a57d06aa25ed405c48369c0a0
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-04 10:49:51 -0500

    Work toward simplification of package

--- R/imports.R
+++ R/imports.R
@@ -1,17 +1,3 @@
-## FIXME ANY MATRIX METHODS NEED TO GO IN ACIDSINGLECELL.
-## FIXME NEED TO REEXPORT PIPETTE AS.DATA.FRAME HERE.
-
-
-
-#' @importClassesFrom GenomicRanges GRanges
-#' @importClassesFrom IRanges IRanges
-#' @importClassesFrom Matrix Matrix
-#' @importClassesFrom S4Vectors Annotated DataFrame List Vector
-#' @importClassesFrom SummarizedExperiment SummarizedExperiment
-NULL
-
-
-
 ## FIXME REDUCE THE NUMBER OF GOALIE IMPORTS.
 
 #' @importFrom AcidBase requireNamespaces
@@ -21,15 +7,8 @@ NULL
 #'   assayNames assayNames<- assays assays<- colData colData<- rowData rowData<-
 #'   rowRanges rowRanges<-
 #' @importFrom goalie areSameLength assert hasColnames hasDimnames hasLength
-#'   hasNames hasRownames
-#'
-#'   hasUniqueCols hasValidDimnames hasValidNames
-#'   isADirectory isAFile isAURL isAlpha isAny isCharacter isFlag isGGScale
-#'   isGreaterThanOrEqualTo isHeaderLevel isHexColorFunction isInClosedRange
-#'   isInLeftOpenRange isInRange isInstalled isInt isIntegerish isMatchingRegex
-#'   isNonNegative isNotMatchingRegex isNumber isPositive isScalar isString
-#'   isSubset isSuperset matchesUniqueGeneNames validNames validate
-#'   validateClasses
+#'   hasNames hasRownames isADirectory isAny isCharacter isFlag isHeaderLevel
+#'   isInRange isInstalled isInt isPositive isString isSubset
 #' @importFrom methods as coerce getMethod is setAs setMethod signature slot
 #'   slotNames validObject .hasSlot
 #' @importFrom stringr str_dup str_length str_pad
@@ -39,7 +18,7 @@ NULL
 
 
 #' @importFrom methods coerce
-#' @importMethodsFrom SummarizedExperiment coerce
+#' @importMethodsFrom AcidExperiment coerce
 #' @importMethodsFrom pipette coerce
 #' @exportMethod coerce
 NULL
