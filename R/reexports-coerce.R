1898403b7240d2bc979a9230e097d7943aa80bc9
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-06 06:40:43 -0500

    Include setAs coercion methods

--- R/reexports-coerce.R
+++ R/reexports-coerce.R
@@ -7,6 +7,97 @@ methods::coerce
 
 
 setAs(
+    from = "DataFrame",
+    to = "data.table",
+    def = getMethod(
+        f = "coerce",
+        signature(
+            from = "DataFrame",
+            to = "data.table"
+        ),
+        where = "pipette"
+    )
+)
+
+setAs(
+    from = "DataFrame",
+    to = "tbl_df",
+    def = getMethod(
+        f = "coerce",
+        signature(
+            from = "DataFrame",
+            to = "tbl_df"
+        ),
+        where = "pipette"
+    )
+)
+
+setAs(
+    from = "GenomicRanges",
+    to = "data.table",
+    def = getMethod(
+        f = "coerce",
+        signature(
+            from = "GenomicRanges",
+            to = "data.table"
+        ),
+        where = "pipette"
+    )
+)
+
+setAs(
+    from = "GenomicRanges",
+    to = "tbl_df",
+    def = getMethod(
+        f = "coerce",
+        signature(
+            from = "GenomicRanges",
+            to = "tbl_df"
+        ),
+        where = "pipette"
+    )
+)
+
+setAs(
+    from = "IRanges",
+    to = "data.frame",
+    def = getMethod(
+        f = "coerce",
+        signature(
+            from = "IRanges",
+            to = "data.frame"
+        ),
+        where = "pipette"
+    )
+)
+
+setAs(
+    from = "IRanges",
+    to = "data.table",
+    def = getMethod(
+        f = "coerce",
+        signature(
+            from = "IRanges",
+            to = "data.table"
+        ),
+        where = "pipette"
+    )
+)
+
+setAs(
+    from = "IRanges",
+    to = "tbl_df",
+    def = getMethod(
+        f = "coerce",
+        signature(
+            from = "IRanges",
+            to = "tbl_df"
+        ),
+        where = "pipette"
+    )
+)
+
+setAs(
     from = "Matrix",
     to = "DataFrame",
     def = getMethod(
@@ -14,6 +105,45 @@ setAs(
         signature(
             from = "Matrix",
             to = "DataFrame"
+        ),
+        where = "pipette"
+    )
+)
+
+setAs(
+    from = "Matrix",
+    to = "data.frame",
+    def = getMethod(
+        f = "coerce",
+        signature(
+            from = "Matrix",
+            to = "data.frame"
+        ),
+        where = "pipette"
+    )
+)
+
+setAs(
+    from = "data.frame",
+    to = "data.table",
+    def = getMethod(
+        f = "coerce",
+        signature(
+            from = "data.frame",
+            to = "data.table"
+        ),
+        where = "pipette"
+    )
+)
+
+setAs(
+    from = "data.frame",
+    to = "tbl_df",
+    def = getMethod(
+        f = "coerce",
+        signature(
+            from = "data.frame",
+            to = "tbl_df"
         ),
         where = "pipette"
     )
@@ -40,140 +170,6 @@ setAs(
         signature(
             from = "tbl_df",
             to = "DataFrame"
-        ),
-        where = "pipette"
-    )
-)
-
-setAs(
-    from = "IRanges",
-    to = "data.frame",
-    def = getMethod(
-        f = "coerce",
-        signature(
-            from = "IRanges",
-            to = "data.frame"
-        ),
-        where = "pipette"
-    )
-)
-
-setAs(
-    from = "Matrix",
-    to = "data.frame",
-    def = getMethod(
-        f = "coerce",
-        signature(
-            from = "Matrix",
-            to = "data.frame"
-        ),
-        where = "pipette"
-    )
-)
-
-
-
-setAs(
-    from = "data.frame",
-    to = "data.table",
-    def = getMethod(
-        f = "coerce",
-        signature(
-            from = "data.frame",
-            to = "data.table"
-        ),
-        where = "pipette"
-    )
-)
-
-setAs(
-    from = "DataFrame",
-    to = "data.table",
-    def = getMethod(
-        f = "coerce",
-        signature(
-            from = "DataFrame",
-            to = "data.table"
-        ),
-        where = "pipette"
-    )
-)
-
-setAs(
-    from = "GenomicRanges",
-    to = "data.table",
-    def = getMethod(
-        f = "coerce",
-        signature(
-            from = "GenomicRanges",
-            to = "data.table"
-        ),
-        where = "pipette"
-    )
-)
-
-setAs(
-    from = "IRanges",
-    to = "data.table",
-    def = getMethod(
-        f = "coerce",
-        signature(
-            from = "IRanges",
-            to = "data.table"
-        ),
-        where = "pipette"
-    )
-)
-
-
-
-setAs(
-    from = "DataFrame",
-    to = "tbl_df",
-    def = getMethod(
-        f = "coerce",
-        signature(
-            from = "DataFrame",
-            to = "tbl_df"
-        ),
-        where = "pipette"
-    )
-)
-
-setAs(
-    from = "GenomicRanges",
-    to = "tbl_df",
-    def = getMethod(
-        f = "coerce",
-        signature(
-            from = "GenomicRanges",
-            to = "tbl_df"
-        ),
-        where = "pipette"
-    )
-)
-
-setAs(
-    from = "IRanges",
-    to = "tbl_df",
-    def = getMethod(
-        f = "coerce",
-        signature(
-            from = "IRanges",
-            to = "tbl_df"
-        ),
-        where = "pipette"
-    )
-)
-
-setAs(
-    from = "data.frame",
-    to = "tbl_df",
-    def = getMethod(
-        f = "coerce",
-        signature(
-            from = "data.frame",
-            to = "tbl_df"
         ),
         where = "pipette"
     )
