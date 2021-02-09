3e5585b84fb7ce05a24b445313c99c3e0fd16c49
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-09 09:50:00 -0500

    Prepare release

--- R/reexports-03-classes.R
+++ R/reexports-03-classes.R
@@ -1,176 +1,221 @@
-#' Reexport S4 classes
-#'
-#' @name reexports-classes
-#' @note Updated 2021-02-09.
-#' @keywords internal
-NULL
-
-
-
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name Annotated-class
 #' @importClassesFrom AcidGenerics Annotated
 #' @exportClass Annotated
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name AsIs-class
 #' @importClassesFrom AcidGenerics AsIs
 #' @exportClass AsIs
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name DataFrame-class
 #' @importClassesFrom pipette DataFrame
 #' @exportClass DataFrame
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name DataFrameList-class
 #' @importClassesFrom pipette DataFrameList
 #' @exportClass DataFrameList
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name Ensembl2Entrez-class
 #' @importClassesFrom AcidGenomes Ensembl2Entrez
 #' @exportClass Ensembl2Entrez
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name Entrez2Ensembl-class
 #' @importClassesFrom AcidGenomes Entrez2Ensembl
 #' @exportClass Entrez2Ensembl
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name EnsemblGenes-class
 #' @importClassesFrom AcidGenomes EnsemblGenes
 #' @exportClass EnsemblGenes
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name EnsemblTranscripts-class
 #' @importClassesFrom AcidGenomes EnsemblTranscripts
 #' @exportClass EnsemblTranscripts
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name FlyBaseGenes-class
 #' @importClassesFrom AcidGenomes FlyBaseGenes
 #' @exportClass FlyBaseGenes
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name FlyBaseTranscripts-class
 #' @importClassesFrom AcidGenomes FlyBaseTranscripts
 #' @exportClass FlyBaseTranscripts
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
+#' @name GRanges-class
+#' @importClassesFrom pipette GRanges
+#' @exportClass GRanges
+NULL
+
+#' @rdname reexports
+#' @name GRangesList-class
+#' @importClassesFrom pipette GRangesList
+#' @exportClass GRangesList
+NULL
+
+#' @rdname reexports
 #' @name GencodeGenes-class
 #' @importClassesFrom AcidGenomes GencodeGenes
 #' @exportClass GencodeGenes
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name GencodeTranscripts-class
 #' @importClassesFrom AcidGenomes GencodeTranscripts
 #' @exportClass GencodeTranscripts
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name Gene2Symbol-class
 #' @importClassesFrom AcidGenomes Gene2Symbol
 #' @exportClass Gene2Symbol
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name HGNC-class
 #' @importClassesFrom AcidGenomes HGNC
 #' @exportClass HGNC
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name HGNC2Ensembl-class
 #' @importClassesFrom AcidGenomes HGNC2Ensembl
 #' @exportClass HGNC2Ensembl
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
+#' @name IRanges-class
+#' @importClassesFrom pipette IRanges
+#' @exportClass IRanges
+NULL
+
+#' @rdname reexports
+#' @name List-class
+#' @importClassesFrom pipette List
+#' @exportClass List
+NULL
+
+#' @rdname reexports
+#' @name Matrix-class
+#' @importClassesFrom pipette Matrix
+#' @exportClass Matrix
+NULL
+
+#' @rdname reexports
 #' @name MGI2Ensembl-class
 #' @importClassesFrom AcidGenomes MGI2Ensembl
 #' @exportClass MGI2Ensembl
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name Protein2Gene-class
 #' @importClassesFrom AcidGenomes Protein2Gene
 #' @exportClass Protein2Gene
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name RangedSummarizedExperiment-class
 #' @importClassesFrom AcidExperiment RangedSummarizedExperiment
 #' @exportClass RangedSummarizedExperiment
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name RefSeqGenes-class
 #' @importClassesFrom AcidGenomes RefSeqGenes
 #' @exportClass RefSeqGenes
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name RefSeqTranscripts-class
 #' @importClassesFrom AcidGenomes RefSeqTranscripts
 #' @exportClass RefSeqTranscripts
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
+#' @name Rle-class
+#' @importClassesFrom pipette Rle
+#' @exportClass Rle
+NULL
+
+#' @rdname reexports
+#' @name SimpleList-class
+#' @importClassesFrom pipette SimpleList
+#' @exportClass SimpleList
+NULL
+
+#' @rdname reexports
 #' @name SingleCellExperiment-class
 #' @importClassesFrom AcidSingleCell SingleCellExperiment
 #' @exportClass SingleCellExperiment
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
+#' @name SplitDataFrameList-class
+#' @importClassesFrom pipette SplitDataFrameList
+#' @exportClass SplitDataFrameList
+NULL
+
+#' @rdname reexports
 #' @name SummarizedExperiment-class
 #' @importClassesFrom AcidExperiment SummarizedExperiment
 #' @exportClass SummarizedExperiment
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name Tx2Gene-class
 #' @importClassesFrom AcidGenomes Tx2Gene
 #' @exportClass Tx2Gene
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name UCSCGenes-class
 #' @importClassesFrom AcidGenomes UCSCGenes
 #' @exportClass UCSCGenes
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name UCSCTranscripts-class
 #' @importClassesFrom AcidGenomes UCSCTranscripts
 #' @exportClass UCSCTranscripts
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name WormBaseGenes-class
 #' @importClassesFrom AcidGenomes WormBaseGenes
 #' @exportClass WormBaseGenes
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name WormBaseTranscripts-class
 #' @importClassesFrom AcidGenomes WormBaseTranscripts
 #' @exportClass WormBaseTranscripts
 NULL
 
-#' @rdname reexports-classes
+#' @rdname reexports
 #' @name missingOrNULL-class
 #' @importClassesFrom AcidGenerics missingOrNULL
 #' @exportClass missingOrNULL
+NULL
+
+#' @rdname reexports
+#' @name sparseMatrix-class
+#' @importClassesFrom pipette sparseMatrix
+#' @exportClass sparseMatrix
 NULL
