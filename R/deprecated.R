5df0d4227f3055bb59f202314af2a8d8ba4aa717
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-08 11:13:02 -0400

    NAMESPACE fixes

---
+++ R/deprecated.R
@@ -0,0 +1,23 @@
+## nocov start
+## nolint start
+
+
+
+#' @name deprecated
+#' @inherit AcidRoxygen::deprecated description examples return seealso title
+#' @inheritParams AcidRoxygen::params
+#' @keywords internal
+NULL
+
+
+
+#' @rdname deprecated
+#' @export
+matchesGene2Symbol <- function(...) {
+    .Defunct()
+}
+
+
+
+## nolint end
+## nocov end
