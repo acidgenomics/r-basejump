7192b2edb71524791912274b5cdea690224e18f8
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-02 11:05:51 -0500

    Reorganize files

---
+++ R/02-classes.R
@@ -0,0 +1,329 @@
+#' @exportClass Annotated
+#' @importClassesFrom S4Vectors Annotated
+#' @name Annotated-class
+#' @rdname reexports
+NULL
+
+#' @exportClass AsIs
+#' @importClassesFrom BiocGenerics AsIs
+#' @name AsIs-class
+#' @rdname reexports
+NULL
+
+#' @exportClass AtomicList
+#' @importClassesFrom IRanges AtomicList
+#' @name AtomicList-class
+#' @rdname reexports
+NULL
+
+#' @exportClass BiocFile
+#' @importClassesFrom BiocIO BiocFile
+#' @name BiocFile-class
+#' @rdname reexports
+NULL
+
+#' @exportClass CellCycleMarkers
+#' @importClassesFrom AcidSingleCell CellCycleMarkers
+#' @name CellCycleMarkers-class
+#' @rdname reexports
+NULL
+
+#' @exportClass CellTypeMarkers
+#' @importClassesFrom AcidSingleCell CellTypeMarkers
+#' @name CellTypeMarkers-class
+#' @rdname reexports
+NULL
+
+#' @exportClass CharacterList
+#' @importClassesFrom IRanges CharacterList
+#' @name CharacterList-class
+#' @rdname reexports
+NULL
+
+#' @exportClass DataFrame
+#' @importClassesFrom S4Vectors DataFrame
+#' @name DataFrame-class
+#' @rdname reexports
+NULL
+
+#' @exportClass DataFrameList
+#' @importClassesFrom IRanges DataFrameList
+#' @name DataFrameList-class
+#' @rdname reexports
+NULL
+
+#' @exportClass Ensembl2Entrez
+#' @importClassesFrom AcidGenomes Ensembl2Entrez
+#' @name Ensembl2Entrez-class
+#' @rdname reexports
+NULL
+
+#' @exportClass EnsemblGenes
+#' @importClassesFrom AcidGenomes EnsemblGenes
+#' @name EnsemblGenes-class
+#' @rdname reexports
+NULL
+
+#' @exportClass EnsemblTranscripts
+#' @importClassesFrom AcidGenomes EnsemblTranscripts
+#' @name EnsemblTranscripts-class
+#' @rdname reexports
+NULL
+
+#' @exportClass Entrez2Ensembl
+#' @importClassesFrom AcidGenomes Entrez2Ensembl
+#' @name Entrez2Ensembl-class
+#' @rdname reexports
+NULL
+
+#' @exportClass EntrezGeneInfo
+#' @importClassesFrom AcidGenomes EntrezGeneInfo
+#' @name EntrezGeneInfo-class
+#' @rdname reexports
+NULL
+
+#' @exportClass Factor
+#' @importClassesFrom S4Vectors Factor
+#' @name Factor-class
+#' @rdname reexports
+NULL
+
+#' @exportClass FactorList
+#' @importClassesFrom IRanges FactorList
+#' @name FactorList-class
+#' @rdname reexports
+NULL
+
+#' @exportClass FlyBaseGenes
+#' @importClassesFrom AcidGenomes FlyBaseGenes
+#' @name FlyBaseGenes-class
+#' @rdname reexports
+NULL
+
+#' @exportClass FlyBaseTranscripts
+#' @importClassesFrom AcidGenomes FlyBaseTranscripts
+#' @name FlyBaseTranscripts-class
+#' @rdname reexports
+NULL
+
+#' @exportClass GencodeGenes
+#' @importClassesFrom AcidGenomes GencodeGenes
+#' @name GencodeGenes-class
+#' @rdname reexports
+NULL
+
+#' @exportClass GencodeTranscripts
+#' @importClassesFrom AcidGenomes GencodeTranscripts
+#' @name GencodeTranscripts-class
+#' @rdname reexports
+NULL
+
+#' @exportClass Gene2Symbol
+#' @importClassesFrom AcidGenomes Gene2Symbol
+#' @name Gene2Symbol-class
+#' @rdname reexports
+NULL
+
+#' @exportClass GenomicRanges
+#' @importClassesFrom GenomicRanges GenomicRanges
+#' @name GenomicRanges-class
+#' @rdname reexports
+NULL
+
+#' @exportClass GenomicRangesList
+#' @importClassesFrom GenomicRanges GenomicRangesList
+#' @name GenomicRangesList-class
+#' @rdname reexports
+NULL
+
+#' @exportClass HGNC
+#' @importClassesFrom AcidGenomes HGNC
+#' @name HGNC-class
+#' @rdname reexports
+NULL
+
+#' @exportClass HGNC2Ensembl
+#' @importClassesFrom AcidGenomes HGNC2Ensembl
+#' @name HGNC2Ensembl-class
+#' @rdname reexports
+NULL
+
+#' @exportClass IntegerList
+#' @importClassesFrom IRanges IntegerList
+#' @name IntegerList-class
+#' @rdname reexports
+NULL
+
+#' @exportClass IntegerRanges
+#' @importClassesFrom IRanges IntegerRanges
+#' @name IntegerRanges-class
+#' @rdname reexports
+NULL
+
+#' @exportClass KnownMarkers
+#' @importClassesFrom AcidSingleCell KnownMarkers
+#' @name KnownMarkers-class
+#' @rdname reexports
+NULL
+
+#' @exportClass LLint
+#' @importClassesFrom S4Vectors LLint
+#' @name LLint-class
+#' @rdname reexports
+NULL
+
+#' @exportClass List
+#' @importClassesFrom S4Vectors List
+#' @name List-class
+#' @rdname reexports
+NULL
+
+#' @exportClass LogicalList
+#' @importClassesFrom IRanges LogicalList
+#' @name LogicalList-class
+#' @rdname reexports
+NULL
+
+#' @exportClass Matrix
+#' @importClassesFrom Matrix Matrix
+#' @name Matrix-class
+#' @rdname reexports
+NULL
+
+#' @exportClass MGI2Ensembl
+#' @importClassesFrom AcidGenomes MGI2Ensembl
+#' @name MGI2Ensembl-class
+#' @rdname reexports
+NULL
+
+#' @exportClass NumericList
+#' @importClassesFrom IRanges NumericList
+#' @name NumericList-class
+#' @rdname reexports
+NULL
+
+#' @exportClass Protein2Gene
+#' @importClassesFrom AcidGenomes Protein2Gene
+#' @name Protein2Gene-class
+#' @rdname reexports
+NULL
+
+#' @exportClass RangedSummarizedExperiment
+#' @importClassesFrom SummarizedExperiment RangedSummarizedExperiment
+#' @name RangedSummarizedExperiment-class
+#' @rdname reexports
+NULL
+
+#' @exportClass Ranges
+#' @importClassesFrom IRanges Ranges
+#' @name Ranges-class
+#' @rdname reexports
+NULL
+
+#' @exportClass RectangularData
+#' @importClassesFrom S4Vectors RectangularData
+#' @name RectangularData-class
+#' @rdname reexports
+NULL
+
+#' @exportClass RefSeqGenes
+#' @importClassesFrom AcidGenomes RefSeqGenes
+#' @name RefSeqGenes-class
+#' @rdname reexports
+NULL
+
+#' @exportClass RefSeqTranscripts
+#' @importClassesFrom AcidGenomes RefSeqTranscripts
+#' @name RefSeqTranscripts-class
+#' @rdname reexports
+NULL
+
+#' @exportClass Rle
+#' @importClassesFrom S4Vectors Rle
+#' @name Rle-class
+#' @rdname reexports
+NULL
+
+#' @exportClass RleList
+#' @importClassesFrom IRanges RleList
+#' @name RleList-class
+#' @rdname reexports
+NULL
+
+#' @exportClass SimpleList
+#' @importClassesFrom S4Vectors SimpleList
+#' @name SimpleList-class
+#' @rdname reexports
+NULL
+
+#' @exportClass SingleCellExperiment
+#' @importClassesFrom SingleCellExperiment SingleCellExperiment
+#' @name SingleCellExperiment-class
+#' @rdname reexports
+NULL
+
+#' @exportClass SplitDataFrameList
+#' @importClassesFrom IRanges SplitDataFrameList
+#' @name SplitDataFrameList-class
+#' @rdname reexports
+NULL
+
+#' @exportClass SummarizedExperiment
+#' @importClassesFrom SummarizedExperiment SummarizedExperiment
+#' @name SummarizedExperiment-class
+#' @rdname reexports
+NULL
+
+#' @exportClass Tx2Gene
+#' @importClassesFrom AcidGenomes Tx2Gene
+#' @name Tx2Gene-class
+#' @rdname reexports
+NULL
+
+#' @exportClass UCSCGenes
+#' @importClassesFrom AcidGenomes UCSCGenes
+#' @name UCSCGenes-class
+#' @rdname reexports
+NULL
+
+#' @exportClass UCSCTranscripts
+#' @importClassesFrom AcidGenomes UCSCTranscripts
+#' @name UCSCTranscripts-class
+#' @rdname reexports
+NULL
+
+#' @exportClass Vector
+#' @importClassesFrom S4Vectors Vector
+#' @name Vector-class
+#' @rdname reexports
+NULL
+
+#' @exportClass WormBaseGenes
+#' @importClassesFrom AcidGenomes WormBaseGenes
+#' @name WormBaseGenes-class
+#' @rdname reexports
+NULL
+
+#' @exportClass WormBaseTranscripts
+#' @importClassesFrom AcidGenomes WormBaseTranscripts
+#' @name WormBaseTranscripts-class
+#' @rdname reexports
+NULL
+
+#' @exportClass atomic
+#' @importClassesFrom S4Vectors atomic
+#' @name atomic-class
+#' @rdname reexports
+NULL
+
+#' @exportClass missingOrNULL
+#' @importClassesFrom AcidBase missingOrNULL
+#' @name missingOrNULL-class
+#' @rdname reexports
+NULL
+
+#' @exportClass sparseMatrix
+#' @importClassesFrom Matrix sparseMatrix
+#' @name sparseMatrix-class
+#' @rdname reexports
+NULL
